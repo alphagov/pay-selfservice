1554efa06fd4025fc814446b598c7ab5534bda35
kasun.kumara <kasun.kumara@gmail.com>
2015-10-08 12:34:31 +0100

    [PP-199]: added transaction list endpoint.
    pass through of the connector data.
    with @mikemey

--- app/controllers/transaction_list_controller.js
+++ app/controllers/transaction_list_controller.js
@@ -1,3 +1,26 @@
+var logger = require('winston');
+var response = require('../utils/response.js').response;
+var Client = require('node-rest-client').Client;
+var client = new Client();
+
+var TRANSACTIONS_LIST_PATH = '/transactions/';
+var CONNECTOR_CHARGE_PATH = '/v1/frontend/charges';
+
 module.exports.bindRoutesTo = function (app) {
- // skeleton
-}
+
+  app.get(TRANSACTIONS_LIST_PATH + ':gatewayAccountId', function (req, res) {
+    var gatewayAccountId = req.params.gatewayAccountId;
+    logger.info('GET ' + TRANSACTIONS_LIST_PATH + gatewayAccountId);
+
+    var connectorUrl = process.env.CONNECTOR_URL + CONNECTOR_CHARGE_PATH
+      + '?gatewayAccountId=' + gatewayAccountId;
+
+    client.get(connectorUrl, function (connectorData, connectorResponse) {
+
+      if (connectorResponse.statusCode === 200) {
+        response(req.headers.accept, res.status(200), 'transactions', connectorData);
+        return;
+      }
+    });
+  });
+};
