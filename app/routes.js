65202f20bc816c95c74deb389f032a13710a447a
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2016-02-04 17:59:04 +0000

    cleanup the routes

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,18 @@
 var response = require(__dirname + '/utils/response.js').response;
 
 var controllers = require('./controllers');
+var transactions_controller = require('./controllers/transaction_controller.js')
 var auth = require('./services/auth_service.js');
+
+
+
+var paths = {
+    transactions: {
+      index: '/selfservice/transactions',
+      download: '/selfservice/transactions/download',
+      show: '/selfservice/transactions/download/:chargeId'
+    }
+}
 
 module.exports.bind = function (app) {
 
@@ -16,6 +27,15 @@ module.exports.bind = function (app) {
     response(req.headers.accept, res, 'style_guide');
   });
 
+  var transactions = paths.transactions
+
+  app.get(transactions.index, auth.enforce, transactions_controller.transactionsIndex);
+  app.post(transactions.index, auth.enforce, transactions_controller.transactionsIndex);
+  app.get(transactions.download, auth.enforce, transactions_controller.transactionsDownload);
+  app.get(transactions.show, auth.enforce, transactions_controller.transactionsShow);
+
 
   controllers.bindRoutesTo(app);
 };
+
+module.exports.paths = paths
