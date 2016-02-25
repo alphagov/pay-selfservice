c049fa21c7ecfa55f38f85f078de0624f76797af
Kasun Kumara <kasun.kumara@gmail.com>
2016-02-25 19:24:11 +0000

    PP-308 reformatted file for 2 space indentation

--- app/services/connector_client.js
+++ app/services/connector_client.js
@@ -12,60 +12,60 @@ var CHARGE_API_PATH = CHARGES_API_PATH + '/{chargeId}';
 
 var CONTENT_TYPE_CSV = 'text/csv';
 
-var createResponseHandler = function(self) {
-    return function(callback) {
-        return function (error, response, body) {
+var createResponseHandler = function (self) {
+  return function (callback) {
+    return function (error, response, body) {
 
-            if (error || (response.statusCode !== 200)) {
-                var errorMessage = '';
-                if (error) {
-                    errorMessage = 'Error while accessing connector, error: ' + JSON.stringify(error);
-                } else {
-                    errorMessage = 'Error from connector, status code: ' + response.statusCode;
-                }
-                logger.error(errorMessage);
-                self.emit('connectorError', error, response, body);
-                return;
-            }
-
-            callback(body);
+      if (error || (response.statusCode !== 200)) {
+        var errorMessage = '';
+        if (error) {
+          errorMessage = 'Error while accessing connector, error: ' + JSON.stringify(error);
+        } else {
+          errorMessage = 'Error from connector, status code: ' + response.statusCode;
         }
+        logger.error(errorMessage);
+        self.emit('connectorError', error, response, body);
+        return;
+      }
+
+      callback(body);
     }
+  }
 };
 
 var createOnResponseEventHandler = function (self) {
-    return function (response) {
+  return function (response) {
 
-        if (response.statusCode === 200) {
-            return;
-        }
-        logger.error('Error from connector, status code: ' + response.statusCode);
-
-        //
-        // Necessary when streaming a response
-        // @See https://github.com/request/request/issues/1268
-        //
-        this.abort();
-
-        self.emit('connectorError', null, response);
+    if (response.statusCode === 200) {
+      return;
     }
+    logger.error('Error from connector, status code: ' + response.statusCode);
+
+    //
+    // Necessary when streaming a response
+    // @See https://github.com/request/request/issues/1268
+    //
+    this.abort();
+
+    self.emit('connectorError', null, response);
+  }
 };
 
 var createOnErrorEventHandler = function (self) {
-    return function (error) {
-        logger.error('Error while accessing connector, ' + JSON.stringify(error));
-        self.emit('connectorError', error, null);
-    }
+  return function (error) {
+    logger.error('Error while accessing connector, ' + JSON.stringify(error));
+    self.emit('connectorError', error, null);
+  }
 };
 
 var ConnectorClient = function (connectorUrl) {
-    this.connectorUrl = connectorUrl;
-    this.client = requestClientInstance();
-    this.responseHandler = createResponseHandler(this);
-    this.onResponseEventHandler = createOnResponseEventHandler(this);
-    this.onErrorEventHandler = createOnErrorEventHandler(this);
+  this.connectorUrl = connectorUrl;
+  this.client = requestClientInstance();
+  this.responseHandler = createResponseHandler(this);
+  this.onResponseEventHandler = createOnResponseEventHandler(this);
+  this.onErrorEventHandler = createOnErrorEventHandler(this);
 
-    EventEmitter.call(this);
+  EventEmitter.call(this);
 };
 
 util.inherits(ConnectorClient, EventEmitter);
@@ -81,10 +81,10 @@ ConnectorClient.prototype.client = null;
  * @returns {ConnectorClient}
  */
 ConnectorClient.prototype.withTransactionList = function (gatewayAccountId, searchParameters, successCallback) {
-    var url = this._searchTransactionsUrlFor(gatewayAccountId, searchParameters);
-    logger.info('CONNECTOR GET ' + url);
-    this.client(url, this.responseHandler(successCallback));
-    return this;
+  var url = this._searchTransactionsUrlFor(gatewayAccountId, searchParameters);
+  logger.info('CONNECTOR GET ' + url);
+  this.client(url, this.responseHandler(successCallback));
+  return this;
 };
 
 /**
@@ -93,21 +93,21 @@ ConnectorClient.prototype.withTransactionList = function (gatewayAccountId, sear
  * @returns {ConnectorClient}
  */
 ConnectorClient.prototype.withTransactionDownload = function (gatewayAccountId, searchParameters, successCallback) {
-    var url = this._searchTransactionsUrlFor(gatewayAccountId, searchParameters);
-    logger.info('CONNECTOR GET ' + url);
+  var url = this._searchTransactionsUrlFor(gatewayAccountId, searchParameters);
+  logger.info('CONNECTOR GET ' + url);
 
-    var options = {
-        url: url,
-        headers: {
-            'Accept': CONTENT_TYPE_CSV
-        }
-    };
+  var options = {
+    url: url,
+    headers: {
+      'Accept': CONTENT_TYPE_CSV
+    }
+  };
 
-    this.client(options)
-        .on('error', this.onErrorEventHandler)
-        .on('response', this.onResponseEventHandler)
-        .pipe(successCallback());
-    return this;
+  this.client(options)
+    .on('error', this.onErrorEventHandler)
+    .on('response', this.onResponseEventHandler)
+    .pipe(successCallback());
+  return this;
 };
 
 /**
@@ -118,10 +118,10 @@ ConnectorClient.prototype.withTransactionDownload = function (gatewayAccountId, 
  * @returns {ConnectorClient}
  */
 ConnectorClient.prototype.withGetCharge = function (gatewayAccountId, chargeId, successCallback) {
-    var url = this._chargeUrlFor(gatewayAccountId, chargeId);
-    logger.info('CONNECTOR GET ' + url);
-    this.client(url, this.responseHandler(successCallback));
-    return this;
+  var url = this._chargeUrlFor(gatewayAccountId, chargeId);
+  logger.info('CONNECTOR GET ' + url);
+  this.client(url, this.responseHandler(successCallback));
+  return this;
 };
 
 /**
@@ -132,29 +132,29 @@ ConnectorClient.prototype.withGetCharge = function (gatewayAccountId, chargeId, 
  * @returns {ConnectorClient}
  */
 ConnectorClient.prototype.withChargeEvents = function (gatewayAccountId, chargeId, successCallback) {
-    var url = this._chargeUrlFor(gatewayAccountId, chargeId) + "/events";
-    logger.info('CONNECTOR GET ' + url);
-    this.client(url, this.responseHandler(successCallback));
-    return this;
+  var url = this._chargeUrlFor(gatewayAccountId, chargeId) + "/events";
+  logger.info('CONNECTOR GET ' + url);
+  this.client(url, this.responseHandler(successCallback));
+  return this;
 };
 
 ConnectorClient.prototype._chargeUrlFor = function (gatewayAccountId, chargeId) {
-    return this.connectorUrl + CHARGE_API_PATH.replace("{accountId}", gatewayAccountId).replace("{chargeId}", chargeId);
+  return this.connectorUrl + CHARGE_API_PATH.replace("{accountId}", gatewayAccountId).replace("{chargeId}", chargeId);
 };
 
 ConnectorClient.prototype._transactionUrlFor = function (gatewayAccountId) {
-    return this.connectorUrl + FRONTEND_CHARGE_PATH + '?gatewayAccountId=' + gatewayAccountId;
+  return this.connectorUrl + FRONTEND_CHARGE_PATH + '?gatewayAccountId=' + gatewayAccountId;
 };
 
 ConnectorClient.prototype._searchTransactionsUrlFor = function (gatewayAccountId, searchParameters) {
-    var query = querystring.stringify({
-        reference: searchParameters.reference,
-        status: searchParameters.status,
-        from_date: dates.userInputToApiFormat(searchParameters.fromDate),
-        to_date: dates.userInputToApiFormat(searchParameters.toDate)
-    });
+  var query = querystring.stringify({
+    reference: searchParameters.reference,
+    status: searchParameters.status,
+    from_date: dates.userInputToApiFormat(searchParameters.fromDate),
+    to_date: dates.userInputToApiFormat(searchParameters.toDate)
+  });
 
-    return this.connectorUrl + CHARGES_API_PATH.replace("{accountId}", gatewayAccountId) +"?" + query;
+  return this.connectorUrl + CHARGES_API_PATH.replace("{accountId}", gatewayAccountId) + "?" + query;
 };
 
 exports.ConnectorClient = ConnectorClient;
