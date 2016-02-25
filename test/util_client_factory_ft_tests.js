3b54aa3ac858869f4ae8c6c2b46cf9578756b7b7
Kasun Kumara <kasun.kumara@gmail.com>
2016-02-25 19:12:01 +0000

    PP-308 factory method for request rest client with HTTPS enabled
     - Default request rest client instances will be HTTPS enabled.

--- test/util_client_factory_ft_tests.js
+++ test/util_client_factory_ft_tests.js
@@ -19,7 +19,31 @@ describe('node rest client', function () {
   it('should not use https/ssl when HTTPS is disabled', function () {
     process.env.DISABLE_INTERNAL_HTTPS = "true";
     var client = clientFactory.nodeClientInstance();
-    assert.deepEqual({}, client.options);
+    assert.deepEqual({}, client.connection);
+  });
+
+  after(function () {
+    fs.unlink(process.env.SSL_DIR + "ca.cert.pem");
+  });
+
+});
+
+describe('request rest client', function () {
+
+  before(function () {
+    fs.writeFile(process.env.SSL_DIR + "ca.cert.pem", "Hey there!");
+  });
+
+  it('should be using https/ssl', function () {
+    process.env.DISABLE_INTERNAL_HTTPS = "false";
+    var client = clientFactory.requestClientInstance();
+    assert.equal('SSL_OP_NO_TLSv1_2', client('http://google.com').agentOptions.secureOptions);
+  });
+
+  it('should not use https/ssl when HTTPS is disabled', function () {
+    process.env.DISABLE_INTERNAL_HTTPS = "true";
+    var client = clientFactory.requestClientInstance();
+    assert.deepEqual({}, client('http://google.com').agentOptions);
   });
 
   after(function () {
