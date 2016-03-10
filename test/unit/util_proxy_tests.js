78e9f23f6c8b3d8bf054dccdc37a17c826e32261
Kasun Kumara <kasun.kumara@gmail.com>
2016-03-10 10:42:55 +0000

    PP-438 enabling proxing requests conditionally
     enabling proxy environment variables conditionally.
     By default (if HTTP_PROXY_ENABLED is undefined) all proxy variables will be cleaned up if they are defined.

---
+++ test/unit/util_proxy_tests.js
@@ -0,0 +1,39 @@
+var should = require('chai').should();
+var assert = require('assert');
+var proxy = require(__dirname + '/../../app/utils/proxy.js');
+
+describe('proxy settings', function () {
+
+  beforeEach(function () {
+    process.env.HTTP_PROXY = 'http://proxy:1234';
+    process.env.HTTPS_PROXY = 'https://proxy:443';
+    process.env.NO_PROXY = 'localhost:8080,localhost:9090';
+  });
+
+  afterEach(function () {
+    delete process.env.HTTP_PROXY;
+    delete process.env.HTTPS_PROXY;
+    delete process.env.NO_PROXY;
+    delete process.env.HTTP_PROXY_ENABLED;
+  });
+
+  it('should be available if HTTP_PROXY_ENABLED=true', function () {
+    process.env.HTTP_PROXY_ENABLED = 'true';
+    proxy.use();
+
+    assert.equal(process.env.HTTP_PROXY, 'http://proxy:1234');
+    assert.equal(process.env.HTTPS_PROXY, 'https://proxy:443');
+    assert.equal(process.env.NO_PROXY, 'localhost:8080,localhost:9090');
+
+  });
+
+  it('should not be available if HTTP_PROXY_ENABLED is undefined', function () {
+    proxy.use();
+
+    assert.equal(process.env.HTTP_PROXY, undefined);
+    assert.equal(process.env.HTTPS_PROXY, undefined);
+    assert.equal(process.env.NO_PROXY, undefined);
+
+  });
+
+});
