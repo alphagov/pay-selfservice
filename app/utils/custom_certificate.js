0f8e224e94f211fa6dc7966f8d557f3dc2964361
Ian Maddison <ian@ianjm.com>
2016-03-28 23:49:07 +0100

    PP-618 Fix certificate security management - Allow loading of more than one certificate for self-signed scenarios - Do away with SSL option that didn't work

--- app/utils/custom_certificate.js
+++ app/utils/custom_certificate.js
@@ -5,19 +5,17 @@ var fs   = require('fs');
 var opts = require('https').globalAgent.options;
 
 module.exports = function () {
-
   var use = function () {
-    var caFilePath = process.env.CA_FILEPATH || '/etc/ssl/ca.cert.pem';
-
-    var caFile = fs.readFileSync(path.resolve(caFilePath));
-
+    var certsPath = process.env.CERTS_PATH || __dirname + '/../../certs';
     opts.ca = opts.ca || [];
-    opts.secureOptions = 'SSL_OP_NO_TLSv1_2';
-    opts.ca.push(caFile);
+    var certs = fs.readdirSync(certsPath).forEach( 
+      (certPath) => opts.ca.push(
+        fs.readFileSync(path.join(certsPath, certPath))
+      )
+    );
   };
-
+  
   return {
     use: use
   };
-
 }();
