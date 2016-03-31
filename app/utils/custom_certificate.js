2ec4d00b54ea4f18c15cb47de9dd5e6a01ddd458
Ian Maddison <ian@ianjm.com>
2016-03-31 20:03:53 +0100

    PP-618 Check more carefully if certs dir default exists

--- app/utils/custom_certificate.js
+++ app/utils/custom_certificate.js
@@ -7,6 +7,16 @@ var opts = require('https').globalAgent.options;
 module.exports = function () {
   var use = function () {
     var certsPath = process.env.CERTS_PATH || __dirname + '/../../certs';
+    
+    try {
+      if (!fs.lstatSync(certsPath).isDirectory()) {
+        return;
+      }
+    }
+    catch (e) {
+      return;
+    }
+    
     opts.ca = opts.ca || [];
     var certs = fs.readdirSync(certsPath).forEach( 
       (certPath) => opts.ca.push(
