0715e536f3c878bc97fa035bb145bab3ed6138f9
Josh Myers <joshuajmyers@gmail.com>
2016-04-21 14:53:19 +0100

    PP-182 SelfService use SSL to Postgres (RDS)
    Postgres needs to talk to RDS using an SSL connection

--- app/utils/session.js
+++ app/utils/session.js
@@ -11,6 +11,9 @@ const sequelizeConfig = new Sequelize(
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD, {
     "dialect": "postgres",
+    "dialectOptions": {
+      ssl: true
+    },
     "host": process.env.DATABASE_HOST,
     "port": process.env.DATABASE_PORT,
     "logging": false
