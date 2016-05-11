b32d89076b97d52b84547f94102e9cbc573e3322
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2016-05-11 12:02:41 +0100

    PP-669 change displayed dates to bst
    with @imranbohoran

--- app/utils/dates.js
+++ app/utils/dates.js
@@ -1,26 +1,26 @@
-var moment = require('moment');
+var moment = require('moment-timezone');
 
 module.exports = function () {
 
-    var DEFAULT_FORMAT = "YYYY-MM-DD HH:mm:ss";
-    var API_FORMAT = "YYYY-MM-DD HH:mm:ss";
+  var DEFAULT_FORMAT = "YYYY-MM-DD HH:mm:ss";
+  var API_FORMAT = "YYYY-MM-DD HH:mm:ss";
 
   var dateToDefaultFormat = function(date){
-    return moment(date).format(DEFAULT_FORMAT);
+    return moment(date).tz('Europe/London').format(DEFAULT_FORMAT);
   };
 
-    var userInputToApiFormat = function (date) {
-        return (date) ? moment(date, 'DD/M/YYYY').toISOString() : "";
-    };
+  var userInputToApiFormat = function (date) {
+    return (date) ? moment(date, 'DD/M/YYYY').toISOString() : "";
+  };
 
-    var utcToDisplay = function (date) {
-        return moment(date).format('DD MMM YYYY — HH:mm:ss')
-    }
+  var utcToDisplay = function (date) {
+    return moment(date).tz('Europe/London').format('DD MMM YYYY — HH:mm:ss')
+  }
 
-    return {
-        dateToDefaultFormat: dateToDefaultFormat,
-        userInputToApiFormat: userInputToApiFormat,
-        utcToDisplay: utcToDisplay
-    }
+  return {
+    dateToDefaultFormat: dateToDefaultFormat,
+    userInputToApiFormat: userInputToApiFormat,
+    utcToDisplay: utcToDisplay
+  }
 
 }();
