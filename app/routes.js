05d43fac08cf7dabef0798cedfbc331139279c4b
Paulo Monteiro <paulo.from.portugal@gmail.com>
2016-05-16 16:26:14 +0100

    PP-698 Removing legacy healthcheck endpoint
     * With the introduction of the `/healthcheck` endpoint and the new
       infrastructure configuration we can now remove the `/greeting`
       endpoint

--- app/routes.js
+++ app/routes.js
@@ -16,10 +16,6 @@ module.exports.generateRoute = generateRoute;
 module.exports.paths = paths;
 
 module.exports.bind = function (app) {
-  app.get('/greeting', function (req, res) {
-    var data = {'greeting': 'Hello', 'name': 'World'};
-    response(req.headers.accept, res, 'greeting', data);
-  });
 
   app.get('/style-guide', function (req, res) {
     response(req.headers.accept, res, 'style_guide');
