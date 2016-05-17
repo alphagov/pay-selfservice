89df630f4ba067cf9887cb64ae710a5d6097ce74
Raul Garcia <raul.garcia@digital.cabinet-office.gov.uk>
2016-05-17 12:24:35 +0100

    PP-699 Move CSV to selfservice
        - Align format as expected, adding date created field
        and integration test.
    
       with @mahmudh

--- app/utils/json_to_csv.js
+++ app/utils/json_to_csv.js
@@ -1,52 +1,61 @@
 var q = require('q');
+var dates = require('../utils/dates.js');
 var json2csv = require('json2csv');
 
-module.exports = function(data){
+module.exports = function (data) {
   var defer = q.defer();
   json2csv(
-  {
-    data: data,
-    default: "NULL",
-    fields:
-    [
     {
-      label: "Charge id",
-      value: "charge_id",
+      data: data,
+      default: 'NULL',
+      quotes: "",
+      fields: [
+        {
+          label: 'Reference',
+          value: "reference"
+        },
+        {
+          label: "Amount",
+          value: function (row) {
+            return (parseInt(row.amount) / 100).toFixed(2);
+          }
+        },
+        {
+          label: 'State',
+          value: "state.status"
+        },
+        {
+          label: 'Finished',
+          value: "state.finished"
+        },
+        {
+          label: 'Error Code',
+          value: "state.code"
+        },
+        {
+          label: 'Error Message',
+          value: "state.message"
+        },
+        {
+          label: 'Gateway Transaction ID',
+          value: "gateway_transaction_id"
+        },
+        {
+          label: 'GOV.UK Pay ID',
+          value: "charge_id"
+        },
+        {
+          label: 'Date Created',
+          value: function (row) {
+            return dates.utcToDisplay(row.created_date);
+          }
+        }
+      ]
     },
-    {
-      label: "Gateway transaction id",
-      value: "gateway_transaction_id",
-    },
-    {
-      label: "Amount",
-      value: function(row){ return (parseInt(row.amount) / 100).toFixed(2);},
-    },
-    {
-      label: "State",
-      value: "state.status",
-    },
-    {
-      label: "Finished",
-      value: "state.finished",
-    },
-    {
-      label: "Error code",
-      value: "state.code",
-    },
-    {
-      label: "Error message",
-      value: "state.message",
-    },
-    {
-      label: "Status",
-      value: "status",
-    }
-    ]
-  },
-  function(err, csv) {
-    if (err) defer.reject();
-    defer.resolve(csv);
-  });
+    function (err, csv) {
+      if (err) defer.reject();
+      defer.resolve(csv);
+    });
   return defer.promise;
 };
 
--- test/integration/transaction_download_ft_tests.js
+++ test/integration/transaction_download_ft_tests.js
@@ -7,12 +7,10 @@ var querystring     = require('querystring');
 var paths           = require(__dirname + '/../../app/paths.js');
 var winston         = require('winston');
 var session     = require(__dirname + '/../test_helpers/mock_session.js');
+var assert      = require('chai').assert;
 
 var gatewayAccountId = 651342;
-
 var app = session.mockValidAccount(_app, gatewayAccountId);
-
-
 var CHARGES_API_PATH = '/v1/api/accounts/' + gatewayAccountId + '/charges';
 var localServer = process.env.CONNECTOR_URL;
 var connectorMock = nock(localServer);
@@ -31,94 +29,118 @@ var connectorMock = nock(localServer);
     }
 
 function download_transaction_list(query) {
-    return request(app)
-        .get(paths.transactions.download + "?" + querystring.stringify(query));
+  return request(app)
+    .get(paths.transactions.download + "?" + querystring.stringify(query));
 }
 
-
-mockJson = { results:
-   [ { amount: 12345,
-       state: {},
-       status: 'SUCCEEDED',
-       description: 'desc',
-       reference: 'red',
-       links: [],
-       charge_id: 'sng0f5po5fms1vcicsu70tilab',
-       gateway_transaction_id: '538fc22c-2dbd-42ce-afb6-b2e4ab7a24ec',
-       return_url: 'https://demoservice.pymnt.localdomain:443/return/red',
-       payment_provider: 'sandbox',
-       created_date: '2016-05-12T16:37:29.245Z' } ] };
+mockJson = {
+  results: [{
+    amount: 12345,
+    state: {status: 'succeeded', finished: false},
+    description: 'desc-red',
+    reference: 'red',
+    links: [],
+    charge_id: 'charge1',
+    gateway_transaction_id: 'transaction-1',
+    return_url: 'https://demoservice.pymnt.localdomain:443/return/red',
+    payment_provider: 'sandbox',
+    created_date: '2016-05-12T16:37:29.245Z'
+  },
+    {
+      amount: 999,
+      state: {status: 'canceled', finished: true, code: 'P01234', message: 'Something happened'},
+      description: 'desc-blue',
+      reference: 'blue',
+      links: [],
+      charge_id: 'charge2',
+      gateway_transaction_id: 'transaction-2',
+      return_url: 'https://demoservice.pymnt.localdomain:443/return/blue',
+      payment_provider: 'worldpay',
+      created_date: '2015-04-12T18:55:29.999Z'
+    }
+  ]
+};
 
 describe('Transaction download endpoints', function () {
 
-    beforeEach(function () {
-        process.env.CONNECTOR_URL = localServer;
-        nock.cleanAll();
+  beforeEach(function () {
+    process.env.CONNECTOR_URL = localServer;
+    nock.cleanAll();
+  });
+
+  before(function () {
+    // Disable logging.
+    winston.level = 'none';
+  });
+
+  describe('The /transactions/download endpoint', function () {
+
+    it('should download a csv file comprising a list of transactions for the gateway account', function (done) {
+
+      connectorMock_responds(200, mockJson, {});
+
+      download_transaction_list()
+        .expect(200)
+        .expect('Content-Type', 'text/csv; charset=utf-8')
+        .expect('Content-disposition', /attachment; filename=GOVUK Pay \d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d.csv/)
+        .expect(function (res) {
+          var csvContent = res.text;
+          console.log('content of csv is: ' + csvContent);
+          var arrayOfLines = csvContent.split("\n");
+          assert(3, arrayOfLines.length);
+          assert.equal('Reference,Amount,State,Finished,Error Code,Error Message,Gateway Transaction ID,GOV.UK Pay ID,Date Created', arrayOfLines[0]);
+          assert.equal('red,123.45,succeeded,false,,,transaction-1,charge1,12 May 2016 — 17:37:29', arrayOfLines[1]);
+          assert.equal('blue,9.99,canceled,true,P01234,Something happened,transaction-2,charge2,12 Apr 2015 — 19:55:29', arrayOfLines[2]);
+        })
+        .end(done);
     });
 
-    before(function () {
-        // Disable logging.
-        winston.level = 'none';
+    it('should download a csv file comprising a list of transactions for the gateway account and the given filter', function (done) {
+
+      connectorMock_responds(200, 'csv data', {
+        reference: 'ref',
+        state: '1234',
+        from_date: '2016-01-11 01:01:01',
+        to_date: '2016-01-11 01:01:01'
+      });
+
+      download_transaction_list({
+        reference: 'ref',
+        state: '1234',
+        from_date: '2016-01-11 01:01:01',
+        to_date: '2016-01-11 01:01:01'
+      })
+        .expect(200)
+        .expect('Content-Type', 'text/csv; charset=utf-8')
+        .expect('Content-disposition', /attachment; filename=GOVUK Pay \d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d.csv/)
+        .end(done);
     });
 
-    describe('The /transactions/download endpoint', function () {
-        it('should download a csv file comprising a list of transactions for the gateway account', function (done) {
+    it('should show error message on a bad request', function (done) {
+      var errorMessage = 'Unable to download list of transactions.';
+      connectorMock_responds(400, {'message': errorMessage}, {});
 
-            connectorMock_responds(200, mockJson, {});
+      download_transaction_list()
+        .expect(500)
+        .end(done);
 
-            download_transaction_list()
-                .expect(200)
-                .expect('Content-Type', 'text/csv; charset=utf-8')
-                .expect('Content-disposition', /attachment; filename=GOVUK Pay \d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d.csv/)
-                .end(done);
-        });
-
-        it('should download a csv file comprising a list of transactions for the gateway account and the given filter', function (done) {
-
-            connectorMock_responds(200, 'csv data', {
-                reference: 'ref',
-                state: '1234',
-                from_date: '2016-01-11 01:01:01',
-                to_date: '2016-01-11 01:01:01'
-            });
-
-            download_transaction_list({
-                reference: 'ref',
-                state: '1234',
-                from_date: '2016-01-11 01:01:01',
-                to_date: '2016-01-11 01:01:01'
-            })
-                .expect(200)
-                .expect('Content-Type', 'text/csv; charset=utf-8')
-                .expect('Content-disposition', /attachment; filename=GOVUK Pay \d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d.csv/)
-                .end(done);
-        });
-
-        it('should show error message on a bad request', function (done) {
-            var errorMessage = 'Unable to download list of transactions.';
-            connectorMock_responds(400, {'message': errorMessage}, {});
-
-            download_transaction_list()
-                .expect(500)
-                .end(done);
-
-        });
-
-        it('should show a generic error message on a connector service error.', function (done) {
-            connectorMock_responds(500, {'message': 'some error from connector'}, {});
-
-            download_transaction_list()
-                .expect(500)
-                .end(done);
-        });
-
-        it('should show internal error message if any error happens while retrieving the list from connector', function (done) {
-            // No connectorMock defined on purpose to mock a network failure
-
-            download_transaction_list()
-                .expect(500)
-                .end(done);
-        });
     });
+
+    it('should show a generic error message on a connector service error.', function (done) {
+      connectorMock_responds(500, {'message': 'some error from connector'}, {});
+
+      download_transaction_list()
+        .expect(500)
+        .end(done);
+    });
+
+    it('should show internal error message if any error happens while retrieving the list from connector', function (done) {
+      // No connectorMock defined on purpose to mock a network failure
+
+      download_transaction_list()
+        .expect(500)
+        .end(done);
+    });
+  });
 });
 
