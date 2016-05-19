92a11df48481a5b14c792f95edc62eaf119fc11e
Paulo Monteiro <paulo.from.portugal@gmail.com>
2016-05-19 17:15:25 +0100

    Removing `underscore` from the project
     * replaced by `lodash`

--- app/utils/transaction_view.js
+++ app/utils/transaction_view.js
@@ -1,4 +1,4 @@
-var _ = require('underscore');
+var _ = require('lodash');
 var changeCase = require('change-case');
 var dates = require('../utils/dates.js');
 var router = require('../routes.js');
