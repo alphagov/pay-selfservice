a2fa89a6d6b7feeb3d176cd92408a041c20266fa
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2016-05-26 16:49:24 +0100

    PP-618 Add tests for egress proxy * Ensure proxy is used for any outgoing calls to non-whitelisted domains * Ensure proxy isn't used for whitelisted domains

--- app/utils/proxy.js
+++ app/utils/proxy.js
@@ -11,23 +11,24 @@ var originalHttpsRequest = https.request;
 // --- capture and rename env values ---
 // only this module should get them otherwise other libraries such as
 // requests will pick them up do a bad job around certificate handling.
+function captureEnvironment() {
+    if (process.env.HTTPS_PROXY) {
+      console.log('Tunnelling HTTPS -> ', process.env.HTTPS_PROXY);
+      process.env.TUNNEL_HTTPS_PROXY = process.env.HTTPS_PROXY;
+      delete process.env.HTTPS_PROXY;
+    }
 
-if (process.env.HTTPS_PROXY) {
-  console.log('Tunnelling HTTPS -> ', process.env.HTTPS_PROXY);
-  process.env.TUNNEL_HTTPS_PROXY = process.env.HTTPS_PROXY;
-  delete process.env.HTTPS_PROXY;
-}
+    if (process.env.HTTP_PROXY) {
+      console.log('Tunnelling HTTP -> ', process.env.HTTP_PROXY);
+      process.env.TUNNEL_HTTP_PROXY = process.env.HTTP_PROXY;
+      delete process.env.HTTP_PROXY;
+    }
 
-if (process.env.HTTP_PROXY) {
-  console.log('Tunnelling HTTP -> ', process.env.HTTP_PROXY);
-  process.env.TUNNEL_HTTP_PROXY = process.env.HTTP_PROXY;
-  delete process.env.HTTP_PROXY;
-}
-
-if (process.env.NO_PROXY) {
-  console.log('No proxy zones: ', process.env.NO_PROXY);
-  process.env.TUNNEL_NO_PROXY = process.env.NO_PROXY;
-  delete process.env.NO_PROXY;
+    if (process.env.NO_PROXY) {
+      console.log('No proxy zones: ', process.env.NO_PROXY);
+      process.env.TUNNEL_NO_PROXY = process.env.NO_PROXY;
+      delete process.env.NO_PROXY;
+    }
 }
 
 /**
@@ -41,15 +42,12 @@ function shouldProxy(options) {
     .map(e => '.' + e.toLowerCase()),
     host, port, found, parsedOptions = options;
 
-  if (typeof options === 'string') {
-    parsedOptions = urlParse(options);
-  }
-
   if (!parsedOptions.port) {
     // port not always set on options
     parsedOptions.port = parsedOptions.uri ? parsedOptions.uri.port : 443; 
   }
-  host = '.' + (parsedOptions.uri ? parsedOptions.uri.hostname : parsedOptions.host).toLowerCase();
+  host = '.' + (parsedOptions.uri ? parsedOptions.uri.hostname : parsedOptions.host);
+
   port = parsedOptions.uri ? parsedOptions.uri.port : parsedOptions.port;
   
   found = _.find(
@@ -61,21 +59,32 @@ function shouldProxy(options) {
 }
 
 // set up proxy interception
-module.exports.use = function () {
-  var httpsProxy = process.env.TUNNEL_HTTPS_PROXY;
-  // only dealing with HTTPS calls for now.
-  if (httpsProxy) {
-    httpsProxy = urlParse(httpsProxy);
+module.exports = {
+  use: function () {
+    captureEnvironment();
+    var httpsProxy = process.env.TUNNEL_HTTPS_PROXY;
+    // only dealing with HTTPS calls for now.
+    if (httpsProxy) {
+      httpsProxy = urlParse(httpsProxy);
 
-    https.request = function(options, callback) {
-      if (shouldProxy(options)) {
-        options.agent = tunnel.httpsOverHttp({
-          proxy: { host: httpsProxy.hostname, port: httpsProxy.port },
-          ca: https.globalAgent.options.ca
-        });
-      }
+      https.request = function(options, callback) {
+        if (typeof options === 'string') {
+          options = urlParse(options);
+        }
 
-      return originalHttpsRequest.call(this, options, callback);
-    };
+        if (shouldProxy(options)) {
+          options.agent = tunnel.httpsOverHttp({
+            proxy: { host: httpsProxy.hostname, port: httpsProxy.port },
+            ca: https.globalAgent.options.ca
+          });
+        }
+
+        return originalHttpsRequest.call(this, options, callback);
+      };
+    }
+  },
+
+  reset: function () {
+    https.request = originalHttpsRequest;
   }
 };
