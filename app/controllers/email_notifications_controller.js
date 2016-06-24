d8520f2b420152b23098b108e0640bf599a75462
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2016-06-24 10:26:53 +0100

    PP-449 add in index view for emails

--- app/controllers/email_notifications_controller.js
+++ app/controllers/email_notifications_controller.js
@@ -1,7 +1,20 @@
 var logger          = require('winston');
 var csrf            = require('csrf');
-
+var response        = require('../utils/response.js').response;
+var ConnectorClient = require('../services/connector_client.js').ConnectorClient;
+var client =        new ConnectorClient(process.env.CONNECTOR_URL);
+var auth            = require('../services/auth_service.js');
 
 module.exports.index = function (req, res) {
-  res.send('hello');
+      var accountId = auth.get_account_id(req);
+      client.withGetAccount(accountId, function(data){
+        console.log(data);
+        response(req.headers.accept, res, "email_notifications/index", {
+          serviceName: data.service_name
+        });
+      })
+      .on('connectorError', ()=>{});
+
+
+
 };
