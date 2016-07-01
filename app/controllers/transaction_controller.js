88263d55b5477b9d74aef32d43ed629cec780588
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2016-07-01 12:16:06 +0100

    PP-736 cleanup transaction controller

--- app/controllers/transaction_controller.js
+++ app/controllers/transaction_controller.js
@@ -10,53 +10,35 @@ var logger = require('winston');
 var router = require('../routes.js');
 var Transaction = require('../models/transaction.js');
 var Charge  = require('../models/charge.js');
-
-var qs = require('qs');
-var check = require('check-types');
-var Paginator = require('../utils/paginator.js');
-
-function getFilters(req) {
-  var all = qs.parse(req.query);
-  return _.omitBy(all, _.isEmpty);
-}
+var getFilters = require('../utils/filters.js').getFilters;
 
 function connectorClient() {
   return new ConnectorClient(process.env.CONNECTOR_URL);
-}
-
-function filledBodyKeys(req) {
-  return _.omitBy(req.body, _.isEmpty);
-}
-
-function validateFilters(filters) {
-  var pageSizeIsNull = !check.assigned(filters.pageSize),
-    pageSizeInRange = check.inRange(Number(filters.pageSize), 1, Paginator.MAX_PAGE_SIZE),
-    pageIsNull = !check.assigned(filters.page),
-    pageIsPositive = check.positive(Number(filters.page));
-
-  return (pageSizeIsNull || pageSizeInRange) &&
-         (pageIsNull || pageIsPositive);
 }
 
 module.exports = {
 
   transactionsIndex: function (req, res) {
     var accountId = auth.get_account_id(req);
-
     var filters = getFilters(req);
+    var init = function(){
+      if (!filters.valid) return error("Invalid search");
+      Transaction
+        .search(accountId, filters.result)
+        .then(render, ()=> error("Unable to retrieve list of transactions."));
+    },
 
-    if (!validateFilters(filters)) {
-      renderErrorView(req, res, "Invalid search");
-    }
-    Transaction
-      .search(accountId, filters)
-      .then(function(json){
-        json.search_path = router.paths.transactions.index;
-        var data = transactionView.buildPaymentList(json, accountId, filters);
-        response(req.headers.accept, res, 'transactions/index', data);
-      }, function(){
-        renderErrorView(req, res, 'Unable to retrieve list of transactions.');
-      });
+    render = function(json){
+      json.search_path = router.paths.transactions.index;
+      var data = transactionView.buildPaymentList(json, accountId, filters.result);
+      response(req.headers.accept, res, 'transactions/index', data);
+    },
+
+    error = function(msg){
+      renderErrorView(req, res, msg);
+    };
+
+    init();
   },
 
   transactionsDownload: function (req, res) {
@@ -66,26 +48,26 @@ module.exports = {
 
     var init = function () {
       Transaction.searchAll(accountId, filters)
-        .then(function (json) {
-          return jsonToCsv(json.results);
-        }, showError)
-        .then(function (csv) {
-          logger.info('Sending csv attachment download -', {'filename': name});
-          res.setHeader('Content-disposition', 'attachment; filename=' + name);
-          res.setHeader('Content-Type', 'text/csv');
-          res.send(csv);
-        });
-    };
+        .then(toCsv)
+        .then(render)
+        .catch(error);
+    },
 
-    var showError = function (connectorError) {
-      res.removeHeader("Content-Type");
-      res.removeHeader("Content-disposition");
-      if (connectorError) {
-        renderErrorView(req, res, 'Internal server error');
-        return;
-      }
+    toCsv = function(json) {
+      return jsonToCsv(json.results);
+    },
 
-      renderErrorView(req, res, 'Unable to download list of transactions.');
+    render = function(csv){
+      logger.info('Sending csv attachment download -', {'filename': name});
+      res.setHeader('Content-disposition', 'attachment; filename=' + name);
+      res.setHeader('Content-Type', 'text/csv');
+      res.send(csv);
+    },
+
+    error = function (connectorError) {
+      var msg = (connectorError) ? 'Internal server error' :
+        'Unable to download list of transactions.';
+      renderErrorView(req, res, msg);
     };
 
     init();
@@ -93,18 +75,24 @@ module.exports = {
 
   transactionsShow: function (req, res) {
     var accountId = auth.get_account_id(req);
-    var chargeId = req.params.chargeId;
-    var defaultError = 'Error processing transaction view';
-    var notFound = 'Charge not found';
+    var chargeId  = req.params.chargeId;
+    var defaultMsg= 'Error processing transaction view';
+    var notFound  = 'Charge not found';
 
+    var init = function(){
+      Charge.findWithEvents(accountId, chargeId)
+      .then(render,error);
+    },
 
-    Charge.findWithEvents(accountId, chargeId).then(function(data){
+    render = function(data) {
       response(req.headers.accept, res, 'transactions/show', data);
-    }, function(err){
-      if (err == 'NOT_FOUND') return renderErrorView(req, res, notFound);
-      renderErrorView(req, res, defaultError);
-    });
+    },
 
+    error = function(err){
+      var msg = (err == 'NOT_FOUND') ? notFound : defaultMsg;
+      renderErrorView(req, res, msg);
+    };
 
+    init();
   }
 };
