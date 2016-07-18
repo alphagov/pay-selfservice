fbca4e003b4bf3f310f06a8dfb3526427b3e279b
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2016-07-18 16:23:00 +0100

    PP-878 add in the pay-selfserive accounts
    this will be built as a seperate container

---
+++ server-accounts.js
@@ -0,0 +1,70 @@
+var express           = require('express');
+var app               = express();
+var Client            = require('node-rest-client').Client;
+var client            = new Client();
+var _                 = require('lodash');
+var emptyResource     = { headers : { "Content-Type": "application/json" }, data: {} };
+var connectorAccount  = process.env.CONNECTOR_URL + "/v1/api/accounts";
+var publicAuthUrl     = process.env.PUBLIC_AUTH_URL;
+var password          = "password";
+var User              = require('./app/models/user.js');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+app.get('/', function (req, res) {
+  res.send('<form action="/create_account" method="post"><input type="submit" /> </form>');
+});
+
+
+var genereateRandomUsername = function(){
+  return  Math.random().toString(36).substring(7);
+};
+
+
+app.post('/create_account', function (req, res) {
+  var connector = _.merge({},emptyResource);
+  var auth      = _.merge({},emptyResource);
+  var authToken = _.merge({},emptyResource);
+  var randomUserName = genereateRandomUsername();
+  var randomEmail = randomUserName + "@foo.com";
+  connector.data.payment_provider = "sandbox";
+
+  client.post(connectorAccount, connector, function (data, response) {
+    var gateway_account_id = data.gateway_account_id;
+    auth.data = {
+      account_id: String(gateway_account_id),
+      email: randomEmail
+    };
+    authToken.data = {
+      account_id: String(gateway_account_id),
+      description: "generated for pay-accept"
+    };
+
+    User.create({
+      username: randomUserName,
+      password: password,
+      gateway_account_id: gateway_account_id,
+      email: randomEmail
+    }).then(function(user){
+      client.post(publicAuthUrl, authToken, function (data, response) {
+        res.send(['<p id="token">token:<span><br/>',
+          data.token,
+          "</span></p><p id='email'>email: <span><br/>",
+          randomEmail,
+          "</span></p><p id='password'>password: <span><br/>",
+          password,
+          "</span></p>"
+          ].join(""));
+      }, function(){ throw new Error('WAAAA'); });
+
+    },function(){
+      defer.reject();
+    });
+
+  });
+});
+
+var port = process.env.PORT || 3001
+app.listen(port, function () {
+  console.log('listening on port ' + port);
+});
