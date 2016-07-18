03c572385af55af54d69bc671ddb19ca1ae6137c
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2016-07-18 14:58:28 +0100

    PP-878 making more sure the password will not leak out of the use rmodel

---
+++ test/models/user_test.js
@@ -0,0 +1,144 @@
+require(__dirname + '/../test_helpers/html_assertions.js');
+var should    = require('chai').should();
+var assert    = require('assert');
+var proxyquire= require('proxyquire');
+var _         = require('lodash');
+var expect    = require("chai").expect;
+var nock      = require('nock');
+var bcrypt    = require('bcrypt');
+
+
+var wrongPromise = function(data){
+  throw new Error('Promise was unexpectedly fulfilled.');
+};
+
+var sequel = {
+  sequelize :
+    {
+      sync: function(){ },
+      define: function(){ return {
+        findOne: function(){ console.log('ARE YOU HERE?');},
+        create: function(){ }
+      };
+    }
+  }
+};
+
+var User = function (mockSequelize) {
+  return proxyquire(__dirname + '/../../app/models/user.js', {
+    'sequelize': { STRING: ""} ,
+    '../utils/sequelize_config.js': mockSequelize
+  });
+};
+
+
+
+describe('user model', function() {
+  describe('find', function(){
+    it('should return a user', function () {
+      var seq = _.cloneDeep(sequel);
+      seq.sequelize.define = function(){
+        return { findOne: function(params){
+          assert(params.where.email == "foo");
+        }};
+      };
+      User(seq).find("foo");
+    });
+
+    it('should never ever ever return a password with user outside the model', function () {
+      var seq = _.cloneDeep(sequel);
+      seq.sequelize.define = function(){
+        return { findOne: function(params){
+          assert(params.where.email == "foo");
+          assert(_.includes(params.attributes,'password') === false);
+        }};
+      };
+      User(seq).find("foo");
+    });
+  });
+  describe('create',function(){
+    it('should create a user', function (done) {
+      var seq = _.cloneDeep(sequel);
+      seq.sequelize.define = function(){
+        return { create: function(user){
+          assert(user.username == "foo");
+          assert(user.password != "password");
+          assert(bcrypt.compareSync('password',user.password))
+          return {then : function (callback) {
+            callback({dataValues: user});
+          }};
+        }};
+      };
+      User(seq).create({
+        username: "foo",
+        password: "password",
+        gateway_account_id: 1,
+        email: "foo@example.com"
+      }).then(function(user){
+        try {
+          assert(user.username == "foo");
+          assert(_.includes(user,'password') === false);
+          done();
+        } catch (e) {
+          done(e);
+        }
+
+      });
+    });
+  });
+
+
+
+  describe('authenticate',function(){
+    it('should authenticate a valid user', function (done) {
+      var seq = _.cloneDeep(sequel);
+      seq.sequelize.define = function(){
+        return { findOne: function(params){
+          return {then: function(callback){
+            callback({dataValues: {
+              password: "$2a$10$jJmJnbJFOLYz/DChN6VMVOfBQ0G94MRGSAjtp25j7BMBBQXpYoDZm",
+              foo: "bar"
+            }});
+          }};
+        }};
+      };
+      User(seq)
+      .authenticate("foo@example.com", "password")
+      .then(function (user) {
+         try {
+          assert(user.foo,"bar");
+          assert(user.password ===undefined);
+          done();
+        } catch (e) {
+          done(e);
+        }
+
+      },wrongPromise);
+    });
+
+    it('should not authenticate an invalid user', function (done) {
+      var seq = _.cloneDeep(sequel);
+      seq.sequelize.define = function(){
+        return { findOne: function(params){
+          return {then: function(callback){
+            callback({dataValues: {
+              password: "$2a$10$jJmJnbJFOLYz/DChN6VMVOfBQ0G94MRGSAjtp25j7BMBBQXpYoDZm",
+              foo: "bar"
+            }});
+          }};
+        }};
+      };
+      User(seq)
+      .authenticate("foo@example.com", "password2")
+      .then(wrongPromise,function(){
+        assert(true);
+        done();
+      });
+    });
+  });
+
+
+
+
+});
+
