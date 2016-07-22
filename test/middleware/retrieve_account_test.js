3cfa1be9f918606b69b569d0f6b84d0afa72e0de
Andrew Hilton <andrew.hilton@digital.cabinet-office.gov.uk>
2016-07-22 15:17:36 +0100

    PP-878 another end point for the e2e

--- test/middleware/retrieve_account_test.js
+++ test/middleware/retrieve_account_test.js
@@ -54,7 +54,7 @@ describe('retrieve param test', function () {
       expect(next.notCalled).to.be.true;
       assert(render.calledWith( "error", { message: 'There is a problem with the payments platform' }));
       done();
-    },40);
+    },100);
   });
 
   it("should set the account and email and call next on success", function(done) {
@@ -86,7 +86,6 @@ describe('retrieve param test', function () {
 
       testPromise.then((result) => {
         try {
-        console.log('THISS')
           expect(status.calledWith(200));
           expect(next.called).to.be.true;
           expect(req.account).to.deep.equal({
