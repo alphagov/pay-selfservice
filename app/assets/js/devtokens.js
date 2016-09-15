f88991e7a0d968e7698027c5b93d419b97e9940c
Silvia <sil.mandala@gmail.com>
2016-09-15 14:29:30 +0100

    PP-1112 Fix cannot edit description api tokens twice
    * As we were dynamically replacing the edit description link, but
      statically binding the on click listener, we could edit the description
      only once.
    * Now, event listeners are dynamically bound to any object with a
      `.js-toggle-*` class

--- app/assets/js/devtokens.js
+++ app/assets/js/devtokens.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
   "use strict";
 
-  $('.js-toggle-description').on('click', toggleDescription);
-  $('.js-toggle-revoke').on('click', toggleRevoke);
-  $('.js-save-description').on('click', saveDescription);
-  $('.js-revoke-token').on('click', revokeToken);
+  var keyList = $('.key-list');
+  keyList.on('click', '.js-toggle-description', toggleDescription);
+  keyList.on('click', '.js-toggle-revoke', toggleRevoke);
+  keyList.on('click', '.js-save-description', saveDescription);
+  keyList.on('click', '.js-revoke-token', revokeToken);
+
 
   function toggleDescription(evt) {
     toggle.call(this, '.js-edit-description');
