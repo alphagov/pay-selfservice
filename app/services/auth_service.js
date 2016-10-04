a12a6c60e768d74cb96aa2931083d9c435fc8c87
David Heath <david.heath@digital.cabinet-office.gov.uk>
2016-10-04 11:10:03 +0100

    Refactor: standardise function declarations
    use `function myFunc(){}` rather than `var myFunc = function(){};
    
    move all function definitions to be stand-alone rather than inline with
    the module exports block.

--- app/services/auth_service.js
+++ app/services/auth_service.js
@@ -8,7 +8,7 @@ var csrf = require('csrf');
 var User = require(__dirname + '/../models/user.js');
 var _ = require('lodash');
 
-var localStrategyAuth = function(username, password, done) {
+function localStrategyAuth(username, password, done) {
   User.authenticate(username,password)
   .then(function(user){
     done(null, user);
@@ -17,12 +17,12 @@ var localStrategyAuth = function(username, password, done) {
   });
 };
 
-var appendCSRF = function(req){
+function appendCSRF(req){
   if (req.session.csrfSecret) return;
   req.session.csrfSecret = csrf().secretSync();
-},
+}
 
- appendLoggedOutCSRF = function(req, res, next){
+function appendLoggedOutCSRF(req, res, next){
   if (req.session.csrfSecret) return next();
   req.session.csrfSecret = csrf().secretSync();
   req.session.save(function(err) {
@@ -33,15 +33,20 @@ var appendCSRF = function(req){
     }
     next();
   });
-},
+}
 
-
-redirectToLogin = function(req,res){
+function redirectToLogin(req,res){
   req.session.last_url = req.originalUrl;
   req.session.save(function () {
     res.redirect(paths.user.logIn);
   });
-};
+}
+
+function get_gateway_account_id(req) {
+  var id = _.get(req,"user.gateway_account_id");
+  if (!id) return null;
+  return parseInt(id);
+}
 
 function enforceUser(req, res, next){
   var hasUser     = _.get(req,"user"),
@@ -54,64 +59,62 @@ function enforceUser(req, res, next){
   next();
 }
 
+function no_access(req, res, next) {
+  if (req.url != paths.user.noAccess) {
+    res.redirect(paths.user.noAccess);
+  }
+  else {
+    next(); // don't redirect again if we're already there
+  }
+}
+
+function enforce(req, res, next) {
+  req.session.reload(function (err) {
+    var hasLoggedInOtp  = _.get(req,"session.secondFactor") == 'totp';
+
+    enforceUser(req, res, function(){
+      if (!hasLoggedInOtp) return res.redirect(paths.user.otpLogIn);
+      next();
+    });
+  });
+}
+
+function initialise(app, override_strategy) {
+  app.use(passport.initialize());
+  app.use(passport.session());
+  passport.use('local',new localStrategy({usernameField: 'email'},localStrategyAuth));
+  passport.use(new TotpStrategy(
+    function(user, done) {
+      return done(null, user.otp_key, 30);
+    }
+  ));
+
+  passport.serializeUser(this.serializeUser);
+
+  passport.deserializeUser(this.deserializeUser);
+
+}
+
+function deserializeUser(email, done) {
+  User.find(email).then(function(user){
+    done(null, user);
+  });
+}
+
+function serializeUser(user, done) {
+  done(null, user.email);
+}
 
 var auth = {
-
   enforceUser: enforceUser,
   appendLoggedOutCSRF: appendLoggedOutCSRF,
-  enforce: function (req, res, next) {
-    req.session.reload(function (err) {
-      var hasLoggedInOtp  = _.get(req,"session.secondFactor") == 'totp';
-
-      enforceUser(req, res, function(){
-        if (!hasLoggedInOtp) return res.redirect(paths.user.otpLogIn);
-        next();
-      });
-    });
-  },
-  initialise: function (app, override_strategy) {
-
-    app.use(passport.initialize());
-    app.use(passport.session());
-    passport.use('local',new localStrategy({usernameField: 'email'},localStrategyAuth));
-    passport.use(new TotpStrategy(
-      function(user, done) {
-        return done(null, user.otp_key, 30);
-      }
-    ));
-
-    passport.serializeUser(this.serializeUser);
-
-    passport.deserializeUser(this.deserializeUser);
-
-  },
-
-  deserializeUser: function (email, done) {
-    User.find(email).then(function(user){
-      done(null, user);
-    });
-  },
-
-  serializeUser: function (user, done) {
-    done(null, user.email);
-  },
-
+  enforce: enforce,
+  initialise: initialise,
+  deserializeUser: deserializeUser,
+  serializeUser: serializeUser,
   localStrategyAuth: localStrategyAuth,
-
-  no_access: function (req, res, next) {
-    if (req.url != paths.user.noAccess) {
-      res.redirect(paths.user.noAccess);
-    }
-    else {
-      next(); // don't redirect again if we're already there
-    }
-  },
-
-  get_gateway_account_id: function (req) {
-    var id = _.get(req,"user.gateway_account_id");
-    if (!id) return null;
-    return parseInt(id);
-  }
+  no_access: no_access,
+  get_gateway_account_id: get_gateway_account_id
 };
 
 module.exports = auth;
