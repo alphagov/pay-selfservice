1ef51309f3172010c5f71dcf5fe7d7fbb84c4f7e
Raul Garcia <raul.garcia@digital.cabinet-office.gov.uk>
2016-11-11 15:39:08 +0000

    PP-1290 Permission middleware
        - Only checks for permissions if exist as parameter, otherwise
        is considered as any user can access the resource.
    
        - Render error if user has not permission. This situacion should not
        happen because user will only view operations (as buttons or links) that
        they have access to do. This situation could only happen if permissions
        change while user is using the site or someone tries to access resources
        manually.
    
        - Added permission (with empty args) to all the relevant resources, so the
        current situation should not change.

--- test/middleware/permission_test.js
+++ test/middleware/permission_test.js
@@ -1,112 +1,119 @@
-var should = require('chai').should();
-var assert = require('assert');
-var sinon  = require('sinon');
-var _      = require('lodash');
-var expect = require('chai').expect;
-var nock   = require('nock');
 var proxyquire = require('proxyquire');
+var sinon = require('sinon');
+var assert = require('assert');
+var expect = require('chai').expect;
 
-describe('permissions test', function () {
+describe('permission test', function () {
 
-  var response = {
-    status: function(){},
-    render: function(){}
-  },
-  status = undefined,
-  render = undefined,
-  next   = undefined,
-  validRequest =  {
-    headers: {
-      accept: ""
-    }
+  var permission = (userMock)=> {
+    return proxyquire(__dirname + '/../../app/middleware/permission.js',
+      {'../models/user.js': userMock});
   };
 
+  it('should be authorised if permissions to check is empty no matter what permissions the user has', function () {
+    var nextSpy = sinon.spy();
+    var permissionMiddleware = permission({});
 
-  beforeEach(function(){
-    status = sinon.stub(response,"status");
-    render = sinon.stub(response,"render");
-    next   = sinon.spy();
-    nock.cleanAll();
+    permissionMiddleware()({body: {username: "foo"}, headers: {}}, {}, nextSpy);
+
+    assert(nextSpy.called);
   });
 
-  afterEach(function(){
-    status.restore();
-    render.restore();
-  });
-
-  var login = (userMock)=> {
-    return proxyquire(__dirname + '/../../app/middleware/login_counter.js',
-    {'../models/user.js': userMock});
-  };
-
-  it('should be authorised if permissions to check in the middleware is empty',function(){
+  it('should be authorised if user has valid permission', function () {
     var user = {
-      find: ()=> {
-        return  {
-          then: (success,fail)=> {
+      findByUsername: (username, correlationId)=> {
+        expect(username).to.be.equal('foo');
+        expect(correlationId).to.be.equal('');
+        return {
+          then: (success, fail)=> {
             success({
-              login_counter: 0,
-              incrementLoginCount: () => {
-                assert("increment is called","increment is called")
-                return { then: (suc,fail)=> suc()}
+              hasPermission: (permission) => {
+                expect(permission).to.be.equal('refunds:create');
+                return {then: (suc, fail)=> suc(true)}
               }
             })
           }
         }
       }
     };
+    var nextSpy = sinon.spy();
+    var permissionMiddleware = permission(user);
 
-    var loginMiddleware = login(user);
-    loginMiddleware.enforce({body: {email: "foo"}, headers:{}},{
-    },()=> assert("next is called","next is called"))
+    permissionMiddleware('refunds:create')({body: {username: "foo"}, headers: {}}, {}, nextSpy);
+
+    assert(nextSpy.called);
   });
 
-  it('should call disable user and render noacess when over limit',function(){
+  it('should render unauthorised error if user has not valid permission', function () {
 
     var user = {
-      find: ()=> { 
-        return  { 
-          then: (success,fail)=> {
+      findByUsername: (username, correlationId)=> {
+        expect(username).to.be.equal('foo');
+        expect(correlationId).to.be.equal('');
+        return {
+          then: (success, fail)=> {
             success({
-              login_counter: 100,
-              incrementLoginCount: () => {
-                assert("increment is called",false)
-                return { then: (suc,fail)=> suc()}
-              },
-              toggleDisabled: (boolean)=> {
-                assert(boolean, true);
-                return { then: (suc)=> suc() }
+              hasPermission: (permission) => {
+                expect(permission).to.be.equal('refunds:create');
+                return {then: (suc, fail)=> suc(false)}
               }
             })
           }
         }
       }
     };
+    var nextSpy = sinon.spy();
+    var res = {render: sinon.spy()};
+    var permissionMiddleware = permission(user);
 
-    var loginMiddleware = login(user);
-    loginMiddleware.enforce({body: {email: "foo"}, headers:{}},{
-      render: (path) => assert("login/noaccess",path)
-    },()=> assert("next is called",false))
+    permissionMiddleware('refunds:create')({body: {username: "foo"}, headers: {}}, res, nextSpy);
+
+    assert(nextSpy.notCalled);
+    assert(res.render.calledWithExactly('error', {'message': 'You are not Authorized to do this operation'}))
   });
 
-  it('should retrieve safely email field',function(){
+  it('should return error if find user fails', function () {
+
     var user = {
-      find: ()=> {
-        return  {
-          then: (success,fail)=> {
+      findByUsername: (username, correlationId)=> {
+        expect(username).to.be.equal('foo');
+        expect(correlationId).to.be.equal('');
+        return { then: (success, fail)=> fail() }
+      }
+    };
+    var nextSpy = sinon.spy();
+    var permissionMiddleware = permission(user);
+
+    expect(() => permissionMiddleware('refunds:create')({body: {username: "foo"}, headers: {}}, {}, nextSpy))
+      .to.throw(Error, "Could not get user");
+
+    assert(nextSpy.notCalled);
+  });
+
+  it('should return error if checking for user permission fails', function () {
+
+    var user = {
+      findByUsername: (username, correlationId)=> {
+        expect(username).to.be.equal('foo');
+        expect(correlationId).to.be.equal('');
+        return {
+          then: (success, fail)=> {
             success({
-              login_counter: 0,
-              incrementLoginCount: () => {
-                assert("increment is called","increment is called");
-                return { then: (suc,fail)=> suc()}
+              hasPermission: (permission) => {
+                expect(permission).to.be.equal('refunds:create');
+                return {then: (suc, fail)=> fail()}
               }
             })
           }
         }
       }
     };
-    var loginMiddleware = login(user);
-    loginMiddleware.enforce({body:{}, headers:{}},{
-    },()=> assert("next is called","next is called"))
+    var nextSpy = sinon.spy();
+    var permissionMiddleware = permission(user);
+
+    expect(() => permissionMiddleware('refunds:create')({body: {username: "foo"}, headers: {}}, {}, nextSpy))
+      .to.throw(Error, "Could not check user permission");
+
+    assert(nextSpy.notCalled);
   });
 });
