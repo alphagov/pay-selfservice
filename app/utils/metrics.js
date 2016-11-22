ac6b617891d47f1947eacf9d0c9dfb7b25357e33
imranbohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2016-11-22 11:17:01 +0000

    PP-1386 Introducing appmetrics+statsd for metrics
    * appmetrics-statsd library allows us to capture nodejs
      runtime metrics as well as custom metrics. Runtime
      metrics are through appmetrics while custom metrics
      can be captured using statsd. The library combines both.
    * The current version of appmetrics-statsd doesn't allow for
      turning off mqtt. This has been raised with the maintainers
      and they are in the midst of rolling out a new release with a fix.
      Until then we can still use it - https://github.com/RuntimeTools/appmetrics/issues/341.
      The library tries to connect to mqtt, but there wont be any mqtt
      actions performed as there there's no mqtt running.
    * This change only initialises the appmetrics-statsd library and
      sends nodejs runtime metrics to a statsd endpoint.
    * The statsd endpoint is expected to be available through the METRICS_HOST
      variable, if not 'localhost' is used.
    * The prefix provided in metrics.js serves as the root node name for the
      application in graphite metrics.
    
    with @simad

---
+++ app/utils/metrics.js
@@ -0,0 +1,19 @@
+'use strict';
+var appmetrics = require('appmetrics');
+var metricsHost = process.env.METRICS_HOST || "localhost";
+var metricsPort = process.env.METRICS_PORT || 8125;
+var metricsPrefix = "selfservice.";
+
+function initialiseMonitoring () {
+  appmetrics.configure({'mqtt':'off'});
+  var appmetricsStatsd = require('appmetrics-statsd');
+
+  return appmetricsStatsd.StatsD(null, metricsHost, metricsPort, metricsPrefix);
+}
+
+module.exports = function() {
+
+  return {
+    metrics: initialiseMonitoring()
+  }
+}();
