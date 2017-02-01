02db4353f8f38728135325d70fa994d0d3d2719f
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2017-02-01 09:36:19 +0000

    PP-1479 clean up static controller tests

--- test/integration/static_controller_test.js
+++ test/integration/static_controller_test.js
@@ -1,19 +1,9 @@
-var dbMock      = require(__dirname + '/../test_helpers/db_mock.js');
+require(__dirname + '/../test_helpers/serialize_mock.js');
 var request     = require('supertest');
-var nock        = require('nock');
 var app         = require(__dirname + '/../../server.js').getApp;
-var winston     = require('winston');
-var paths       = require(__dirname + '/../../app/paths.js');
-var session     = require(__dirname + '/../test_helpers/mock_session.js');
 var _           = require('lodash');
 
-
 describe('static controller', function () {
-  before(function () {
-        // Disable logging.
-        winston.level = 'none';
-      });
-
   _.each(['get','post','delete','put','patch'],function(verb){
     it('should return an error page', function (done) {
       request(app)
