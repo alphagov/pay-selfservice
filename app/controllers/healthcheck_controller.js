8a64cd5bf55eff530bec34ed04c4f1f6ef39ee12
Kasun Kumara <kasun.kumara@gmail.com>
2017-02-02 15:03:25 +0000

    PP-1479  Refactored healthcheck resource   - Removed database check + sequalize
    with  @whpearson

--- app/controllers/healthcheck_controller.js
+++ app/controllers/healthcheck_controller.js
@@ -1,21 +1,6 @@
-var logger          = require('winston');
 var responseHandler = require('../utils/response.js');
-var sequelizeConfig = require('../utils/sequelize_config.js');
-var sessions        = require('../utils/session.js');
 
 module.exports.healthcheck = function (req, res) {
-  var data = {'ping': {'healthy': true}, 'database': {'healthy': true}};
-  
-  sequelizeConfig.sequelize
-    .authenticate()
-    .then(function (err) {
-      logger.debug('Connection has been established successfully');
-      data.database.healthy = true;
-      responseHandler.healthCheckResponse(req.headers.accept, res, data);
-    }, function (err) {
-      logger.error('Unable to connect to the database -', {'err': err});
-      data.database.healthy = false;
-      res.status(503);
-      responseHandler.healthCheckResponse(req.headers.accept, res, data);
-    });
+  var data = {'ping': {'healthy': true}};
+  responseHandler.healthCheckResponse(req.headers.accept, res, data);
 };
