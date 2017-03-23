f4d3cbf99575ef0da2fc7d4d7a4a8a946dbfe9da
Danail Minchev <danail.minchev@gmail.com>
2017-03-23 14:45:42 +0000

    PP-1717 Added new random UUID generator
    with @rauligs

--- app/utils/random.js
+++ app/utils/random.js
@@ -1,15 +1,32 @@
-module.exports.key = function(len) {
-  var buf = []
+'use strict';
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomUuid() {
+  // See:
+  // http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+  return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    let r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+}
+
+function key(length) {
+  let buf = []
     , chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
     , charlen = chars.length;
 
-  for (var i = 0; i < len; ++i) {
-    buf.push(chars[getRandomInt(0, charlen - 1)]);
+  for (let i = 0; i < length; ++i) {
+    buf.push(chars[randomInt(0, charlen - 1)]);
   }
 
   return buf.join('');
-};
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+module.exports = {
+  randomInt: randomInt,
+  randomUuid: randomUuid,
+  key: key
+};
