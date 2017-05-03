00f25d66e7baf79faa2b251f600db11cce14612c
Kasun Kumara <kasun.kumara@gmail.com>
2017-05-03 17:39:19 +0100

    PP-1879 adding ui tests to invite team member
    with @SandorArpa

---
+++ test/ui/invite_user_ui_tests.js
@@ -0,0 +1,32 @@
+let should = require('chai').should();
+let renderTemplate = require(__dirname + '/../test_helpers/html_assertions.js').render;
+let paths = require('../../app/paths.js');
+
+describe('Invite a team member view', function () {
+
+  it('should render invite team member view', function () {
+
+    let templateData = {
+      admin: {id: 2},
+      viewAndRefund: {id: 3},
+      view: {id: 4},
+    };
+
+    let body = renderTemplate('services/team_member_invite', templateData);
+
+    body.should.containSelector('form#invite-member-form').withAttribute('action', paths.teamMembers.invite);
+    body.should.containSelector('input#role-admin-input')
+      .withAttribute('type', 'radio')
+      .withAttribute('value', '2')
+      .withNoAttribute('checked');
+    body.should.containSelector('input#role-view-and-refund-input')
+      .withAttribute('type', 'radio')
+      .withAttribute('value', '3')
+      .withNoAttribute('checked');
+    body.should.containSelector('input#role-view-input')
+      .withAttribute('type', 'radio')
+      .withAttribute('value', '4')
+      .withAttribute('checked');
+  });
+
+});
