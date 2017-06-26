c4b91d25ebe8aedeced2db82038d745b671a13a6
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2017-06-26 15:42:35 +0100

    PP-1615 Add error handling for submit otp code for verification

--- app/controllers/create_service_controller.js
+++ app/controllers/create_service_controller.js
@@ -2,11 +2,14 @@
 
 const logger = require('winston');
 const _ = require('lodash');
+const q = require('q');
+
 const paths = require('../paths.js');
 const response = require('../utils/response');
 const errorResponse = response.renderErrorView;
 const registrationService = require('../services/service_registration_service');
 const validations = require('../utils/registration_validations');
+const loginController = require('./login_controller');
 const validateRegistrationInputs = validations.validateServiceRegistrationInputs;
 const serviceRegistrationEnabled = process.env.SERVICE_REGISTRATION_ENABLED === 'true';
 
@@ -83,7 +86,7 @@ module.exports = {
     const password = req.body['password'];
 
     const handleErrorCode = (err) => {
-      _.set(req, 'session.pageData.submitRegistration', {
+      _.set(req, 'session.pageData.submitRegistrationPageData', {
         email,
         telephone_number: telephoneNumber
       });
@@ -118,29 +121,34 @@ module.exports = {
         }).catch((err) => handleError(err));
     };
 
-    if (serviceRegistrationEnabled) {
-      return validateRegistrationInputs(email, telephoneNumber, password)
-        .then(proceedToRegistration)
-        .catch(
-          (err) => handleError(err));
-    } else {
-      return errorResponse(req, res, 'Invalid route', 404);
-    }
+    return validateRegistrationInputs(email, telephoneNumber, password)
+      .then(proceedToRegistration)
+      .catch(
+        (err) => handleError(err));
   },
 
-  submitOtpVerify: (req, res) => {
+  createPopulatedService: (req, res) => {
     const correlationId = req.correlationId;
-    const code = req.body.code;
-    const otpCode = req.body['verify-code'];
+    const email = undefined;//req.register_invite.email;
+    const phoneNumber = req.register_invite.telephone_number;
+    const role = 'admin';
 
-    const handleInvalidOtp = (message) => {
-      logger.debug(`[requestId=${correlationId}] invalid user input - otp code`);
-      req.flash('genericError', message);
-      res.redirect(303, paths.selfCreateService.otpVerify);
+    const redirectToAutoLogin = (req, res) => {
+      res.redirect(303, paths.selfCreateService.serviceNaming);
     };
 
-    const handleError = (req, res, err) => {
+    const handleOtpError = (req, res, err) => {
+      const handleInvalidOtp = (message) => {
+        // logger.debug(`[requestId=${correlationId}] invalid user input - otp code`);
+
+        console.log('submitOtpVerify failed' + JSON.stringify(err));
+
+        req.flash('genericError', message);
+        res.redirect(303, paths.selfCreateService.otpVerify);
+      };
+
       logger.warn(`[requestId=${req.correlationId}] Invalid invite code attempted ${req.code}, error = ${err.errorCode}`);
+
       switch (err.errorCode) {
         case 401:
           handleInvalidOtp('Invalid verification code');
@@ -156,14 +164,70 @@ module.exports = {
       }
     };
 
-    const validateServiceOtpCode = (code, otpCode) => {
-      registrationService.submitServiceInviteOtpCode(code, otpCode, correlationId)
-        .then(() => res.send(200))
-        .catch(
-          (err) => handleError(req, res, err)
-        );
+    const handleError = (req, res, err) => {
+      errorResponse(req, res, 'Unable to process registration at this time', 500);
     };
 
-    return validateServiceOtpCode(code, otpCode);
+
+    const handleOrchastration = () => {
+      registrationService.createPopulatedService({email, role, phoneNumber}, correlationId)
+        .then((user) => {
+          loginController.setupDirectLoginAfterRegister(req, res, user);
+          redirectToAutoLogin(req, res);
+        })
+        .catch(err => {
+          console.log('createPopulatedService failed' + JSON.stringify(err));
+          handleError(err);
+        });
+    };
+
+    return submitOtpVerify(req)
+      .then(handleOrchastration)
+      .catch(err => handleOtpError(err));
+  },
+
+  submitServiceNameChange: (req, res) => {
+    console.log('=============== ' + JSON.stringify(req.register_invite));
+    const correlationId = req.correlationId;
+    const serviceExternalId = req.register_invite.service_ext_id;
+    const newName = req.body['service-name'];
+
+    const submitNameChange = (serviceExternalId, newName, correlationId) => {
+      return registrationService.renameService(serviceExternalId, newName, correlationId);
+    };
+
+    const redirectToHomePage = res => {
+      res.redirect(303, paths.user.loggedIn);
+    };
+
+
+    const handleError = (req, res, err) => {
+      // logger.warn(`[requestId=${req.correlationId}] Invalid invite code attempted ${req.code}, error = ${err.errorCode}`);
+      console.log('ERROR!1 : ' + JSON.stringify(err));
+      console.log('ERROR!2 : ' + err);
+      switch (err.errorCode) {
+        case 401:
+          handleInvalidOtp('Invalid verification code');
+          break;
+        case 404:
+          errorResponse(req, res, 'Unable to process registration at this time', 404);
+          break;
+        case 410:
+          errorResponse(req, res, 'This invitation is no longer valid', 410);
+          break;
+        default:
+          errorResponse(req, res, 'Unable to process registration at this time', 500);
+      }
+      console.log('ERROR!3 : ' + JSON.stringify(err));
+      // redirectToHomePage(res);
+    };
+
+
+    return submitNameChange(serviceExternalId, newName, correlationId)
+      .then(() => {
+        redirectToHomePage(res);
+      })
+      .catch(err => handleError(req, res, err));
   }
+
 };
