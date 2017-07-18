2f86bf6e6eb541f2a721315fcb55ea0d7b7755b3
Mark Bridgett <mark.bridgett@digital.cabinet-office.gov.uk>
2017-07-18 15:51:17 +0100

    PP-2351 Applied standard linter fixes to test/test_helpers

--- test/test_helpers/mock_session.js
+++ test/test_helpers/mock_session.js
@@ -9,66 +9,62 @@ const sinon = require('sinon')
 const userFixture = require('../fixtures/user_fixtures')
 
 const getUser = (opts) => {
-    return userFixture.validUser(opts).getAsObject()
-  },
+  return userFixture.validUser(opts).getAsObject()
+}
 
-  createAppWithSession = function (app, sessionData, gatewayAccountData, registerInviteData) {
-    var proxyApp = express();
-    proxyApp.all("*", function (req, res, next) {
-      sessionData.destroy = sinon.stub();
-      req.session = sessionData || {};
-      req.gateway_account = gatewayAccountData || {
-          currentGatewayAccountId: _.get(sessionData, 'passport.user.serviceRoles[0].service.gatewayAccountIds[0]')
-        };
-      req.register_invite = registerInviteData || {};
-      next();
-    });
-    proxyApp.use(app);
-    return proxyApp;
-  },
+const createAppWithSession = function (app, sessionData, gatewayAccountData, registerInviteData) {
+  var proxyApp = express()
+  proxyApp.all('*', function (req, res, next) {
+    sessionData.destroy = sinon.stub()
+    req.session = sessionData || {}
+    req.gateway_account = gatewayAccountData || {
+      currentGatewayAccountId: _.get(sessionData, 'passport.user.serviceRoles[0].service.gatewayAccountIds[0]')
+    }
+    req.register_invite = registerInviteData || {}
+    next()
+  })
+  proxyApp.use(app)
+  return proxyApp
+}
 
-  getAppWithLoggedInUser = function (app, user) {
-    const validSession = getMockSession(user)
-    return createAppWithSession(app, validSession)
-  },
+const getAppWithLoggedInUser = function (app, user) {
+  const validSession = getMockSession(user)
+  return createAppWithSession(app, validSession)
+}
 
-  getAppWithSession = function (app, sessionData) {
-    return createAppWithSession(app, sessionData);
-  },
+const getAppWithSessionAndGatewayAccountCookies = function (app, sessionData, gatewayAccountData) {
+  return createAppWithSession(app, sessionData, gatewayAccountData)
+}
 
-  getAppWithSessionAndGatewayAccountCookies = function (app, sessionData, gatewayAccountData) {
-    return createAppWithSession(app, sessionData, gatewayAccountData)
-  },
+const getAppWithRegisterInvitesCookie = function (app, registerInviteData) {
+  return createAppWithSession(app, {csrfSecret: '123'}, {}, registerInviteData)
+}
 
-  getAppWithRegisterInvitesCookie = function (app, registerInviteData) {
-    return createAppWithSession(app, {csrfSecret: '123'}, {}, registerInviteData)
-  },
+const getAppWithLoggedOutSession = function (app, session) {
+  session = session || {}
+  session.csrfSecret = '123'
+  return createAppWithSession(app, session, {})
+}
 
-  getAppWithLoggedOutSession = function (app, session) {
-    session = session || {}
-    session.csrfSecret = '123'
-    return createAppWithSession(app, session, {})
-  },
+const getAppWithSessionWithoutSecondFactor = function (app, user) {
+  const session = getMockSession(user)
+  delete session.secondFactor
 
-  getAppWithSessionWithoutSecondFactor = function (app, user) {
-    const session = getMockSession(user)
-    delete session.secondFactor
+  return createAppWithSession(app, session)
+}
 
-    return createAppWithSession(app, session)
-  },
-
-  getMockSession = function (user) {
-    return _.cloneDeep({
-      csrfSecret: '123',
-      12345: {refunded_amount: 5},
-      passport: {
-        user: user,
-      },
-      secondFactor: 'totp',
-      last_url: 'last_url',
-      version: user.sessionVersion
-    })
-  }
+const getMockSession = function (user) {
+  return _.cloneDeep({
+    csrfSecret: '123',
+    12345: {refunded_amount: 5},
+    passport: {
+      user: user
+    },
+    secondFactor: 'totp',
+    last_url: 'last_url',
+    version: user.sessionVersion
+  })
+}
 
 module.exports = {
   createAppWithSession: createAppWithSession,
