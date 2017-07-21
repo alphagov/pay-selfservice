ecee89747a481f3f34d1f04dcf0129cfca67b851
maxcbc <maxcbc@live.com>
2017-07-21 15:03:21 +0100

    PP-1708 - Restored Get gateway account integration test
    - restored get gateway account integration test which had been deleted in error
    - altered to hit root instead of service-name page which has now been removed

---
+++ test/integration/get_gateway_account_ft_test.js
@@ -0,0 +1,62 @@
+const nock = require('nock')
+const supertest = require('supertest')
+const path = require('path')
+
+const session = require(path.join(__dirname, '/../test_helpers/mock_session.js'))
+const getApp = require(path.join(__dirname, '/../../server.js')).getApp
+
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const expect = chai.expect
+
+let app
+
+describe('get account', function () {
+  afterEach((done) => {
+    nock.cleanAll()
+    app = null
+    done()
+  })
+
+  it('should get account', function (done) {
+    const user = session.getUser({
+      service_roles: [
+        {
+          service: {
+            external_id: '1234',
+            gateway_account_ids: ['1', '2', '5']
+          },
+          role: {
+            permissions: [{name: 'transactions:read'}]
+          }
+        }
+      ]
+
+    })
+    const mockSession = session.getMockSession(user)
+    session.currentGatewayAccountId = '2'
+    app = session.getAppWithSessionAndGatewayAccountCookies(getApp(), mockSession)
+    const connectorMock = nock(process.env.CONNECTOR_URL)
+
+    connectorMock.get('/v1/frontend/accounts/1').times(2).reply(200, {
+      bob: 'bob',
+      type: 'test',
+      payment_provider: 'sandbox'
+    })
+
+    supertest(app)
+      .get('/')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect(data => {
+        expect(data.body.currentGatewayAccount).to.deep.equal({
+          bob: 'bob',
+          type: 'test',
+          payment_provider: 'sandbox',
+          full_type: 'sandbox test'
+        })
+      })
+      .end(done)
+  })
+})
