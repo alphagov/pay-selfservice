282e4169586b8dd2beb383c91539204462ba9bd1
Danail Minchev <danail.minchev@gmail.com>
2017-11-06 12:51:53 +0000

    PP-2688 Show submitted data (on error as well)

--- app/controllers/edit_merchant_details/get.js
+++ app/controllers/edit_merchant_details/get.js
@@ -3,14 +3,14 @@ const responses = require('../../utils/response')
 const countries = require('../../services/countries.js')
 
 exports.get = (req, res) => {
-  const merchantDetails = lodash.get(req, 'service.merchantDetails')
   let pageData = lodash.get(req, 'session.pageData.editMerchantDetails')
   if (pageData) {
     delete req.session.pageData.editMerchantDetails
   } else {
-    pageData = {}
+    pageData = {
+      merchant_details: lodash.get(req, 'service.merchantDetails')
+    }
   }
-  pageData.merchant_details = merchantDetails
-  pageData.countries = countries.retrieveCountries(lodash.get(merchantDetails, 'address_country'))
+  pageData.countries = countries.retrieveCountries(lodash.get(pageData.merchant_details, 'address_country'))
   return responses.response(req, res, 'merchant_details/edit_merchant_details', pageData)
 }
