e334612808b81769658f266e7c0595639e5520e9
Jon Heslop <jon@jonheslop.com>
2017-11-10 13:54:48 +0000

    PP-2543 - Multi select browser bug fixes and tweaks
    * Edited label to make it clearer it's a multi-select
    * Moved to browsered as name is clearer
    * Capitalise first letter of filter states, couldnt do with CSSas didnt work in FF
    * removed merchant details as view-only users dont get to see that
    * only constrain dropdownn height if there isnt enough room for it
    * Updated aria and accessibility labels to make it easier for screen reader users how to use the multi-select

--- app/browsered.js
+++ app/browsered.js
@@ -1,7 +1,7 @@
 'use strict'
 // NPM dependencies
 const $ = require('jquery')
-const multiSelects = require('./client-side-scripts/multi-select')
+const multiSelects = require('./browsered/multi-select')
 const fieldValidation = require('./browsered/field-validation')
 
 // This adds jquery globally for non-browserified contexts
