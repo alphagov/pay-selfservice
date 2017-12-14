1b95613bb6ff72d8c25ff38d6cc496c1f001b1d7
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2017-12-14 13:37:43 +0000

    Log reception of healthcheck
    We are trying to diagnose ECS problems, and
    having confirmation that slefservice is receiving
    healthcheck will be helpul.

--- app/controllers/healthcheck_controller.js
+++ app/controllers/healthcheck_controller.js
@@ -1,6 +1,9 @@
+const logger = require('winston')
 var responseHandler = require('../utils/response.js')
 
 module.exports.healthcheck = function (req, res) {
+  const correlationId = _.get(req, 'headers.' + CORRELATION_HEADER, '')
+  logger.info(`[${correlationId}] successful healthcheck`)
   var data = {'ping': {'healthy': true}}
   responseHandler.healthCheckResponse(req, res, data)
 }
