6f94033a40fa87d2af693609576377eed8c445db
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2017-12-18 08:27:26 +0000

    BAU: Define lodash and correlation header in hc
    solo @tlwr

--- app/controllers/healthcheck_controller.js
+++ app/controllers/healthcheck_controller.js
@@ -1,4 +1,6 @@
 const logger = require('winston')
+const _ = require('lodash')
+const CORRELATION_HEADER = require('../utils/correlation_header.js').CORRELATION_HEADER
 var responseHandler = require('../utils/response.js')
 
 module.exports.healthcheck = function (req, res) {
