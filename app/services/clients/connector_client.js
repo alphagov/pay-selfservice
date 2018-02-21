0b22da8ff6eac2cf1d3ffaaacfa83c31eddaf913
Kasun Kumara <kasun.kumara@digital.cabinet-office.gov.uk>
2018-02-21 14:32:48 +0000

    PP-3355 Reverting default page sizing (to 100)
    To verify if there is any relationship on the CPU spikes

--- app/services/clients/connector_client.js
+++ app/services/clients/connector_client.js
@@ -172,7 +172,6 @@ ConnectorClient.prototype = {
   getAllTransactions (params, successCallback) {
     var results = []
     var connectorClient = this
-    params.pageSize = params.pageSize || 500
     var recursiveRetrieve = function (recursiveParams) {
       connectorClient.searchTransactions(recursiveParams, function (data) {
         var next = _.get(data, '_links.next_page')
