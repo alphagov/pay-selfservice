7438318d243392f56a7204fa6896332a17870149
Jon Heslop <jon@jonheslop.com>
2018-02-22 17:14:09 +0000

    PP-3344 - Added logic to get payment links from products
    Fetching links and displaying them on manage page.

--- app/controllers/payment-links/get-manage-controller.js
+++ app/controllers/payment-links/get-manage-controller.js
@@ -1,15 +1,30 @@
 'use strict'
 
+// NPM dependencies
+const logger = require('winston')
 
 // Local dependencies
 const {response} = require('../../utils/response.js')
 const paths = require('../../paths')
+const productsClient = require('../../services/clients/products_client.js')
+const authService = require('../../services/auth_service.js')
+const errorView = require('../../utils/response.js').renderErrorView
 
 const PAGE_PARAMS = {
-  returnToStart: paths.paymentLinks.index,
+  returnToStart: paths.paymentLinks.start,
   manage: paths.paymentLinks.manage
 }
 
 module.exports = (req, res) => {
-  return response(req, res, 'payment-links/manage', PAGE_PARAMS)
+  productsClient.product.getByGatewayAccountId(authService.getCurrentGatewayAccountId(req))
+    .then(products => {
+      const paymentLinks = products.filter(product => product.type === 'ADHOC')
+      PAGE_PARAMS.productsLength = paymentLinks.length
+      PAGE_PARAMS.products = paymentLinks
+      return response(req, res, 'payment-links/manage', PAGE_PARAMS)
+    })
+    .catch((err) => {
+      logger.error(`[requestId=${req.correlationId}] Get ADHOC product by gateway account id failed - ${err.message}`)
+      errorView(req, res, 'Internal server error')
+    })
 }
