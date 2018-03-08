c6c88875a7aa9a37b72777f824d3d029c60d3301
george racu <george.racu@digital.cabinet-office.gov.uk>
2018-03-08 16:01:23 +0000

    PP-3449 Enable 3DS for EPDQ
    - Will display functionality for enabling 3DS for EPDQ
    - Will hide helper text in 3DS area that is specific for worldpay
    - It uses an env var to toggle displaying the 3DS button

--- test/unit/middleware/get_gateway_account_test.js
+++ test/unit/middleware/get_gateway_account_test.js
@@ -71,11 +71,28 @@ describe('middleware: getGatewayAccount', () => {
       done()
     })
   })
-  it('should extend the account data with supports3ds set to false if the account type is not worldpay', done => {
+  it('should extend the account data with supports3ds set to true if the account type is epdq', done => {
+    if (process.env.EPDQ_3DS_ENABLED === 'true') {
+      lodash.set(req, 'user.serviceRoles[0]', {gatewayAccountIds: ['1', '2', '3']})
+      const getGatewayAccount = setupGetGatewayAccount('1', 'epdq')
+      getGatewayAccount(req, res, next).then(() => {
+        expect(req.account).to.deep.equal({id: '1', payment_provider: 'epdq', supports3ds: true})
+        done()
+      })
+    } else {
+      lodash.set(req, 'user.serviceRoles[0]', {gatewayAccountIds: ['1', '2', '3']})
+      const getGatewayAccount = setupGetGatewayAccount('1', 'epdq')
+      getGatewayAccount(req, res, next).then(() => {
+        expect(req.account).to.deep.equal({id: '1', payment_provider: 'epdq', supports3ds: false})
+        done()
+      })
+    }
+  })
+  it('should extend the account data with supports3ds set to false if the account type is not worldpay or epdq', done => {
     lodash.set(req, 'user.serviceRoles[0]', {gatewayAccountIds: ['1', '2', '3']})
-    const getGatewayAccount = setupGetGatewayAccount('1', 'epdq')
+    const getGatewayAccount = setupGetGatewayAccount('1', 'smartpay')
     getGatewayAccount(req, res, next).then(() => {
-      expect(req.account).to.deep.equal({id: '1', payment_provider: 'epdq', supports3ds: false})
+      expect(req.account).to.deep.equal({id: '1', payment_provider: 'smartpay', supports3ds: false})
       done()
     })
   })
