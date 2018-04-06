3616930fc224637fc991499d36cb88f4c96f23d0
Raul Garcia <raul.garcia@thenesor.com>
2018-04-06 14:08:38 +0100

    Autopublish results to broker

--- run-pact-provider-tests.sh
+++ run-pact-provider-tests.sh
@@ -7,5 +7,6 @@ fi
 git clone git@github.com:alphagov/pay-adminusers.git
 cd pay-adminusers
 git checkout PP-Test-Pact-Versioning
+branch=$(git rev-parse --abbrev-ref HEAD)
 
-mvn test -Dtest=uk.gov.pay.adminusers.pact.UsersApiTest -DpactTags=$3 -DpactBrokerUsername=$1 -DpactBrokerPassword=$2 -DpactBrokerHost=governmentdigitalservice.pact.dius.com.au -DpactBrokerPort=443
+mvn test -Dtest=uk.gov.pay.adminusers.pact.UsersApiTest -DargLine="-Dpact.provider.version=${branch}" -DpactTags=$3 -DpactBrokerUsername=$1 -DpactBrokerPassword=$2 -DpactBrokerHost=governmentdigitalservice.pact.dius.com.au -DpactBrokerPort=443
