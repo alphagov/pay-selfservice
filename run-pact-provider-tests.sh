3d437071630e81a59bf5b483d5e3e92e60eac04c
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2018-04-06 11:59:35 +0100

    Run pact provider tests against a branch

--- run-pact-provider-tests.sh
+++ run-pact-provider-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git clone git@github.com:alphagov/pay-adminusers.git
-
 cd pay-adminusers
+git checkout PP-Test-Pact-Versioning
 
-mvn test -Dtest=uk.gov.pay.adminusers.pact.ContractTestSuite -DargLine="-DpactSource=broker -DpactTags=$3" -DpactBrokerUsername=$1 -DpactBrokerPassword=$2 -DpactBrokerHost=governmentdigitalservice.pact.dius.com.au -DpactBrokerPort=443
+mvn test -Dtest=uk.gov.pay.adminusers.pact.ContractTestSuite -DpactTags=$3 -DpactBrokerUsername=$1 -DpactBrokerPassword=$2 -DpactBrokerHost=governmentdigitalservice.pact.dius.com.au -DpactBrokerPort=443
