daa7b170619bdcc17feb1c70abb3361523083f0d
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2018-04-06 11:26:32 +0100

    remove some env vars
    These are already set in the pay-jenkins-library

--- env.sh
+++ env.sh
@@ -10,9 +10,4 @@ fi
 export CERTS_PATH=$WORKSPACE/pay-scripts/services/ssl/certs
 export PGSSLROOTCERT=$CERTS_PATH/selfservice.db.pymnt.localdomain.crt
 
-export PACT_CONSUMER_VERSION=5
-export PACT_BROKER_USERNAME=Qdb1JETG8s9HZ0THcD55LZ2r15V7ZPt
-export PACT_BROKER_PASSWORD=62VMf2Vjxh0szgnm0mjGgboPlDfJnAv
-export PACT_CONSUMER_TAG=test-tag
-
 eval "$@"
