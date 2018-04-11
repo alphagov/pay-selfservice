ab34a9dafd4ee2d3fab3038faf6585d4305c40b9
george racu <george.racu@digital.cabinet-office.gov.uk>
2018-04-11 11:44:58 +0100

    PP-3525 Refactor tests
    - Refactored new tests
    - Removed tests added previously as they are replaced by the new ones

--- test/ui/toggle_3ds_ui_tests.js
+++ test/ui/toggle_3ds_ui_tests.js
@@ -92,40 +92,6 @@ describe('The toggle 3D Secure page when 3D Secure is off', function () {
 
     body.should.containNoSelector('#3ds-toggled')
   })
-
-  it('should display helper test for worldpay', function () {
-    let templateData = {
-      'supports3ds': true,
-      'requires3ds': false,
-      'justToggled': false,
-      'showHelper3ds': true,
-      permissions: {
-        toggle_3ds_read: true,
-        toggle_3ds_update: true
-      }
-    }
-
-    let body = renderTemplate('3d_secure/index', templateData)
-
-    body.should.containSelector('#3ds-helper-text-worldpay')
-  })
-
-  it('should not display helper test if not worldpay', function () {
-    let templateData = {
-      'supports3ds': true,
-      'requires3ds': false,
-      'justToggled': false,
-      'showHelper3ds': false,
-      permissions: {
-        toggle_3ds_read: true,
-        toggle_3ds_update: true
-      }
-    }
-
-    let body = renderTemplate('3d_secure/index', templateData)
-
-    body.should.containNoSelector('#3ds-helper-text-worldpay')
-  })
 })
 
 describe('The toggle 3D Secure page when 3D Secure is on', function () {
@@ -316,35 +282,5 @@ describe('The turn 3D Secure on confirmation page', function () {
     let body = renderTemplate('3d_secure/on_confirm', templateData)
 
     body.should.containNoSelector('#3ds-confirm-on-button')
-  })
-
-  it('should not display helper test if not worldpay', function () {
-    let templateData = {
-      'supports3ds': true,
-      'showHelper3ds': false,
-      permissions: {
-        toggle_3ds_read: true,
-        toggle_3ds_update: true
-      }
-    }
-
-    let body = renderTemplate('3d_secure/on_confirm', templateData)
-
-    body.should.containNoSelector('#3ds-helper-text-worldpay')
-  })
-
-  it('should display helper test if worldpay', function () {
-    let templateData = {
-      'supports3ds': true,
-      'showHelper3ds': true,
-      permissions: {
-        toggle_3ds_read: true,
-        toggle_3ds_update: true
-      }
-    }
-
-    let body = renderTemplate('3d_secure/on_confirm', templateData)
-
-    body.should.containSelector('#3ds-helper-text-worldpay')
   })
 })
--- test/unit/controller/toggle_3ds_controller/toggle_3ds_controller_test.js
+++ test/unit/controller/toggle_3ds_controller/toggle_3ds_controller_test.js
@@ -17,6 +17,7 @@ const CONNECTOR_ACCOUNT_PATH = '/v1/frontend/accounts/' + ACCOUNT_ID
 const CONNECTOR_ACCEPTED_CARD_TYPES_FRONTEND_PATH = CONNECTOR_ACCOUNT_PATH + '/card-types'
 const connectorMock = nock(process.env.CONNECTOR_URL, aCorrelationHeader)
 const csrf = require('csrf')
+const ID_3DS_HELPER_TEXT = '#3ds-helper-text-worldpay'
 
 let app, response, $
 
@@ -25,162 +26,104 @@ function mockConnectorAcceptedCardTypesEndpoint (acceptedCardTypes) {
     .reply(200, acceptedCardTypes)
 }
 
-describe.only('The 3D Secure index endpoint', () => {
+function runConnectorMockGet (accountName) {
+  connectorMock.get(CONNECTOR_ACCOUNT_PATH)
+    .reply(200, {
+      'payment_provider': accountName,
+      'requires3ds': false
+    })
+  mockConnectorAcceptedCardTypesEndpoint({'card_types': []})
+}
+
+function getToggle3DsIndex (accountName, done) {
+  runConnectorMockGet(accountName)
+
+  supertest(app)
+    .get(paths.toggle3ds.index)
+    .set('x-request-id', requestId)
+    .end((err, res) => {
+      response = res
+      $ = cheerio.load(res.text || '')
+      done(err)
+    })
+}
+
+function postToggle3DsIndexConfirm (accountName, done) {
+  runConnectorMockGet(accountName)
+
+  const csrfToken = csrf().create('123')
+  supertest(app)
+    .post(paths.toggle3ds.onConfirm)
+    .send({'csrfToken': csrfToken})
+    .set('x-request-id', requestId)
+    .end((err, res) => {
+      response = res
+      $ = cheerio.load(res.text || '')
+      done(err)
+    })
+}
+
+function assertStatusAndTitle () {
+  expect(response.statusCode).to.equal(200)
+  expect($('.page-title').text()).to.contain('3D Secure')
+}
+
+describe('The 3D Secure ', () => {
+  const permissions = [{name: 'toggle-3ds:update'}, {name: 'toggle-3ds:read'}]
+  const user = session.getUser({
+    gateway_account_ids: [ACCOUNT_ID], permissions: permissions
+  })
+
+  beforeEach(function () {
+    app = session.getAppWithLoggedInUser(getApp(), user)
+  })
+
   afterEach(function () {
     nock.cleanAll()
     app = null
   })
 
-  beforeEach(function (done) {
-    const permissions = [{name: 'toggle-3ds:update'}, {name: 'toggle-3ds:read'}]
-    const user = session.getUser({
-      gateway_account_ids: [ACCOUNT_ID], permissions: permissions
+  describe('index endpoint', () => {
+    describe('for WorldPay account', () => {
+      beforeEach(function (done) {
+        getToggle3DsIndex('worldpay', done)
+      })
+      it(`should display helper text`, () => {
+        assertStatusAndTitle()
+        expect($(ID_3DS_HELPER_TEXT).text()).to.contain('Activating 3D Secure')
+      })
     })
-    app = session.getAppWithLoggedInUser(getApp(), user)
 
-    connectorMock.get(CONNECTOR_ACCOUNT_PATH)
-      .reply(200, {
-        'payment_provider': 'worldpay',
-        'requires3ds': false
+    describe('for non WorldPay account', () => {
+      beforeEach(function (done) {
+        getToggle3DsIndex('smartpay', done)
       })
-
-    mockConnectorAcceptedCardTypesEndpoint({'card_types': []})
-
-    supertest(app)
-      .get(paths.toggle3ds.index)
-      .set('x-request-id', requestId)
-      .end((err, res) => {
-        response = res
-        $ = cheerio.load(res.text || '')
-        done(err)
+      it(`should not display helper text`, () => {
+        assertStatusAndTitle()
+        expect($(ID_3DS_HELPER_TEXT).text()).to.equal('')
       })
-  })
-
-  it(`should display helper text for WordlPay 3DS`, () => {
-    expect(response.statusCode).to.equal(200)
-    expect($('.page-title').text()).to.contain('3D Secure')
-    expect($('#3ds-helper-text-worldpay').text()).to.contain('Activating 3D Secure')
-  })
-})
-
-describe.only('The 3D Secure index endpoint', () => {
-  afterEach(function () {
-    nock.cleanAll()
-    app = null
-  })
-
-  beforeEach(function (done) {
-    const permissions = [{name: 'toggle-3ds:update'}, {name: 'toggle-3ds:read'}]
-    const user = session.getUser({
-      gateway_account_ids: [ACCOUNT_ID], permissions: permissions
     })
-    app = session.getAppWithLoggedInUser(getApp(), user)
+  })
 
-    connectorMock.get(CONNECTOR_ACCOUNT_PATH)
-      .reply(200, {
-        'payment_provider': 'smartpay',
-        'requires3ds': false
+  describe('confirmation page', () => {
+    describe('for WorldPay account', () => {
+      beforeEach((done) => {
+        postToggle3DsIndexConfirm('worldpay', done)
       })
-
-    mockConnectorAcceptedCardTypesEndpoint({'card_types': []})
-
-    supertest(app)
-      .get(paths.toggle3ds.index)
-      .set('x-request-id', requestId)
-      .end((err, res) => {
-        response = res
-        $ = cheerio.load(res.text || '')
-        done(err)
+      it(`should display helper text`, () => {
+        assertStatusAndTitle()
+        expect($(ID_3DS_HELPER_TEXT).text()).to.contain('If you haven’t confirmed this with Worldpay')
       })
-  })
-
-  it(`should not display helper text if not WordlPay 3DS`, () => {
-    expect(response.statusCode).to.equal(200)
-    expect($('.page-title').text()).to.contain('3D Secure')
-    expect($('#3ds-helper-text-worldpay').text()).to.equal('')
-  })
-})
-
-describe.only('The 3D Secure confirmation page for WorldPay account', () => {
-  afterEach(function () {
-    nock.cleanAll()
-    app = null
-  })
-
-  beforeEach(function (done) {
-    const permissions = [{name: 'toggle-3ds:update'}, {name: 'toggle-3ds:read'}]
-    const user = session.getUser({
-      gateway_account_ids: [ACCOUNT_ID], permissions: permissions
     })
-    app = session.getAppWithLoggedInUser(getApp(), user)
 
-    connectorMock.get(CONNECTOR_ACCOUNT_PATH)
-      .reply(200, {
-        'payment_provider': 'worldpay',
-        'requires3ds': false
+    describe('for non WorldPay account', () => {
+      beforeEach((done) => {
+        postToggle3DsIndexConfirm('smartpay', done)
       })
-
-    mockConnectorAcceptedCardTypesEndpoint({'card_types': []})
-
-    const csrfToken = csrf().create('123')
-
-    supertest(app)
-      .post(paths.toggle3ds.onConfirm)
-      .send({'csrfToken': csrfToken})
-      .set('x-request-id', requestId)
-      .end((err, res) => {
-        response = res
-        $ = cheerio.load(res.text || '')
-        done(err)
+      it('should not display helper text', () => {
+        assertStatusAndTitle()
+        expect($(ID_3DS_HELPER_TEXT).text()).to.equal('')
       })
-  })
-
-  it(`should display helper text if WordlPay 3DS`, () => {
-    expect(response.statusCode).to.equal(200)
-    expect($('.page-title').text()).to.contain('3D Secure')
-    expect($('#3ds-helper-text-worldpay').text()).to.contain('If you haven’t confirmed this with Worldpay')
-  })
-})
-
-describe.only('The 3D Secure confirmation page for non WorldPay account', () => {
-  afterEach(function () {
-    nock.cleanAll()
-    app = null
-  })
-
-  beforeEach(function (done) {
-    const permissions = [{name: 'toggle-3ds:update'}, {name: 'toggle-3ds:read'}]
-    const user = session.getUser({
-      gateway_account_ids: [ACCOUNT_ID], permissions: permissions
     })
-    app = session.getAppWithLoggedInUser(getApp(), user)
-
-    connectorMock.get(CONNECTOR_ACCOUNT_PATH)
-      .reply(200, {
-        'payment_provider': 'smartpay',
-        'requires3ds': false
-      })
-
-    mockConnectorAcceptedCardTypesEndpoint({'card_types': []})
-
-    const csrfToken = csrf().create('123')
-
-    supertest(app)
-      .post(paths.toggle3ds.onConfirm)
-      .send({'csrfToken': csrfToken})
-      // .set('Accept', 'application/json')
-      // .set('Content-Type', 'application/x-www-form-urlencoded')
-      .set('x-request-id', requestId)
-      .end((err, res) => {
-        response = res
-        $ = cheerio.load(res.text || '')
-        done(err)
-      })
-  })
-
-  it(`should not display helper text if not WordlPay 3DS`, () => {
-    expect(response.statusCode).to.equal(200)
-    expect($('.page-title').text()).to.contain('3D Secure')
-    expect($('#3ds-helper-text-worldpay').text()).to.equal('')
   })
 })
