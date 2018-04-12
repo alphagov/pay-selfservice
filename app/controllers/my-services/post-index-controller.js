e9eb13cd051013974bfbaf8ad7332f27fd74d4d2
Jon Heslop <jon@jonheslop.com>
2018-04-12 13:34:31 +0100

    PP-3603 - refactoring old style controllers

---
+++ app/controllers/my-services/post-index-controller.js
@@ -0,0 +1,21 @@
+const _ = require('lodash')
+const logger = require('winston')
+
+const paths = require('../../paths')
+
+const validAccountId = (accountId, user) => {
+  const gatewayAccountIds = _.flattenDeep(_.concat(user.serviceRoles.map(serviceRole => serviceRole.service.gatewayAccountIds)))
+  return accountId && gatewayAccountIds.indexOf(accountId) !== -1
+}
+
+module.exports = (req, res) => {
+  let newAccountId = _.get(req, 'body.gatewayAccountId')
+
+  if (validAccountId(newAccountId, req.user)) {
+    req.gateway_account.currentGatewayAccountId = newAccountId
+    res.redirect(302, paths.dashboard.index)
+  } else {
+    logger.warn(`Attempted to switch to invalid account ${newAccountId}`)
+    res.redirect(302, paths.serviceSwitcher.index)
+  }
+}
