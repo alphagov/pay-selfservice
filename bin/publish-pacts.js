ffce3c20b2a49b4b4c195ca0166791b77a9f4e80
Raul Garcia <raul.garcia@thenesor.com>
2018-04-20 13:39:10 +0100

    PP-3614: Use environment variables correctly
    @rauligs @oswaldquek

--- bin/publish-pacts.js
+++ bin/publish-pacts.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
-/* eslint-disable no-template-curly-in-string */
 let pact = require('@pact-foundation/pact-node')
 let opts = {
   pactFilesOrDirs: [`${__dirname}/../pacts/*`],
-  pactBroker: '${PACT_BROKER_URL}',
-  consumerVersion: '${PACT_CONSUMER_VERSION}',
-  pactBrokerUsername: '${PACT_BROKER_USERNAME}',
-  pactBrokerPassword: '${PACT_BROKER_PASSWORD}',
-  tags: '${PACT_CONSUMER_TAG}'
+  pactBroker: process.env.PACT_BROKER_URL,
+  consumerVersion: process.env.PACT_CONSUMER_VERSION,
+  pactBrokerUsername: process.env.PACT_BROKER_USERNAME,
+  pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
+  tags: process.env.PACT_CONSUMER_TAG
 }
 
 pact.publishPacts(opts).then(function () {
