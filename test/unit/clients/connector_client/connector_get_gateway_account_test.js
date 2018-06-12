15f89c0ebbc7e50f293fa427c56f5e4f549340ca
Mark Bridgett <mark.bridgett@digital.cabinet-office.gov.uk>
2018-06-12 13:54:59 +0100

    PP-3813 Ensured pact inputs match connector functionality for passing 'sandbox' as a payment_provider

--- test/unit/clients/connector_client/connector_get_gateway_account_test.js
+++ test/unit/clients/connector_client/connector_get_gateway_account_test.js
@@ -36,7 +36,7 @@ describe('connector client - get gateway account', function () {
 
   describe('get gateway account - success', () => {
     const params = {
-      gateway_account_id: '666'
+      gateway_account_id: 666
     }
     const validGetGatewayAccountResponse = gatewayAccountFixtures.validGatewayAccountResponse(params)
 
@@ -44,7 +44,7 @@ describe('connector client - get gateway account', function () {
       provider.addInteraction(
         new PactInteractionBuilder(`${ACCOUNTS_RESOURCE}/${params.gateway_account_id}`)
           .withUponReceiving('a valid get gateway account request')
-          .withState(`User ${params.gateway_account} exists in the database`)
+          .withState(`User ${params.gateway_account_id} exists in the database`)
           .withMethod('GET')
           .withResponseBody(validGetGatewayAccountResponse.getPactified())
           .withStatusCode(200)
