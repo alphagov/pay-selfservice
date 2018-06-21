572a0fb68fe7a21b0dbbaa789c7054acc2115e92
Jon Heslop <jon@jonheslop.com>
2018-06-21 14:17:11 +0100

    BAU - use arrow function

--- app/middleware/has_services.js
+++ app/middleware/has_services.js
@@ -2,7 +2,7 @@ const logger = require('winston')
 const _ = require('lodash')
 const paths = require('../paths')
 
-module.exports = function (req, res, next) {
+module.exports = (req, res, next) => {
   const serviceRoles = _.get(req, 'user.serviceRoles', [])
   if (req.url !== paths.serviceSwitcher.index && serviceRoles.length === 0) {
     logger.info('User does not belong to any service user_external_id=', _.get(req, 'user.externalId'))
