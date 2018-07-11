96cb063a709bf74156ac87084598e7e6389a47d3
Mark Bridgett <mark.bridgett@digital.cabinet-office.gov.uk>
2018-07-11 16:07:10 +0100

    PP-3990 Prevents 'to-be' pacts from being published by changing their file extension before/after being published

--- docker/build_and_test.sh
+++ docker/build_and_test.sh
@@ -6,4 +6,6 @@ npm run compile
 # disabled while promise rejections not returning error objects need to be addressed
 # npm run lint
 npm test -- --forbid-only --forbid-pending
+# prevent publish of any pacts with 'to-be' in their name
+for i in ./pacts/*-to-be-*.json; do mv "$i" "${i%.json}.ignore"; done
 npm run publish-pacts
