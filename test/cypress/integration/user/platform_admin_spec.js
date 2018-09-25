f79576c899a28b94a8e649d9f114310ee8635d2a
Mark Bridgett <mark.bridgett@digital.cabinet-office.gov.uk>
2018-09-25 16:19:06 +0100

    PP-4234 Added basic browser test for dashboard access for platform admin

--- test/cypress/integration/user/platform_admin_spec.js
+++ test/cypress/integration/user/platform_admin_spec.js
@@ -1,22 +1,13 @@
 describe('Dashboard', () => {
-  const selfServiceUsers = require('../../../fixtures/config/self_service_user.json')
 
   beforeEach(() => {
-    cy.setCookie('session', Cypress.env('encryptedSessionCookieDefaultUser'))
-    cy.setCookie('gateway_account', Cypress.env('encryptedGatewayAccountCookieDefaultUser'))
+    cy.setCookie('session', Cypress.env('encryptedSessionCookiePlatformAdmin'))
+    cy.setCookie('gateway_account', Cypress.env('encryptedGatewayAccountCookiePlatformAdmin'))
   })
 
   describe('Homepage', () => {
-    const platformAdminUser = selfServiceUsers.config.users.filter(fil => fil.is_primary)[0]
-
-    // Note : these from/to datetime strings exactly match those in the pact/contract, so are essential to match against stubs
-    // Either change everything together, or map these do a single place like a .json document so the contracts/tests refer to one place
-    const from = encodeURIComponent(platformAdminUser.sections.dashboard.transaction_summary.from_date)
-    const to = encodeURIComponent(platformAdminUser.sections.dashboard.transaction_summary.to_date)
-
     it('should have the page title \'Dashboard - System Generated test - GOV.UK Pay\'', () => {
-      const dashboardUrl = `/?period=custom&fromDateTime=${from}&toDateTime=${to}`
-      cy.visit(dashboardUrl)
+      cy.visit('/')
       cy.title().should('eq', 'Dashboard - System Generated test - GOV.UK Pay')
     })
   })
--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -18,29 +18,46 @@ module.exports = (on, config) => {
 
   // The same fixed user config is used to generate pacts/contracts, so generating cookies from this config
   // ensures our pact-stub server is ready to return canned responses.
-  const ssUserConfig = require('../../fixtures/config/self_service_user.json')
+  const selfServiceUserConfig = require('../../fixtures/config/self_service_user.json')
 
-  const ssUser = ssUserConfig.config.users.filter(fil => fil.is_primary)[0]
+  const selfServiceDefaultUser = selfServiceUserConfig.config.users.filter(fil => fil.is_primary)[0]
+  const selfServicePlatformAdmin = selfServiceUserConfig.config.users.filter(fil => !fil.is_primary && fil.is_platform_admin)[0]
 
+  // Cookies for the default user
   const encryptedSessionCookieDefaultUser = cookieMonster.getCookie('session', config.env.TEST_SESSION_ENCRYPTION_KEY,
     {
-      passport: {user: ssUser.external_id},
+      passport: {user: selfServiceDefaultUser.external_id},
       secondFactor: 'totp',
       version: 0,
-      icamefrom: 'cypress.io'
+      icamefrom: 'Cypress.io (Default user)'
     })
 
   const encryptedGatewayAccountCookieDefaultUser = cookieMonster.getCookie('gateway_account', config.env.TEST_SESSION_ENCRYPTION_KEY,
     {
-      currentGatewayAccountId: ssUser.gateway_accounts.filter(fil => fil.isPrimary === 'true')[0].id,
-      icamefrom: 'cypress.io'
+      currentGatewayAccountId: selfServiceDefaultUser.gateway_accounts.filter(fil => fil.isPrimary === 'true')[0].id,
+      icamefrom: 'Cypress.io (Default user)'
     })
 
+  // Cookies for the platform admin user
+  const encryptedSessionCookiePlatformAdmin = cookieMonster.getCookie('session', config.env.TEST_SESSION_ENCRYPTION_KEY,
+    {
+      passport: {user: selfServicePlatformAdmin.external_id},
+      secondFactor: 'totp',
+      version: 0,
+      icamefrom: 'Cypress.io (Platform admin)'
+    })
+
+  const encryptedGatewayAccountCookiePlatformAdmin = cookieMonster.getCookie('gateway_account', config.env.TEST_SESSION_ENCRYPTION_KEY,
+    {
+      currentGatewayAccountId: '', // No current gateway account
+      icamefrom: 'Cypress.io (Platform admin)'
+    })
+
+  // Set the environment variables for Cypress
   config.env.encryptedSessionCookieDefaultUser = encryptedSessionCookieDefaultUser
   config.env.encryptedGatewayAccountCookieDefaultUser = encryptedGatewayAccountCookieDefaultUser
-
-  console.log(`test encrypted session cookie: ${encryptedSessionCookieDefaultUser}`)
-  console.log(`test encrypted gateway account cookie: ${encryptedGatewayAccountCookieDefaultUser}`)
+  config.env.encryptedSessionCookiePlatformAdmin = encryptedSessionCookiePlatformAdmin
+  config.env.encryptedGatewayAccountCookiePlatformAdmin = encryptedGatewayAccountCookiePlatformAdmin
 
   // send back the modified config object
   return config
