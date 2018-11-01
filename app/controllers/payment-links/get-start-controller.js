a3063290db57729d393cdf1cec2bb363a728995b
Jon Heslop <jon@jonheslop.com>
2018-11-01 14:48:06 +0000

    PP-4335 - Migrate payment links index to GOV.UK Frontend

--- app/controllers/payment-links/get-start-controller.js
+++ app/controllers/payment-links/get-start-controller.js
@@ -5,16 +5,9 @@ const lodash = require('lodash')
 
 // Local dependencies
 const {response} = require('../../utils/response.js')
-const paths = require('../../paths')
-
-const PAGE_PARAMS = {
-  nextPage: paths.paymentLinks.information,
-  returnToStart: paths.paymentLinks.start,
-  manage: paths.paymentLinks.manage
-}
 
 module.exports = (req, res) => {
   lodash.set(req, 'session.pageData.createPaymentLink', {})
 
-  return response(req, res, 'payment-links/index', PAGE_PARAMS)
+  return response(req, res, 'payment-links/index', {})
 }
