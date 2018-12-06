84d9ea58ff8010d48acf22a1ba59a1c20c609bd1
Jon Heslop <jon@jonheslop.com>
2018-12-06 10:51:56 +0000

    PP-4472 - tidying up middleware to match pay’s JS styling

--- app/middleware/resolve_service.js
+++ app/middleware/resolve_service.js
@@ -1,17 +1,46 @@
-const _ = require('lodash')
-const {renderErrorView} = require('../utils/response.js')
-const {isADirectDebitAccount} = require('../services/clients/direct_debit_connector_client')
+'use strict'
 
-function notAuthorised (req, res) {
+// NPM modules
+const _ = require('lodash')
+
+// Local modules
+const { renderErrorView } = require('../utils/response.js')
+const { isADirectDebitAccount } = require('../services/clients/direct_debit_connector_client')
+
+const notAuthorised = (req, res) => {
   return renderErrorView(req, res, 'You do not have the rights to access this service.', 403)
 }
 
-/**
- * This middleware resolves the current service in context
- *
- */
-module.exports = function (req, res, next) {
-  const externalServiceId = req.params.externalServiceId
+const getServiceFromUserByExternalId = (req, externalServiceId) => {
+  return _.get(req.user.serviceRoles.find(serviceRole => serviceRole.service.externalId === externalServiceId), 'service')
+}
+
+const gatewayAccountType = req => {
+  let hasDDAccount = false
+  let hasCardAccount = false
+
+  if (req.service.gatewayAccountIds) {
+    req.service.gatewayAccountIds.forEach(account => {
+      if (isADirectDebitAccount(account)) {
+        hasDDAccount = true
+      } else {
+        hasCardAccount = true
+      }
+    })
+  }
+
+  if (hasDDAccount && hasCardAccount) {
+    return 'has_card_and_dd_gateway_account'
+  } else if (hasDDAccount) {
+    return 'has_direct_debit_gateway_account'
+  } else if (hasCardAccount) {
+    return 'has_card_gateway_account'
+  }
+}
+
+// This middleware resolves the current service in context
+module.exports = (req, res, next) => {
+  const { externalServiceId } = req.params
   const gatewayAccountId = _.get(req, 'gateway_account.currentGatewayAccountId')
 
   if (externalServiceId) {
@@ -23,7 +52,7 @@ module.exports = function (req, res, next) {
     req.service = _.get(req.user.serviceRoles.find(serviceRole => serviceRole.service.gatewayAccountIds.includes(gatewayAccountId)), 'service')
   }
 
-  if (!req.service && req.user.serviceRoles.length) {
+  if (!req.service && req.user.serviceRoles.length > 0) {
     req.service = _.get(req.user.serviceRoles[0], 'service')
   }
 
@@ -38,30 +67,4 @@ module.exports = function (req, res, next) {
   req.service.hasCardAndDirectDebitGatewayAccount = gatewayAccountType(req) === 'has_card_and_dd_gateway_account'
 
   next()
-}
-
-function getServiceFromUserByExternalId (req, externalServiceId) {
-  return _.get(req.user.serviceRoles.find(serviceRole => serviceRole.service.externalId === externalServiceId), 'service')
-}
-
-function gatewayAccountType (req) {
-  let hasDDAccount = false
-  let hasCardAccount = false
-
-  if (req.service.gatewayAccountIds) {
-    req.service.gatewayAccountIds.forEach((element) => {
-      if (isADirectDebitAccount(element)) {
-        hasDDAccount = true
-      } else {
-        hasCardAccount = true
-      }
-    })
-  }
-  if (hasDDAccount && hasCardAccount) {
-    return 'has_card_and_dd_gateway_account'
-  } else if (hasDDAccount) {
-    return 'has_direct_debit_gateway_account'
-  } else if (hasCardAccount) {
-    return 'has_card_gateway_account'
-  }
 }
