6c6a21bdde5f5bf33b0b0724eb9c4fecdd7f62b1
Danail Minchev <danail.minchev@gmail.com>
2018-12-31 11:56:35 +0000

    PP-4449 Add TODO comments for clarity
    - Mark the next work with TODO comments

--- app/controllers/request-to-go-live/index/get.controller.js
+++ app/controllers/request-to-go-live/index/get.controller.js
@@ -4,6 +4,7 @@
 const response = require('../../../utils/response')
 
 module.exports = (req, res) => {
+  // TODO: finish the UI implementation and logic
   const externalServiceId = req.service.externalId
   return response.response(req, res, 'request-to-go-live/index', {externalServiceId})
 }
--- app/controllers/request-to-go-live/index/post.controller.js
+++ app/controllers/request-to-go-live/index/post.controller.js
@@ -4,6 +4,7 @@
 const paths = require('../../../paths')
 
 module.exports = (req, res) => {
+  // TODO: finish the UI implementation and logic
   const externalServiceId = req.service.externalId
   res.redirect(303, paths.requestToGoLive.index.replace(':externalServiceId', externalServiceId))
 }
