62c8db875ae70d785bfc09e19871a36cf3d7f2db
Stephen Daly <stephencdaly@gmail.com>
2019-01-30 13:25:27 +0000

    PP-4678 Revise comments on Cypress index files

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -1,3 +1,10 @@
+// ***********************************************************
+// This file is used to load plugins.
+//
+// You can read more about Cypress plugins here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
 'use strict'
 
 // NPM dependencies
@@ -8,19 +15,8 @@ const request = require('request')
 const cookieMonster = require('../integration/utils/cookie-monster')
 const stubs = require('./stubs')
 
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
 module.exports = (on, config) => {
   const mountebankImpostersUrl = config.env.MOUNTEBANK_URL + '/imposters'
 
--- test/cypress/support/index.js
+++ test/cypress/support/index.js
@@ -1,3 +1,14 @@
+// ***********************************************************
+// This file is processed and loaded automatically before
+// Cypress test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
 require('./commands')
 
 beforeEach(() => {
