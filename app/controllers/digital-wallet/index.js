f0d4e4b1d61ceab5d2d922ab35e96164e448c70f
Jon Heslop <jon@jonheslop.com>
2019-02-14 10:57:37 +0000

    PP-4819 - Add templates for enabling Google Pay

--- app/controllers/digital-wallet/index.js
+++ app/controllers/digital-wallet/index.js
@@ -3,3 +3,5 @@
 exports.getSummary = require('./get-summary-controller')
 exports.getEnableApplePay = require('./get-enable-apple-pay-controller')
 exports.postEnableApplePay = require('./post-enable-apple-pay-controller')
+exports.getEnableGooglePay = require('./get-enable-google-pay-controller')
+exports.postEnableGooglePay = require('./post-enable-google-pay-controller')
