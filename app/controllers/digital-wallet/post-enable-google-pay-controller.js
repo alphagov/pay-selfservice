f581c4a92d25c8810c3fcfb03fd7f38054dc1dd8
Gideon Goldberg <gidsgoldberg@gmail.com>
2019-02-19 09:35:49 +0000

    patch merchant ID

--- app/controllers/digital-wallet/post-enable-google-pay-controller.js
+++ app/controllers/digital-wallet/post-enable-google-pay-controller.js
@@ -10,19 +10,21 @@ const connector = new ConnectorClient(process.env.CONNECTOR_URL)
 module.exports = (req, res) => {
   const gatewayAccountId = auth.getCurrentGatewayAccountId(req)
   const correlationId = req.headers[CORRELATION_HEADER] || ''
-
-  console.log(ConnectorClient)
-  const patch = {'op': 'replace', 'path': 'allow_google_pay', 'value': 'true'}
-  const args = {
-    gatewayAccountId,
-    correlationId,
-    payload: patch
-  }
-  console.log(args)
+  const gatewayMerchantId = req.body.merchantId
+  const enableGooglePayPayload = { 'op': 'replace', 'path': 'allow_google_pay', 'value': 'true' }
   connector.toggleGooglePayEnabled({
     gatewayAccountId,
     correlationId,
-    payload: patch
+    payload: enableGooglePayPayload
+  }, (success) => {
+    console.log(success)
+  })
+
+  const setGatewayMerchantIdPayload = { 'op': 'add', 'path': 'credentials/gateway_merchant_id', 'value': gatewayMerchantId }
+  connector.toggleGooglePayEnabled({
+    gatewayAccountId,
+    correlationId,
+    payload: setGatewayMerchantIdPayload
   }, (success) => {
     console.log(success)
   })
