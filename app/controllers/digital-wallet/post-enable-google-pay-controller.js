ab44a2d02ccc5df4a8a9ca0f0af73f16743fd4e0
Gideon Goldberg <gidsgoldberg@gmail.com>
2019-02-20 09:48:36 +0000

    Handle non-array error message case

--- app/controllers/digital-wallet/post-enable-google-pay-controller.js
+++ app/controllers/digital-wallet/post-enable-google-pay-controller.js
@@ -7,6 +7,10 @@ const auth = require('../../services/auth_service')
 const { CORRELATION_HEADER } = require('../../utils/correlation_header.js')
 const connector = new ConnectorClient(process.env.CONNECTOR_URL)
 
+const displayErrorMessage = (error) => {
+  return Array.isArray(error.message.errors) ? error.message.errors.join(', ') : error.message.message
+}
+
 module.exports = (req, res) => {
   const gatewayAccountId = auth.getCurrentGatewayAccountId(req)
   const correlationId = req.headers[CORRELATION_HEADER] || ''
@@ -26,11 +30,11 @@ module.exports = (req, res) => {
     payload: setGatewayMerchantIdPayload
   })
 
-  Promise.all([enableGooglePayBoolean, setGatewayMerchantId]).then(toggleGooglePayResponse => {
+  Promise.all([setGatewayMerchantId, enableGooglePayBoolean]).then(() => {
     req.flash('generic', '<h2>Google Pay successfully enabled.</h2>')
     return res.redirect(paths.digitalWallet.summary)
   }).catch(err => {
-    req.flash('genericError', `<h2>Something went wrong</h2><p>${err.message.errors.join(', ')}</p>`)
+    req.flash('genericError', `<h2>Something went wrong</h2><p>${displayErrorMessage(err)}</p>`)
     return res.redirect(paths.digitalWallet.confirmGooglePay)
   })
 }
