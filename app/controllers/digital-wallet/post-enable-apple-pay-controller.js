715082107b5d9667872529aa9dad618f2117cb60
Gideon Goldberg <gidsgoldberg@gmail.com>
2019-02-21 10:47:53 +0000

    patch enable apple pay to connector

--- app/controllers/digital-wallet/post-enable-apple-pay-controller.js
+++ app/controllers/digital-wallet/post-enable-apple-pay-controller.js
@@ -2,7 +2,31 @@
 
 // Local dependencies
 const paths = require('../../paths')
+const { ConnectorClient } = require('../../services/clients/connector_client')
+const auth = require('../../services/auth_service')
+const { CORRELATION_HEADER } = require('../../utils/correlation_header.js')
+const connector = new ConnectorClient(process.env.CONNECTOR_URL)
+
+const displayErrorMessage = (error) => {
+  return Array.isArray(error.message.errors) ? error.message.errors.join(', ') : error.message.message
+}
 
 module.exports = (req, res) => {
-  return res.redirect(paths.digitalWallet.summary)
+  const gatewayAccountId = auth.getCurrentGatewayAccountId(req)
+  const correlationId = req.headers[CORRELATION_HEADER] || ''
+  const enableApplePayPayload = { 'op': 'replace', 'path': 'allow_apple_pay', 'value': 'true' }
+
+  const enableApplePayBoolean = connector.patchAccount({
+    gatewayAccountId,
+    correlationId,
+    payload: enableApplePayPayload
+  })
+
+  enableApplePayBoolean.then(() => {
+    req.flash('generic', '<h2>Apple Pay successfully enabled.</h2>')
+    return res.redirect(paths.digitalWallet.summary)
+  }).catch(err => {
+    req.flash('genericError', `<h2>Something went wrong</h2><p>${displayErrorMessage(err)}</p>`)
+    return res.redirect(paths.digitalWallet.confirmGooglePay)
+  })
 }
