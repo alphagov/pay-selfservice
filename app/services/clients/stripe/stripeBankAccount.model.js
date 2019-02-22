0ef4f156bb3843bda9c30ef646fc7ad8df1fd226
Stephen Daly <stephencdaly@gmail.com>
2019-02-22 11:35:00 +0000

    PP-4462 Add Stripe client
    Add a stripe client to make calls to the stripe API through the stripe
    node package.
    
    Add a model for the payload sent to stripe to add bank account details
    to a stripe account.
    
    with @DanailMinchev

---
+++ app/services/clients/stripe/stripeBankAccount.model.js
@@ -0,0 +1,41 @@
+'use strict'
+
+// NPM dependencies
+const Joi = require('joi')
+
+const schema = {
+  bank_account_sort_code: Joi.string().required(),
+  bank_account_number: Joi.string().required()
+}
+
+class StripeBankAccount {
+  constructor (body) {
+    const params = Object.assign({}, body)
+    const { error, value: model } = Joi.validate(params, schema, { allowUnknown: true, stripUnknown: true })
+
+    if (error) {
+      throw new Error(`StripeBankAccount ${error.details[0].message}`)
+    }
+
+    Object.assign(this, build(model))
+  }
+
+  basicObject () {
+    return Object.assign({}, this)
+  }
+}
+
+function build (params) {
+  return {
+    external_account: {
+      object: 'bank_account',
+      country: 'GB',
+      currency: 'GBP',
+      account_holder_type: 'company',
+      routing_number: params.bank_account_sort_code,
+      account_number: params.bank_account_number
+    }
+  }
+}
+
+module.exports = StripeBankAccount
