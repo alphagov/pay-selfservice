8879aff0c0aee0478eb9374888e1ee5c7ffa0c49
Danail Minchev <danail.minchev@gmail.com>
2019-03-06 11:24:02 +0000

    PP-4462 Add get-stripe-account middleware
    - Add `get-stripe-account` middleware which is retrieving `stripeAccount` using `gateway_account_id` and `connector.getStripeAccount` client method.
      It then saves the response in `res.locals.stripeAccount`

---
+++ app/middleware/stripe-setup/get-stripe-account.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// Local dependencies
+const { ConnectorClient } = require('../../services/clients/connector_client')
+const connector = new ConnectorClient(process.env.CONNECTOR_URL)
+const { renderErrorView } = require('../../utils/response')
+
+module.exports = function getStripeAccount (req, res, next) {
+  if (!req.account) {
+    renderErrorView(req, res, 'Internal server error')
+    return
+  }
+
+  connector.getStripeAccount(req.account.gateway_account_id, req.correlationId).then(stripeAccountResponse => {
+    res.locals.stripeAccount = stripeAccountResponse
+    next()
+  }).catch(() => {
+    renderErrorView(req, res, 'Please try again or contact support team')
+  })
+}
