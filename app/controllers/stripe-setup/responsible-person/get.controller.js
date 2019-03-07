dbfd43fcb1725e80d3868bd199e95a0981110c00
Alex Bishop <alexbishop1@users.noreply.github.com>
2019-03-07 17:56:23 +0000

    PP-4463 Middleware to check responsible person flag not set
    Middleware to check that the Stripe responsible person flag is not
    already set before allowing access to the page.
    
    Heavily based on the equivalent middleware for the bank account
    details flag.

--- app/controllers/stripe-setup/responsible-person/get.controller.js
+++ app/controllers/stripe-setup/responsible-person/get.controller.js
@@ -4,10 +4,5 @@
 const response = require('../../../utils/response')
 
 module.exports = (req, res) => {
-  if (req.account.payment_provider.toLowerCase() !== 'stripe') {
-    res.status(404)
-    res.render('404')
-    return
-  }
   return response.response(req, res, 'stripe-setup/responsible-person/index')
 }
---
+++ app/middleware/stripe-setup/check-responsible-person-not-submitted.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// Local dependencies
+const { ConnectorClient } = require('../../services/clients/connector_client')
+const connector = new ConnectorClient(process.env.CONNECTOR_URL)
+const { renderErrorView } = require('../../utils/response')
+const paths = require('../../paths')
+
+module.exports = function checkResponsiblePersonNotSubmitted (req, res, next) {
+  if (!req.account) {
+    renderErrorView(req, res, 'Internal server error')
+    return
+  }
+
+  connector.getStripeAccountSetup(req.account.gateway_account_id, req.correlationId).then(stripeSetupResponse => {
+    if (stripeSetupResponse.responsiblePerson) {
+      req.flash('genericError', 'You’ve already nominated your responsible person.<br>Contact GOV.UK Pay support if you need to change them')
+      res.redirect(303, paths.dashboard.index)
+    } else {
+      next()
+    }
+  }).catch(() => {
+    renderErrorView(req, res, 'Please try again or contact support team')
+  })
+}
