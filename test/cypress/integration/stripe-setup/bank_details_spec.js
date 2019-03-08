576adbdd2a5a9169797c0516c5bc0e6ce0397e99
Danail Minchev <danail.minchev@gmail.com>
2019-03-08 13:00:54 +0000

    PP-4462 Modify bank details post controller to have sanitised and display variables
    - Modify bank details post controller to have:
      `raw*` body variables used in validation and displaying the data in the inputs (except when the user wants to change the data and the inputs are already submitted in normalised form)
      `sanitised*` body variables used to send to Stripe
      `display*` body variables used to display the content in normalised way

--- test/cypress/integration/stripe-setup/bank_details_spec.js
+++ test/cypress/integration/stripe-setup/bank_details_spec.js
@@ -7,6 +7,7 @@ describe('Stripe setup: bank details page', () => {
   const userExternalId = 'userExternalId'
   const accountNumber = '00012345'
   const sortCode = '108800'
+  const displaySortCode = '10 88 00'
 
   describe('Card gateway account', () => {
     const getStripeSetupStub = function getStripeSetupStub (bankAccountCompleted) {
@@ -127,7 +128,7 @@ describe('Stripe setup: bank details page', () => {
         cy.get('#stripe-setup-bank-details-form > button[type=submit]').click()
 
         cy.get('#stripe-setup-account-number-value').should('contain', accountNumber)
-        cy.get('#stripe-setup-sort-code-value').should('contain', sortCode)
+        cy.get('#stripe-setup-sort-code-value').should('contain', displaySortCode)
 
         cy.get('h1').should('contain', 'Check details before saving')
       })
@@ -272,7 +273,7 @@ describe('Stripe setup: bank details page', () => {
 
       it('should go to check your answers page when inputs are valid', () => {
         cy.get('#stripe-setup-account-number-value').should('contain', accountNumber)
-        cy.get('#stripe-setup-sort-code-value').should('contain', sortCode)
+        cy.get('#stripe-setup-sort-code-value').should('contain', displaySortCode)
 
         cy.get('h1').should('contain', 'Check details before saving')
       })
@@ -283,7 +284,7 @@ describe('Stripe setup: bank details page', () => {
 
         cy.get('h1').should('contain', 'Add bank details')
         cy.get('#stripe-setup-account-number-input').should('have.value', accountNumber)
-        cy.get('#stripe-setup-sort-code-input').should('have.value', sortCode)
+        cy.get('#stripe-setup-sort-code-input').should('have.value', displaySortCode)
       })
 
       it('should go back to index page when change sort code link clicked', () => {
@@ -292,7 +293,7 @@ describe('Stripe setup: bank details page', () => {
 
         cy.get('h1').should('contain', 'Add bank details')
         cy.get('#stripe-setup-account-number-input').should('have.value', accountNumber)
-        cy.get('#stripe-setup-sort-code-input').should('have.value', sortCode)
+        cy.get('#stripe-setup-sort-code-input').should('have.value', displaySortCode)
       })
     })
   })
