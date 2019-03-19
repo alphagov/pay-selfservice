c4b8c61f31b7f8c860e8364a8122cfb98fde35f3
Stephen Daly <stephencdaly@gmail.com>
2019-03-19 14:49:17 +0000

    PP-4976 Add validation for organisation address page
    Add post controller for the organisation page with server-side
    validation. Currently this will not submit the organisation details if
    validation is not successful.
    Use error messages produced by the validation in the template, rather
    than error messages hardcoded in the template.
    
    Co-authored-by: Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>

--- test/cypress/integration/request-to-go-live/organisation_address_spec.js
+++ test/cypress/integration/request-to-go-live/organisation_address_spec.js
@@ -6,6 +6,9 @@ const { userExternalId, gatewayAccountId, serviceExternalId } = utils.variables
 const pageUrl = `/service/${serviceExternalId}/request-to-go-live/organisation-address`
 
 describe('The organisation address page', () => {
+  const longText = 'This text is 201 ...............................................................................' +
+    '..........................................................................................characters long'
+
   beforeEach(() => {
     cy.setEncryptedCookies(userExternalId, gatewayAccountId)
   })
@@ -40,6 +43,109 @@ describe('The organisation address page', () => {
           cy.get('label[for="telephone-number"]').should('exist')
           cy.get('span#telephone-number-hint').should('exist')
           cy.get('input#telephone-number[name="telephone-number"]').should('exist')
+        })
+    })
+
+    it('should display errors when validation fails', () => {
+      cy.get(`form[method=post][action="/service/${serviceExternalId}/request-to-go-live/organisation-address"]`)
+        .within(() => {
+          // create errors for all fields except country by leaving them blank or inputting invalid values
+          cy.get('#address-line2').type(longText, { delay: 0 })
+          cy.get('button[type=submit]').click()
+        })
+
+      cy.get('.govuk-error-summary').find('a').should('have.length', 4)
+      cy.get('.govuk-error-summary').should('exist').within(() => {
+        cy.get('a[href="#address-line1"]').should('contain', 'Building and street')
+        cy.get('a[href="#address-city"]').should('contain', 'Town or city')
+        cy.get('a[href="#address-postcode"]').should('contain', 'Postcode')
+        cy.get('a[href="#telephone-number"]').should('contain', 'Telephone number')
+      })
+
+      cy.get(`form[method=post][action="/service/${serviceExternalId}/request-to-go-live/organisation-address"]`)
+        .within(() => {
+          cy.get('.govuk-form-group--error > input#address-line1').parent().should('exist').within(() => {
+            cy.get('.govuk-error-message').should('contain', 'This field cannot be blank')
+          })
+          cy.get('.govuk-form-group--error > input#address-line2').parent().should('exist').within(() => {
+            cy.get('.govuk-error-message').should('contain', 'The text is too long')
+            cy.get('input#address-line2').should('have.value', longText)
+          })
+          cy.get('.govuk-form-group--error > input#address-city').parent().should('exist').within(() => {
+            cy.get('.govuk-error-message').should('contain', 'This field cannot be blank')
+          })
+          cy.get('.govuk-form-group--error > input#address-postcode').parent().should('exist').within(() => {
+            cy.get('.govuk-error-message').should('contain', 'This field cannot be blank')
+          })
+          cy.get('.govuk-form-group--error > input#telephone-number').parent().should('exist').within(() => {
+            cy.get('.govuk-error-message').should('contain', 'This field cannot be blank')
+          })
+        })
+    })
+
+    it('should keep entered responses when validation fails', () => {
+      const validLine1 = 'A building'
+      const validLine2 = 'A street'
+      const validCity = 'A city'
+      const country = 'IE'
+      const validPostcode = 'D01 F5P2'
+      const invalidTelephoneNumber = 'abd'
+
+      cy.get(`form[method=post][action="/service/${serviceExternalId}/request-to-go-live/organisation-address"]`)
+        .within(() => {
+          cy.get('#address-line1').type(validLine1)
+          cy.get('#address-line2').type(validLine2)
+          cy.get('#address-city').type(validCity)
+          cy.get('#address-country').select(country)
+          cy.get('#address-postcode').type(validPostcode)
+          cy.get('#telephone-number').type(invalidTelephoneNumber)
+          cy.get('button[type=submit]').click()
+        })
+
+      cy.get('.govuk-error-summary').find('a').should('have.length', 1)
+      cy.get('.govuk-error-summary').should('exist').within(() => {
+        cy.get('a[href="#telephone-number"]').should('contain', 'Telephone number')
+      })
+
+      cy.get(`form[method=post][action="/service/${serviceExternalId}/request-to-go-live/organisation-address"]`)
+        .within(() => {
+          cy.get('.govuk-form-group--error > input#telephone-number').parent().should('exist').within(() => {
+            cy.get('.govuk-error-message').should('contain', 'Invalid telephone number')
+          })
+
+          cy.get('#address-line1').should('have.value', validLine1)
+          cy.get('#address-line2').should('have.value', validLine2)
+          cy.get('#address-city').should('have.value', validCity)
+          cy.get('#address-country').should('have.value', country)
+          cy.get('#address-postcode').should('have.value', validPostcode)
+          cy.get('#telephone-number').should('have.value', invalidTelephoneNumber)
+        })
+    })
+
+    it('should show errors for invalid postcode', () => {
+      const invalidPostcode = '123'
+
+      cy.get(`form[method=post][action="/service/${serviceExternalId}/request-to-go-live/organisation-address"]`)
+        .within(() => {
+          cy.get('#address-line1').type('A building')
+          cy.get('#address-line2').type('A street')
+          cy.get('#address-city').type('A city')
+          cy.get('#address-country').select('GB')
+          cy.get('#address-postcode').type(invalidPostcode)
+          cy.get('#telephone-number').type('01134960000')
+          cy.get('button[type=submit]').click()
+        })
+
+      cy.get('.govuk-error-summary').find('a').should('have.length', 1)
+      cy.get('.govuk-error-summary').should('exist').within(() => {
+        cy.get('a[href="#address-postcode"]').should('contain', 'Postcode')
+      })
+
+      cy.get(`form[method=post][action="/service/${serviceExternalId}/request-to-go-live/organisation-address"]`)
+        .within(() => {
+          cy.get('.govuk-form-group--error > input#address-postcode').parent().should('exist').within(() => {
+            cy.get('.govuk-error-message').should('contain', 'Please enter a real postcode')
+          })
         })
     })
   })
