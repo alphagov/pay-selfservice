5344f1004cdbca3e2d4992be7587402392a5178a
Danail Minchev <danail.minchev@gmail.com>
2019-03-26 17:26:11 +0000

    PP-4949 Add ability to pass `pageData` to the `session` cookie

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -1,7 +1,8 @@
-Cypress.Commands.add('setEncryptedCookies', (userId, gatewayAccountId) => {
+Cypress.Commands.add('setEncryptedCookies', (userId, gatewayAccountId, pageData = {}) => {
   cy.task('getCookies', {
     user_external_id: userId,
-    gateway_account_id: gatewayAccountId
+    gateway_account_id: gatewayAccountId,
+    pageData
   }).then(cookies => {
     cy.setCookie('session', cookies.encryptedSessionCookie)
     cy.setCookie('gateway_account', cookies.encryptedGatewayAccountCookie)
