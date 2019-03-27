f7d07d60a9ce414b040f85cc42001d83fc32aafb
Danail Minchev <danail.minchev@gmail.com>
2019-03-27 17:16:38 +0000

    PP-4949 Company number template - small improvements

--- app/controllers/stripe-setup/vat-number-company-number/company-number/company-number-validations.test.js
+++ app/controllers/stripe-setup/vat-number-company-number/company-number/company-number-validations.test.js
@@ -6,7 +6,7 @@ const { expect } = require('chai')
 // Local dependencies
 const companyNumberValidations = require('./company-number-validations')
 
-describe.only('Company number validations', () => {
+describe('Company number validations', () => {
   it('should not be valid when mandatory text is blank', () => {
     expect(companyNumberValidations.validateCompanyNumber('')).to.deep.equal({
       valid: false,
