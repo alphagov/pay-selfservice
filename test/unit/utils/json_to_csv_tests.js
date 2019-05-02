b9d5415f3e367cf9476878698346e98d174e2fa4
danworth <dan.worth@digital.cabinet-office.gov.uk>
2019-05-02 14:20:48 +0100

    PP-4888 Add metadata to csv download
    - User research proved that using each metadata key as a column was the most useful
    approach to adding metadata to the csv download. The keys are sorted in ascending
    ascii order as per the default behaviour of JS list.sort() function.
    - The maximum number of metadata related columns is set to 100. 100 has been used
    as this seems a sensible limit when the current maximum metadata keys per charge
    is set at 10. This permits some variation in key names for a service whilst preventing
    the maximum number of columns in the csv download becoming too great. This is calculated
    by first sorting the unique metadata keys and taking the first 100.
    - Unit and ITests have been updated.

--- test/unit/utils/json_to_csv_tests.js
+++ test/unit/utils/json_to_csv_tests.js
@@ -10,11 +10,10 @@ const jsonToCSV = require('../../../app/utils/json_to_csv')
 // Constants and Setup
 chai.use(chaiAsPromised)
 const expect = chai.expect
+const { transactions, transactionsWithMetadata, refunds } = require('./json/transaction_download_json_to_csv_tests.json')
 
 describe('json2csv module', () => {
-  it('should transform JSON data to CSV format', () => {
-    const { transactions } = require('./json/transaction_download_json_to_csv_tests.json')
-
+  it('should transform JSON data to CSV format for charges witout metadata', () => {
     const csvDataPromise = jsonToCSV(transactions)
 
     const csvDataExpected = `"Reference","Description","Email","Amount","Card Brand","Cardholder Name","Card Expiry Date","Card Number","State","Finished","Error Code","Error Message","Provider ID","GOV.UK Payment ID","Issued By","Date Created","Time Created","Corporate Card Surcharge","Total Amount","Wallet Type"
@@ -22,7 +21,24 @@ describe('json2csv module', () => {
 "blue","desc-blue","alice.222@mail.fake","9.99","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","9.99",""
 "red","desc-red","alice.111@mail.fake","12.34","Visa","TEST01","12/19","4242","Success",false,"","","transaction-1","charge1","","12 May 2016","17:37:29","0.00","12.34",""
 "blue","desc-blue","alice.222@mail.fake","1.23","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","1.23",""
-"red","desc-red","alice.111@mail.fake","12.34","Visa","TEST01","12/19","4242","Success",false,"","","transaction-1","charge1","","12 May 2016","17:37:29","0.00","12.34","Apple Pay"`
+"red","this charge has Apple Pay","alice.111@mail.fake","12.34","Visa","TEST01","12/19","4242","Success",false,"","","transaction-1","charge1","","12 May 2016","17:37:29","0.00","12.34","Apple Pay"`
+
+    return csvDataPromise.then(csvData => {
+      return expect(csvData).to.deep.equal(csvDataExpected)
+    })
+  })
+
+  it('should transform JSON data to CSV format for charges with metadata', () => {
+    const csvDataPromise = jsonToCSV([...transactions, ...transactionsWithMetadata])
+
+    const csvDataExpected = `"Reference","Description","Email","Amount","Card Brand","Cardholder Name","Card Expiry Date","Card Number","State","Finished","Error Code","Error Message","Provider ID","GOV.UK Payment ID","Issued By","Date Created","Time Created","Corporate Card Surcharge","Total Amount","Wallet Type","key1","key2","key3"
+"red","desc-red","alice.111@mail.fake","123.45","Visa","TEST01","12/19","4242","Success",false,"","","transaction-1","charge1","","12 May 2016","17:37:29","0.00","123.45","","","",""
+"blue","desc-blue","alice.222@mail.fake","9.99","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","9.99","","","",""
+"red","desc-red","alice.111@mail.fake","12.34","Visa","TEST01","12/19","4242","Success",false,"","","transaction-1","charge1","","12 May 2016","17:37:29","0.00","12.34","","","",""
+"blue","desc-blue","alice.222@mail.fake","1.23","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","1.23","","","",""
+"red","this charge has Apple Pay","alice.111@mail.fake","12.34","Visa","TEST01","12/19","4242","Success",false,"","","transaction-1","charge1","","12 May 2016","17:37:29","0.00","12.34","Apple Pay","","",""
+"blue","this charge has metadata 1","alice.222@mail.fake","1.23","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","1.23","","some string","",123
+"blue","this charge has metadata 2","alice.222@mail.fake","1.23","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","1.23","","some other string",true,""`
 
     return csvDataPromise.then(csvData => {
       return expect(csvData).to.deep.equal(csvDataExpected)
@@ -30,16 +46,17 @@ describe('json2csv module', () => {
   })
 
   it('should transform JSON data containing refund to CSV format', () => {
-    const { transactionsWithRefunds } = require('./json/transaction_download_json_to_csv_tests.json')
-    
-    const csvDataPromise = jsonToCSV(transactionsWithRefunds)
+    const csvDataPromise = jsonToCSV([...transactions, ...refunds, ...transactionsWithMetadata])
 
-    const csvDataExpected = `"Reference","Description","Email","Amount","Card Brand","Cardholder Name","Card Expiry Date","Card Number","State","Finished","Error Code","Error Message","Provider ID","GOV.UK Payment ID","Issued By","Date Created","Time Created","Corporate Card Surcharge","Total Amount","Wallet Type"
-"red","desc-red","alice.111@mail.fake","123.45","Visa","TEST01","12/19","4242","Success",false,"","","transaction-1","charge1","","12 May 2016","17:37:29","0.00","123.45",""
-"blue","desc-blue","alice.222@mail.fake","9.99","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","9.99",""
-"red","desc-red","alice.111@mail.fake","12.34","Visa","TEST01","12/19","4242","Success",false,"","","transaction-1","charge1","","12 May 2016","17:37:29","0.00","12.34",""
-"refund_test","desc-refund","alice.111@mail.fake","-45.67","Visa","TEST01","12/19","4242","Refund success",true,"","","transaction-1","refund1","alice.111@mail.fake","12 May 2016","17:37:29","0.00","-45.67",""
-"blue","desc-blue","alice.222@mail.fake","1.23","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","1.23",""`
+    const csvDataExpected = `"Reference","Description","Email","Amount","Card Brand","Cardholder Name","Card Expiry Date","Card Number","State","Finished","Error Code","Error Message","Provider ID","GOV.UK Payment ID","Issued By","Date Created","Time Created","Corporate Card Surcharge","Total Amount","Wallet Type","key1","key2","key3"
+"red","desc-red","alice.111@mail.fake","123.45","Visa","TEST01","12/19","4242","Success",false,"","","transaction-1","charge1","","12 May 2016","17:37:29","0.00","123.45","","","",""
+"blue","desc-blue","alice.222@mail.fake","9.99","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","9.99","","","",""
+"red","desc-red","alice.111@mail.fake","12.34","Visa","TEST01","12/19","4242","Success",false,"","","transaction-1","charge1","","12 May 2016","17:37:29","0.00","12.34","","","",""
+"blue","desc-blue","alice.222@mail.fake","1.23","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","1.23","","","",""
+"red","this charge has Apple Pay","alice.111@mail.fake","12.34","Visa","TEST01","12/19","4242","Success",false,"","","transaction-1","charge1","","12 May 2016","17:37:29","0.00","12.34","Apple Pay","","",""
+"refund_test","desc-refund","alice.111@mail.fake","-45.67","Visa","TEST01","12/19","4242","Refund success",true,"","","transaction-1","refund1","alice.111@mail.fake","12 May 2016","17:37:29","0.00","-45.67","","","",""
+"blue","this charge has metadata 1","alice.222@mail.fake","1.23","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","1.23","","some string","",123
+"blue","this charge has metadata 2","alice.222@mail.fake","1.23","Mastercard","TEST02","12/19","4241","Cancelled",true,"P01234","Something happened","transaction-2","charge2","","12 Apr 2015","19:55:29","0.00","1.23","","some other string",true,""`
 
     return csvDataPromise.then(csvData => {
       return expect(csvData).to.deep.equal(csvDataExpected)
