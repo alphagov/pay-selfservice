27736b3295fda4cbe54d6c91aba8ca6dc807b019
Jon Heslop <jon@jonheslop.com>
2019-05-15 16:27:54 +0100

    PP-5180 - revamp tests for updated controllers

--- test/ui/toggle_collect_billing_address_ui_test.js
+++ test/ui/toggle_collect_billing_address_ui_test.js
@@ -4,7 +4,7 @@ const path = require('path')
 const renderTemplate = require(path.join(__dirname, '/../test_helpers/html_assertions.js')).render
 
 describe('The toggle Billing Address page', function () {
-  it('should display content when setting is off', () => {
+  it('should show collections are turned off', () => {
     const templateData = {
       'collectBillingAddress': false,
       permissions: {
@@ -15,25 +15,10 @@ describe('The toggle Billing Address page', function () {
 
     const body = renderTemplate('billing-address/index', templateData)
 
-    body.should.containSelector('h1').withExactText('You are not collecting the billing address')
-    body.should.containSelector('#billing-address-on-button').withExactText('Turn on billing address')
-    body.should.containSelector('#billing-address-add-section-message').withText('Turning on the billing address adds the billing address section to the payment page.')
+    body.should.containSelector('#billing-address-toggle-2').withAttribute('checked')
   })
-  it('should display the confirmation page', () => {
-    const templateData = {
-      'collectBillingAddress': false,
-      permissions: {
-        toggle_billing_address_read: true,
-        toggle_billing_address_update: true
-      }
-    }
 
-    const body = renderTemplate('billing-address/confirm-off', templateData)
-
-    body.should.containSelector('h1').withExactText('Are you sure you want to turn off the billing address?')
-    body.should.containSelector('#billing-address-confirm-off-button').withExactText('Yes, turn off billing address')
-  })
-  it('should display the read only view', () => {
+  it('if readonly, checkboxes and submit buttons should be disabled', () => {
     const templateData = {
       'collectBillingAddress': false,
       permissions: {
@@ -43,10 +28,14 @@ describe('The toggle Billing Address page', function () {
     }
 
     const body = renderTemplate('billing-address/index', templateData)
-    body.should.containSelector('h1').withExactText('You are not collecting the billing address')
+
     body.should.containSelector('.pay-info-warning-box').withExactText('You don’t have permission to manage settings. Contact your service admin if you would like to manage 3D secure, accepted card types, email notifications, or billing address.')
+    body.should.containSelector('#billing-address-toggle-2').withAttribute('checked')
+    body.should.containSelector('#billing-address-toggle-2').withAttribute('disabled')
+    body.should.containSelector('#billing-address-toggle-button').withAttribute('disabled')
   })
-  it('should display content when setting is on', () => {
+
+  it('should show collections are turned on', () => {
     const templateData = {
       'collectBillingAddress': true,
       permissions: {
@@ -57,7 +46,6 @@ describe('The toggle Billing Address page', function () {
 
     const body = renderTemplate('billing-address/index', templateData)
 
-    body.should.containSelector('h1').withExactText('You are collecting the billing address')
-    body.should.containSelector('#billing-address-off-button').withExactText('Turn off billing address')
+    body.should.containSelector('#billing-address-toggle-1').withAttribute('checked')
   })
 })
