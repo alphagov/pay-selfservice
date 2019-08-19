b50193f6dd4e173d27a29627cb24e33c574400a8
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2019-08-19 16:14:46 +0100

    PP-5537 add get single transaction pact test
    - add ledger transaction fixtures
    - refactor ledger client to to use optional transaction_type query
    - add get transaction in created state details pact test

--- app/services/clients/ledger_client.js
+++ app/services/clients/ledger_client.js
@@ -18,7 +18,10 @@ const defaultOptions = {
 const transaction = function transaction (id, gatewayAccountId, options = {}) {
   const configuration = Object.assign({
     url: `/v1/transaction/${id}`,
-    qs: { account_id: gatewayAccountId },
+    qs: {
+      account_id: gatewayAccountId,
+      ...options.transaction_type && { transaction_type: options.transaction_type }
+    },
     description: 'Get individual transaction details',
     transform: legacyConnectorTransactionParity
   }, defaultOptions, options)
