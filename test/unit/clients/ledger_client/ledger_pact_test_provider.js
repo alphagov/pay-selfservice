b0f3533dcc6fc0bc76d38742289d66033a84f9c7
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2019-09-12 13:13:52 +0100

    PP-5537 add Ledger get transaction events pact test
    - add pact test that verifies the interaction between ledger and
      selfservice for get transaction events
    - add relevant methods to fixtures
    - add a provider class (wrapper around Pact). We don't want to refactor
      ledger_client to make port configurable for tests' sake. Using this
      provider allow us to share the pre-configured port between Ledger pact
      tests.
    - refactor get transaction details test to use the new provider class

---
+++ test/unit/clients/ledger_client/ledger_pact_test_provider.js
@@ -0,0 +1,35 @@
+'use strict'
+
+// NPM dependencies
+const Pact = require('pact')
+
+// Custom dependencies
+const path = require('path')
+const port = parseInt(process.env.LEDGER_URL.match(/\d+(\.\d+)?$/g)[0], 10)
+
+const pactProvider = Pact({
+  consumer: 'selfservice',
+  provider: 'ledger',
+  port: port,
+  log: path.resolve(process.cwd(), 'logs', 'mockserver-integration.log'),
+  dir: path.resolve(process.cwd(), 'pacts'),
+  spec: 2,
+  pactfileWriteMode: 'merge'
+})
+module.exports = {
+  getPort: function () {
+    return port
+  },
+  addInteraction: function (pact) {
+    return pactProvider.addInteraction(pact)
+  },
+  setup: function () {
+    return pactProvider.setup()
+  },
+  verify: function () {
+    return pactProvider.verify()
+  },
+  finalize: function () {
+    return pactProvider.finalize()
+  }
+}
