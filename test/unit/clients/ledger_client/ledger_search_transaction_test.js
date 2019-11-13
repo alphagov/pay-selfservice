30febd4b4aa51ebbdc661dfb069224d2327bb768
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2019-11-13 16:02:16 +0000

    PP-5770 add pact tests for filtering by to_date and specific time
    - add a pact tests that filters transaction by to date

--- test/unit/clients/ledger_client/ledger_search_transaction_test.js
+++ test/unit/clients/ledger_client/ledger_search_transaction_test.js
@@ -303,4 +303,67 @@ describe('ledger client', function () {
         })
     })
   })
+
+  describe('search transactions with \'to_date\' with specific time', () => {
+    const params = {
+      account_id: existingGatewayAccountId,
+      filters: {
+        toDate: '21/9/2019',
+        toTime: '15:00:00'
+      }
+    }
+
+    const toDateTime = '2019-09-21T14:00:01.000Z'
+
+    const validTransactionSearchResponse = transactionDetailsFixtures.validTransactionSearchResponse({
+      gateway_account_id: existingGatewayAccountId,
+      transactions: [
+        {
+          amount: 2000,
+          state: {
+            status: 'success',
+            finished: true
+          },
+          transaction_id: '222222',
+          created_date: '2019-09-21T13:14:16.067Z',
+          refund_summary_status: 'available',
+          refund_summary_available: 2000,
+          amount_submitted: 0,
+          amount_refunded: 0,
+          type: 'payment',
+          card_brand: 'visa',
+          email: 'j.doe@example.org',
+          capture_submit_time: '2019-09-21T13:14:16.067Z',
+          captured_date: '2019-09-21'
+        }
+      ]
+    })
+    before(() => {
+      const pactified = validTransactionSearchResponse.getPactified()
+      return pactTestProvider.addInteraction(
+        new PactInteractionBuilder(`${TRANSACTION_RESOURCE}`)
+          .withQuery('account_id', params.account_id)
+          .withQuery('with_parent_transaction', 'true')
+          .withQuery('page', '1')
+          .withQuery('display_size', '100')
+          .withQuery('to_date', toDateTime)
+          .withUponReceiving('a valid transaction with to date filter request')
+          .withState('a payment with all fields and a corresponding refund exists')
+          .withMethod('GET')
+          .withStatusCode(200)
+          .withResponseBody(pactified)
+          .build()
+      )
+    })
+
+    afterEach(() => pactTestProvider.verify())
+
+    it('should filter transaction successfully', function () {
+      const searchTransactionDetails = legacyConnectorParityTransformer.legacyConnectorTransactionsParity(validTransactionSearchResponse.getPlain())
+      return ledgerClient.transactions(params.account_id, params.filters)
+        .then((ledgerResponse) => {
+          expect(ledgerResponse).to.deep.equal(searchTransactionDetails)
+        })
+    })
+  })
 })
