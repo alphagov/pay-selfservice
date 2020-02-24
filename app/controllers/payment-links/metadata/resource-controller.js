125599425304c7aac7bd71cb537260383f232226
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-02-24 13:40:06 +0000

    PP-6152: products client add metadata to payment link
    * update products client with method to POST new message
    * add temporary server validation parsing, simply display message if an
    error is returned from products -- when products sends specific codes
    this can be updated

--- app/controllers/payment-links/metadata/resource-controller.js
+++ app/controllers/payment-links/metadata/resource-controller.js
@@ -1,25 +1,33 @@
-const { response } = require('../../../utils/response.js')
+const { response } = require('../../../utils/response')
 const formattedPathFor = require('../../../utils/replace_params_in_path')
 const paths = require('../../../paths')
-const lodash = require('lodash')
+const { product } = require('../../../services/clients/products_client.js')
 
-const addMetadataPage = function addMetadataPage (req, res, next) {
-  // @TODO(sfount) rethink how response works, get repsonse context from library and render _in_ the contrller
+const addMetadataPage = function addMetadataPage (req, res) {
   const pageData = {
     self: formattedPathFor(paths.paymentLinks.metadata.add, req.params.productExternalId)
   }
-
   response(req, res, 'payment-links/metadata/edit-metadata', pageData)
 }
 
-const postMetadataPage = (req, res) => {
-  const pageData = lodash.get(req, 'session.pageData.createPaymentLink', {})
-  let updatedPageData = lodash.cloneDeep(pageData)
+const postMetadataPage = async function postMetadataPage (req, res) {
+  const key = req.body['metadata-column-header']
+  const value = req.body['metadata-cell-value']
 
-  updatedPageData.metadataColumnHeader = req.body['metadata-column-header']
-  updatedPageData.PaymentLinkDescription = req.body['payment-link-description']
-
-  return res.redirect(paths.paymentLinks.information)
+  try {
+    await product.addMetadataToProduct(req.params.productExternalId, key, value)
+    req.flash('generic', `Reporting column ${key} was added`)
+    res.redirect(formattedPathFor(paths.paymentLinks.edit, req.params.productExternalId))
+  } catch (error) {
+    const pageData = {
+      self: formattedPathFor(paths.paymentLinks.metadata.add, req.params.productExternalId),
+      error: error.message,
+      metadataColumnHeader: key,
+      metadataColumnValue: value
+    }
+    req.flash('genericError', error.message)
+    response(req, res, 'payment-links/metadata/edit-metadata', pageData)
+  }
 }
 
 module.exports = {
