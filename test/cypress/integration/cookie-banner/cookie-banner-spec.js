09966b3d98f069c723b0efd3f257956cf5afb4fb
Krish <krishna.bottla@gmail.com>
2020-04-28 13:32:24 +0100

    PP-6300 Cypress tests for cookie banner

---
+++ test/cypress/integration/cookie-banner/cookie-banner-spec.js
@@ -0,0 +1,47 @@
+'use strict'
+
+describe('Cookie banner', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  describe('Visiting application for first time', () => {
+    it('Should show cookie banner on page load', () => {
+      cy.get('#pay-cookie-banner').should('have.css', 'display', 'block')
+    })
+
+    it('should show accepted message and set analytics cookie to true if consented is accepted', () => {
+      cy.get('button[data-accept-cookies=true]').click()
+
+      cy.getCookie('govuk_pay_cookie_policy')
+        .should('have.property', 'value', '{"analytics":true}')
+      cy.get('.pay-cookie-banner__confirmation-message').contains('You’ve accepted analytics cookies')
+      cy.get('.pay-cookie-banner__wrapper').should('have.css', 'display', 'none')
+    })
+
+    it('should show rejected message and set analytics cookie to false if consented is rejected', () => {
+      cy.visit('/')
+      cy.get('button[data-accept-cookies=false]').click()
+
+      cy.getCookie('govuk_pay_cookie_policy')
+        .should('have.property', 'value', '{"analytics":false}')
+      cy.get('.pay-cookie-banner__confirmation-message').contains('You told us not to use analytics cookies')
+      cy.get('.pay-cookie-banner__wrapper').should('have.css', 'display', 'none')
+    })
+  })
+
+  describe('For revisiting users', () => {
+    it('Should not show cookie banner on page load if consent has been accepted previously', () => {
+      cy.setCookie('govuk_pay_cookie_policy', '{"analytics":true}')
+      cy.visit('/')
+
+      cy.get('#pay-cookie-banner').should('have.css', 'display', 'none')
+    })
+    it('Should not show cookie banner on page load if consent has been rejected previously', () => {
+      cy.setCookie('govuk_pay_cookie_policy', '{"analytics":false}')
+      cy.visit('/')
+
+      cy.get('#pay-cookie-banner').should('have.css', 'display', 'none')
+    })
+  })
+})
