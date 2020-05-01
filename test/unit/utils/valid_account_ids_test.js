2e7f3976defe3d463f0a37405fb9761cb1aaeb5d
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-05-01 17:01:24 +0100

    PP-6345: cover account id parsing for all accounts

--- test/unit/utils/valid_account_ids_test.js
+++ test/unit/utils/valid_account_ids_test.js
@@ -1,9 +1,11 @@
 const { expect } = require('chai')
+const proxyquire = require('proxyquire')
 
-const { userServicesContainsGatewayAccount } = require('./../../../app/utils/valid_account_id')
 const { validUser } = require('./../../fixtures/user_fixtures')
+const { validGatewayAccountResponse } = require('./../../fixtures/gateway_account_fixtures')
 
 describe('gateway account filter utiltiies', () => {
+  const { userServicesContainsGatewayAccount } = require('./../../../app/utils/valid_account_id')
   describe('gateway account exists on users service roles', () => {
     it('returns valid for gateway account belonging to user', () => {
       const opts = {
@@ -20,6 +22,86 @@ describe('gateway account filter utiltiies', () => {
       const user = validUser(opts).getAsObject()
       const valid = userServicesContainsGatewayAccount('4', user)
       expect(valid).to.equal(false)
+    })
+  })
+
+  describe('live accounts for a given user', () => {
+    const opts = {
+      gateway_account_ids: ['1', '2', '3']
+    }
+    const user = validUser(opts).getAsObject()
+    it('correctly identifies stripe and moto headers for relavent accounts', async () => {
+      const { liveUserServicesGatewayAccounts } = proxyquire('./../../../app/utils/valid_account_id', {
+        '../services/clients/connector_client.js': {
+          ConnectorClient: class {
+            async getAccounts () {
+              return {
+                accounts: [
+                  validGatewayAccountResponse({
+                    payment_provider: 'stripe'
+                  }).getPlain(),
+                  validGatewayAccountResponse({
+                    allow_moto: true
+                  }).getPlain()
+                ]
+              }
+            }
+          }
+        }
+      })
+      const result = await liveUserServicesGatewayAccounts(user)
+
+      expect(result.headers.shouldGetStripeHeaders).to.be.true // eslint-disable-lin
+      expect(result.headers.shouldGetMotoHeaders).to.be.true // eslint-disable-lin
+    })
+
+    it('correctly identifies non stripe and moto headers', async () => {
+      const { liveUserServicesGatewayAccounts } = proxyquire('./../../../app/utils/valid_account_id', {
+        '../services/clients/connector_client.js': {
+          ConnectorClient: class {
+            async getAccounts () {
+              return {
+                accounts: [
+                  validGatewayAccountResponse().getPlain()
+                ]
+              }
+            }
+          }
+        }
+      })
+      const result = await liveUserServicesGatewayAccounts(user)
+
+      expect(result.headers.shouldGetStripeHeaders).to.be.false // eslint-disable-lin
+      expect(result.headers.shouldGetMotoHeaders).to.be.false // eslint-disable-lin
+    })
+
+    it('correctly filters live accounts', async () => {
+      const { liveUserServicesGatewayAccounts } = proxyquire('./../../../app/utils/valid_account_id', {
+        '../services/clients/connector_client.js': {
+          ConnectorClient: class {
+            async getAccounts () {
+              return {
+                accounts: [
+                  validGatewayAccountResponse({
+                    gateway_account_id: '1',
+                    type: 'live'
+                  }).getPlain(),
+                  validGatewayAccountResponse({
+                    gateway_account_id: '2',
+                    type: 'test'
+                  }).getPlain(),
+                  validGatewayAccountResponse({
+                    gateway_account_id: '3',
+                    type: 'live'
+                  }).getPlain()
+                ]
+              }
+            }
+          }
+        }
+      })
+      const result = await liveUserServicesGatewayAccounts(user)
+      expect(result.accounts).to.equal('1,3')
     })
   })
 })
