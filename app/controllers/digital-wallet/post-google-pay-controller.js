150375a5fc03ba2d7576fe3d4812e3fb10c67424
Stephen Daly <stephencdaly@gmail.com>
2020-05-05 15:18:02 +0100

    PP-6466 Fix google pay settings page
    At some point, we stopped sending the merchant ID to connector. This
    means that although we were changing the `allow_google_pay` setting on
    the gateway account, it never appeared as enabled because when connector
    returns the setting, it always checks that the merchant ID is there.
    
    Also fix an issue where it was not possible to turn off google pay due
    to the `required: "true"` attribute on the merchant ID field. Instead
    do server side validation for this.

--- app/controllers/digital-wallet/post-google-pay-controller.js
+++ app/controllers/digital-wallet/post-google-pay-controller.js
@@ -2,6 +2,7 @@
 
 // Local dependencies
 const paths = require('../../paths')
+const logger = require('../../utils/logger')(__filename)
 const { renderErrorView } = require('../../utils/response')
 const { ConnectorClient } = require('../../services/clients/connector_client')
 const { CORRELATION_HEADER } = require('../../utils/correlation_header')
@@ -11,14 +12,36 @@ module.exports = async (req, res) => {
   const gatewayAccountId = req.account.gateway_account_id
   const correlationId = req.headers[CORRELATION_HEADER] || ''
   const enable = req.body['google-pay'] === 'on'
+  const gatewayMerchantId = req.body.merchantId
+
+  if (enable && !gatewayMerchantId) {
+    req.flash('genericError', '<h2>Please enter a valid Merchant ID</h2>')
+    return res.redirect(paths.digitalWallet.googlePay)
+  }
+
+  if (enable) {
+    try {
+      await connector.setGatewayMerchantId(gatewayAccountId, gatewayMerchantId, correlationId)
+      logger.info(`${correlationId} set google pay merchant ID for ${gatewayAccountId}`)
+    } catch (error) {
+      logger.info(`${correlationId} error setting google pay merchant ID for ${gatewayAccountId}: `, error)
+      if (error.errorCode === 400) {
+        req.flash('genericError', `<h2>There was an error enabling google pay. Check that the Merchant ID you entered is correct and that your PSP account credentials have been set.</h2>`)
+        return res.redirect(paths.digitalWallet.googlePay)
+      } else {
+        return renderErrorView(req, res, false, error.errorCode)
+      }
+    }
+  }
 
   try {
     await connector.toggleGooglePay(gatewayAccountId, enable, correlationId)
+    logger.info(`${correlationId} ${enable ? 'enabled' : 'disabled'} google pay boolean for ${gatewayAccountId}`)
 
     req.flash('generic', `<h2>Google Pay successfully ${enable ? 'enabled' : 'disabled'}.</h2>`)
     return res.redirect(paths.digitalWallet.googlePay)
   } catch (error) {
-    req.flash('genericError', `<h2>Something went wrong</h2>`)
+    logger.error(`${correlationId} error enabling google pay for ${gatewayAccountId}: ${error}`)
     return renderErrorView(req, res, false, error.errorCode)
   }
 }
