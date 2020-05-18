026eb422b3fef990126ee082d1b960efdd2e9472
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-05-18 14:11:39 +0100

    BAU: scripts to configure local developemnt env
    * add simple start file that won't try to manage threads or add overhead
    * scripts to connect to different environments running locally
      * tunnel: any AWS environment that the developer has tunneled into
      * local: microservices running locally, usually managed by Docker
      * docker: this expects the service to running inside of Docker and is
      connecting to a network in Docker compose
    
    Optimisations
    * only include google analytics if we're in a production environment;
    there's no need to include empty values locally

--- server.js
+++ server.js
@@ -94,6 +94,7 @@ function initialiseTemplateEngine (app) {
   // if it's not production we want to re-evaluate the assets on each file change
   nunjucksEnvironment.addGlobal('css_path', staticify.getVersionedPath('/stylesheets/application.min.css'))
   nunjucksEnvironment.addGlobal('js_path', NODE_ENV === 'production' ? JAVASCRIPT_PATH : staticify.getVersionedPath('/js/application.js'))
+  nunjucksEnvironment.addGlobal('isProduction', NODE_ENV === 'production')
 
   // Load custom Nunjucks filters
   for (let name in nunjucksFilters) {
