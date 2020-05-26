975d53cf89e786b72e14dc444c8956e12baa60a8
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2020-05-26 17:41:50 +0100

    PP-6491 add ledger payout search pact test
    - add a new search payouts contract test

--- test/fixtures/payout_fixtures.js
+++ test/fixtures/payout_fixtures.js
@@ -4,14 +4,14 @@ const pact = pactBase()
 
 function buildPayout (opts) {
   return {
-    gateway_payout_id: 'some-gateway-payout-id',
+    gateway_payout_id: opts.gatewayPayoutId || 'some-gateway-payout-id',
     gateway_account_id: opts.gatewayAccountId || 1,
-    amount: 10000,
-    created_date: '2019-01-29T08:00:00.000000Z',
+    amount: opts.amount || 10000,
+    created_date: opts.createdDate || '2019-01-29T08:00:00.000000Z',
     paid_out_date: opts.paidOutDate || '2019-01-29T11:00:00.000000Z',
     state: {
       status: 'paidout',
-      finshed: true
+      finished: true
     }
   }
 }
