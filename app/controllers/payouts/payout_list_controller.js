d8e317ed57753506c5cc489d5abccc33988d32cd
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-06-03 16:22:51 +0100

    PP-6591: log activity for payout feature
    Add info logs that can be used to monitor feature usage.

--- app/controllers/payouts/payout_list_controller.js
+++ app/controllers/payouts/payout_list_controller.js
@@ -1,4 +1,5 @@
 const paths = require('../../../app/paths')
+const logger = require('../../utils/logger')(__filename)
 const { response, renderErrorView } = require('../../utils/response.js')
 const { liveUserServicesGatewayAccounts } = require('./../../utils/valid_account_id')
 const payoutService = require('./payouts_service')
@@ -8,6 +9,11 @@ const listAllServicesPayouts = async function listAllServicesPayouts (req, res) 
     const { page } = req.query
     const gatewayAccounts = await liveUserServicesGatewayAccounts(req.user)
     const payoutSearchResult = await payoutService.payouts(gatewayAccounts.accounts, req.user, page)
+    logger.info('Fetched page of payouts for all services', {
+      user_id: req.user && req.user.externalId,
+      gateway_accounts: gatewayAccounts,
+      current_page: page
+    })
     response(req, res, 'payouts/list', { payoutSearchResult, paths })
   } catch (error) {
     renderErrorView(req, res, 'Failed to fetch payouts')
