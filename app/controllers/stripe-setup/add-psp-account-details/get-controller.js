60be50aefc73e2fb7c7f76db398e36637dfde931
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2020-06-16 13:45:14 +0100

    PP-6638 Add Try  & Catch around controller

--- app/controllers/stripe-setup/add-psp-account-details/get-controller.js
+++ app/controllers/stripe-setup/add-psp-account-details/get-controller.js
@@ -1,21 +1,27 @@
 'use strict'
 
 const paths = require('../../../paths')
-const { response } = require('../../../utils/response')
+const logger = require('../../../utils/logger')(__filename)
+const { response, renderErrorView } = require('../../../utils/response')
 const { ConnectorClient } = require('../../../services/clients/connector_client')
 const connectorClient = new ConnectorClient(process.env.CONNECTOR_URL)
 
 module.exports = async (req, res) => {
-  const stripeAccountSetup = await connectorClient.getStripeAccountSetup(
-    req.account.gateway_account_id, req.correlationId)
+  try {
+    const stripeAccountSetup = await connectorClient.getStripeAccountSetup(
+      req.account.gateway_account_id, req.correlationId)
 
-  if (!stripeAccountSetup.bankAccount) {
-    res.redirect(303, paths.stripeSetup.bankDetails)
-  } else if (!stripeAccountSetup.responsiblePerson) {
-    res.redirect(303, paths.stripeSetup.responsiblePerson)
-  } else if (!stripeAccountSetup.vatNumberCompanyNumber) {
-    res.redirect(303, paths.stripeSetup.vatNumberCompanyNumber)
-  } else {
-    response(req, res, 'stripe-setup/go-live-complete')
+    if (!stripeAccountSetup.bankAccount) {
+      res.redirect(303, paths.stripeSetup.bankDetails)
+    } else if (!stripeAccountSetup.responsiblePerson) {
+      res.redirect(303, paths.stripeSetup.responsiblePerson)
+    } else if (!stripeAccountSetup.vatNumberCompanyNumber) {
+      res.redirect(303, paths.stripeSetup.vatNumberCompanyNumber)
+    } else {
+      response(req, res, 'stripe-setup/go-live-complete')
+    }
+  } catch (error) {
+    logger.error(`${req.correlationId} error with Stripe > Add PSP account details : ${error}`)
+    return renderErrorView(req, res, false, error.errorCode)
   }
 }
