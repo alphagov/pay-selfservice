3bf2e164e8fa92d85c69450c3c2681c98b73b3fb
Stephen Daly <stephencdaly@gmail.com>
2020-08-20 14:12:54 +0100

    PP-6951 Handle and show refund errors better
    Make it clearer which errors can come from connector, and how this
    relates to what we display to the user.
    
    Use the `error_identifier` from the connector response rather than the
    status code to determine what went wrong. To allow for this, switch to
    using `base.client` rather than the old client to make the request to
    connector.
    
    Remove HTML from the refund success and error messages, instead pass
    a plain string for flash to display.
    
    Remove `transaction-details-refunds.ft.test.js` as these tests didn't
    assert on anything meaningful and the behaviour they should cover is
    covered by the Cypress tests for the transaction details page and the
    controller integration test.
    
    Fix `transaction-refund.controller.it.test.js` which didn't actually
    assert on anything for the error cases, and would have failed if they
    did 😬.

--- app/controllers/transactions/transaction-refund.controller.js
+++ app/controllers/transactions/transaction-refund.controller.js
@@ -6,16 +6,6 @@ const router = require('../../routes.js')
 const { CORRELATION_HEADER } = require('../../utils/correlation-header.js')
 const { safeConvertPoundsStringToPence } = require('../../utils/currency-formatter')
 
-const reasonMessages = {
-  'refund_complete': '<h2>Refund successful</h2> It may take up to 6 days to process.',
-  'REFUND_FAILED': '<h2>Refund failed</h2> We couldn’t process this refund. Try again later.',
-  'full': '<h2>Repeat request</h2> This refund request has already been submitted. Refresh your transactions list.',
-  'amount_not_available': '<h2>Select another amount</h2> The amount you tried to refund is greater than the transaction total',
-  'amount_min_validation': '<h2>Select another amount</h2> The amount you tried to refund is less than the accepted minimum for this transaction.',
-  'refund_amount_available_mismatch': '<h2>Repeat request</h2> This refund request has already been submitted. Refresh your transactions list.',
-  'invalid_chars': '<h2>Use valid characters only</h2> Choose an amount to refund in pounds and pence using digits and a decimal point. For example “10.50”'
-}
-
 const refundTransaction = async function refundTransaction (req, res, next) {
   try {
     const correlationId = req.headers[CORRELATION_HEADER]
@@ -31,18 +21,17 @@ const refundTransaction = async function refundTransaction (req, res, next) {
 
     const refundAmountInPence = safeConvertPoundsStringToPence(refundAmount)
     if (!refundAmountInPence) {
-      req.flash('genericError', reasonMessages['invalid_chars'])
+      req.flash('genericError', 'Choose an amount to refund in pounds and pence using digits and a decimal point. For example “10.50”')
       return res.redirect(transactionDetailPath)
     }
 
     try {
       await refund(accountId, chargeId, refundAmountInPence, refundAmountAvailableInPence, userExternalId, userEmail, correlationId)
-      req.flash('generic', reasonMessages['refund_complete'])
-      res.redirect(transactionDetailPath)
+      req.flash('generic', 'Refund successful. It may take up to 6 days to process.')
     } catch (err) {
-      req.flash('genericError', reasonMessages[err] ? reasonMessages[err] : reasonMessages.REFUND_FAILED)
-      res.redirect(transactionDetailPath)
+      req.flash('genericError', err.message)
     }
+    res.redirect(transactionDetailPath)
   } catch (err) {
     next(err)
   }
