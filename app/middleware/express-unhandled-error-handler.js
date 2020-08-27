3e0199f4bdf22caeda438751838bad44763f5063
Stephen Daly <stephencdaly@gmail.com>
2020-08-27 17:45:07 +0100

    PP-7002 Improve what we log for unhandled errors
    Log the correlation id (x-request-id) as a structured field.
    Move the stack to a structured field and truncate it to avoid other
    useful information that's logged being truncated before it reaches
    Splunk.

--- app/middleware/express-unhandled-error-handler.js
+++ app/middleware/express-unhandled-error-handler.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const logger = require('../utils/logger')(__filename)
+const { keys } = require('@govuk-pay/pay-js-commons').logging
+const { CORRELATION_HEADER } = require('../utils/correlation-header')
 const { renderErrorView } = require('../utils/response')
 
 const UNHANDLED_ERROR_MESSAGE = 'There is a problem with the payments platform. Please contact the support team.'
@@ -11,6 +13,11 @@ module.exports = function errorHandler (err, req, res, next) {
     return next(err)
   }
 
-  logger.error('Unhandled error caught - ' + err.stack)
+  const logContext = {
+    // trim the stack as long log messages are truncated by Splunk
+    stack: err.stack.substring(0, 300),
+  }
+  logContext[keys.CORRELATION_ID] = req.headers[CORRELATION_HEADER]
+  logger.error(`Unhandled error caught: ${err.message}`, logContext)
   renderErrorView(req, res, UNHANDLED_ERROR_MESSAGE, UNHANDLED_ERROR_STATUS)
 }
