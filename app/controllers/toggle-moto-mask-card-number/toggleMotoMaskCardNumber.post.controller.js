4708dcbddb47d540e5e615764844dbce3bd5b23f
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-09-03 12:04:25 +0100

    PP-6977: toggle mask settings module level Connector client
    Move the Connector client instantiation outisde of the response handler;
    given how Node modules work with a singelton pattern by default this
    will mean all responses are able to use the same client helper methods
    without creating a new object per request.

--- app/controllers/toggle-moto-mask-card-number/toggleMotoMaskCardNumber.post.controller.js
+++ app/controllers/toggle-moto-mask-card-number/toggleMotoMaskCardNumber.post.controller.js
@@ -4,9 +4,9 @@ const paths = require('../../paths')
 const { renderErrorView } = require('../../utils/response')
 const { ConnectorClient } = require('../../services/clients/connector.client')
 const { correlationHeader } = require('../../utils/correlation-header')
+const connector = new ConnectorClient(process.env.CONNECTOR_URL)
 
-module.exports = async function toggleMaskCardNumber(req, res) {
-  const connector = new ConnectorClient(process.env.CONNECTOR_URL)
+module.exports = async function toggleMaskCardNumber (req, res) {
   const correlationId = req.headers[correlationHeader] || ''
   const accountId = req.account.gateway_account_id
   const enableMaskCardNumber = req.body['moto-mask-card-number-input-toggle'] === 'on'
--- app/controllers/toggle-moto-mask-security-code/maskSecurityCode.post.controller.js
+++ app/controllers/toggle-moto-mask-security-code/maskSecurityCode.post.controller.js
@@ -5,9 +5,9 @@ const paths = require('../../paths')
 const { renderErrorView } = require('../../utils/response')
 const { ConnectorClient } = require('../../services/clients/connector.client')
 const { correlationHeader } = require('../../utils/correlation-header')
+const connector = new ConnectorClient(process.env.CONNECTOR_URL)
 
-module.exports = async function toggleMaskCardSecurityCode(req, res) {
-  const connector = new ConnectorClient(process.env.CONNECTOR_URL)
+module.exports = async function toggleMaskCardSecurityCode (req, res) {
   const correlationId = req.headers[correlationHeader] || ''
   const accountId = req.account.gateway_account_id
   const enableMaskSecurityCode = req.body['moto-mask-security-code-input-toggle'] === 'on'
