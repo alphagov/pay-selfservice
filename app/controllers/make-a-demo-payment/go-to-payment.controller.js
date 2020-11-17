f83574b44e6566c8c1070c76baa2f0f626428214
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-11-17 12:55:16 +0000

    PP-6129: refactor make a demo payment controller
    * favour async/ await
    * name all methods
    * clarify naming

--- app/controllers/make-a-demo-payment/go-to-payment.controller.js
+++ app/controllers/make-a-demo-payment/go-to-payment.controller.js
@@ -9,37 +9,38 @@ const productTypes = require('../../utils/product-types')
 const publicAuthClient = require('../../services/clients/public-auth.client')
 const auth = require('../../services/auth.service.js')
 
-module.exports = (req, res) => {
+module.exports = async function makeDemoPayment (req, res) {
   const gatewayAccountId = auth.getCurrentGatewayAccountId(req)
   const { paymentAmount, paymentDescription } = lodash.get(req, 'session.pageData.makeADemoPayment', {})
 
   if (!paymentAmount || !paymentDescription) {
     return res.redirect(paths.prototyping.demoPayment.index)
   }
-  publicAuthClient.createTokenForAccount({
-    accountId: gatewayAccountId,
-    correlationId: req.correlationId,
-    payload: {
-      account_id: gatewayAccountId,
-      created_by: req.user.email,
-      description: `Token for Demo Payment`,
-      type: 'PRODUCTS'
-    }
-  })
-    .then(publicAuthData => productsClient.product.create({
-      payApiToken: publicAuthData.token,
+
+  try {
+    const createTokenResponse = await publicAuthClient.createTokenForAccount({
+      accountId: gatewayAccountId,
+      correlationId: req.correlationId,
+      payload: {
+        account_id: gatewayAccountId,
+        created_by: req.user.email,
+        description: `Token for Demo Payment`,
+        type: 'PRODUCTS'
+      }
+    })
+    const createProductResponse = await productsClient.product.create({
+      payApiToken: createTokenResponse.token,
       gatewayAccountId,
       name: paymentDescription,
       price: paymentAmount,
       type: productTypes.DEMO
-    }))
-    .then(product => {
-      lodash.unset(req, 'session.pageData.makeADemoPayment')
-      res.redirect(product.links.pay.href)
     })
-    .catch((err) => {
-      logger.error(`[requestId=${req.correlationId}] Making a demo payment failed - ${err.message}`)
-      req.flash('genericError', 'Something went wrong. Please try again.')
-      return res.redirect(paths.prototyping.demoPayment.index)
-    })
+
+    lodash.unset(req, 'session.pageData.makeADemoPayment')
+    res.redirect(createProductResponse.links.pay.href)
+  } catch (error) {
+    logger.error(`[requestId=${req.correlationId}] Making a demo payment failed - ${error.message}`)
+    req.flash('genericError', 'Something went wrong. Please try again.')
+    res.redirect(paths.prototyping.demoPayment.index)
+  }
 }
