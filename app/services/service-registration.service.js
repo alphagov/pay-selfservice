82f8bbb150cafb84a61222550e7cf94a490a2695
Stephen Daly <stephencdaly@gmail.com>
2020-11-30 17:55:23 +0000

    PP-7458 Refactor service-registration.service.js
    - Move exports to bottom of file
    - Name anonymous functions
    - Remove function comment blocks that don't add anything
    - Use async/await
    - Inline single line functions that are used internally in one place

--- app/services/service-registration.service.js
+++ app/services/service-registration.service.js
@@ -1,83 +1,34 @@
 'use strict'
 
-const logger = require('../utils/logger')(__filename)
 const getAdminUsersClient = require('./clients/adminusers.client')
 const ConnectorClient = require('./clients/connector.client').ConnectorClient
 const connectorClient = () => new ConnectorClient(process.env.CONNECTOR_URL)
 
-const completeServiceInvite = (inviteCode, gatewayAccountIds, correlationId) => {
-  return getAdminUsersClient({ correlationId }).completeInvite(inviteCode, gatewayAccountIds)
+function submitRegistration (email, phoneNumber, password, correlationId) {
+  return getAdminUsersClient({ correlationId }).submitServiceRegistration(email, phoneNumber, password)
 }
 
-const createGatewayAccount = function (correlationId) {
-  return connectorClient().createGatewayAccount('sandbox', 'test', null, null, correlationId)
+function submitServiceInviteOtpCode (code, otpCode, correlationId) {
+  return getAdminUsersClient({ correlationId }).verifyOtpForServiceInvite(code, otpCode)
+}
+
+async function createPopulatedService (inviteCode, correlationId) {
+  const gatewayAccount = await connectorClient().createGatewayAccount('sandbox', 'test', null, null, correlationId)
+  return getAdminUsersClient({ correlationId }).completeInvite(inviteCode, [gatewayAccount.gateway_account_id])
+}
+
+function generateServiceInviteOtpCode (inviteCode, correlationId) {
+  return getAdminUsersClient({ correlationId }).generateInviteOtpCode(inviteCode)
+}
+
+function resendOtpCode (code, phoneNumber, correlationId) {
+  return getAdminUsersClient({ correlationId }).resendOtpCode(code, phoneNumber)
 }
 
 module.exports = {
-
-  /**
-   * Submit the user details for self registration of a service
-   *
-   * @param email
-   * @param phoneNumber
-   * @param password
-   * @param correlationId
-   */
-  submitRegistration: function (email, phoneNumber, password, correlationId) {
-    return getAdminUsersClient({ correlationId }).submitServiceRegistration(email, phoneNumber, password)
-  },
-
-  /**
-   * Submit otp code for verification
-   *
-   * @param code
-   * @param otpCode
-   * @param correlationId
-   */
-  submitServiceInviteOtpCode: (code, otpCode, correlationId) => {
-    return getAdminUsersClient({ correlationId }).verifyOtpForServiceInvite(code, otpCode)
-  },
-
-  /**
-   * Creates a service containing a sandbox gateway account and a user
-   */
-  createPopulatedService: (inviteCode, correlationId) => {
-    return new Promise(function (resolve, reject) {
-      let gatewayAccountId
-      createGatewayAccount(correlationId)
-        .then(gatewayAccount => {
-          gatewayAccountId = gatewayAccount.gateway_account_id
-          return completeServiceInvite(inviteCode, [gatewayAccountId], correlationId)
-        })
-        .then(completeServiceInviteResponse => {
-          resolve(completeServiceInviteResponse)
-        })
-        .catch(error => {
-          logger.error(`[requestId=${correlationId}] Create populated service orchestration error`, error)
-          reject(error)
-        })
-    })
-  },
-
-  /**
-   * Generate OTP code for service invite
-   *
-   * @param inviteCode
-   * @param correlationId
-   * @returns {*|Constructor}
-   */
-  generateServiceInviteOtpCode: function (inviteCode, correlationId) {
-    return getAdminUsersClient({ correlationId }).generateInviteOtpCode(inviteCode)
-  },
-
-  /**
-   * Resend otp code
-   *
-   * @param code
-   * @param phoneNumber
-   * @param correlationId
-   */
-  resendOtpCode: function (code, phoneNumber, correlationId) {
-    return getAdminUsersClient({ correlationId }).resendOtpCode(code, phoneNumber)
-  }
+  submitRegistration,
+  submitServiceInviteOtpCode,
+  createPopulatedService,
+  generateServiceInviteOtpCode,
+  resendOtpCode
 }
