c10b72659e9e6e6207ffa8964774d7ed10a5d9d6
Krish <krishna.bottla@gmail.com>
2020-12-08 11:50:38 +0000

    PP-7489 Add account and service middleware
    - Added new middleware to get account and service based on URL path params
    
      Middleware expects atleast one parameter (gatewayAccountExternalId or serviceExternalId) to be available on request to get gateway account/service. If both are available, additionally checks for gatewayAccount to be part of the service.

---
+++ app/middleware/get-service-and-gateway-account.middleware.js
@@ -0,0 +1,106 @@
+'use strict'
+
+const _ = require('lodash')
+
+const logger = require('../utils/logger')(__filename)
+const paths = require('../../app/paths')
+const { NotFoundError } = require('../../app/errors')
+const Connector = require('../services/clients/connector.client.js').ConnectorClient
+
+const { keys } = require('@govuk-pay/pay-js-commons').logging
+const connectorClient = new Connector(process.env.CONNECTOR_URL)
+
+async function getGatewayAccountByExternalId (gatewayAccountExternalId, correlationId) {
+  try {
+    const params = {
+      gatewayAccountExternalId: gatewayAccountExternalId,
+      correlationId: correlationId
+    }
+    // TODO: PP-7489 - add additional attributes derived by getAccount middleware
+    return await connectorClient.getAccountByExternalId(params)
+  } catch (err) {
+    const logContext = {}
+    logContext['error'] = err.message
+    logContext['GATEWAY_ACCOUNT_EXTERNAL_ID'] = gatewayAccountExternalId
+    logContext[keys.CORRELATION_ID] = correlationId
+
+    if (err.errorCode === 404) {
+      logger.info('Gateway account not found', logContext)
+      throw new NotFoundError('Gateway account not found')
+    } else {
+      logger.error('Error when attempting to retrieve gateway account', logContext)
+      throw new Error('Error retrieving Gateway account')
+    }
+  }
+}
+
+function getService (user, serviceExternalId, gatewayAccount, correlationId) {
+  let service
+  const serviceRoles = _.get(user, 'serviceRoles', [])
+
+  if (serviceRoles.length > 0) {
+    if (serviceExternalId) {
+      service = _.get(serviceRoles.find(serviceRole => {
+        return (serviceRole.service.externalId === serviceExternalId &&
+          (!gatewayAccount || serviceRole.service.gatewayAccountIds.includes(gatewayAccount.gateway_account_id)))
+      }), 'service')
+    } else {
+      if (gatewayAccount) {
+        service = _.get(serviceRoles.find(serviceRole => {
+          return serviceRole.service.gatewayAccountIds.includes(gatewayAccount.gateway_account_id)
+        }), 'service')
+      }
+    }
+  }
+
+  if (service) {
+    // hasCardGatewayAccount is needed to show relevant message (card or directdebit or both) on merchant details page.
+    // Since it is only card currently supported, value is always 'true'
+    service.hasCardGatewayAccount = true
+    return service
+  } else {
+    const logContext = {}
+    logContext[keys.USER_EXTERNAL_ID] = user.externalId
+    logContext[keys.SERVICE_EXTERNAL_ID] = serviceExternalId
+    if (gatewayAccount) {
+      logContext[keys.GATEWAY_ACCOUNT_ID] = gatewayAccount.gateway_account_id
+    }
+    logContext[keys.CORRELATION_ID] = correlationId
+    logger.info('Service not found for user', logContext)
+    throw new NotFoundError('Service not found for user')
+  }
+}
+
+module.exports = async function (req, res, next) {
+  try {
+    const correlationId = req.correlationId
+    const serviceExternalId = req.params[paths.keys.SERVICE_EXTERNAL_ID]
+    const gatewayAccountExternalId = req.params[paths.keys.GATEWAY_ACCOUNT_EXTERNAL_ID]
+
+    if (!serviceExternalId && !gatewayAccountExternalId) {
+      const logContext = {}
+      logContext[keys.CORRELATION_ID] = correlationId
+      logger.info('Could not resolve gateway account external ID or service external ID from request params')
+      throw new Error('Could not resolve gateway account external ID or service external ID from request params')
+    }
+
+    let gatewayAccount
+    if (gatewayAccountExternalId) {
+      gatewayAccount = await getGatewayAccountByExternalId(gatewayAccountExternalId, correlationId)
+      if (gatewayAccount) {
+        req.account = gatewayAccount
+        // TODO: To be removed once URLs are updated to use the format /service/:serviceExternalId/account/:gatewayAccountExternalId/xxx.
+        // Currently authService.getCurrentGatewayAccountId() sets below if account is available on session or derives one from user services.
+        req.gateway_account = { currentGatewayAccountId: gatewayAccount.gateway_account_id }
+      }
+    }
+
+    // uses req.user object which is set by passport (auth.service.js) and has all user services information to find service by serviceExternalId or gatewayAccountId.
+    // A separate API call to adminusers to find service makes it independent of user object but most of tests setup currently relies on req.user
+    req.service = getService(req.user, serviceExternalId, gatewayAccount, correlationId)
+
+    next()
+  } catch (err) {
+    next(err)
+  }
+}
