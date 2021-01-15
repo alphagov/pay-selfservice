2e8ef82927bb7e5cbb34eaaa569f4d65509d5fd0
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2021-01-15 16:27:41 +0000

    PP-7445: make sure account external id is available on login
    As we transition URL structure for accounts only new middelware routes
    and the service switcher will be setting the external id required to
    redirect to the correct URLs.
    
    Ensure the external ID is set when the user logs in, this should then be
    correct until the ID is changed by a known redirect controller.

--- app/controllers/login/post-login.controller.js
+++ app/controllers/login/post-login.controller.js
@@ -16,6 +16,11 @@ const paths = require('../../paths')
 module.exports = (req, res) => {
   const correlationId = _.get(req, 'headers.' + CORRELATION_HEADER, '')
   req.session = _.pick(req.session, ['passport', 'last_url', 'currentGatewayAccountId'])
+  if (req.gateway_account) {
+    // make sure the accounts external ID is set correctly when the user logs in
+    // following this only controlled code (service switcher, redirect controllers) should be able to update the ID
+    req.gateway_account.currentGatewayAccountExternalId = req.account && req.account.external_id
+  }
   logger.info(`[${correlationId}] successfully attempted username/password combination`)
   res.redirect(paths.user.otpLogIn)
 }
