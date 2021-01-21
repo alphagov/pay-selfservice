b4ead889c62fb47c6d724430b5eac6237c851b47
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2021-01-21 17:14:34 +0000

    PP-7629: add upgrade url coverage for complex urls

--- test/integration/routes.it.test.js
+++ test/integration/routes.it.test.js
@@ -3,7 +3,7 @@ const request = require('supertest')
 const { getApp } = require('../../server')
 const session = require('../test-helpers/mock-session.js')
 
-describe.only('URL upgrade utility', () => {
+describe('URL upgrade utility', () => {
   it('correctly upgrades URLs in the account specific paths', () => {
     const app = session.getAppWithLoggedInUser(getApp(), session.getUser())
     return request(app)
@@ -11,6 +11,16 @@ describe.only('URL upgrade utility', () => {
       .expect(302)
       .then((res) => {
         res.header['location'].should.include('/account/external-id-set-by-create-app-with-session/billing-address') // eslint-disable-line
+      })
+  })
+
+  it('correctly upgrades URLs in the account specific paths with complex templated values', () => {
+    const app = session.getAppWithLoggedInUser(getApp(), session.getUser())
+    return request(app)
+      .get('/create-payment-link/manage/some-product-external-id/add-reporting-column/some-metadata-key')
+      .expect(302)
+      .then((res) => {
+        res.header['location'].should.include('/account/external-id-set-by-create-app-with-session/create-payment-link/manage/some-product-external-id/add-reporting-column/some-metadata-key') // eslint-disable-line
       })
   })
 
