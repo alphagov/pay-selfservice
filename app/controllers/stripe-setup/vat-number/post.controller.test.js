51c1f4bdb2ed0bd2f0afe1a60025f60d5dfc591d
Krish <krishna.bottla@gmail.com>
2021-01-28 23:55:05 +0000

    PP-7641 Remove check-vat-number-not-submitted middleware
    - Removed check vat number details not submitted middleware and inlined the same checks in get & post controllers
    - Added tests for both GET & POST VAT number controllers

---
+++ app/controllers/stripe-setup/vat-number/post.controller.test.js
@@ -0,0 +1,127 @@
+'use strict'
+
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+const paths = require('../../../paths')
+
+describe('VAT number POST controller', () => {
+  const postBody = {
+    'vat-number': 'GB999999973'
+  }
+
+  let req
+  let next
+  let res
+  let setStripeAccountSetupFlagMock
+  let updateCompanyMock
+
+  function getControllerWithMocks () {
+    return proxyquire('./post.controller', {
+      '../../../services/clients/stripe/stripe.client': {
+        updateCompany: updateCompanyMock
+      },
+      '../../../services/clients/connector.client': {
+        ConnectorClient: function () {
+          this.setStripeAccountSetupFlag = setStripeAccountSetupFlagMock
+          this.getStripeAccount = () => Promise.resolve({
+            stripeAccountId: 'acct_123example123'
+          })
+        }
+      }
+    })
+  }
+
+  beforeEach(() => {
+    req = {
+      correlationId: 'correlation-id',
+      account: {
+        gateway_account_id: '1',
+        external_id: 'a-valid-external-id',
+        connectorGatewayAccountStripeProgress: {}
+      },
+      flash: sinon.spy()
+    }
+    res = {
+      setHeader: sinon.stub(),
+      status: sinon.spy(),
+      redirect: sinon.spy(),
+      render: sinon.spy(),
+      locals: {
+        stripeAccount: {
+          stripeAccountId: 'acct_123example123'
+        }
+      }
+    }
+    next = sinon.spy()
+  })
+
+  it('should call Stripe with vat number, then connector, then redirect to add psp account details route', async function () {
+    updateCompanyMock = sinon.spy(() => Promise.resolve())
+    setStripeAccountSetupFlagMock = sinon.spy(() => Promise.resolve())
+    const controller = getControllerWithMocks()
+
+    req.body = { ...postBody }
+
+    await controller(req, res)
+
+    sinon.assert.calledWith(updateCompanyMock, res.locals.stripeAccount.stripeAccountId, {
+      'vat_id': 'GB999999973'
+    })
+    sinon.assert.calledWith(setStripeAccountSetupFlagMock, req.account.gateway_account_id, 'vat_number', req.correlationId)
+    sinon.assert.calledWith(res.redirect, 303, `/account/a-valid-external-id${paths.account.stripe.addPspAccountDetails}`)
+  })
+
+  it('should render error page when stripe setup is not available on request', async () => {
+    const controller = getControllerWithMocks()
+    req.account.connectorGatewayAccountStripeProgress = undefined
+
+    await controller(req, res, next)
+
+    sinon.assert.notCalled(res.redirect)
+    const expectedError = sinon.match.instanceOf(Error)
+      .and(sinon.match.has('message', 'Stripe setup progress is not available on request'))
+    sinon.assert.calledWith(next, expectedError)
+  })
+
+  it('should redirect to dashboard if VAT number is already provided ', async () => {
+    const controller = getControllerWithMocks()
+    req.account.connectorGatewayAccountStripeProgress = { vatNumber: true }
+
+    await controller(req, res)
+
+    sinon.assert.calledWith(req.flash, 'genericError', 'You’ve already provided your VAT number. Contact GOV.UK Pay support if you need to update it.')
+    sinon.assert.calledWith(res.redirect, 303, `/account/a-valid-external-id/dashboard`)
+  })
+
+  it('should render error when Stripe returns error, not call connector, and not redirect', async function () {
+    updateCompanyMock = sinon.spy(() => Promise.reject(new Error()))
+    setStripeAccountSetupFlagMock = sinon.spy(() => Promise.resolve())
+    const controller = getControllerWithMocks()
+
+    req.body = { ...postBody }
+
+    await controller(req, res)
+
+    sinon.assert.called(updateCompanyMock)
+    sinon.assert.notCalled(setStripeAccountSetupFlagMock)
+    sinon.assert.notCalled(res.redirect)
+    sinon.assert.calledWith(res.status, 500)
+    sinon.assert.calledWith(res.render, 'error', { message: 'Please try again or contact support team' })
+  })
+
+  it('should render error when connector returns error', async function () {
+    updateCompanyMock = sinon.spy(() => Promise.resolve())
+    setStripeAccountSetupFlagMock = sinon.spy(() => Promise.reject(new Error()))
+    const controller = getControllerWithMocks()
+
+    req.body = { ...postBody }
+
+    await controller(req, res)
+
+    sinon.assert.called(updateCompanyMock)
+    sinon.assert.calledWith(setStripeAccountSetupFlagMock, req.account.gateway_account_id, 'vat_number', req.correlationId)
+    sinon.assert.notCalled(res.redirect)
+    sinon.assert.calledWith(res.status, 500)
+    sinon.assert.calledWith(res.render, 'error', { message: 'Please try again or contact support team' })
+  })
+})
--- app/routes.js
+++ app/routes.js
@@ -23,7 +23,6 @@ const correlationIdMiddleware = require('./middleware/correlation-id')
 const getRequestContext = require('./middleware/get-request-context').middleware
 const restrictToSandbox = require('./middleware/restrict-to-sandbox')
 const restrictToLiveStripeAccount = require('./middleware/stripe-setup/restrict-to-live-stripe-account')
-const checkVatNumberNotSubmitted = require('./middleware/stripe-setup/check-vat-number-not-submitted')
 const checkCompanyNumberNotSubmitted = require('./middleware/stripe-setup/check-company-number-not-submitted')
 
 // Controllers
@@ -409,8 +408,8 @@ module.exports.bind = function (app) {
   account.post(stripeSetup.bankDetails, permission('stripe-bank-details:update'), restrictToLiveStripeAccount, stripeSetupBankDetailsController.post)
   account.get(stripeSetup.responsiblePerson, permission('stripe-responsible-person:update'), restrictToLiveStripeAccount, stripeSetupResponsiblePersonController.get)
   account.post(stripeSetup.responsiblePerson, permission('stripe-responsible-person:update'), restrictToLiveStripeAccount, stripeSetupResponsiblePersonController.post)
-  account.get(stripeSetup.vatNumber, permission('stripe-vat-number-company-number:update'), restrictToLiveStripeAccount, checkVatNumberNotSubmitted, stripeSetupVatNumberController.get)
-  account.post(stripeSetup.vatNumber, permission('stripe-vat-number-company-number:update'), restrictToLiveStripeAccount, checkVatNumberNotSubmitted, stripeSetupVatNumberController.post)
+  account.get(stripeSetup.vatNumber, permission('stripe-vat-number-company-number:update'), restrictToLiveStripeAccount, stripeSetupVatNumberController.get)
+  account.post(stripeSetup.vatNumber, permission('stripe-vat-number-company-number:update'), restrictToLiveStripeAccount, stripeSetupVatNumberController.post)
   account.get(stripeSetup.companyNumber, permission('stripe-vat-number-company-number:update'), restrictToLiveStripeAccount, checkCompanyNumberNotSubmitted, stripeSetupCompanyNumberController.get)
   account.post(stripeSetup.companyNumber, permission('stripe-vat-number-company-number:update'), restrictToLiveStripeAccount, checkCompanyNumberNotSubmitted, stripeSetupCompanyNumberController.post)
   account.get(stripe.addPspAccountDetails, permission('stripe-account-details:update'), restrictToLiveStripeAccount, stripeSetupAddPspAccountDetailsController.get)
