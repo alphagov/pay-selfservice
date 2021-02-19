62061f8f6a206226ef6189ae8c8b8bcf433fc3a4
Krish <krishna.bottla@gmail.com>
2021-02-19 09:57:25 +0000

    PP-7766 Add request Stripe test account link
    - Added `Request Stripe test account` link to sandbox account dashboard.
    - Available when the flag ENABLE_STRIPE_TEST_ACCOUNT_REQUEST is enabled.
    - Also removed 'Manage team members' link on account dashboard (as per design prototype) as the link is available on Myservices page (which will be the default landing page soon) and doesn't belong to specific gateway account.

--- test/cypress/integration/dashboard/dashboard-links.cy.test.js
+++ test/cypress/integration/dashboard/dashboard-links.cy.test.js
@@ -9,10 +9,15 @@ const gatewayAccountId = '42'
 const gatewayAccountExternalId = 'a-gateway-account-external-id'
 const dashboardUrl = `/account/${gatewayAccountExternalId}/dashboard`
 
-function getStubsForDashboard (gatewayAccountId, type, paymentProvider) {
+function getStubsForDashboard (gatewayAccountId, type, paymentProvider, goLiveStage, pspTestAccountStage) {
   return [
-    userStubs.getUserSuccess({ userExternalId, gatewayAccountId }),
-    gatewayAccountStubs.getGatewayAccountByExternalIdSuccess({ gatewayAccountId, gatewayAccountExternalId, type, paymentProvider }),
+    userStubs.getUserSuccess({ userExternalId, gatewayAccountId, goLiveStage, pspTestAccountStage }),
+    gatewayAccountStubs.getGatewayAccountByExternalIdSuccess({
+      gatewayAccountId,
+      gatewayAccountExternalId,
+      type,
+      paymentProvider
+    }),
     transactionsSummaryStubs.getDashboardStatistics()
   ]
 }
@@ -23,40 +28,33 @@ describe('the links are displayed correctly on the dashboard', () => {
       cy.setEncryptedCookies(userExternalId, gatewayAccountId)
     })
 
-    it('should display 3 links for a live sandbox account', () => {
-      cy.task('setupStubs', getStubsForDashboard(gatewayAccountId, 'live', 'sandbox'))
-
-      cy.visit(dashboardUrl)
-      cy.get('.links__box').should('have.length', 3)
-
-      cy.get('#demo-payment-link').should('exist')
-      cy.get('#demo-payment-link').should('have.class', 'flex-grid--column-third')
-      cy.get('#demo-payment-link').should('not.have.class', 'border-bottom')
-
-      cy.get('#test-payment-link-link').should('exist')
-      cy.get('#test-payment-link-link').should('have.class', 'flex-grid--column-third')
-      cy.get('#test-payment-link-link').should('not.have.class', 'border-bottom')
-
-      cy.get('#manage-service-link').should('exist')
-      cy.get('#manage-service-link').should('have.class', 'flex-grid--column-third')
-      cy.get('#manage-service-link').should('not.have.class', 'border-bottom')
-    })
-
-    it('should display 2 links for a live non-sandbox account', () => {
-      cy.task('setupStubs', getStubsForDashboard(gatewayAccountId, 'live', 'worldpay'))
+    it('should display 2 links for a live sandbox account', () => {
+      cy.task('setupStubs', getStubsForDashboard(gatewayAccountId, 'live', 'sandbox', 'LIVE'))
 
       cy.visit(dashboardUrl)
       cy.get('.links__box').should('have.length', 2)
 
+      cy.get('#demo-payment-link').should('exist')
+      cy.get('#demo-payment-link').should('have.class', 'flex-grid--column-half')
+      cy.get('#demo-payment-link').should('not.have.class', 'border-bottom')
+
+      cy.get('#test-payment-link-link').should('exist')
+      cy.get('#test-payment-link-link').should('have.class', 'flex-grid--column-half')
+      cy.get('#test-payment-link-link').should('not.have.class', 'border-bottom')
+    })
+
+    it('should display 1 link for a live non-sandbox account', () => {
+      cy.task('setupStubs', getStubsForDashboard(gatewayAccountId, 'live', 'worldpay', 'LIVE'))
+
+      cy.visit(dashboardUrl)
+      cy.get('.links__box').should('have.length', 1)
+
       cy.get('#payment-links-link').should('exist')
       cy.get('#payment-links-link').should('have.class', 'flex-grid--column-half')
-
-      cy.get('#manage-service-link').should('exist')
-      cy.get('#manage-service-link').should('have.class', 'flex-grid--column-half')
     })
 
     it('should display 4 links for a test sandbox account', () => {
-      cy.task('setupStubs', getStubsForDashboard(gatewayAccountId, 'test', 'sandbox', 'an-id', 'NOT_STARTED'))
+      cy.task('setupStubs', getStubsForDashboard(gatewayAccountId, 'test', 'sandbox', 'NOT_STARTED'))
 
       cy.visit(dashboardUrl)
       cy.get('.links__box').should('have.length', 4)
@@ -67,27 +65,43 @@ describe('the links are displayed correctly on the dashboard', () => {
       cy.get('#test-payment-link-link').should('exist')
       cy.get('#test-payment-link-link').should('have.class', 'flex-grid--column-half')
 
-      cy.get('#manage-service-link').should('exist')
-      cy.get('#manage-service-link').should('have.class', 'flex-grid--column-half')
+      cy.get('#request-to-go-live-link').should('exist')
+      cy.get('#request-to-go-live-link').should('have.class', 'flex-grid--column-half')
+
+      cy.get('#request-stripe-test-account').should('exist')
+      cy.get('#request-stripe-test-account').should('have.class', 'flex-grid--column-half')
+    })
+
+    it('should display 2 links for a test non-sandbox account', () => {
+      cy.task('setupStubs', getStubsForDashboard(gatewayAccountId, 'test', 'worldpay', 'NOT_STARTED'))
+
+      cy.visit(dashboardUrl)
+      cy.get('.links__box').should('have.length', 2)
+
+      cy.get('#payment-links-link').should('exist')
+      cy.get('#payment-links-link').should('have.class', 'flex-grid--column-half')
 
       cy.get('#request-to-go-live-link').should('exist')
       cy.get('#request-to-go-live-link').should('have.class', 'flex-grid--column-half')
     })
 
-    it('should display 3 links for a test non-sandbox account', () => {
-      cy.task('setupStubs', getStubsForDashboard(gatewayAccountId, 'test', 'worldpay'))
+    it('should display `Stripe test account requested` section if request has been submitted', () => {
+      cy.task('setupStubs', getStubsForDashboard(gatewayAccountId, 'test', 'sandbox', 'NOT_STARTED', 'REQUEST_SUBMITTED'))
 
       cy.visit(dashboardUrl)
-      cy.get('.links__box').should('have.length', 3)
+      cy.get('.links__box').should('have.length', 4)
 
-      cy.get('#payment-links-link').should('exist')
-      cy.get('#payment-links-link').should('have.class', 'flex-grid--column-third')
+      cy.get('#demo-payment-link').should('exist')
+      cy.get('#demo-payment-link').should('have.class', 'flex-grid--column-half')
 
-      cy.get('#manage-service-link').should('exist')
-      cy.get('#manage-service-link').should('have.class', 'flex-grid--column-third')
+      cy.get('#test-payment-link-link').should('exist')
+      cy.get('#test-payment-link-link').should('have.class', 'flex-grid--column-half')
 
       cy.get('#request-to-go-live-link').should('exist')
-      cy.get('#request-to-go-live-link').should('have.class', 'flex-grid--column-third')
+      cy.get('#request-to-go-live-link').should('have.class', 'flex-grid--column-half')
+
+      cy.get('#stripe-test-account-requested').should('exist')
+      cy.get('#stripe-test-account-requested').should('have.class', 'flex-grid--column-half')
     })
   })
 })
