6221fd44d34bc7062c96c279b389acc408cc4d8a
Stephen Daly <stephencdaly@gmail.com>
2021-03-24 12:38:51 +0000

    PP-7733 Remove feature flag
    Remove all usages of the `ENABLE_MY_SERVICES_AS_DEFAULT_VIEW` feature
    flag and always default to "My services" page rather than account
    dashboard.

--- test/unit/controller/root/index.controller.test.js
+++ test/unit/controller/root/index.controller.test.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const sinon = require('sinon')
-const { expect } = require('chai')
 
 const rootController = require('../../../../app/controllers/root/index.controller')
 
@@ -13,18 +12,8 @@ describe('Root controller', () => {
       redirect: sinon.spy()
     }
   })
-  it('should redirect to My services page when ENABLE_MY_SERVICES_AS_DEFAULT_VIEW flag is enabled', () => {
-    process.env.ENABLE_MY_SERVICES_AS_DEFAULT_VIEW = 'true'
+  it('should redirect to My services page', () => {
     rootController.get(req, res)
-
-    expect(res.redirect.calledWith('/my-services')).to.equal(true)
-  })
-
-  it('should redirect to account dashboard when ENABLE_MY_SERVICES_AS_DEFAULT_VIEW flag is not enabled', () => {
-    process.env.ENABLE_MY_SERVICES_AS_DEFAULT_VIEW = 'false'
-    req.account = { external_id: 'account-external-id' }
-    rootController.get(req, res)
-
-    expect(res.redirect.calledWith('/account/account-external-id/dashboard')).to.equal(true)
+    sinon.assert.calledWith(res.redirect, '/my-services')
   })
 })
