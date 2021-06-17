ebc3b63b5b711243381dabefe6932f4c9719a1b8
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2021-06-17 15:06:28 +0100

    PP-8216 Add Cypress tests for task list
    Add Cypress tests to check the task list is displayed properly with
    correct tags etc. and to test the journey for completing the first step.

--- test/cypress/integration/settings/switch-psp.cy.test.js
+++ test/cypress/integration/settings/switch-psp.cy.test.js
@@ -6,12 +6,15 @@ const gatewayAccountStubs = require('../../stubs/gateway-account-stubs')
 const userExternalId = 'cd0fa54cf3b7408a80ae2f1b93e7c16e'
 const gatewayAccountId = '42'
 const gatewayAccountExternalId = 'a-valid-external-id'
+const merchantId = 'abc'
+const username = 'me'
+const password = '1'
 
-function setupStubs (paymentProvider, providerSwitchEnabled, gatewayAccountCredentials) {
-  cy.task('setupStubs', [
+function getUserAndAccountStubs (paymentProvider, providerSwitchEnabled, gatewayAccountCredentials) {
+  return [
     userStubs.getUserSuccess({ gatewayAccountId, userExternalId }),
     gatewayAccountStubs.getGatewayAccountByExternalIdSuccess({ gatewayAccountId, gatewayAccountExternalId, providerSwitchEnabled, paymentProvider, ...gatewayAccountCredentials && { gatewayAccountCredentials } })
-  ])
+  ]
 }
 
 describe('Switch PSP settings page', () => {
@@ -21,7 +24,7 @@ describe('Switch PSP settings page', () => {
 
   describe('When using an account with switching flag disabled', () => {
     beforeEach(() => {
-      setupStubs('smartpay', false)
+      cy.task('setupStubs', getUserAndAccountStubs('smartpay', false))
     })
 
     it('should not show link to Switch PSP in the side navigation', () => {
@@ -32,19 +35,66 @@ describe('Switch PSP settings page', () => {
   })
 
   describe('When using an account with switching flag enabled', () => {
-    beforeEach(() => {
-      setupStubs('smartpay', true, [
-        { payment_provider: 'smartpay', state: 'ACTIVE' },
-        { payment_provider: 'worldpay', state: 'CREATED' }
-      ])
+    describe('Switching is not started', () => {
+
+      beforeEach(() => {
+        cy.task('setupStubs', getUserAndAccountStubs('smartpay', true, [
+          { payment_provider: 'smartpay', state: 'ACTIVE' },
+          { payment_provider: 'worldpay', state: 'CREATED' }
+        ]))
+      })
+
+      it('should show the switch PSP page for switching to Worldpay', () => {
+        cy.setEncryptedCookies(userExternalId)
+        cy.visit(`/account/${gatewayAccountExternalId}/switch-psp`)
+        cy.get('#navigation-menu-switch-psp').should('have.length', 1)
+        cy.get('h1').should('contain', 'Switch payment service provider')
+        cy.get('li').contains('your Worldpay account credentials: Merchant code, username and password').should('exist')
+        cy.get('#switch-psp-action-step').should('contain', 'Switch PSP to Worldpay')
+      })
+
+      it('should have task list for Worldpay with correct tags', () => {
+        cy.get('.app-task-list>li').eq(0).should('contain', 'Get ready to switch PSP')
+          .within(() => {
+            cy.get('.app-task-list__item').eq(0).should('contain', 'Link your Worldpay account with GOV.UK Pay')
+              .find('.app-task-list__tag').should('have.text', 'not started')
+          })
+      })
+
+      it('should navigate to link Worldpay account step', () => {
+        cy.get('.app-task-list__item').contains('Link your Worldpay account with GOV.UK Pay').click()
+        cy.get('h1').should('contain', 'Your Worldpay credentials')
+        cy.get('.govuk-back-link').should('contain', 'Back to Switching payment service provider (PSP)')
+      })
     })
 
-    it('should show the switch PSP page', () => {
-      cy.setEncryptedCookies(userExternalId)
-      cy.visit(`/account/${gatewayAccountExternalId}/switch-psp`)
-      cy.get('#navigation-menu-switch-psp').should('have.length', 1)
-      cy.get('h1').should('contain', 'Switch payment service provider')
-      cy.get('#switch-psp-action-step').should('contain', 'Switch PSP to Worldpay')
+    describe('Worldpay account linked', () => {
+      beforeEach(() => {
+        cy.task('setupStubs', [
+          ...getUserAndAccountStubs('smartpay', true, [
+            { payment_provider: 'smartpay', state: 'ACTIVE' },
+            { payment_provider: 'worldpay', state: 'ENTERED' }
+          ]),
+          gatewayAccountStubs.postCheckWorldpayCredentials({
+            gatewayAccountId,
+            merchant_id: merchantId,
+            username,
+            password
+          })
+        ])
+      })
+
+      it('should submit Worldpay credentials', () => {
+        cy.get('#merchantId').type('abc')
+        cy.get('#username').type('me')
+        cy.get('#password').type('1')
+        cy.get('button').contains('Save credentials').click()
+      })
+
+      it('should go back to task list with the link Worldpay account step complete', () => {
+        cy.get('.app-task-list__item').eq(0).should('contain', 'Link your Worldpay account with GOV.UK Pay')
+          .find('.app-task-list__tag').should('have.text', 'completed')
+      })
     })
   })
 })
