2764c25e1207d44238eee2093beaa3ff2e1487c7
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2021-10-08 15:01:22 +0100

    PP-8750 Remove client-side valdition for demo payment
    Reproduce client-side validation on the server for the edit demo payment
    details journey.
    
    Removing client-side validation as it is against design system guidance,
    makes it difficult to test validation, and is not consistent with the
    server side validation.
    
    Restructure the controllers to split out the edit controllers so the
    code is easier to understand.
    
    Replace the functional tests with Cypress tests, and add unit tests for
    the edit amount controller to test all possible validation errors.

--- app/controllers/make-a-demo-payment/edit.controller.js
+++ app/controllers/make-a-demo-payment/edit.controller.js
@@ -3,14 +3,77 @@
 const lodash = require('lodash')
 
 const { response } = require('../../utils/response.js')
-const { editDescription } = require('../../paths').account.prototyping.demoPayment
+const { index } = require('../../paths').account.prototyping.demoPayment
+const { validateMandatoryField } = require('../../utils/validation/server-side-form-validations')
+const formatAccountPathsFor = require('../../utils/format-account-paths-for')
+const { isCurrency, isAboveMaxAmount } = require('../../browsered/field-validation-checks')
+const { safeConvertPoundsStringToPence } = require('../../utils/currency-formatter')
 
-module.exports = (req, res) => {
-  const pageData = lodash.get(req, 'session.pageData.makeADemoPayment', {})
-  const template = req.path === editDescription ? 'dashboard/demo-payment/edit-description' : 'dashboard/demo-payment/edit-amount'
+const DESCRIPTION_MAX_LENGTH = 255
 
-  response(req, res, template, {
-    paymentDescription: pageData.paymentDescription,
-    paymentAmount: pageData.paymentAmount
+function getEditDescription (req, res) {
+  const demoPaymentDetails = lodash.get(req, 'session.pageData.makeADemoPayment', {})
+  response(req, res, 'dashboard/demo-payment/edit-description', {
+    paymentDescription: demoPaymentDetails.paymentDescription
   })
+}
+
+function updateDescription (req, res, next) {
+  const sessionData = req.session && req.session.pageData && req.session.pageData.makeADemoPayment
+  if (!sessionData) {
+    return next(new Error('Demo payment details not found on session cookie'))
+  }
+  const paymentDescription = req.body['payment-description']
+  const validateResult = validateMandatoryField(paymentDescription, DESCRIPTION_MAX_LENGTH, 'payment description', true)
+  if (!validateResult.valid) {
+    return response(req, res, 'dashboard/demo-payment/edit-description', {
+      errors: {
+        description: validateResult.message
+      },
+      paymentDescription
+    })
+  }
+
+  sessionData.paymentDescription = paymentDescription
+  res.redirect(formatAccountPathsFor(index, req.account && req.account.external_id))
+}
+
+function getEditAmount (req, res) {
+  const demoPaymentDetails = lodash.get(req, 'session.pageData.makeADemoPayment', {})
+  response(req, res, 'dashboard/demo-payment/edit-amount', {
+    paymentAmount: demoPaymentDetails.paymentAmount
+  })
+}
+
+function updateAmount (req, res, next) {
+  const sessionData = req.session && req.session.pageData && req.session.pageData.makeADemoPayment
+  if (!sessionData) {
+    return next(new Error('Demo payment details not found on session cookie'))
+  }
+  const paymentAmount = req.body['payment-amount']
+  const amountInPence = safeConvertPoundsStringToPence(paymentAmount)
+
+  let error = isCurrency(paymentAmount)
+  if (!error) {
+    error = isAboveMaxAmount(paymentAmount)
+  }
+  if (error) {
+    return response(req, res, 'dashboard/demo-payment/edit-amount', {
+      errors: {
+        amount: error
+      },
+      // set back to exiting payment amount as text input will try to transform invalid input into a number
+      paymentAmount: sessionData.paymentAmount
+    })
+  }
+
+  sessionData.paymentAmount = amountInPence
+  res.redirect(formatAccountPathsFor(index, req.account && req.account.external_id))
+}
+
+module.exports = {
+  getEditDescription,
+  updateDescription,
+  getEditAmount,
+  updateAmount
 }
