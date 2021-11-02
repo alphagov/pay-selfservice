1e74ce7fdcc89352aac7d38007019dfd6e5e397e
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2021-11-02 15:36:30 +0000

    PP-8519 Google Analytics - set cookie domain.
    - Fix bug so that when the Google Analytic cookies are created -
      we explicitly set a domain.
    - Update the `ga('create'...)` to use the cookieFunctions.getDomainCookie
      method.

--- test/unit/browsered/analytics.test.js
+++ test/unit/browsered/analytics.test.js
@@ -13,18 +13,18 @@ describe('analytics setup', () => {
   })
   it('should invoke analytics with correct params ', () => {
     window = new jsdom.JSDOM(``, {
-      url: 'https://example.org/search?from_date=2020-01-01'
+      url: 'https://selfservice.service.payments.gov.uk/search?from_date=2020-01-01'
     }).window
     document = window.document
     analytics.setupAnalytics()
     expect(ga.callCount).equals(11)
-    expect(ga.getCall(0).calledWith('create', 'test-analytics-id', 'auto')).equals(true)
+    expect(ga.getCall(0).calledWith('create', 'test-analytics-id', '.service.payments.gov.uk')).equals(true)
     expect(ga.getCall(1).calledWith('set', 'anonymizeIp', true)).equals(true)
     expect(ga.getCall(2).calledWith('set', 'displayFeaturesTask', null)).equals(true)
     expect(ga.getCall(3).calledWith('set', 'transport', 'beacon')).equals(true)
 
     expect(ga.getCall(4).calledWith('create',
-      'linked-tracking-id', 'auto', 'govuk_shared', { 'allowLinker': true })).equals(true)
+      'linked-tracking-id', '.service.payments.gov.uk', 'govuk_shared', { 'allowLinker': true })).equals(true)
     expect(ga.getCall(5).calledWith('govuk_shared.require', 'linker')).equals(true)
     expect(ga.getCall(6).calledWith('govuk_shared.linker.set', 'anonymizeIp')).equals(true)
     expect(ga.getCall(7).calledWith('govuk_shared.linker:autoLink', ['www.gov.uk'])).equals(true)
@@ -33,7 +33,7 @@ describe('analytics setup', () => {
 
   it('should filter PII correctly ', () => {
     window = new jsdom.JSDOM(``, {
-      url: 'https://example.org/search?from_date=2020-01-01&email=test@example.org&to_date=2020-01-01'
+      url: 'https://selfservice.service.payments.gov.uk/search?from_date=2020-01-01&email=test@example.org&to_date=2020-01-01'
     }).window
     document = window.document
     analytics.setupAnalytics()
