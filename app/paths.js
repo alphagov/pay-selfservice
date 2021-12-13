a5b37f405d65dff7ec0de8defd0bd8836d03b880
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2021-12-13 22:54:25 +0000

    PP-8499: toggle webhook status page
    Add template, path and routes for a page to manage and toggle webhook
    status.

--- app/paths.js
+++ app/paths.js
@@ -159,6 +159,7 @@ module.exports = {
       detail: '/webhooks/:webhookId',
       update: '/webhooks/:webhookId/update',
       signingSecret: '/webhooks/:webhookId/signing-secret',
+      toggleActive: '/webhooks/:webhookId/status',
       create: '/webhooks/create'
     }
   },
