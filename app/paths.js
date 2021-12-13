13fffd84d437c0be0f96706c4148301d5cea5d60
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2021-12-13 21:39:04 +0000

    PP-8499: add signing secret page
    Use the separate route to access Webhook signing secret. Display secret
    on detail page.
    
    Cypress test for happy path coverage.

--- app/paths.js
+++ app/paths.js
@@ -158,6 +158,7 @@ module.exports = {
       index: '/webhooks',
       detail: '/webhooks/:webhookId',
       update: '/webhooks/:webhookId/update',
+      signingSecret: '/webhooks/:webhookId/signing-secret',
       create: '/webhooks/create'
     }
   },
