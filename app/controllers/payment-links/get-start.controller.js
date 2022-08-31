7c693bb17d2ecbf996b48daa4d6d4d0046354bed
Alex Bishop <alexbishop1@users.noreply.github.com>
2022-08-31 11:55:39 +0100

    PP-9882 Create payment link page: warn if Worldpay MOTO service
    Add a warning to the create payment link page imploring the user
    to not create a payment link if and only if the gateway account
    has current credentials for Worldpay using a merchant code that
    ends in ‘MOTO’ (which is how Worldpay identify MOTO merchant
    codes).
    
    The existing warning on the create payment link page, which informs
    users about the existence of telephone payment links (agent-
    initiated MOTO payments) is changed to inset text so we do not end
    up with two warnings on single page.

--- app/controllers/payment-links/get-start.controller.js
+++ app/controllers/payment-links/get-start.controller.js
@@ -3,9 +3,18 @@
 const lodash = require('lodash')
 
 const { response } = require('../../utils/response.js')
+const { getCurrentCredential } = require('../../utils/credentials')
 
 module.exports = (req, res) => {
   lodash.set(req, 'session.pageData.createPaymentLink', {})
 
-  return response(req, res, 'payment-links/index', {})
+  const credential = getCurrentCredential(req.account)
+
+  const accountUsesWorldpayMotoMerchantCode = lodash.get(credential, 'payment_provider', '') === 'worldpay'
+      && lodash.get(credential, 'credentials.merchant_id', '').endsWith('MOTO')
+
+  return response(req, res, 'payment-links/index', {
+    accountUsesWorldpayMotoMerchantCode: accountUsesWorldpayMotoMerchantCode
+  })
+
 }
---
+++ test/unit/controller/payment-links/get-start.controller.test.js
@@ -0,0 +1,128 @@
+'use strict'
+
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+const { expect } = require('chai')
+const mockResponses = {}
+
+const getController = function (mockResponses) {
+  return proxyquire('../../../../app/controllers/payment-links/get-start.controller', {
+    '../../utils/response': mockResponses
+  })
+}
+
+describe('The Worldpay MOTO account warning', () => {
+  let req
+  let res
+  let createPaymentLinkStartController
+
+  beforeEach(() => {
+    mockResponses.response = sinon.spy()
+    createPaymentLinkStartController = getController(mockResponses)
+    res = {}
+  })
+
+  describe('when the gateway account has a Worldpay credentials object with a merchant code ending with ‘MOTO’', () => {
+    it('should pass accountUsesWorldpayMotoMerchantCode with a value of true', () => {
+      req = {
+        account: {
+          gateway_account_credentials: [{
+            state: 'ACTIVE',
+            payment_provider: 'worldpay',
+            credentials: {
+                merchant_id: 'merchant-code-ends-with-MOTO'
+            }
+          }]
+        }
+      }
+
+      createPaymentLinkStartController(req, res)
+      expect(mockResponses.response.args[0][3]).to.have.property('accountUsesWorldpayMotoMerchantCode').to.equal(true)
+    })
+  })
+
+  describe('when the gateway account has a Worldpay credentials object with a merchant code not ending with ‘MOTO’', () => {
+    it('should pass accountUsesWorldpayMotoMerchantCode with a value of false', () => {
+      req = {
+        account: {
+          gateway_account_credentials: [{
+            state: 'ACTIVE',
+            payment_provider: 'worldpay',
+            credentials: {
+                merchant_id: 'merchant-code-ends-with-MOTO-ah-no-it-does-not'
+            }
+          }]
+        }
+      }
+
+      createPaymentLinkStartController(req, res)
+      expect(mockResponses.response.args[0][3]).to.have.property('accountUsesWorldpayMotoMerchantCode').to.equal(false)
+    })
+  })
+
+  describe('when the gateway account has a non-Worldpay credentials object with a merchant code ending with ‘MOTO’', () => {
+    it('should pass accountUsesWorldpayMotoMerchantCode with a value of false', () => {
+      req = {
+        account: {
+          gateway_account_credentials: [{
+            state: 'ACTIVE',
+            payment_provider: 'not-worldpay',
+            credentials: {
+                merchant_id: 'merchant-code-ends-with-MOTO'
+            }
+          }]
+        }
+      }
+
+      createPaymentLinkStartController(req, res)
+      expect(mockResponses.response.args[0][3]).to.have.property('accountUsesWorldpayMotoMerchantCode').to.equal(false)
+    })
+  })
+
+  describe('when the gateway account has a Worldpay credentials object without a merchant_id', () => {
+    it('should pass accountUsesWorldpayMotoMerchantCode with a value of false', () => {
+      req = {
+        account: {
+          gateway_account_credentials: [{
+            state: 'ACTIVE',
+            payment_provider: 'worldpay',
+            credentials: {}
+          }]
+        }
+      }
+
+      createPaymentLinkStartController(req, res)
+      expect(mockResponses.response.args[0][3]).to.have.property('accountUsesWorldpayMotoMerchantCode').to.equal(false)
+    })
+  })
+
+  describe('when the gateway account has no credentials object', () => {
+    it('should pass accountUsesWorldpayMotoMerchantCode with a value of false', () => {
+      req = {
+        account: {
+          gateway_account_credentials: [{
+            state: 'ACTIVE',
+            payment_provider: 'worldpay'
+          }]
+        }
+      }
+
+      createPaymentLinkStartController(req, res)
+      expect(mockResponses.response.args[0][3]).to.have.property('accountUsesWorldpayMotoMerchantCode').to.equal(false)
+    })
+  })
+
+  describe('when the gateway account has no credentials at all', () => {
+    it('should pass accountUsesWorldpayMotoMerchantCode with a value of false', () => {
+      req = {
+        account: {
+          gateway_account_credentials: []
+        }
+      }
+
+      createPaymentLinkStartController(req, res)
+      expect(mockResponses.response.args[0][3]).to.have.property('accountUsesWorldpayMotoMerchantCode').to.equal(false)
+    })
+  })
+
+})
