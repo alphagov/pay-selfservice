2478c925dcf0caa65ab4cd0d74e19122d4cf148b
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2022-12-05 10:42:20 +0000

    BAU Log invite code where it is present
    For the new registration journey, add an invite_code field to the log
    context that is added in the middleware that checks the registration
    cookie is present. This will cause the invite_code to be logged for all
    lines logged for requests made to the registration routes.

--- app/middleware/invite-cookie-is-present.js
+++ app/middleware/invite-cookie-is-present.js
@@ -2,10 +2,12 @@
 
 const { RegistrationSessionMissingError } = require('../errors')
 const { INVITE_SESSION_COOKIE_NAME } = require('../utils/constants')
+const { addField } = require('../utils/request-context')
 
 module.exports = function inviteCookieIsPresent (req, res, next) {
   const cookie = req[INVITE_SESSION_COOKIE_NAME]
   if (cookie && cookie.email && cookie.code) {
+    addField('invite_code', cookie.code)
     return next()
   }
   next(new RegistrationSessionMissingError('Registration cookie is not present for request'))
