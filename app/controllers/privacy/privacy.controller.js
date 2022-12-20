0fcbdcb69b08c59dbd51f128cda6855f482100dd
Iqbal AHmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2022-12-20 15:30:40 +0000

    PP-10242 Privacy page - use login status
    - Set page data so that the page is displayed correctly according
      to their login status
      - Header - show sign in / log link out depending on login status.
      - If logged in, show the `My services` nav bar.
      - Hide the account specific nav.
    - Update Cypress tests.

--- app/controllers/privacy/privacy.controller.js
+++ app/controllers/privacy/privacy.controller.js
@@ -1,7 +1,14 @@
 'use strict'
 
+const sessionValidator = require('./../../services/session-validator')
+
 function getPage (req, res) {
-  return res.render('privacy/privacy')
+  const loggedIn = sessionValidator.validate(req.user, req.session)
+
+  return res.render('privacy/privacy', {
+    loggedIn,
+    hideServiceNav: true
+  })
 }
 
 module.exports = {
