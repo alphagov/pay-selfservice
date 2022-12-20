b77359313bc526a2206c3c6b846dcbd0b9103837
Iqbal AHmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2022-12-20 17:02:29 +0000

    PP-10242 Privacy page - logged in status
    - Update Privacy page show that it appears correctly whether a user is
      logged in or out
      - Header - show the sign in / sign out link correctly.
      - Only display the phase banner if the user is signed in.
    - Update Cypress tests.

--- test/cypress/integration/privacy/privacy.cy.test.js
+++ test/cypress/integration/privacy/privacy.cy.test.js
@@ -1,14 +1,39 @@
 'use strict'
+const userStubs = require('../../stubs/user-stubs')
+
+const userExternalId = 'a-user-id'
+const gatewayAccountId = 42
+const gatewayAccountExternalId = 'a-valid-account-id'
 
 describe('Privacy page', () => {
-  it('should show the privacy page', () => {
-    cy.visit('/privacy')
+  describe('Logged out user', () => {
+    it('should show the privacy page correctly', () => {
+      cy.visit('/privacy')
 
-    cy.get('h1').should('contain', 'Privacy notice')
+      cy.get('h1').should('contain', 'Privacy notice')
 
-    cy.get('[data-cy=sub-navigation]').should('exist')
-    cy.get('[data-cy=sub-navigation] li').should('have.length', 10)
+      cy.get('[data-cy=sub-navigation]').should('exist')
+      cy.get('[data-cy=sub-navigation] li').should('have.length', 10)
 
-    cy.get('main h2').should('have.length', 10)
+      cy.get('main h2').should('have.length', 10)
+
+      cy.get('#navigation').should('contain', 'Sign in')
+      cy.get('[data-cy=breadcrumbs]').should('not.exist')
+    })
+  })
+
+  describe('Logged in user', () => {
+    it('should show the privacy page correctly', () => {
+      cy.task('setupStubs', [
+        userStubs.getUserSuccess({ userExternalId, gatewayAccountId, gatewayAccountExternalId })
+      ])
+
+      cy.setEncryptedCookies(userExternalId)
+
+      cy.visit('/privacy')
+
+      cy.get('#navigation').should('contain', 'Sign out')
+      cy.get('[data-cy=breadcrumbs]').should('exist')
+    })
   })
 })
