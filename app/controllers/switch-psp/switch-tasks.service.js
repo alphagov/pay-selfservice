8fe0ff2af6f18f6c55f6ee0c7472a9d041f6cf41
Iqbal AHmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2023-01-06 15:25:56 +0000

    PP-10313 Switch PSP for Moto accounts - update Switch tasks service
    Update switch tasks service:
    - When switching a MOTO gateway account to Worldpay, do not require
      the `link flex credentials`

--- app/controllers/switch-psp/switch-tasks.service.js
+++ app/controllers/switch-psp/switch-tasks.service.js
@@ -1,36 +1,48 @@
 'use strict'
 
-const { CREDENTIAL_STATE } = require('../../utils/credentials')
+const {CREDENTIAL_STATE} = require('../../utils/credentials')
 const lodash = require('lodash')
 
-function linkCredentialsComplete (targetCredential) {
+function linkCredentialsComplete(targetCredential) {
   return [CREDENTIAL_STATE.ENTERED, CREDENTIAL_STATE.VERIFIED, CREDENTIAL_STATE.ACTIVE, CREDENTIAL_STATE.RETIRED]
     .includes(targetCredential.state)
 }
 
-function linkFlexCredentialsComplete (account) {
+function linkFlexCredentialsComplete(account) {
   return ((account.requires3ds === true) && (account.integration_version_3ds === 2))
 }
 
-function verifyPSPIntegrationComplete (targetCredential) {
+function verifyPSPIntegrationComplete(targetCredential) {
   return [CREDENTIAL_STATE.VERIFIED, CREDENTIAL_STATE.ACTIVE, CREDENTIAL_STATE.RETIRED]
     .includes(targetCredential.state)
 }
 
-function stripeSetupStageComplete (account, stage) {
+function stripeSetupStageComplete(account, stage) {
   if (account.connectorGatewayAccountStripeProgress) {
     return account.connectorGatewayAccountStripeProgress[stage]
   }
   return false
 }
 
-function organisationUrlComplete (service) {
+function organisationUrlComplete(service) {
   const organisationUrl = lodash.get(service, 'merchantDetails.url', '')
   return organisationUrl && organisationUrl.length > 0
 }
 
-function getTaskList (targetCredential, account, service) {
+function getTaskList(targetCredential, account, service) {
   if (targetCredential.payment_provider === 'worldpay') {
+    if (account.allow_moto) {
+      return {
+        'LINK_CREDENTIALS': {
+          enabled: true,
+          complete: linkCredentialsComplete(targetCredential)
+        },
+        'VERIFY_PSP_INTEGRATION': {
+          enabled: linkCredentialsComplete(targetCredential),
+          complete: verifyPSPIntegrationComplete(targetCredential)
+        }
+      }
+    }
     return {
       'LINK_CREDENTIALS': {
         enabled: true,
@@ -96,8 +108,8 @@ function getTaskList (targetCredential, account, service) {
   throw new Error('Unsupported payment provider')
 }
 
-function isComplete (taskList) {
+function isComplete(taskList) {
   return Object.values(taskList).every(task => task.complete)
 }
 
-module.exports = { getTaskList, isComplete }
+module.exports = {getTaskList, isComplete}
