54b99fbb63d3efa83d59b07d2b8e79387183c0a4
AbdirizakIdris <abzak7@hotmail.co.uk>
2023-01-17 16:30:12 +0000

    PP-10352 redirect user when VAT complete
    - Updated VAT's post controller so when ENABLE_STRIPE_ONBOARDING_TASK_LIST is set to true it redirects to task-list page
    - Updated VAT cypress test

--- app/controllers/stripe-setup/vat-number/post.controller.js
+++ app/controllers/stripe-setup/vat-number/post.controller.js
@@ -18,6 +18,7 @@ const VAT_NUMBER_FIELD = 'vat-number'
 const VAT_NUMBER_PROVIDED_FIELD = 'vat-number-declaration'
 
 module.exports = async (req, res, next) => {
+  const enabledStripeOnboardingTaskList = (process.env.ENABLE_STRIPE_ONBOARDING_TASK_LIST === 'true')
   const isSwitchingCredentials = isSwitchingCredentialsRoute(req)
   const stripeAccountSetup = req.account.connectorGatewayAccountStripeProgress
 
@@ -67,6 +68,8 @@ module.exports = async (req, res, next) => {
       })
       if (isSwitchingCredentials) {
         return res.redirect(303, formatAccountPathsFor(paths.account.switchPSP.index, req.account.external_id))
+      } else if (enabledStripeOnboardingTaskList) {
+        return res.redirect(303, formatAccountPathsFor(paths.account.yourPsp.index, req.account && req.account.external_id, req.account.gateway_account_credentials[0].external_id))
       } else {
         return res.redirect(303, formatAccountPathsFor(paths.account.stripe.addPspAccountDetails, req.account && req.account.external_id))
       }
