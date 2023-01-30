0661662659cf60b76174598f4810121959aaf932
Iqbal AHmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2023-01-30 16:16:17 +0000

    PP-10242 - Privace page - content updates

--- test/cypress/integration/privacy/privacy.cy.js
+++ test/cypress/integration/privacy/privacy.cy.js
@@ -13,9 +13,9 @@ describe('Privacy page', () => {
       cy.get('h1').should('contain', 'Privacy notice')
 
       cy.get('[data-cy=sub-navigation]').should('exist')
-      cy.get('[data-cy=sub-navigation] li').should('have.length', 10)
+      cy.get('[data-cy=sub-navigation] li').should('have.length', 11)
 
-      cy.get('main h2').should('have.length', 10)
+      cy.get('main h2').should('have.length', 11)
 
       cy.get('#navigation').should('contain', 'Sign in')
       cy.get('[data-cy=breadcrumbs]').should('not.exist')
