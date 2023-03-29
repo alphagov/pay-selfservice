5182585522e25bddfd51f65fbcf756d9e32c8a9f
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2023-03-29 11:33:27 +0100

    PP-10862 Make settings Cypress tests standalone
    Cypress best practices suggest that having tests rely on the state of
    previous tests is an anti-pattern.
    
    Combine tests and remove use of Cypress.Cookies.preserveOnce() so that
    tests can be run independently.
    
    Simplify payment-types.cy.js to not attempt to check what the new
    selected card types are after updating. This was only checking that the
    page has the enabled card types returned by connector selected and not
    any selfservice logic. Instead, just check that the page is successfully
    reloaded with a success message.

--- test/fixtures/card.fixtures.js
+++ test/fixtures/card.fixtures.js
@@ -112,46 +112,5 @@ module.exports = {
     }
 
     return data
-  },
-  validUpdatedAcceptedCardTypesResponse: () => {
-    return {
-      card_types: [{
-        id: 'a1200c73-204d-45f5-8fca-e4ee5ed1b1a7',
-        brand: 'visa',
-        label: 'Visa',
-        type: 'DEBIT',
-        requires3ds: false
-      }, {
-        id: 'd03f5008-f793-4ea5-8a23-87f40ceb6c81',
-        brand: 'visa',
-        label: 'Visa',
-        type: 'CREDIT',
-        requires3ds: false
-      }, {
-        id: '491df314-fcb1-428f-9ea8-320865cf2a29',
-        brand: 'master-card',
-        label: 'Mastercard',
-        type: 'DEBIT',
-        requires3ds: false
-      }, {
-        id: '667cad4d-8ef9-418b-8023-5c8922f82d7a',
-        brand: 'master-card',
-        label: 'Mastercard',
-        type: 'CREDIT',
-        requires3ds: false
-      }, {
-        id: 'fff66fd7-e1d6-4c65-846d-9199c906e368',
-        brand: 'american-express',
-        label: 'American Express',
-        type: 'CREDIT',
-        requires3ds: false
-      }, {
-        id: '2d5462a8-fb95-4a5a-b991-3891e40312f3',
-        brand: 'diners-club',
-        label: 'Diners Club',
-        type: 'CREDIT',
-        requires3ds: false
-      }]
-    }
   }
 }
