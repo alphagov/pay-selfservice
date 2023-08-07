c9885d81d231dfc38435d84f2414d6a0fcce8e6b
Iqbal AHmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2023-08-07 17:56:06 +0100

    PP-10823 - Check reference for PAN - add JS validation
    - Adds client side JS validation to check if a reference used during a search
      might be a PAN.
    - Displays an inline error only when the user moves focus from the field.
    - Add Cypress tests

--- app/browsered.js
+++ app/browsered.js
@@ -12,6 +12,7 @@ const accessibleAutocomplete = require('./browsered/autocomplete')
 const checkboxRowSelection = require('./browsered/checkbox-row-selection')
 const cookieBanner = require('./browsered/cookie-banner')
 const printButton = require('./browsered/print-button')
+const transactionsSearch = require('./browsered/transactions-search')
 
 // GOV.UK Frontend js bundle
 const GOVUKFrontend = require('govuk-frontend')
@@ -29,3 +30,4 @@ accessibleAutocomplete()
 checkboxRowSelection()
 cookieBanner.initCookieBanner()
 printButton()
+transactionsSearch()
