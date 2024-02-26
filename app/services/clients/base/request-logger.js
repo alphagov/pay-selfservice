ef521eb6a0180347e11632b9a1098ada495dce28
John Shields <145327947+JFSGDS@users.noreply.github.com>
2024-02-26 10:22:40 +0000

    PP-11681 Replace `request` library with `axios`. (#4180)
    - Initial set up.
    - Use latest version of 'pay-js-commons' library containing 'axios-base-client' code.
    - Add 'clients/base' files for use with axis-base-client calls.

---
+++ app/services/clients/base/request-logger.js
@@ -0,0 +1,53 @@
+const logger = require('../../../utils/logger')(__filename)
+
+module.exports = {
+  logRequestStart: context => {
+    logger.info(`Calling ${context.service} to ${context.description}`, {
+      service: context.service,
+      method: context.method,
+      url: context.url,
+      description: context.description,
+      ...context.additionalLoggingFields
+    })
+  },
+
+  logRequestEnd: (context) => {
+    const responseTime = (context.startTime && new Date() - context.startTime) || context.responseTime
+    logger.info(`${context.method} to ${context.url} ended - elapsed time: ${responseTime} ms`, {
+      service: context.service,
+      method: context.method,
+      url: context.url,
+      description: context.description,
+      response_time: responseTime,
+      status: context.status,
+      ...context.additionalLoggingFields
+    })
+  },
+
+  logRequestFailure: (context) => {
+    let message = `Calling ${context.service} to ${context.description} failed`
+    if (context.retry) {
+      message = message + ' - request will be retried'
+    }
+
+    logger.info(message, {
+      service: context.service,
+      method: context.method,
+      url: context.url,
+      description: context.description,
+      status: context.status,
+      ...context.additionalLoggingFields
+    })
+  },
+
+  logRequestError: (context, error) => {
+    logger.error(`Calling ${context.service} to ${context.description} threw exception`, {
+      service: context.service,
+      method: context.method,
+      url: context.url,
+      description: context.description,
+      error: error,
+      ...context.additionalLoggingFields
+    })
+  }
+}
