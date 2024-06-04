51fbc49721f0899e3a701334ecf43989d34128d6
James Peacock <james.peacock@digital.cabinet-office.gov.uk>
2024-06-04 13:46:51 +0100

    PP-12620 place condition in template

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -3,5 +3,5 @@
 module.exports = {
   DEFAULT_SERVICE_NAME: 'System Generated',
   INVITE_SESSION_COOKIE_NAME: 'register_invite',
-  SHOW_DEGATEWAY_SETTINGS: process.env.SHOW_DEGATEWAY_SETTINGS === 'true'
+  SHOW_DEGATEWAY_SETTINGS: process.env.SHOW_DEGATEWAY_SETTINGS === 'true' && process.env.DEGATEWAY_FLAG === 'true'
 }
