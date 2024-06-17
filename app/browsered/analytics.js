4565d3148bcbd1a91d59fc41ffaaa8b944abab7c
Hugo Jeffreys <hugo.jeffreys@digital.cabinet-office.gov.uk>
2024-06-17 16:41:40 +0100

    PP-12558 Upgrade to GA4

--- app/browsered/analytics.js
+++ app/browsered/analytics.js
@@ -1,45 +1,24 @@
 'use strict'
 
-const cookieFunctions = require('./cookie-functions')
-
-function loadGoogleAnalytics () { /* eslint-disable */
-  (function(i, s, o, g, r, a, m){ i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-    (i[r].q = i[r].q || []).push(arguments) }, i[r].l = 1 * new Date(); a = s.createElement(o),
-    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga')
-}
-
 function setupAnalytics () {
-  // analyticsTrackingId is configurable and set globally in head.njk
-  ga('create', analyticsTrackingId, cookieFunctions.getCookieDomain())
-  ga('set', 'anonymizeIp', true)
-  ga('set', 'displayFeaturesTask', null)
-  ga('set', 'transport', 'beacon')
+  var gtagScript = document.createElement('script')
+  gtagScript.async = true
+  gtagScript.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=G-XE9K05CFFE')
+  document.head.appendChild(gtagScript)
 
-  ga('create', linkedTrackingId, cookieFunctions.getCookieDomain(), 'govuk_shared', { 'allowLinker': true })
-  ga('govuk_shared.require', 'linker')
-  ga('govuk_shared.set', 'anonymizeIp', true)
-  ga('govuk_shared.linker:autoLink', ['www.gov.uk'])
+  window.dataLayer = window.dataLayer || [];
 
-  ga('set', 'location', getPathWithoutPII())
-  ga('govuk_shared.set', 'location', getPathWithoutPII())
+  function gtag() {
+      dataLayer.push(arguments);
+  }
 
-  ga('send', 'pageview')
-  ga('govuk_shared.send', 'pageview')
-}
-
-function getPathWithoutPII() {
-  var openTextFieldsInQueryString = [ 'reference', 'email', 'cardholdername', 'metadatavalue' ]
-  var queryStringKeyValuePairRegex = new RegExp(
-    '(' + openTextFieldsInQueryString.join('|') + ')=([^&]+)',
-    'gi'
-  )
-
-  return document.location.href.replace(queryStringKeyValuePairRegex, '$1=USER_PROVIDED_VALUE_WAS_REMOVED')
+  gtagScript.onload = function () {
+      gtag('js', new Date());
+      gtag('config', 'G-XE9K05CFFE');
+  }
 }
 
 module.exports.init = () => {
-  loadGoogleAnalytics()
   setupAnalytics()
 }
 
