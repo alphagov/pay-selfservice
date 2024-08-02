3233b4bb091651a83e46ec1010a2dde03d97e701
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2024-08-02 17:16:13 +0100

    wip

--- app/controllers/request-psp-test-account/post.controller.js
+++ app/controllers/request-psp-test-account/post.controller.js
@@ -1,32 +1,18 @@
 'use strict'
 
 const { response } = require('../../utils/response')
-const zendeskClient = require('../../services/clients/zendesk.client')
 const getAdminUsersClient = require('../../services/clients/adminusers.client')
+const getConnectorClient = require('../../services/clients/connector.client')
 const logger = require('../../utils/logger')(__filename)
 const { CREATED, NOT_STARTED, REQUEST_SUBMITTED } = require('../../models/psp-test-account-stage')
 const adminUsersClient = getAdminUsersClient()
+const connectorClient = getConnectorClient()
 
 async function submitRequestAndUpdatePspTestAccountStatus (req) {
-  const message = `Service name: ${req.service.name}
-    Service ID: ${req.service.externalId}
-    PSP: 'Stripe'
-    Email address: ${req.user.email}
-    Time: ${new Date().toISOString()}
-    Service created at: ${req.service.createdDate || '(service was created before we captured this date)'}`
-
-  const zendeskOpts = {
-    email: req.user.email,
-    name: req.user.email,
-    type: 'task',
-    subject: `Request for Stripe test account from service (${req.service.name})`,
-    tags: ['govuk_pay_support'],
-    message: message
-  }
-
-  await zendeskClient.createTicket(zendeskOpts)
-  await adminUsersClient.updatePspTestAccountStage(req.service.externalId, REQUEST_SUBMITTED)
+  await connectorClient.requestStripeTestAccount
+  await adminUsersClient.updatePspTestAccountStage(req.service.externalId, CREATED)
   logger.info('Request submitted for Stripe test account')
+  // disable sandbox API keys
 }
 
 module.exports = async function submitRequestForPspTestAccount (req, res, next) {
