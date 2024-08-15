c793a55f11a2a6715f2eb87abd9d178782073ab5
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2024-08-15 12:25:56 +0100

    fix cypress test

--- test/cypress/integration/request-psp-test-account/submit-request-for-psp-test-account.cy.js
+++ test/cypress/integration/request-psp-test-account/submit-request-for-psp-test-account.cy.js
@@ -36,34 +36,16 @@ describe('Request PSP test account: submit request', () => {
     cy.setEncryptedCookies(userExternalId)
   })
 
-  describe.only('PSP test account stage is NOT_STARTED', () => {
+  describe('PSP test account stage is NOT_STARTED', () => {
     it('should submit request for Stripe test account', () => {
       setupStubs('NOT_STARTED', 'NOT_STARTED')
       cy.visit(requestStripeTestAccountUrl)
 
       cy.get('button').contains('Get a Stripe test account').click()
 
-      cy.get('h1').should('contain', 'Thanks for requesting a Stripe test account')
-    })
-  })
-
-  describe('PSP test account stage is REQUEST_SUBMITTED', () => {
-    it('should show "Account already requested" page', () => {
-      setupStubs('NOT_STARTED', 'REQUEST_SUBMITTED')
-      cy.visit(requestStripeTestAccountUrl)
-
-      cy.get('button').contains('Submit request').click()
-      cy.get('h1').should('contain', 'Account already requested')
-    })
-  })
-
-  describe('PSP test account stage is CREATED', () => {
-    it('should show "Stripe test account already set up" page', () => {
-      setupStubs('NOT_STARTED', 'CREATED')
-      cy.visit(requestStripeTestAccountUrl)
-
-      cy.get('button').contains('Submit request').click()
-      cy.get('h1').should('contain', 'Stripe test account already set up')
+      cy.location().should((location) => {
+        expect(location.pathname).to.eq(`/account/${stripeGatewayAccountExternalId}/dashboard`)
+      })
     })
   })
 })
