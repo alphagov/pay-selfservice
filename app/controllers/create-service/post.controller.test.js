799a0d4e361f4a8c5c43942b159f0e47aa83a401
Nathaniel Steers <nathaniel.steers@digital.cabinet-office.gov.uk>
2024-08-27 18:31:42 +0100

    PP-13091 unit test select org type controller

--- app/controllers/create-service/post.controller.test.js
+++ app/controllers/create-service/post.controller.test.js
@@ -3,9 +3,15 @@
 const proxyquire = require('proxyquire')
 const sinon = require('sinon')
 const { expect } = require('chai')
+const formatAccountPathsFor = require('../../utils/format-account-paths-for')
+const paths = require('../../paths')
 const mockResponses = {}
 const mockServiceService = {}
 const mockUserService = {}
+
+const SERVICE_NAME = 'A brand spanking new service name'
+const WELSH_SERVICE_NAME = 'Some Cymraeg new service name'
+
 let req, res, next
 
 const getController = function (mockResponses, mockServiceService, mockUserService) {
@@ -25,41 +31,62 @@ function initialiseSpies () {
 }
 
 describe('Controller: createService, Method: post', () => {
-  describe('when the service name is not empty', () => {
-    mockServiceService.createService = sinon.stub().resolves({ external_id: 'r378y387y8weriyi' })
+  describe('when organisation type is provided', () => {
+    mockServiceService.createService = sinon.stub().resolves({
+      service: {
+        externalId: 'def456'
+      },
+      externalAccountId: 'abc123'
+    })
     mockUserService.assignServiceRole = sinon.stub().resolves()
-    const serviceName = 'A brand spanking new service name'
-    const welshServiceName = 'Some Cymraeg new service name'
 
     before(async () => {
       const addServiceCtrl = getController(mockResponses, mockServiceService, mockUserService)
       req = {
         user: { externalId: '38475y38q4758ow4' },
+        session: {
+          pageData: {
+            createService: {
+              current_name: SERVICE_NAME,
+              current_name_cy: WELSH_SERVICE_NAME,
+              service_selected_cy: true
+            }
+          }
+        },
         body: {
-          'service-name': serviceName,
-          'service-name-cy': welshServiceName,
-          'welsh-service-name-bool': true
-        }
+          'select-org-type': 'central'
+        },
+        flash: sinon.spy()
       }
       initialiseSpies()
       await addServiceCtrl.post(req, res, next)
     })
 
-    it(`should call 'res.redirect' with '/my-service'`, () => {
+    it(`should redirect to newly created service dashboard`, () => {
+      sinon.assert.calledWith(mockServiceService.createService, SERVICE_NAME, WELSH_SERVICE_NAME, 'central')
+      sinon.assert.calledWith(mockUserService.assignServiceRole, '38475y38q4758ow4', 'def456', 'admin')
+      sinon.assert.calledWith(req.flash, 'messages', { state: 'success', icon: '&check;', content: 'We\'ve created your service.' })
       expect(res.redirect.called).to.equal(true)
-      expect(res.redirect.args[0]).to.include('/my-services')
-      sinon.assert.calledWith(mockServiceService.createService, serviceName, welshServiceName, req.user)
+      expect(res.redirect.args[0][0]).to.equal(formatAccountPathsFor(paths.account.dashboard.index, 'abc123'))
     })
   })
 
-  describe('when the service name is not empty, but the update call fails', () => {
+  describe('when organisation type is provided, but create service fails', () => {
     before(async () => {
       mockServiceService.createService = sinon.stub().rejects(new Error('something went wrong'))
       const addServiceCtrl = getController(mockResponses, mockServiceService, mockUserService)
       req = {
+        session: {
+          pageData: {
+            createService: {
+              current_name: SERVICE_NAME,
+              current_name_cy: WELSH_SERVICE_NAME,
+              service_selected_cy: true
+            }
+          }
+        },
         body: {
-          'service-name': 'A brand spanking new service name',
-          'service-name-cy': 'Some Cymraeg new service name'
+          'select-org-type': 'central'
         }
       }
       initialiseSpies()
@@ -72,16 +99,29 @@ describe('Controller: createService, Method: post', () => {
     })
   })
 
-  describe('when the service name is not empty, and the create service succeeds, but the assign service role call fails', () => {
+  describe('when organisation type is provided, and the create service succeeds, but the assign service role call fails', () => {
     before(async () => {
-      mockServiceService.createService = sinon.stub().resolves({ external_id: 'r378y387y8weriyi' })
+      mockServiceService.createService = sinon.stub().resolves({
+        service: {
+          externalId: 'def456'
+        },
+        externalAccountId: 'abc123'
+      })
       mockUserService.assignServiceRole = sinon.stub().rejects(new Error('something went wrong'))
       const addServiceCtrl = getController(mockResponses, mockServiceService, mockUserService)
       req = {
         user: { externalId: '38475y38q4758ow4' },
+        session: {
+          pageData: {
+            createService: {
+              current_name: SERVICE_NAME,
+              current_name_cy: WELSH_SERVICE_NAME,
+              service_selected_cy: true
+            }
+          }
+        },
         body: {
-          'service-name': 'A brand spanking new service name',
-          'service-name-cy': 'Some Cymraeg new service name'
+          'select-org-type': 'central'
         }
       }
       initialiseSpies()
@@ -94,82 +134,37 @@ describe('Controller: createService, Method: post', () => {
     })
   })
 
-  describe('when the service name is empty', () => {
+  describe('when organisation type is not provided', () => {
     before(async () => {
-      mockServiceService.createService = sinon.stub().resolves({ external_id: 'r378y387y8weriyi' })
-      mockUserService.assignServiceRole = sinon.stub().resolves()
+      mockServiceService.createService = sinon.stub()
+      mockUserService.assignServiceRole = sinon.stub()
       const addServiceCtrl = getController(mockResponses, mockServiceService, mockUserService)
       req = {
         user: { externalId: '38475y38q4758ow4' },
-        body: {
-          'service-name': ''
-        }
+        session: {
+          pageData: {
+            createService: {
+              current_name: SERVICE_NAME,
+              current_name_cy: WELSH_SERVICE_NAME,
+              service_selected_cy: true
+            }
+          }
+        },
+        body: {}
       }
       initialiseSpies()
       await addServiceCtrl.post(req, res, next)
     })
 
-    it(`should call 'res.redirect' with a to create service`, () => {
-      expect(res.redirect.called).to.equal(true)
-      expect(res.redirect.args[0]).to.include(`/my-services/create`)
+    it('should redirect back to select org type', () => {
+      sinon.assert.calledWith(res.redirect, paths.serviceSwitcher.create.selectOrgType)
     })
 
-    it(`should set prexisting pageData that includes the 'current_name' and errors`, () => {
-      expect(req.session.pageData.createServiceName).to.have.property('current_name').to.equal(req.body['service-name'])
-      expect(req.session.pageData.createServiceName).to.have.property('errors').to.deep.equal({ service_name: 'Enter a service name' })
-    })
-  })
-
-  describe('when the service name is too long', () => {
-    before(async () => {
-      mockServiceService.createService = sinon.stub().resolves({ external_id: 'r378y387y8weriyi' })
-      mockUserService.assignServiceRole = sinon.stub().resolves()
-      const addServiceCtrl = getController(mockResponses, mockServiceService, mockUserService)
-      req = {
-        user: { externalId: '38475y38q4758ow4' },
-        body: {
-          'service-name': 'Lorem ipsum dolor sit amet, consectetuer adipiscing',
-          'service-name-cy': 'Lorem ipsum dolor sit amet, consectetuer adipiscing',
-          'welsh-service-name-bool': true
-        }
-      }
-      initialiseSpies()
-      await addServiceCtrl.post(req, res, next)
-    })
-
-    it(`should call 'res.redirect' with a to create service`, () => {
-      expect(res.redirect.called).to.equal(true)
-      expect(res.redirect.args[0]).to.include(`/my-services/create`)
-    })
-
-    it(`should set prexisting pageData that includes the 'current_name' and errors`, () => {
-      expect(req.session.pageData.createServiceName).to.have.property('current_name').to.equal(req.body['service-name'])
-      expect(req.session.pageData.createServiceName).to.have.property('errors').to.deep.equal({
-        service_name: 'Service name must be 50 characters or fewer',
-        service_name_cy: 'Welsh service name must be 50 characters or fewer'
-      })
-    })
-  })
-
-  describe('when the Welsh service name is empty', () => {
-    before(async () => {
-      mockServiceService.createService = sinon.stub().resolves({ external_id: 'r378y387y8weriyi' })
-      mockUserService.assignServiceRole = sinon.stub().resolves()
-      const addServiceCtrl = getController(mockResponses, mockServiceService, mockUserService)
-      req = {
-        user: { externalId: '38475y38q4758ow4' },
-        body: {
-          'service-name': 'A brand spanking new service name',
-          'service-name-cy': ''
-        }
-      }
-      initialiseSpies()
-      await addServiceCtrl.post(req, res, next)
-    })
-
-    it(`should call 'res.redirect' with '/my-service'`, () => {
-      expect(res.redirect.called).to.equal(true)
-      expect(res.redirect.args[0]).to.include('/my-services')
+    it('should set error information on the session', () => {
+      expect(req.session.pageData.createService).to.have.property('current_name').to.equal(SERVICE_NAME)
+      expect(req.session.pageData.createService).to.have.property('current_name_cy').to.equal(WELSH_SERVICE_NAME)
+      expect(req.session.pageData.createService).to.have.property('service_selected_cy').to.equal(true)
+      expect(req.session.pageData.createService).to.have.property('errors').to.deep.equal({ organisation_type: 'Organisation type is required' })
     })
   })
 })
---
+++ app/controllers/create-service/select-organisation-type/select-organisation-type.test.js
@@ -0,0 +1,125 @@
+'use strict'
+
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+const { expect } = require('chai')
+const paths = require('../../../paths')
+const mockResponse = {}
+
+const controller = function (mockResponses) {
+  return proxyquire('./select-organisation-type.controller', {
+    '../../../utils/response': mockResponses
+  })
+}
+
+const SERVICE_NAME = 'Garden Gnome Removal Service'
+const WELSH_SERVICE_NAME = 'Gwasanaeth Tynnu Corachod Gardd'
+
+let req, res
+
+describe('Controller: selectOrganisationType, Method: get', () => {
+  describe('when there is no pre-existing pageData', () => {
+    it('should redirect to the create service controller', () => {
+      const selectOrganisationTypeController = controller(mockResponse)
+      res = {
+        redirect: sinon.spy()
+      }
+      selectOrganisationTypeController.get(req, res)
+      sinon.assert.calledWith(res.redirect, paths.serviceSwitcher.create.index)
+    })
+  })
+
+  describe('when there is pre-existing pageData', () => {
+    before(() => {
+      mockResponse.response = sinon.spy()
+      const selectOrganisationTypeController = controller(mockResponse)
+      res = {
+        render: sinon.spy()
+      }
+      req = {
+        session: {
+          pageData: {
+            createService: {
+              current_name: SERVICE_NAME,
+              current_name_cy: WELSH_SERVICE_NAME,
+              errors: {
+                organisation_type: 'Organisation type is required'
+              }
+            }
+          }
+        }
+      }
+      selectOrganisationTypeController.get(req, res)
+    })
+
+    it('should call the response method with appropriate context', () => {
+      expect(mockResponse.response.called).to.equal(true)
+      const responseContext = mockResponse.response.args[0][3]
+      expect(responseContext).to.have.property('current_name').to.equal(SERVICE_NAME)
+      expect(responseContext).to.have.property('current_name_cy').to.equal(WELSH_SERVICE_NAME)
+      expect(responseContext).to.have.property('errors').to.deep.equal({
+        organisation_type: 'Organisation type is required'
+      })
+      expect(responseContext).to.have.property('back_link').to.equal(paths.serviceSwitcher.create.index)
+      expect(responseContext).to.have.property('submit_link').to.equal(paths.serviceSwitcher.create.index)
+    })
+
+    it(`should remove errors pageData from the session`, () => {
+      expect(req.session.pageData.createService).to.not.have.property('errors')
+    })
+  })
+})
+
+describe('Controller: selectOrganisationType, Method: post', () => {
+  describe('when request passes validation', () => {
+    before(async () => {
+      mockResponse.response = sinon.spy()
+      const selectOrganisationTypeController = controller(mockResponse)
+      res = {}
+      req = {
+        body: {
+          'service-name': SERVICE_NAME,
+          'service-name-cy': WELSH_SERVICE_NAME,
+          'welsh-service-name-bool': true
+        }
+      }
+      selectOrganisationTypeController.post(req, res)
+    })
+
+    it('should call the response method with appropriate context and set the session data', () => {
+      expect(mockResponse.response.called).to.equal(true)
+      const createServiceState = req.session.pageData.createService
+      expect(createServiceState).to.have.property('current_name').to.equal(SERVICE_NAME)
+      expect(createServiceState).to.have.property('current_name_cy').to.equal(WELSH_SERVICE_NAME)
+      expect(createServiceState).to.have.property('service_selected_cy').to.equal(true)
+      const responseContext = mockResponse.response.args[0][3]
+      expect(responseContext).to.have.property('back_link').to.equal(paths.serviceSwitcher.create.index)
+      expect(responseContext).to.have.property('submit_link').to.equal(paths.serviceSwitcher.create.index)
+    })
+  })
+  describe('when request fails validation', () => {
+    before(async () => {
+      mockResponse.response = sinon.spy()
+      const selectOrganisationTypeController = controller(mockResponse)
+      res = {
+        redirect: sinon.spy()
+      }
+      req = {
+        body: {
+          'service-name': 'a veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeery long service name oh nooooooooo!!!',
+          'service-name-cy': 'a veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeery long welsh service name oh nooooooooo!!!',
+          'welsh-service-name-bool': true
+        }
+      }
+      selectOrganisationTypeController.post(req, res)
+    })
+
+    it('should redirect to the create service controller and set the session data', () => {
+      expect(mockResponse.response.called).to.equal(false)
+      sinon.assert.calledWith(res.redirect, paths.serviceSwitcher.create.index)
+      const expectedErrors = req.session.pageData.createService.errors
+      expect(expectedErrors).to.have.property('service_name').to.equal('Service name must be 50 characters or fewer')
+      expect(expectedErrors).to.have.property('service_name_cy').to.equal('Welsh service name must be 50 characters or fewer')
+    })
+  })
+})
