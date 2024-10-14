15566b59bee5f91344235f807dff023cb1218b95
Dom Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2024-10-14 15:10:48 +0100

    PP-13174 Update cutoff time for removing link to request stripe test account (#4288)

--- app/controllers/dashboard/dashboard-activity.controller.js
+++ app/controllers/dashboard/dashboard-activity.controller.js
@@ -93,9 +93,10 @@ const displayGoLiveLink = (service, account, user) => {
 }
 
 const displayRequestTestStripeAccountLink = (service, account, user) => {
-  //Since 29/08/2024, services that identified as local govt were automatically allocated Stripe test accounts
-  //So services created after that date don't need a link to create a Stripe test account
-  const serviceCreatedBeforeOrgTypeCaptured = new Date(service.createdDate) <= new Date('2024-08-29');
+  // Since 29/08/2024, services that identified as local govt were automatically allocated Stripe test accounts
+  // So services created after that date don't need a link to create a Stripe test account
+  // this change was deployed in selfservice PR #4256 at approx. 10:08 UTC so this time is used as the cutoff
+  const serviceCreatedBeforeOrgTypeCaptured = new Date(service.createdDate) <= new Date('2024-08-29T10:08:00Z')
   return account.payment_provider === 'sandbox' && service.currentGoLiveStage !== LIVE && serviceCreatedBeforeOrgTypeCaptured &&
     service.currentPspTestAccountStage !== pspTestAccountStage.CREATED &&
     user.hasPermission(service.externalId, 'psp-test-account-stage:update')
