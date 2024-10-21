802f99bb9581566a2fc4a201c0d4a133049fee91
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2024-10-21 10:55:43 +0100

    PP-13135: Email notifications landing page
    Initial email notifications landing page. This commit deals with what the user
    is allowed to see, depending on whether the user is an admin or not.

--- app/controllers/simplified-account/settings/email-notifications/email-notifications.controller.js
+++ app/controllers/simplified-account/settings/email-notifications/email-notifications.controller.js
@@ -1,7 +1,16 @@
 const { response } = require('../../../../utils/response')
+const humaniseEmailMode = require('../../../../utils/humanise-email-mode')
 
 function get (req, res) {
-  return response(req, res, 'simplified-account/settings/email-notifications/index')
+  const service = req.service
+
+  const pageData = {
+    emailCollectionMode: humaniseEmailMode(req.account.email_collection_mode),
+    confirmationEmailEnabled: req.account.email_notifications.PAYMENT_CONFIRMED.enabled,
+    refundEmailEnabled: req.account.email_notifications.REFUND_ISSUED && req.account.email_notifications.REFUND_ISSUED.enabled,
+    isServiceAdmin: req.user.isAdminUserForService(service.externalId)
+  }
+  return response(req, res, 'simplified-account/settings/email-notifications/index', pageData)
 }
 
 module.exports = {
