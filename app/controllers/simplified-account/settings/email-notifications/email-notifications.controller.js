00dc4efbbe5d3285591fb85b56a69b2d638c3d23
oswaldquek <oswaldquek@users.noreply.github.com>
2024-10-22 16:07:41 +0100

    PP-13135: Email notifications landing page (#4297)
    * PP-13135: Email notifications landing page
    
    Initial email notifications landing page. This commit deals with what the user
    is allowed to see, depending on whether the user is an admin or not.
    
    Cypress tests will come in a future commit.

--- app/controllers/simplified-account/settings/email-notifications/email-notifications.controller.js
+++ app/controllers/simplified-account/settings/email-notifications/email-notifications.controller.js
@@ -1,7 +1,17 @@
 const { response } = require('../../../../utils/response')
+const humaniseEmailMode = require('../../../../utils/humanise-email-mode')
 
 function get (req, res) {
-  return response(req, res, 'simplified-account/settings/email-notifications/index')
+  const service = req.service
+  const account = req.account
+
+  const context = {
+    emailCollectionMode: humaniseEmailMode(account.email_collection_mode),
+    confirmationEmailEnabled: account.email_notifications?.PAYMENT_CONFIRMED?.enabled ?? false,
+    refundEmailEnabled: account.email_notifications?.REFUND_ISSUED?.enabled ?? false,
+    isServiceAdmin: req.user.isAdminUserForService(service.externalId)
+  }
+  return response(req, res, 'simplified-account/settings/email-notifications/index', context)
 }
 
 module.exports = {
--- app/controllers/simplified-account/settings/email-notifications/email-notifications.controller.test.js
+++ app/controllers/simplified-account/settings/email-notifications/email-notifications.controller.test.js
@@ -1,1 +1,78 @@
-// TODO
+const { expect } = require('chai')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+const User = require('../../../../models/User.class')
+
+const ACCOUNT_TYPE = 'test'
+const SERVICE_ID = 'service-id-123abc'
+
+let req, res, responseStub, emailNotificationsController
+
+const getController = (stubs = {}) => {
+  return proxyquire('./email-notifications.controller', {
+    '../../../../utils/response': { response: stubs.response }
+  })
+}
+
+const setupTest = () => {
+  responseStub = sinon.spy()
+  emailNotificationsController = getController({
+    response: responseStub
+  })
+  res = {
+    redirect: sinon.spy()
+  }
+  req = {
+    account: {
+      type: ACCOUNT_TYPE,
+      email_collection_mode: 'MANDATORY',
+      email_notifications: {
+        PAYMENT_CONFIRMED: {
+          enabled: true
+        },
+        REFUND_ISSUED: {
+          enabled: true
+        }
+      }
+    },
+    service: {
+      externalId: SERVICE_ID
+    },
+    user: new User({
+      service_roles: [
+        {
+          role: {
+            name: 'admin'
+          },
+          service: {
+            external_id: SERVICE_ID
+          }
+        }
+      ]
+    })
+  }
+  emailNotificationsController.get(req, res)
+}
+
+describe('Controller: settings/email-notifications', () => {
+  describe('get', () => {
+    before(() => setupTest())
+
+    it('should call the response method', () => {
+      expect(responseStub.called).to.be.true // eslint-disable-line
+    })
+
+    it('should pass req, res and template path to the response method', () => {
+      expect(responseStub.args[0]).to.include(req)
+      expect(responseStub.args[0]).to.include(res)
+      expect(responseStub.args[0]).to.include('simplified-account/settings/email-notifications/index')
+    })
+
+    it('should pass context data to the response method', () => {
+      expect(responseStub.args[0][3]).to.have.property('emailCollectionMode').to.equal('On (mandatory)')
+      expect(responseStub.args[0][3]).to.have.property('confirmationEmailEnabled').to.equal(true)
+      expect(responseStub.args[0][3]).to.have.property('refundEmailEnabled').to.equal(true)
+      expect(responseStub.args[0][3]).to.have.property('isServiceAdmin').to.equal(true)
+    })
+  })
+})
