8a3ccbe813d2fbf8f71733030bb6dcdbace17686
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2024-10-22 14:47:33 +0100

    [squash]: revert object keys to camel case

--- app/controllers/simplified-account/settings/email-notifications/email-notifications.controller.js
+++ app/controllers/simplified-account/settings/email-notifications/email-notifications.controller.js
@@ -6,10 +6,10 @@ function get (req, res) {
   const account = req.account
 
   const context = {
-    email_collection_mode: humaniseEmailMode(account.email_collection_mode),
-    confirmation_email_enabled: account.email_notifications.PAYMENT_CONFIRMED.enabled,
-    refund_email_enabled: req.account.email_notifications?.REFUND_ISSUED?.enabled ?? false,
-    is_service_admin: req.user.isAdminUserForService(service.externalId)
+    emailCollectionMode: humaniseEmailMode(account.email_collection_mode),
+    confirmationEmailEnabled: account.email_notifications.PAYMENT_CONFIRMED.enabled,
+    refundEmailEnabled: req.account.email_notifications?.REFUND_ISSUED?.enabled ?? false,
+    isServiceAdmin: req.user.isAdminUserForService(service.externalId)
   }
   return response(req, res, 'simplified-account/settings/email-notifications/index', context)
 }
