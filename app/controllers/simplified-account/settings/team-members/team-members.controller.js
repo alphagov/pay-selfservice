620dcf72348f374dbf047122d7f1d841b90c7452
James Peacock <james.peacock@digital.cabinet-office.gov.uk>
2024-10-29 16:08:07 +0000

    PP-13298 Call adminservice to remove user

--- app/controllers/simplified-account/settings/team-members/team-members.controller.js
+++ app/controllers/simplified-account/settings/team-members/team-members.controller.js
@@ -1,9 +1,10 @@
 const paths = require('../../../../paths')
 const formatSimplifiedAccountPathsFor = require('../../../../utils/simplified-account/format/format-simplified-account-paths-for')
-const { response } = require('../../../../utils/response')
+const { response, renderErrorView } = require('../../../../utils/response')
 const { getServiceUsers, getInvitedUsers, findByExternalId } = require('../../../../services/user.service')
 const { mapTeamMembersByRoles, mapInvitedTeamMembersByRoles } = require('../../../../utils/simplified-account/format/arrange-users-by-role')
 const { roles } = require('../../../../utils/roles')
+const userService = require('../../../../services/user.service')
 
 async function get (req, res, next) {
   const externalServiceId = req.service.externalId
@@ -50,17 +51,44 @@ async function getRemoveUser (req, res, next) {
 
 async function postRemoveUser (req, res, next) {
   console.log(req)
+  const userToRemoveExternalId = req.params.externalUserId
+  const removerExternalId = req.user.externalId
   const externalServiceId = req.service.externalId
   const accountType = req.account.type
+
   if (req.body.confirmRemoveUser === 'no') {
     //redirect to team members page
     res.redirect(formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.teamMembers.index, externalServiceId, accountType))
   }
   if (req.body.confirmRemoveUser === 'yes') {
-    //need to remove the user
-
-    //need to show a notification when redirecting to team members page
-    res.redirect(formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.teamMembers.index, externalServiceId, accountType))
+    if (userToRemoveExternalId === removerExternalId) {
+      renderErrorView(req, res, 'It is not possible to remove yourself from a service', 403)
+      return
+    }
+    //remove the user
+    try {
+      const userToRemove = await userService.findByExternalId(userToRemoveExternalId)
+      await userService.delete(externalServiceId, removerExternalId, userToRemoveExternalId)
+      req.flash('generic', userToRemove.email + ' was successfully removed')
+      res.redirect(formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.teamMembers.index, externalServiceId, accountType))
+    } catch (err) {
+      if (err.errorCode === 404) {
+        const messageUserHasBeenDeleted = {
+          error: {
+            title: 'This person has already been removed',
+            message: 'This person has already been removed by another administrator.'
+          },
+          link: {
+            link: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.teamMembers.index, externalServiceId, accountType),
+            text: 'View all team members'
+          },
+          enable_link: true
+        }
+        response(req, res, 'error-with-link', messageUserHasBeenDeleted)
+      } else {
+        next(err)
+      }
+    }
   }
 }
 
