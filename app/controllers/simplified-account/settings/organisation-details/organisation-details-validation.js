026828df6cc5cca61b484e0427d2c0545f0969ee
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2024-11-15 14:35:40 +0000

    [squash] delete duplicated validator

--- app/controllers/simplified-account/settings/organisation-details/organisation-details-validation.js
+++
@@ -1,9 +0,0 @@
-const { body } = require('express-validator')
-
-const ORGANISATION_NAME_MAX_LENGTH = 100
-
-module.exports.validators = [
-  body('organisation-name').trim()
-    .notEmpty().withMessage('Enter an organisation name')
-    .isLength({ max: ORGANISATION_NAME_MAX_LENGTH }).withMessage(`Organisation name  must be ${ORGANISATION_NAME_MAX_LENGTH} characters or fewer`)
-]
