be1bced2874da16aa8cc8ccddbdafeee27883ecc
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2024-11-19 11:19:45 +0000

    [squash] add ServiceUpdateRequest.class tests back

--- app/models/ServiceUpdateRequest.class.test.js
+++ app/models/ServiceUpdateRequest.class.test.js
@@ -1,50 +1,62 @@
-// 'use strict'
-//
-// const { expect } = require('chai')
-// const { ServiceUpdateRequest } = require('./ServiceUpdateRequest.class')
-//
-// describe('the ServiceUpdateRequest model', () => {
-//   it('should successfully add a "replace" request', () => {
-//     const payload = new ServiceUpdateRequest().replace('foo', 'bar').formatPayload()
-//     expect(payload).to.deep.equal([{
-//       op: 'replace',
-//       path: 'foo',
-//       value: 'bar'
-//     }])
-//   })
-//
-//   it('should successfully add an "add" request', () => {
-//     const payload = new ServiceUpdateRequest().add('foo', 'bar').formatPayload()
-//     expect(payload).to.deep.equal([{
-//       op: 'add',
-//       path: 'foo',
-//       value: 'bar'
-//     }])
-//   })
-//
-//   it('should successfully add multiple requests', () => {
-//     const payload = new ServiceUpdateRequest()
-//       .replace('replace-path-1', 'replace-value-1')
-//       .replace('replace-path-2', 'replace-value-2')
-//       .add('add-path', 'add-value')
-//       .formatPayload()
-//
-//     expect(payload).to.deep.equal([
-//       {
-//         op: 'replace',
-//         path: 'replace-path-1',
-//         value: 'replace-value-1'
-//       },
-//       {
-//         op: 'replace',
-//         path: 'replace-path-2',
-//         value: 'replace-value-2'
-//       },
-//       {
-//         op: 'add',
-//         path: 'add-path',
-//         value: 'add-value'
-//       }
-//     ])
-//   })
-// })
+'use strict'
+
+const { expect } = require('chai')
+const { ServiceUpdateRequest } = require('./ServiceUpdateRequest.class')
+
+describe('the ServiceUpdateRequest model', () => {
+  it('should successfully add a "replace" request', () => {
+    const payload = new ServiceUpdateRequest().replace().currentGoLiveStage('live').formatPayload()
+    expect(payload).to.deep.equal([{
+      op: 'replace',
+      path: 'current_go_live_stage',
+      value: 'live'
+    }])
+  })
+
+  it('should successfully add an "add" request', () => {
+    const payload = new ServiceUpdateRequest().add().currentGoLiveStage('live').formatPayload()
+    expect(payload).to.deep.equal([{
+      op: 'add',
+      path: 'current_go_live_stage',
+      value: 'live'
+    }])
+  })
+
+  it('should successfully add multiple requests', () => {
+    const payload = new ServiceUpdateRequest()
+      .replace().currentGoLiveStage('live')
+      .replace().currentPspTestAccountStage('a-stage')
+      .replace().merchantDetails.name('my-org')
+      .add().merchantDetails.addressPostcode('SW11AA')
+      .add().merchantDetails.addressCountry('GB')
+      .formatPayload()
+
+    expect(payload).to.deep.equal([
+      {
+        op: 'replace',
+        path: 'current_go_live_stage',
+        value: 'live'
+      },
+      {
+        op: 'replace',
+        path: 'current_psp_test_account_stage',
+        value: 'a-stage'
+      },
+      {
+        op: 'replace',
+        path: 'merchant_details/name',
+        value: 'my-org'
+      },
+      {
+        op: 'add',
+        path: 'merchant_details/address_postcode',
+        value: 'SW11AA'
+      },
+      {
+        op: 'add',
+        path: 'merchant_details/address_country',
+        value: 'GB'
+      }
+    ])
+  })
+})
