90160919a7eb37fe3808e510485a775595469528
James Peacock <james.peacock@digital.cabinet-office.gov.uk>
2024-11-22 14:57:22 +0000

    PP-13297 Correct failing tests

--- app/utils/roles.test.js
+++ app/utils/roles.test.js
@@ -25,9 +25,8 @@ describe('roles module', function () {
   it('should return the correct roles for a service without agent-initiated moto', () => {
     const rolesForService = getAvailableRolesForService(false)
 
-    expect(rolesForService).to.have.length(3)
-    expect(rolesForService[0]).to.deep.equal({
-      name: 'admin',
+    expect(rolesForService).to.have.all.keys('admin', 'view-and-refund', 'view-only')
+    expect(rolesForService).to.have.property('admin').to.deep.equal({
       description: 'Administrator',
       explanation: 'They can view transactions, refund payments and manage settings'
     })
@@ -36,9 +35,8 @@ describe('roles module', function () {
   it('should return the correct roles for a service with agent-initiated moto', () => {
     const rolesForService = getAvailableRolesForService(true)
 
-    expect(rolesForService).to.have.length(5)
-    expect(rolesForService[0]).to.deep.equal({
-      name: 'admin',
+    expect(rolesForService).to.have.all.keys('admin', 'view-and-refund', 'view-only', 'view-and-initiate-moto', 'view-refund-and-initiate-moto')
+    expect(rolesForService).to.have.property('admin').to.deep.equal({
       description: 'Administrator',
       explanation: 'They can view transactions, refund payments, take telephone payments and manage settings'
     })
