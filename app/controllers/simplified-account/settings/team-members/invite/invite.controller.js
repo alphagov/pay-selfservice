57edb6305e77f9d42db214dafa218688cdd64807
james-peacock-gds <114914553+james-peacock-gds@users.noreply.github.com>
2024-11-26 15:50:40 +0000

    PP-13296 invite post controller (#4352)
    * PP-13296 invite post controller

--- app/controllers/simplified-account/settings/team-members/invite/invite.controller.js
+++ app/controllers/simplified-account/settings/team-members/invite/invite.controller.js
@@ -2,21 +2,60 @@ const { response } = require('@utils/response')
 const { getAvailableRolesForService } = require('@utils/roles')
 const formatSimplifiedAccountPathsFor = require('@utils/simplified-account/format/format-simplified-account-paths-for')
 const paths = require('@root/paths')
+const { body, validationResult } = require('express-validator')
+const formatValidationErrors = require('@utils/simplified-account/format/format-validation-errors')
+const userService = require('@services/user.service')
 
 async function get (req, res) {
   const serviceId = req.service.externalId
   const accountType = req.account.type
-  const serviceHasAgentInitiatedMotoEnabled = req.service.agentInitiatedMotoEnabled ?? false
-  const availableRoles = getAvailableRolesForService(serviceHasAgentInitiatedMotoEnabled)
   response(req, res, 'simplified-account/settings/team-members/invite',
     {
-      availableRoles,
+      availableRoles: getAvailableRolesForService(req.service.agentInitiatedMotoEnabled ?? false),
       backLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.teamMembers.index, serviceId, accountType)
     })
 }
 
 async function post (req, res, next) {
-  // TODO
+  const externalServiceId = req.service.externalId
+  const accountType = req.account.type
+  const adminUserExternalId = req.user.externalId
+  const invitedUserEmail = req.body.invitedUserEmail
+  const invitedUserRole = req.body.invitedUserRole
+  const teamMembersIndexPath = formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.teamMembers.index, externalServiceId, accountType)
+
+  const validations = [
+    body('invitedUserRole')
+      .not().isEmpty().withMessage('Select a permission level'),
+    body('invitedUserEmail')
+      .isEmail().withMessage('Enter a valid email address')
+  ]
+  await Promise.all(validations.map(validation => validation.run(req)))
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    const formattedErrors = formatValidationErrors(errors)
+
+    return response(req, res, 'simplified-account/settings/team-members/invite', {
+      errors: {
+        summary: formattedErrors.errorSummary,
+        formErrors: formattedErrors.formErrors
+      },
+      invitedUserEmail,
+      checkedRole: invitedUserRole,
+      availableRoles: getAvailableRolesForService(req.service.agentInitiatedMotoEnabled ?? false),
+      backLink: teamMembersIndexPath
+    })
+  }
+
+  try {
+    await userService.createInviteToJoinService(invitedUserEmail, adminUserExternalId, externalServiceId, invitedUserRole)
+    req.flash('messages', { state: 'success', icon: '&check;', heading: 'Team member invitation sent to ' + req.body.invitedUserEmail })
+    res.redirect(teamMembersIndexPath)
+  } catch (err) {
+    // TODO - handle 412 (user already invited or already a team member)
+    next(err)
+  }
 }
 
 module.exports = {
--- app/controllers/simplified-account/settings/team-members/invite/invite.controller.test.js
+++ app/controllers/simplified-account/settings/team-members/invite/invite.controller.test.js
@@ -1,43 +1,100 @@
-const ControllerTestBuilder = require('@test/test-helpers/simplified-account/controllers/ControllerTestBuilder.class')
 const sinon = require('sinon')
 const { expect } = require('chai')
-
-const mockResponse = sinon.spy()
+const proxyquire = require('proxyquire')
+const User = require('@models/User.class')
+const userFixtures = require('@test/fixtures/user.fixtures')
+const paths = require('@root/paths')
 
 const ACCOUNT_TYPE = 'test'
 const SERVICE_ID = 'service-id-123abc'
 
-const {
-  req,
-  res,
-  call
-} = new ControllerTestBuilder('@controllers/simplified-account/settings/team-members/invite/invite.controller')
-  .withServiceExternalId(SERVICE_ID)
-  .withAccountType(ACCOUNT_TYPE)
-  .withStubs({
-    '@utils/response': { response: mockResponse }
+let req, res, responseStub, createInviteToJoinServiceStub, inviteController
+
+const getController = (stubs = {}) => {
+  return proxyquire('./invite.controller', {
+    '@utils/response': { response: stubs.response, renderErrorView: stubs.renderErrorView },
+    '@services/user.service':
+      { createInviteToJoinService: stubs.createInviteToJoinService }
   })
-  .build()
+}
+
+const adminUser = new User(userFixtures.validUserResponse({
+  external_id: 'user-id-for-admin-user',
+  email: 'admin-user@users.gov.uk',
+  service_roles: {
+    service: {
+      service: { external_id: SERVICE_ID },
+      role: { name: 'admin' }
+    }
+  }
+}))
+
+const setupTest = (method, additionalReqProps = {}) => {
+  responseStub = sinon.spy()
+  createInviteToJoinServiceStub = sinon.stub()
+
+  inviteController = getController({
+    response: responseStub,
+    createInviteToJoinService: createInviteToJoinServiceStub
+  })
+  res = {
+    redirect: sinon.spy()
+  }
+  req = {
+    user: adminUser,
+    service: {
+      externalId: SERVICE_ID
+    },
+    account: {
+      type: ACCOUNT_TYPE
+    },
+    ...additionalReqProps
+  }
+  inviteController[method](req, res)
+}
 
 describe('Controller: settings/team-members/invite', () => {
   describe('get', () => {
-    before(() => {
-      call('get')
-    })
+    before(() => setupTest('get'))
 
     it('should call the response method', () => {
-      expect(mockResponse.called).to.be.true // eslint-disable-line
+      expect(responseStub.called).to.be.true // eslint-disable-line
     })
 
     it('should pass req, res and template path to the response method', () => {
-      expect(mockResponse.args[0][0]).to.deep.equal(req)
-      expect(mockResponse.args[0][1]).to.deep.equal(res)
-      expect(mockResponse.args[0][2]).to.equal('simplified-account/settings/team-members/invite')
+      expect(responseStub.args[0][0]).to.deep.equal(req)
+      expect(responseStub.args[0][1]).to.deep.equal(res)
+      expect(responseStub.args[0][2]).to.equal('simplified-account/settings/team-members/invite')
     })
 
     it('should pass context data to the response method', () => {
-      expect(mockResponse.args[0][3]).to.have.property('availableRoles').to.have.length(3)
-      expect(mockResponse.args[0][3]).to.have.property('backLink').to.equal('/simplified/service/service-id-123abc/account/test/settings/team-members')
+      expect(responseStub.args[0][3]).to.have.property('availableRoles').to.have.length(3)
+      expect(responseStub.args[0][3]).to.have.property('backLink').to.equal('/simplified/service/service-id-123abc/account/test/settings/team-members')
+    })
+  })
+
+  describe('post', () => {
+    describe('success', () => {
+      before(() => setupTest('post',
+        {
+          body: { invitedUserEmail: 'user-to-invite@users.gov.uk', invitedUserRole: 'view-only' },
+          flash: sinon.stub()
+        }
+      ))
+
+      it('should call adminusers to send an invite', () => {
+        expect(createInviteToJoinServiceStub.calledWith('user-to-invite@users.gov.uk', adminUser.externalId, SERVICE_ID, 'view-only')).to.be.true // eslint-disable-line
+      })
+
+      it('should redirect to the team members index page', () => {
+        sinon.assert.calledWith(req.flash, 'messages', {
+          state: 'success',
+          icon: '&check;',
+          heading: 'Team member invitation sent to user-to-invite@users.gov.uk'
+        })
+        expect(res.redirect.calledOnce).to.be.true // eslint-disable-line
+        expect(res.redirect.args[0][0]).to.include(paths.simplifiedAccount.settings.teamMembers.index)
+      })
     })
   })
 })
