9c770fd5f4541f8e9a9a6cc86718f91f664b44a8
James Peacock <james.peacock@digital.cabinet-office.gov.uk>
2024-12-06 11:31:53 +0000

    PP-13312 [squash] refactor format-card-types

--- app/utils/simplified-account/format/format-card-types.js
+++ app/utils/simplified-account/format/format-card-types.js
@@ -1,46 +1,52 @@
 const formatLabel = (card) => {
   if (card.brand === 'visa' || card.brand === 'master-card') {
     return `${card.label} ${card.type.toLowerCase()}`
-  } else {
-    return card.brand === 'jcb' ? card.label.toUpperCase() : card.label
+  }
+  return card.brand === 'jcb' ? card.label.toUpperCase() : card.label
+}
+
+const createCardTypeChecklistItem = (card, acceptedCards) => {
+  return {
+    value: card.id,
+    text: formatLabel(card),
+    checked: acceptedCards.filter(accepted => accepted.id === card.id).length !== 0,
+    requires3ds: card.requires3ds
   }
 }
 
-const formatCardTypesForAdminTemplate = (allCards, acceptedCards, account) => {
-  const cardDataChecklistItem = (card) => {
+const disableCheckboxIf3dsRequiredButNotEnabled = (cardTypeChecklistItem, accountType, accountRequires3ds) => {
+  if (cardTypeChecklistItem.requires3ds && !accountRequires3ds) {
     return {
-      value: card.id,
-      text: formatLabel(card),
-      checked: acceptedCards.filter(accepted => accepted.id === card.id).length !== 0,
-      requires3ds: card.requires3ds
-    }
-  }
-  const disableCheckboxIf3dsRequiredButNotEnabled = (cardTypeChecklistItem) => {
-    if (cardTypeChecklistItem.requires3ds && !account.requires3ds) {
-      cardTypeChecklistItem.disabled = true
-      cardTypeChecklistItem.hint = {
-        html: account.type === 'test' ? `${cardTypeChecklistItem.text} is not available on test accounts` : `${cardTypeChecklistItem.text} cannot be used because 3D Secure is not available. Please contact support`
+      ...cardTypeChecklistItem,
+      disabled: true,
+      hint: {
+        html: accountType === 'test' ? `${cardTypeChecklistItem.text} is not available on test accounts` : `${cardTypeChecklistItem.text} cannot be used because 3D Secure is not available. Please contact support`
       }
     }
-    return cardTypeChecklistItem
   }
-  const addHintForAmexAndUnionpay = (cardTypeChecklistItem) => {
-    if (['American Express', 'Union Pay'].includes(cardTypeChecklistItem.text)) {
-      if (account.paymentProvider === 'worldpay') {
-        cardTypeChecklistItem.hint = {
-          html: 'You must have already enabled this with Worldpay'
-        }
+  return cardTypeChecklistItem
+}
+
+const addHintForAmexAndUnionpayIfWorldpay = (cardTypeChecklistItem, paymentProvider) => {
+  if (['American Express', 'Union Pay'].includes(cardTypeChecklistItem.text) && paymentProvider === 'worldpay') {
+    return {
+      ...cardTypeChecklistItem,
+      hint: {
+        html: 'You must have already enabled this with Worldpay'
       }
     }
-    return cardTypeChecklistItem
   }
+  return cardTypeChecklistItem
+}
+
+const formatCardTypesForAdminTemplate = (allCards, acceptedCards, account) => {
   const debitCardChecklistItems = allCards.filter(card => card.type === 'DEBIT')
-    .map(card => cardDataChecklistItem(card))
-    .map(cardTypeChecklistItem => disableCheckboxIf3dsRequiredButNotEnabled(cardTypeChecklistItem))
+    .map(card => createCardTypeChecklistItem(card, acceptedCards))
+    .map(cardTypeChecklistItem => disableCheckboxIf3dsRequiredButNotEnabled(cardTypeChecklistItem, account.type, account.requires3ds))
 
   const creditCardChecklistItems = allCards.filter(card => card.type === 'CREDIT')
-    .map(card => cardDataChecklistItem(card))
-    .map(cardTypeChecklistItem => addHintForAmexAndUnionpay(cardTypeChecklistItem))
+    .map(card => createCardTypeChecklistItem(card, acceptedCards))
+    .map(cardTypeChecklistItem => addHintForAmexAndUnionpayIfWorldpay(cardTypeChecklistItem, account.paymentProvider))
 
   return { debitCards: debitCardChecklistItems, creditCards: creditCardChecklistItems }
 }
