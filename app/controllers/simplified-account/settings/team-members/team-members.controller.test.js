5e9c2ece30199ca2ea0dbdda3cfba1e01279f9ba
James Peacock <james.peacock@digital.cabinet-office.gov.uk>
2024-12-11 16:46:35 +0000

    BAU [squash] respond to comments

--- app/controllers/simplified-account/settings/team-members/team-members.controller.test.js
+++ app/controllers/simplified-account/settings/team-members/team-members.controller.test.js
@@ -83,6 +83,47 @@ describe('Controller: settings/team-members', () => {
       expect(mockResponse.args[0][3]).to.have.property('inviteTeamMemberLink')
         .to.equal('/simplified/service/service-id-123abc/account/test/settings/team-members/invite')
       expect(mockResponse.args[0][3]).to.have.property('numberInvitedMembers').to.equal(2)
+      expect(mockResponse.args[0][3]).to.have.property('isServiceAdmin').to.be.true // eslint-disable-line no-unused-expressions
+      expect(mockResponse.args[0][3]).to.have.property('roles').to.have.keys('admin', 'view-and-initiate-moto', 'view-and-refund', 'view-only', 'view-refund-and-initiate-moto')
+    })
+  })
+
+  describe('get', () => {
+    before(() => {
+      nextRequest({
+        user: adminUser
+      })
+      call('get')
+    })
+
+    it('should call the response method', () => {
+      expect(mockGetServiceUsers.called).to.be.true // eslint-disable-line
+      expect(mockResponse.called).to.be.true // eslint-disable-line
+      expect(mockResponse.called).to.be.true // eslint-disable-line
+    })
+
+    it('should pass req, res and template path to the response method', () => {
+      expect(mockResponse.args[0][0].user).to.deep.include(adminUser)
+      expect(mockResponse.args[0]).to.include(res)
+      expect(mockResponse.args[0]).to.include('simplified-account/settings/team-members/index')
+    })
+
+    it('should pass context data to the response method', () => {
+      expect(mockResponse.args[0][3]).to.have.property('teamMembers').to.have.property('admin').to.have.length(1)
+      expect(mockResponse.args[0][3].teamMembers.admin[0]).to.have.property('externalId').to.equal('user-id-for-admin-user')
+      expect(mockResponse.args[0][3].teamMembers).to.have.property('view-only').to.have.length(1)
+      expect(mockResponse.args[0][3].teamMembers['view-only'][0]).to.have.property('externalId').to.equal('user-id-for-view-only-user')
+
+      expect(mockResponse.args[0][3]).to.have.property('invitedTeamMembers').to.have.property('admin').to.have.length(1)
+      expect(mockResponse.args[0][3].invitedTeamMembers.admin[0]).to.have.property('email').to.equal('invited-admin-user@user.gov.uk')
+      expect(mockResponse.args[0][3].invitedTeamMembers).to.have.property('view-only').to.have.length(1)
+      expect(mockResponse.args[0][3].invitedTeamMembers['view-only'][0]).to.have.property('email').to.equal('invited-view-only-user@user.gov.uk')
+
+      expect(mockResponse.args[0][3]).to.have.property('inviteTeamMemberLink')
+        .to.equal('/simplified/service/service-id-123abc/account/test/settings/team-members/invite')
+      expect(mockResponse.args[0][3]).to.have.property('numberInvitedMembers').to.equal(2)
+      expect(mockResponse.args[0][3]).to.have.property('isServiceAdmin').to.be.true // eslint-disable-line no-unused-expressions
+      expect(mockResponse.args[0][3]).to.have.property('roles').to.have.keys('admin', 'view-and-initiate-moto', 'view-and-refund', 'view-only', 'view-refund-and-initiate-moto')
     })
   })
 })
