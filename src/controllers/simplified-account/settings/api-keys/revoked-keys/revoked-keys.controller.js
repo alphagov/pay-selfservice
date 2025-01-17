8934dbe7b93b482f7c1c27910f1aa6c56ec6b8c0
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2025-01-17 09:32:47 +0000

    PP-12395: Cypress test for the Show revoked API keys page

--- src/controllers/simplified-account/settings/api-keys/revoked-keys/revoked-keys.controller.js
+++ src/controllers/simplified-account/settings/api-keys/revoked-keys/revoked-keys.controller.js
@@ -2,9 +2,13 @@ const { response } = require('@utils/response')
 const { getRevokedKeys } = require('@services/api-keys.service')
 const formatSimplifiedAccountPathsFor = require('@utils/simplified-account/format/format-simplified-account-paths-for')
 const paths = require('@root/paths')
+const { NotFoundError } = require('@root/errors')
 
-async function get (req, res) {
+async function get (req, res, next) {
   const revokedKeys = await getRevokedKeys(req.account.id)
+  if (revokedKeys.length === 0) {
+    return next(new NotFoundError('Refusing to render page as there are no revoked keys on the gateway account'))
+  }
   return response(req, res, 'simplified-account/settings/api-keys/revoked-keys', {
     tokens: revokedKeys,
     backLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.apiKeys.index, req.service.externalId, req.account.type)
