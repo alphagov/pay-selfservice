b048fa191d8e006dccf05186ebc801b842fb8760
Natalie.Carey <natalie.carey@digital.cabinet-office.gov.uk>
2025-01-24 15:04:00 +0000

    PP-12416: Added tests for google and apple pay, specifying IDs to avoid auto-generated IDs

--- test/cypress/integration/simplified-account/service-settings/card-payments/updates.cy.js
+++ test/cypress/integration/simplified-account/service-settings/card-payments/updates.cy.js
@@ -5,7 +5,11 @@ const {
   SERVICE_EXTERNAL_ID,
   ACCOUNT_TYPE
 } = require('@test/cypress/integration/simplified-account/service-settings/card-payments/util')
-const { patchUpdateDefaultBillingAddressCountrySuccess, patchUpdateCollectBillingAddressSuccess } = require('@test/cypress/stubs/service-stubs')
+const {
+  patchUpdateDefaultBillingAddressCountrySuccess,
+  patchUpdateCollectBillingAddressSuccess
+} = require('@test/cypress/stubs/service-stubs')
+const { patchAccountUpdateApplePaySuccess, patchAccountUpdateGooglePaySuccess } = require('@test/cypress/stubs/gateway-account-stubs')
 
 const baseUrl = `/simplified/service/${SERVICE_EXTERNAL_ID}/account/${ACCOUNT_TYPE}/settings/card-payments`
 
@@ -16,7 +20,9 @@ describe('Card payment updates', () => {
   })
 
   it('should allow update of Collect billing address', () => {
-    setupStubs()
+    setupStubs({
+      collectBillingAddress: true
+    })
     cy.task('setupStubs', [
       patchUpdateCollectBillingAddressSuccess({
         gatewayAccountId: GATEWAY_ACCOUNT_ID,
@@ -26,7 +32,7 @@ describe('Card payment updates', () => {
     ])
     cy.visit(baseUrl + '/collect-billing-address')
     cy.get('h1').should('contain.text', 'Billing address')
-    cy.get('input#collectBillingAddress-2').click()
+    cy.get('input#collect-billing-address-off').click()
     cy.contains('button', 'Save changes').click()
     cy.get('#govuk-notification-banner-title').should('contain.text', 'Success')
   })
@@ -44,30 +50,36 @@ describe('Card payment updates', () => {
     ])
     cy.visit(baseUrl + '/default-billing-address-country')
     cy.get('h1').should('contain.text', 'Set United Kingdom as the default billing address country')
-    cy.get('input#defaultBillingAddress').click()
+    cy.get('input#default-billing-address-on').click()
     cy.contains('button', 'Save changes').click()
     cy.get('#govuk-notification-banner-title').should('contain.text', 'Success')
   })
 
-  // it('should allow update of Apple Pay', () => {
-  //   setupStubs()
-  //   cy.visit(baseUrl + '/apple-pay')
-  //   cy.get('h1').should('contain.text', 'Apple Pay')
-  // })
-  //
-  // it('should allow update of Google Pay', () => {
-  //   setupStubs()
-  //   cy.visit(baseUrl + '/google-pay')
-  //   cy.get('h1').should('contain.text', 'Google Pay')
-  // })
-})
-
-function setupServicePatchStub () {
-  cy.task('setupStubs', [
-    patchUpdateDefaultBillingAddressCountrySuccess({
-      gatewayAccountId: GATEWAY_ACCOUNT_ID,
-      serviceExternalId: SERVICE_EXTERNAL_ID,
-      country: 'GB'
+  it('should allow update of Apple Pay', () => {
+    setupStubs({
+      allowApplePay: false
     })
-  ])
-}
+    cy.task('setupStubs', [
+      patchAccountUpdateApplePaySuccess(GATEWAY_ACCOUNT_ID, true)
+    ])
+    cy.visit(baseUrl + '/apple-pay')
+    cy.get('h1').should('contain.text', 'Apple Pay')
+    cy.get('input#apple-pay-on').click()
+    cy.contains('button', 'Save changes').click()
+    cy.get('#govuk-notification-banner-title').should('contain.text', 'Success')
+  })
+
+  it('should allow update of Google Pay', () => {
+    setupStubs({
+      allowGooglePay: false
+    })
+    cy.task('setupStubs', [
+      patchAccountUpdateGooglePaySuccess(GATEWAY_ACCOUNT_ID, true)
+    ])
+    cy.visit(baseUrl + '/google-pay')
+    cy.get('h1').should('contain.text', 'Google Pay')
+    cy.get('input#google-pay-on').click()
+    cy.contains('button', 'Save changes').click()
+    cy.get('#govuk-notification-banner-title').should('contain.text', 'Success')
+  })
+})
