e4b9ee73a6506b62997463834cbeabe6e9338b05
James Peacock <james.peacock@digital.cabinet-office.gov.uk>
2025-01-31 16:44:14 +0000

    PP-13136 updates in response to comments

--- src/utils/simplified-account/validation/webhook.schema.test.js
+++ src/utils/simplified-account/validation/webhook.schema.test.js
@@ -3,9 +3,9 @@ const { webhookSchema } = require('./webhook.schema')
 const { validationResult } = require('express-validator')
 
 describe('Webhook Validation', () => {
-  let BASE_REQ
+  let VALID_REQUEST
   beforeEach(() => {
-    BASE_REQ = {
+    VALID_REQUEST = {
       body: {
         callbackUrl: 'https://www.gov.uk',
         description: 'A valid webhook description',
@@ -16,13 +16,13 @@ describe('Webhook Validation', () => {
 
   describe('Callback url validation', () => {
     it('should pass with a valid callback url', async () => {
-      await webhookSchema.callbackUrl.validate.run(BASE_REQ)
-      expect(validationResult(BASE_REQ).isEmpty()).to.be.true // eslint-disable-line
+      await webhookSchema.callbackUrl.validate.run(VALID_REQUEST)
+      expect(validationResult(VALID_REQUEST).isEmpty()).to.be.true // eslint-disable-line
     })
 
     it('should fail when callback url is empty', async () => {
       const invalidReq = {
-        body: Object.assign({}, BASE_REQ.body, { callbackUrl: '' })
+        body: Object.assign({}, VALID_REQUEST.body, { callbackUrl: '' })
       }
       await webhookSchema.callbackUrl.validate.run(invalidReq)
       const errors = validationResult(invalidReq)
@@ -31,7 +31,7 @@ describe('Webhook Validation', () => {
 
     it('should fail when callback url is http', async () => {
       const invalidReq = {
-        body: Object.assign({}, BASE_REQ.body, { callbackUrl: 'http://www.gov.uk' })
+        body: Object.assign({}, VALID_REQUEST.body, { callbackUrl: 'http://www.gov.uk' })
       }
       await webhookSchema.callbackUrl.validate.run(invalidReq)
       const errors = validationResult(invalidReq)
@@ -40,7 +40,7 @@ describe('Webhook Validation', () => {
 
     it('should fail when callback url is malformed', async () => {
       const invalidReq = {
-        body: Object.assign({}, BASE_REQ.body, { callbackUrl: 'https://malformed-url' })
+        body: Object.assign({}, VALID_REQUEST.body, { callbackUrl: 'https://malformed-url' })
       }
       await webhookSchema.callbackUrl.validate.run(invalidReq)
       const errors = validationResult(invalidReq)
@@ -50,13 +50,13 @@ describe('Webhook Validation', () => {
 
   describe('description validation', () => {
     it('should pass with a valid description', async () => {
-      await webhookSchema.description.validate.run(BASE_REQ)
-      expect(validationResult(BASE_REQ).isEmpty()).to.be.true // eslint-disable-line
+      await webhookSchema.description.validate.run(VALID_REQUEST)
+      expect(validationResult(VALID_REQUEST).isEmpty()).to.be.true // eslint-disable-line
     })
 
     it('should fail when description is empty', async () => {
       const invalidReq = {
-        body: Object.assign({}, BASE_REQ.body, { description: '' })
+        body: Object.assign({}, VALID_REQUEST.body, { description: '' })
       }
       await webhookSchema.description.validate.run(invalidReq)
       const errors = validationResult(invalidReq)
@@ -65,7 +65,7 @@ describe('Webhook Validation', () => {
 
     it('should fail when description is too long', async () => {
       const invalidReq = {
-        body: Object.assign({}, BASE_REQ.body, { description: 'This 41 character description is too long' })
+        body: Object.assign({}, VALID_REQUEST.body, { description: 'This 41 character description is too long' })
       }
       await webhookSchema.description.validate.run(invalidReq)
       const errors = validationResult(invalidReq)
@@ -75,16 +75,16 @@ describe('Webhook Validation', () => {
 
   describe('subscriptions validation', () => {
     it('should pass with more than one subscriptions', async () => {
-      await webhookSchema.subscriptions.validate.run(BASE_REQ)
-      expect(validationResult(BASE_REQ).isEmpty()).to.be.true // eslint-disable-line
+      await webhookSchema.subscriptions.validate.run(VALID_REQUEST)
+      expect(validationResult(VALID_REQUEST).isEmpty()).to.be.true // eslint-disable-line
     })
 
     it('should pass with one subscription', async () => {
-      const validReq = {
-        body: Object.assign({}, BASE_REQ.body, { subscriptions: 'card_payment_succeeded' })
+      const validRequestWithOneSubscription = {
+        body: Object.assign({}, VALID_REQUEST.body, { subscriptions: 'card_payment_succeeded' })
       }
-      await webhookSchema.subscriptions.validate.run(validReq)
-      expect(validationResult(BASE_REQ).isEmpty()).to.be.true // eslint-disable-line
+      await webhookSchema.subscriptions.validate.run(validRequestWithOneSubscription)
+      expect(validationResult(validRequestWithOneSubscription).isEmpty()).to.be.true // eslint-disable-line
     })
 
     it('should fail with no subscriptions', async () => {
@@ -99,5 +99,4 @@ describe('Webhook Validation', () => {
       expect(errors.array()[0].msg).to.equal('Select at least one payment event')
     })
   })
-
 })
