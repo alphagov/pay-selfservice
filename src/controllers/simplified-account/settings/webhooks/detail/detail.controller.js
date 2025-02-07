03fd98dae06eb3e71e4702b2bc2874e0945b7fba
James Peacock <james.peacock@digital.cabinet-office.gov.uk>
2025-02-07 16:12:51 +0000

    WIP - PP-13521 webhooks detail page

--- src/controllers/simplified-account/settings/webhooks/detail/detail.controller.js
+++ src/controllers/simplified-account/settings/webhooks/detail/detail.controller.js
@@ -1,11 +1,16 @@
 const paths = require('@root/paths')
 const { response } = require('@utils/response')
+const webhooksService = require('@services/webhooks.service')
 const formatSimplifiedAccountPathsFor = require('@utils/simplified-account/format/format-simplified-account-paths-for')
+const { constants } = require('@govuk-pay/pay-js-commons')
 
 async function get (req, res) {
+  const webhook = await webhooksService.getWebhook(req.params.webhookId, req.service.externalId, req.account.id)
+  console.log('webhook: ------ ' + JSON.stringify(webhook))
   response(req, res, 'simplified-account/settings/webhooks/detail', {
-    description: 'Description from the webhook',
-    backLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.webhooks.index, req.service.externalId, req.account.type)
+    webhook,
+    eventTypes: constants.webhooks.humanReadableSubscriptions,
+    backToWebhooksLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.webhooks.index, req.service.externalId, req.account.type)
   })
 }
 
