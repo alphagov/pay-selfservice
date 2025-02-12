13e4969fbf24b09c88f0a5438c0480e64cfda19c
James Peacock <james.peacock@digital.cabinet-office.gov.uk>
2025-02-12 15:43:29 +0000

    PP-13521 Align tag and add signing secret

--- src/controllers/simplified-account/settings/webhooks/detail/detail.controller.js
+++ src/controllers/simplified-account/settings/webhooks/detail/detail.controller.js
@@ -6,9 +6,11 @@ const { constants } = require('@govuk-pay/pay-js-commons')
 
 async function get (req, res) {
   const webhook = await webhooksService.getWebhook(req.params.webhookId, req.service.externalId, req.account.id)
-  console.log('webhook: ------ ' + JSON.stringify(webhook))
+  const signingSecret = await webhooksService.getSigningSecret(req.params.webhookId, req.service.externalId, req.account.id)
+  // TODO get webhook messages and pass them into the template
   response(req, res, 'simplified-account/settings/webhooks/detail', {
     webhook,
+    signingSecret,
     eventTypes: constants.webhooks.humanReadableSubscriptions,
     backToWebhooksLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.webhooks.index, req.service.externalId, req.account.type)
   })
