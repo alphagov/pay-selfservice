799d0897126bb48eac26d63e245f2da1a089ca07
james-peacock-gds <114914553+james-peacock-gds@users.noreply.github.com>
2025-02-13 15:49:39 +0000

    Pp 13521 webhook details page (#4459)
    PP-13521 webhooks detail page

--- src/controllers/simplified-account/settings/webhooks/detail/detail.controller.js
+++ src/controllers/simplified-account/settings/webhooks/detail/detail.controller.js
@@ -1,11 +1,18 @@
 const paths = require('@root/paths')
 const { response } = require('@utils/response')
+const webhooksService = require('@services/webhooks.service')
 const formatSimplifiedAccountPathsFor = require('@utils/simplified-account/format/format-simplified-account-paths-for')
+const { constants } = require('@govuk-pay/pay-js-commons')
 
 async function get (req, res) {
+  const webhook = await webhooksService.getWebhook(req.params.webhookId, req.service.externalId, req.account.id)
+  const signingSecret = await webhooksService.getSigningSecret(req.params.webhookId, req.service.externalId, req.account.id)
+  // TODO get webhook messages and pass them into the template
   response(req, res, 'simplified-account/settings/webhooks/detail', {
-    description: 'Description from the webhook',
-    backLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.webhooks.index, req.service.externalId, req.account.type)
+    webhook,
+    signingSecret,
+    eventTypes: constants.webhooks.humanReadableSubscriptions,
+    backToWebhooksLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.webhooks.index, req.service.externalId, req.account.type)
   })
 }
 
