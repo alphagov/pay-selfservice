a9935f106156800143a16b7f58a2ab5cf790af08
Kerr Rainey <kerr.rainey@digital.cabinet-office.gov.uk>
2025-02-25 15:22:47 +0000

    PP-13521: Add pagination to webhook details events
    Implement pagination control to the webhook detail view. We have done this by building the pagination object in the controller and passing that the nunjucks view.

--- src/controllers/simplified-account/settings/webhooks/detail/detail.controller.js
+++ src/controllers/simplified-account/settings/webhooks/detail/detail.controller.js
@@ -8,9 +8,19 @@ async function get (req, res) {
   const status = (req.query.deliveryStatus === 'successful' || req.query.deliveryStatus === 'failed')
     ? req.query.deliveryStatus
     : undefined
-  const page = req.query.page || 1
+  const page = parseInt(req.query.page || 1)
+  const baseUrl = formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.webhooks.detail, req.service.externalId, req.account.type, req.params.webhookId)
+  if (Number.isNaN(page) || page < 1) {
+    res.redirect(baseUrl)
+  }
+
   const webhook = await webhooksService.getWebhook(req.params.webhookId, req.service.externalId, req.account.id)
   const messages = await webhooksService.getWebhookMessages(req.params.webhookId, { page, ...status && { status } })
+  const totalPages = Math.ceil(messages.total / 10)
+  if (totalPages > 0 && page > totalPages) {
+    res.redirect(baseUrl)
+  }
+
   const webhookEvents = messages.results.map(result => ({
     resourceId: result.resource_id,
     eventType: result.event_type,
@@ -20,14 +30,39 @@ async function get (req, res) {
   }))
 
   const signingSecret = await webhooksService.getSigningSecret(req.params.webhookId, req.service.externalId, req.account.id)
+  const deliveryStatus = status || 'all'
+  const paginationDetails = getPaginationDetails(page, messages.total, baseUrl, deliveryStatus)
+
   response(req, res, 'simplified-account/settings/webhooks/detail', {
     webhook,
     signingSecret,
-    deliveryStatus: status || 'all',
+    deliveryStatus,
     eventTypes: constants.webhooks.humanReadableSubscriptions,
     backToWebhooksLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.webhooks.index, req.service.externalId, req.account.type),
-    webhookEvents
+    webhookEvents,
+    paginationDetails
   })
+}
+
+function getPaginationDetails (currentPage, total, baseUrl, deliveryStatus) {
+  if (total <= 10) {
+    return {}
+  }
+  const totalPages = Math.ceil(total / 10)
+  const result = { items: [] }
+  if (currentPage !== 1) {
+    result.previous = { href: `${baseUrl}?deliveryStatus=${deliveryStatus}&page=${currentPage - 1}` }
+  }
+  if (currentPage !== totalPages) {
+    result.next = { href: `${baseUrl}?deliveryStatus=${deliveryStatus}&page=${currentPage + 1}` }
+  }
+  for (let i = 0; i < totalPages; i++) {
+    result.items[i] = { number: i + 1, href: `${baseUrl}?deliveryStatus=${deliveryStatus}&page=${i + 1}` }
+    if (currentPage === i + 1) {
+      result.items[i].current = true
+    }
+  }
+  return result
 }
 
 module.exports = {
