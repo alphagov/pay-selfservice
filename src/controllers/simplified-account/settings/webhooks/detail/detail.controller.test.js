0d7d58a8ed840fae084b2063b4a1b78ed00677a0
James Peacock <james.peacock@digital.cabinet-office.gov.uk>
2025-02-26 13:42:57 +0000

    PP-13521 tidy up detail controller and template and update unit test

--- src/controllers/simplified-account/settings/webhooks/detail/detail.controller.test.js
+++ src/controllers/simplified-account/settings/webhooks/detail/detail.controller.test.js
@@ -14,11 +14,29 @@ const webhook = {
   status: 'ACTIVE'
 }
 const signingSecret = { signing_key: '123-signing-key-456'}
+const webhookMessages = {
+  count: 10,
+  page: 1,
+  total: 11,
+  results: [
+    { resource_id: 'payment_id-1' },
+    { resource_id: 'payment_id-2' },
+    { resource_id: 'payment_id-3' },
+    { resource_id: 'payment_id-4' },
+    { resource_id: 'payment_id-5' },
+    { resource_id: 'payment_id-6' },
+    { resource_id: 'payment_id-7' },
+    { resource_id: 'payment_id-8' },
+    { resource_id: 'payment_id-9' },
+    { resource_id: 'payment_id-10' },
+    { resource_id: 'payment_id-11' }
+  ]
+}
 
 const mockResponse = sinon.spy()
 const mockGetWebhook = sinon.stub().resolves(webhook)
 const mockGetSigningSecret = sinon.stub().resolves(signingSecret)
-const mockGetWebhookMessages = sinon.stub().resolves({ results: [] })
+const mockGetWebhookMessages = sinon.stub().resolves(webhookMessages)
 
 const { res, nextRequest, call } = new ControllerTestBuilder('@controllers/simplified-account/settings/webhooks/detail/detail.controller')
   .withServiceExternalId(SERVICE_ID)
@@ -56,6 +74,25 @@ describe('Controller: settings/webhooks/detail', () => {
     it('should pass context data to the response method', () => {
       expect(mockResponse.args[0][3]).to.have.property('webhook').to.deep.equal(webhook)
       expect(mockResponse.args[0][3]).to.have.property('signingSecret').to.deep.equal(signingSecret)
+      expect(mockResponse.args[0][3]).to.have.property('deliveryStatus').to.equal('all')
+      expect(mockResponse.args[0][3]).to.have.property('webhookEvents').to.have.length(11)
+      expect(mockResponse.args[0][3].webhookEvents[0]).to.have.property('resourceId').to.equal('payment_id-1')
+      expect(mockResponse.args[0][3]).to.have.property('paginationDetails').to.deep.equal({
+        items: [
+          {
+            number: 1,
+            href: '/simplified/service/service-id-123abc/account/test/settings/webhooks/my-webhook-id-789?deliveryStatus=all&page=1',
+            current: true
+          },
+          {
+            number: 2,
+            href: '/simplified/service/service-id-123abc/account/test/settings/webhooks/my-webhook-id-789?deliveryStatus=all&page=2'
+          }
+        ],
+        next: {
+          href: '/simplified/service/service-id-123abc/account/test/settings/webhooks/my-webhook-id-789?deliveryStatus=all&page=2'
+        }
+      })
       expect(mockResponse.args[0][3]).to.have.property('eventTypes').to.have.property('CARD_PAYMENT_SUCCEEDED').to.equal('Payment succeeded')
       expect(mockResponse.args[0][3]).to.have.property('backToWebhooksLink')
         .to.equal('/simplified/service/service-id-123abc/account/test/settings/webhooks')
