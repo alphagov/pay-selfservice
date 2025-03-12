fb383e28ec880ab7a99f4117c0a3735f632fb041
Nathaniel Steers <nathaniel.steers@digital.cabinet-office.gov.uk>
2025-03-12 11:43:34 +0000

    PP-13354 unit test moto security controllers
    - add unit tests for mask card number and security code settings controllers
    - update google pay controller test to include worldpay specific google pay merchant id logic
    - remove MOTO UI flag from my services view

--- src/controllers/simplified-account/settings/card-payments/google-pay.controller.test.js
+++ src/controllers/simplified-account/settings/card-payments/google-pay.controller.test.js
@@ -2,64 +2,148 @@ const ControllerTestBuilder = require('@test/test-helpers/simplified-account/con
 const { expect } = require('chai')
 const sinon = require('sinon')
 const paths = require('@root/paths')
+const { WORLDPAY, STRIPE } = require('@models/constants/payment-providers')
+const { formatSimplifiedAccountPathsFor } = require('@utils/simplified-account/format')
 
 const ACCOUNT_TYPE = 'test'
 const SERVICE_EXTERNAL_ID = 'service-id-123abc'
 
-const GATEWAY_ACCOUNT_ID = '123'
-
 const mockResponse = sinon.spy()
 const mockUpdateGooglePay = sinon.spy()
+const mockUpdateGooglePayMerchantId = sinon.spy()
 
-const { req, res, nextRequest, call } = new ControllerTestBuilder('@controllers/simplified-account/settings/card-payments/google-pay.controller')
+const {
+  req,
+  res,
+  nextRequest,
+  call
+} = new ControllerTestBuilder('@controllers/simplified-account/settings/card-payments/google-pay.controller')
   .withAccount({
     type: ACCOUNT_TYPE,
-    id: GATEWAY_ACCOUNT_ID,
-    allowGooglePay: false
+    allowGooglePay: false,
+    paymentProvider: STRIPE
   })
   .withServiceExternalId(SERVICE_EXTERNAL_ID)
   .withStubs({
     '@utils/response': { response: mockResponse },
-    '@services/card-payments.service': { updateAllowGooglePay: mockUpdateGooglePay }
+    '@services/card-payments.service': { updateAllowGooglePay: mockUpdateGooglePay },
+    '@services/worldpay-details.service': { updateGooglePayMerchantId: mockUpdateGooglePayMerchantId }
   })
   .build()
 
 describe('Controller: settings/card-payments/google-pay', () => {
   describe('get', () => {
-    before(() => {
-      call('get', 1)
-    })
-    it('should call the response method', () => {
-      expect(mockResponse).to.have.been.calledOnce  // eslint-disable-line
-    })
-    it('should pass req, res and template path to the response method', () => {
-      expect(mockResponse.args[0][0]).to.deep.equal(req)
-      expect(mockResponse.args[0][1]).to.deep.equal(res)
-      expect(mockResponse.args[0][2]).to.equal('simplified-account/settings/card-payments/google-pay')
+    describe('a non-worldpay account', () => {
+      before(() => {
+        call('get', 1)
+      })
+      it('should call the response method', () => {
+        expect(mockResponse).to.have.been.calledOnce  // eslint-disable-line
+      })
+      it('should pass req, res and template path to the response method', () => {
+        expect(mockResponse.args[0][0]).to.deep.equal(req)
+        expect(mockResponse.args[0][1]).to.deep.equal(res)
+        expect(mockResponse.args[0][2]).to.equal('simplified-account/settings/card-payments/google-pay')
+      })
+
+      it('should pass context data to the response method', () => {
+        const context = mockResponse.args[0][3]
+        expect(context).to.not.have.property('currentGooglePayMerchantId')
+        expect(context).to.have.property('currentState').to.equal('off')
+        expect(context).to.have.property('backLink').to.equal(`/simplified/service/${SERVICE_EXTERNAL_ID}/account/test/settings/card-payments`)
+      })
     })
 
-    it('should pass context data to the response method', () => {
-      const context = mockResponse.args[0][3]
-      expect(context).to.have.property('currentState').to.equal('off')
-      expect(context).to.have.property('backLink').to.equal(`/simplified/service/${SERVICE_EXTERNAL_ID}/account/test/settings/card-payments`)
+    describe('a worldpay account', () => {
+      before(() => {
+        nextRequest({
+          account: {
+            paymentProvider: WORLDPAY,
+            getCurrentCredential: () => {
+              return {
+                credentials: {
+                  googlePayMerchantId: 'blah'
+                }
+              }
+            }
+          }
+        })
+        call('get', 1)
+      })
+
+      it('should include google pay merchant id in the context', () => {
+        const context = mockResponse.args[0][3]
+        expect(context).to.have.property('currentGooglePayMerchantId').to.equal('blah')
+      })
     })
   })
   describe('post', () => {
-    before(() => {
-      nextRequest({
-        body: { googlePay: 'on' }
+    describe('a non-worldpay account', () => {
+      before(() => {
+        nextRequest({
+          body: { googlePay: 'on' }
+        })
+        call('post', 1)
       })
-      call('post', 1)
+
+      it('should not update Google Pay merchant id', () => {
+        sinon.assert.notCalled(mockUpdateGooglePayMerchantId)
+      })
+
+      it('should update allow Google Pay enabled', () => {
+        expect(mockUpdateGooglePay.calledOnce).to.be.true // eslint-disable-line
+        sinon.assert.calledWith(mockUpdateGooglePay, SERVICE_EXTERNAL_ID, ACCOUNT_TYPE, true)
+      })
+
+      it('should redirect to the card payments index page', () => {
+        sinon.assert.calledOnceWithExactly(res.redirect,
+          formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.cardPayments.index, SERVICE_EXTERNAL_ID, ACCOUNT_TYPE)
+        )
+      })
     })
 
-    it('should update allow Google Pay enabled', () => {
-      expect(mockUpdateGooglePay.calledOnce).to.be.true // eslint-disable-line
-      expect(mockUpdateGooglePay.calledWith(SERVICE_EXTERNAL_ID, ACCOUNT_TYPE, true)).to.be.true // eslint-disable-line
-    })
+    describe('a worldpay account', () => {
+      before(() => {
+        nextRequest({
+          body: {
+            googlePay: 'on',
+            googlePayMerchantId: '0123456789abcde'
+          },
+          user: {
+            externalId: 'user-123-abc'
+          },
+          account: {
+            paymentProvider: WORLDPAY,
+            getCurrentCredential: () => {
+              return {
+                externalId: 'credential-123-abc'
+              }
+            }
+          }
+        })
+        call('post', 1)
+      })
 
-    it('should redirect to the card payments index page', () => {
-      expect(res.redirect.calledOnce).to.be.true // eslint-disable-line
-      expect(res.redirect.args[0][0]).to.include(paths.simplifiedAccount.settings.cardPayments.index)
+      it('should update Google Pay merchant id', () => {
+        sinon.assert.calledOnceWithExactly(mockUpdateGooglePayMerchantId,
+          SERVICE_EXTERNAL_ID,
+          ACCOUNT_TYPE,
+          'credential-123-abc',
+          'user-123-abc',
+          '0123456789abcde'
+        )
+      })
+
+      it('should update allow Google Pay enabled', () => {
+        expect(mockUpdateGooglePay.calledOnce).to.be.true // eslint-disable-line
+        sinon.assert.calledWith(mockUpdateGooglePay, SERVICE_EXTERNAL_ID, ACCOUNT_TYPE, true)
+      })
+
+      it('should redirect to the card payments index page', () => {
+        sinon.assert.calledOnceWithExactly(res.redirect,
+          formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.cardPayments.index, SERVICE_EXTERNAL_ID, ACCOUNT_TYPE)
+        )
+      })
     })
   })
 })
