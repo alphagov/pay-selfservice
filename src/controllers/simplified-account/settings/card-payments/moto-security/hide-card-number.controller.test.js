bc6dbc38b29736869d903caec2506c8802620646
Nathaniel Steers <nathaniel.steers@digital.cabinet-office.gov.uk>
2025-03-12 11:40:31 +0000

    PP-13354 unit test moto security controllers
    - add unit tests for mask card number and security code settings controllers
    - update google pay controller test to include worldpay specific google pay merchant id logic
    - remove MOTO UI flag from my services view

---
+++ src/controllers/simplified-account/settings/card-payments/moto-security/hide-card-number.controller.test.js
@@ -0,0 +1,111 @@
+const sinon = require('sinon')
+const ControllerTestBuilder = require('@test/test-helpers/simplified-account/controllers/ControllerTestBuilder.class')
+const { formatSimplifiedAccountPathsFor } = require('@utils/simplified-account/format')
+const paths = require('@root/paths')
+
+const ACCOUNT_TYPE = 'live'
+const SERVICE_EXTERNAL_ID = 'service-id-123abc'
+
+const mockResponse = sinon.spy()
+const mockUpdateMotoMaskCardNumber = sinon.spy()
+
+const {
+  req,
+  res,
+  nextRequest,
+  call
+} = new ControllerTestBuilder('@controllers/simplified-account/settings/card-payments/moto-security/hide-card-number.controller')
+  .withAccount({
+    type: ACCOUNT_TYPE,
+    allowMoto: true,
+    motoMaskCardNumber: false
+  })
+  .withServiceExternalId(SERVICE_EXTERNAL_ID)
+  .withStubs({
+    '@utils/response': { response: mockResponse },
+    '@services/card-payments.service': { updateMotoMaskCardNumber: mockUpdateMotoMaskCardNumber }
+  })
+  .build()
+
+describe('Controller: settings/card-payments/moto-security/hide-card-number', () => {
+  describe('get', () => {
+    before(() => {
+      call('get')
+    })
+    it('should call the response method with context', () => {
+      sinon.assert.calledOnceWithExactly(mockResponse,
+        req,
+        res,
+        'simplified-account/settings/card-payments/moto-security/hide-card-number',
+        {
+          backLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.cardPayments.index, SERVICE_EXTERNAL_ID, ACCOUNT_TYPE),
+          currentState: 'off'
+        }
+      )
+    })
+  })
+
+  describe('post', () => {
+    describe('valid submission', () => {
+      before(() => {
+        nextRequest({
+          body: {
+            hideCardNumber: 'on'
+          }
+        })
+        call('post')
+      })
+
+      it('should call updateMotoMaskCardNumber', () => {
+        sinon.assert.calledOnceWithExactly(mockUpdateMotoMaskCardNumber,
+          SERVICE_EXTERNAL_ID,
+          ACCOUNT_TYPE,
+          true
+        )
+      })
+
+      it('should redirect the user on success', () => {
+        sinon.assert.calledOnceWithExactly(res.redirect,
+          formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.cardPayments.index, SERVICE_EXTERNAL_ID, ACCOUNT_TYPE)
+        )
+      })
+    })
+    describe('invalid submission', () => {
+      before(() => {
+        nextRequest({
+          body: {
+            NOTHideCardNumber: 'blah'
+          }
+        })
+        call('post')
+      })
+
+      it('should not call updateMotoMaskCardNumber', () => {
+        sinon.assert.notCalled(mockUpdateMotoMaskCardNumber)
+      })
+
+      it('should not redirect the user', () => {
+        sinon.assert.notCalled(res.redirect)
+      })
+
+      it('should call the response method with errors', () => {
+        sinon.assert.calledWithMatch(mockResponse,
+          sinon.match.any,
+          sinon.match.any,
+          sinon.match.any,
+          {
+            errors: {
+              formErrors: {
+                hideCardNumber: 'Select an option'
+              },
+              summary: [{
+                text: 'Select an option',
+                href: '#hide-card-number'
+              }]
+            }
+          }
+        )
+      })
+    })
+  })
+})
