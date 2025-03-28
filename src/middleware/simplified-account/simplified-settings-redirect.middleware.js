803ce6425aaca072af9f3a0c102b022996f14ebe
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-03-28 10:18:36 +0000

    [squash] improve logging for redirects

--- src/middleware/simplified-account/simplified-settings-redirect.middleware.js
+++ src/middleware/simplified-account/simplified-settings-redirect.middleware.js
@@ -1,7 +1,7 @@
 const { formatSimplifiedAccountPathsFor } = require('@utils/simplified-account/format')
 const paths = require('@root/paths')
 const gatewayAccountsService = require('@services/gateway-accounts.service')
-const logger = require('@utils/logger')(__filename)
+const logger = require('@utils/logger')('simplified-settings-redirect.middleware.js')
 
 module.exports = async (req, res, next) => {
   if (!req.user?.isDegatewayed()) {
@@ -14,12 +14,19 @@ module.exports = async (req, res, next) => {
   }
 
   if (req.account) {
+    logger.info('Redirecting to simplified settings using account specified in URL')
     return res.redirect(formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.index, req.service.externalId, req.account.type))
   }
 
   const liveAccountExists = await (gatewayAccountsService.getGatewayAccountByServiceExternalIdAndType(req.service.externalId, 'live')
     .then(() => true)
     .catch(() => false))
+
+  if (liveAccountExists) {
+    logger.info('Live account exists for service. Redirecting to simplified settings for "live" account type')
+  } else {
+    logger.info('No ive account exists for service. Redirecting to simplified settings for "test" account type')
+  }
 
   return res.redirect(formatSimplifiedAccountPathsFor(paths.simplifiedAccount.settings.index, req.service.externalId, liveAccountExists ? 'live' : 'test'))
 }
