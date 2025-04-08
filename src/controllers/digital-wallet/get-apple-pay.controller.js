f00f4605e2e139569b5afc1c2936de1232cc2e94
Kerr Rainey <kerr.rainey@digital.cabinet-office.gov.uk>
2025-04-08 12:31:27 +0100

    PP-13526: Remove old unused Digital Wallet code
    We no longer need old Digital Wallet from pre simplified account settings.
    Remove controllers, views and routes.

--- src/controllers/digital-wallet/get-apple-pay.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const { response } = require('../../utils/response')
-
-module.exports = (req, res) => {
-  return response(req, res, 'digital-wallet/apple-pay', {})
-}
--- src/controllers/digital-wallet/get-google-pay.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const { response } = require('../../utils/response')
-
-module.exports = function showGooglePaySettings (req, res) {
-  return response(req, res, 'digital-wallet/google-pay', {
-    enabled: req.account && req.account.allow_google_pay
-  })
-}
