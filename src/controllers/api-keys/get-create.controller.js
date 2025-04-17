f6111f231edf4629b8d7245f8a109c420bdbda9c
Dom Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-04-17 17:05:04 +0100

    PP-13526 Remove old API keys settings (#4577)

--- src/controllers/api-keys/get-create.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const { response } = require('../../utils/response.js')
-
-module.exports = function showCreateApiKey (req, res) {
-  const accountId = req.account.gateway_account_id
-  response(req, res, 'api-keys/create', { account_id: accountId })
-}
