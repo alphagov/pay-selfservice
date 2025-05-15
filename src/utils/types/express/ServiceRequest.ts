fa67dfa1f2b67d773a940cb6e02f30082147c0ce
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-05-15 14:56:27 +0100

    PP-13526 Cleanup old team members code

--- src/utils/types/express/ServiceRequest.ts
+++ src/utils/types/express/ServiceRequest.ts
@@ -1,25 +1,8 @@
-import type User from '@models/User.class'
 import type Service from '@models/Service.class'
 import type GatewayAccount from '@models/GatewayAccount.class'
-import type { Request } from 'express'
-import type StripeAccountSetup from '@models/StripeAccountSetup.class'
-import ClientSessionsCookie from '@utils/types/client-sessions/ClientSessionsCookie'
+import UserRequest from "@utils/types/express/UserRequest";
 
-type MessageState = 'success' | 'error' | 'info'
-
-interface Message {
-  state: MessageState
-  icon?: string
-  heading: string
-  body?: string
-}
-
-export default interface ServiceRequest<T = never> extends Request {
-  user: User
+export default interface ServiceRequest<T = never> extends UserRequest {
   service: Service
   account: GatewayAccount
-  gatewayAccountStripeProgress?: StripeAccountSetup
-  flash(type: string, message: Message): void
-  body: T,
-  session: ClientSessionsCookie
 }
