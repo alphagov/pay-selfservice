2c78c8e538d696e2f470dac739bb8cb76d530105
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-05-15 16:09:03 +0100

    [squash] fix request body type

--- src/utils/types/express/ServiceRequest.ts
+++ src/utils/types/express/ServiceRequest.ts
@@ -2,7 +2,7 @@ import type Service from '@models/Service.class'
 import type GatewayAccount from '@models/GatewayAccount.class'
 import UserRequest from "@utils/types/express/UserRequest";
 
-export default interface ServiceRequest<T = never> extends UserRequest {
+export default interface ServiceRequest<T = never> extends UserRequest<T> {
   service: Service
   account: GatewayAccount
 }
