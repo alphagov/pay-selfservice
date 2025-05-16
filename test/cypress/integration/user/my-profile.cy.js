eef0703f0f74e3cd4c955430d53106e1c4dce51a
Dom Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-05-16 14:31:11 +0100

    PP-14067 Add link to set mobile number if no number is set (#4611)
    * PP-14067 Add link to set mobile number if no number is set

--- test/cypress/integration/user/my-profile.cy.js
+++ test/cypress/integration/user/my-profile.cy.js
@@ -4,6 +4,20 @@ describe('My profile page', () => {
   const userExternalId = 'cd0fa54cf3b7408a80ae2f1b93e7c16e'
   const testPhoneNumber = '+441234567890'
 
+  describe('accessibility check', () => {
+    beforeEach(() => {
+      cy.setEncryptedCookies(userExternalId)
+      cy.task('setupStubs', [
+        userStubs.getUserSuccess({ userExternalId, telephoneNumber: null })
+      ])
+    })
+
+    it('should check accessibility of the page', { defaultCommandTimeout: 15000 }, () => {
+      cy.visit('/my-profile')
+      cy.a11yCheck()
+    })
+  })
+
   describe('User does not have telephone number', () => {
     beforeEach(() => {
       cy.setEncryptedCookies(userExternalId)
@@ -12,9 +26,16 @@ describe('My profile page', () => {
       ])
     })
 
-    it('should not show telephone number row', () => {
+    it('should show telephone number row with link to add number', () => {
       cy.visit('/my-profile')
-      cy.get('[data-cy=telephone-number-row]').should('not.exist')
+
+      cy.get('[data-cy=telephone-number-row]').should('exist')
+      cy.get('[data-cy=telephone-number-row]>dd').within(() => {
+        cy.get('a')
+          .should('contain', 'Add mobile number')
+          .click()
+      })
+      cy.location('pathname').should('eq', '/my-profile/phone-number')
     })
   })
 
@@ -28,8 +49,20 @@ describe('My profile page', () => {
 
     it('should show telephone number row', () => {
       cy.visit('/my-profile')
+
       cy.get('[data-cy=telephone-number-row]').should('exist')
       cy.get('[data-cy=telephone-number-row]>dd').should('contain', testPhoneNumber)
+    })
+
+    it('should have link to change phone number', () => {
+      cy.visit('/my-profile')
+
+      cy.get('[data-cy=telephone-number-row]>dd').within(() => {
+        cy.get('a')
+          .should('contain', 'Change')
+          .click()
+      })
+      cy.location('pathname').should('eq', '/my-profile/phone-number')
     })
   })
 })
