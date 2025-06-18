cf253a7625f6852bda55ef63fe2dfc711b4492b6
Nathaniel Steers <nathaniel.steers@digital.cabinet-office.gov.uk>
2025-06-18 13:49:41 +0100

    PP-14219 payment links landing page impl
    - implement controller and view for payment links landing page
    - new css classes
      - override display of accordion controls
      - embolden list markers

--- src/controllers/simplified-account/services/payment-links/delete/delete-payment-link.controller.ts
+++ src/controllers/simplified-account/services/payment-links/delete/delete-payment-link.controller.ts
@@ -1,8 +1,9 @@
 import { ServiceRequest, ServiceResponse } from '@utils/types/express'
-import { response } from '@utils/response'
 
 function get (req: ServiceRequest, res: ServiceResponse) {
-  return response(req, res, 'simplified-account/services/payment-links/delete/index', {})
+  return res.status(501).json({
+    message: 'not implemented'
+  })
 }
 
 function post (req: ServiceRequest, res: ServiceResponse) {
