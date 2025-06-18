0534665c2902d5f3316cbd54c0b2ee53d843faeb
Nathaniel Steers <nlsteers@users.noreply.github.com>
2025-06-18 17:26:09 +0100

    PP-14219 payment links landing page impl (#4634)
    - implement controller and view for payment links landing page
    - new css classes
      - override display of accordion controls
      - embolden list markers

--- src/controllers/simplified-account/services/payment-links/create/create-payment-link.controller.ts
+++ src/controllers/simplified-account/services/payment-links/create/create-payment-link.controller.ts
@@ -1,8 +1,9 @@
-import { response } from '@utils/response'
 import { ServiceRequest, ServiceResponse } from '@utils/types/express'
 
 function get (req: ServiceRequest, res: ServiceResponse) {
-  return response(req, res, 'simplified-account/services/payment-links/create/index', {})
+  return res.status(501).json({
+    message: 'not implemented'
+  })
 }
 
 function post (req: ServiceRequest, res: ServiceResponse) {
--- src/controllers/simplified-account/services/payment-links/payment-links.controller.ts
+++ src/controllers/simplified-account/services/payment-links/payment-links.controller.ts
@@ -3,20 +3,35 @@ import * as edit from './edit/edit-payment-link.controller'
 import * as remove from './delete/delete-payment-link.controller'
 import { ServiceRequest, ServiceResponse } from '@utils/types/express'
 import { response } from '@utils/response'
+import { getProducts } from '@services/products.service'
+import ProductType from '@models/products/product-type'
+import { penceToPoundsWithCurrency } from '@utils/currency-formatter'
+import formatServiceAndAccountPathsFor from '@utils/simplified-account/format/format-service-and-account-paths-for'
+import paths from '@root/paths'
 
-function get (req: ServiceRequest, res: ServiceResponse) {
-  return response(req, res, 'simplified-account/services/payment-links/index', {})
-}
-
-function post (req: ServiceRequest, res: ServiceResponse) {
-  return res.status(501).json({
-    message: 'not implemented'
+async function get (req: ServiceRequest, res: ServiceResponse) {
+  const products = await getProducts(req.account.id, ProductType.ADHOC)
+  return response(req, res, 'simplified-account/services/payment-links/index', {
+    messages: res.locals.flash?.messages ?? [],
+    serviceMode: req.account.type,
+    createLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.paymentLinks.create, req.service.externalId, req.account.type),
+    products: products.reduce<Record<string, string>[]>((acc, product) => {
+      acc.push({
+        name: product.name,
+        href: product.links.pay.href,
+        reference: product.referenceEnabled ? product.referenceLabel! : 'Created by GOV.UK Pay',
+        details: product.description ? product.description : 'None given',
+        amount: product.price ? penceToPoundsWithCurrency(product.price) : 'User can choose',
+        editLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.paymentLinks.edit, req.service.externalId, req.account.type, product.externalId),
+        deleteLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.paymentLinks.delete, req.service.externalId, req.account.type, product.externalId)
+      })
+      return acc
+    }, [])
   })
 }
 
 export {
   get,
-  post,
   create,
   edit,
   remove
