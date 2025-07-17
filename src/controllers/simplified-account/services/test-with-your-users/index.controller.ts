8f75af2f5860893d865d20c185ca1e07df953b2b
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-07-17 16:49:48 +0100

    [squash] fix ts compile errors

--- src/controllers/simplified-account/services/test-with-your-users/index.controller.ts
+++ src/controllers/simplified-account/services/test-with-your-users/index.controller.ts
@@ -1,14 +1,14 @@
 import { response } from '@utils/response.js'
-import { formatSimplifiedAccountPathsFor } from '@utils/simplified-account/format'
 import paths from '@root/paths'
 import { ServiceRequest, ServiceResponse } from "@utils/types/express";
+import formatServiceAndAccountPathsFor from "@utils/simplified-account/format/format-service-and-account-paths-for";
 
 function get (req: ServiceRequest, res: ServiceResponse) {
   const context = {
     productsTab: false,
-    createLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.testWithYourUsers.create, req.service.externalId, req.account.type),
-    prototypesLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.testWithYourUsers.links,  req.service.externalId, req.account.type),
-    backLink: formatSimplifiedAccountPathsFor(paths.simplifiedAccount.dashboard.index, req.service.externalId, req.account.type)
+    createLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.testWithYourUsers.create, req.service.externalId, req.account.type),
+    prototypesLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.testWithYourUsers.links,  req.service.externalId, req.account.type),
+    backLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.dashboard.index, req.service.externalId, req.account.type)
   }
 
   return response(req, res, 'dashboard/demo-service/index', context)
