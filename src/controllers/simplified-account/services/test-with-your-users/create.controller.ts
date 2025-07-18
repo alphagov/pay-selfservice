d4547b5637eea7573e376fc4ae3ac33dfc7da3de
Dom Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-07-18 17:27:15 +0100

    PP-14193 Convert create, disable and links controllers to typescript (#4667)
    * PP-14193 Convert create, disable and links controllers to typescript

---
+++ src/controllers/simplified-account/services/test-with-your-users/create.controller.ts
@@ -0,0 +1,19 @@
+import lodash from 'lodash'
+import { response } from'@utils/response.js'
+import paths from '@root/paths'
+import formatServiceAndAccountPathsFor from "@utils/simplified-account/format/format-service-and-account-paths-for";
+import {ServiceRequest, ServiceResponse} from "@utils/types/express";
+
+function get (req: ServiceRequest, res: ServiceResponse) {
+  const context = {
+    backLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.testWithYourUsers.links,  req.service.externalId, req.account.type),
+    confirmLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.testWithYourUsers.confirm,  req.service.externalId, req.account.type),
+    ...lodash.get(req, 'session.pageData.createPrototypeLink', {})
+  }
+
+  return response(req, res, 'dashboard/demo-service/create', context)
+}
+
+export {
+  get
+}
---
+++ src/controllers/simplified-account/services/test-with-your-users/links.controller.ts
@@ -0,0 +1,26 @@
+import { response } from '@utils/response.js'
+import paths from '@root/paths'
+import productsClient from '@services/clients/products.client.js'
+import formatServiceAndAccountPathsFor from "@utils/simplified-account/format/format-service-and-account-paths-for";
+import { ServiceRequest, ServiceResponse } from "@utils/types/express";
+
+async function get (req: ServiceRequest, res: ServiceResponse) {
+  const prototypeProducts = await productsClient.product.getByGatewayAccountIdAndType(`${req.account.id}`, 'PROTOTYPE')
+
+  const context = {
+    messages: res.locals?.flash?.messages ?? [],
+    productsTab: true,
+    createLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.testWithYourUsers.create, req.service.externalId, req.account.type),
+    indexLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.testWithYourUsers.index, req.service.externalId, req.account.type),
+    prototypesLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.testWithYourUsers.links, req.service.externalId, req.account.type),
+    backLink: formatServiceAndAccountPathsFor(paths.simplifiedAccount.testWithYourUsers.index, req.service.externalId, req.account.type),
+    productsLength: prototypeProducts.length,
+    products: prototypeProducts
+  }
+
+  return response(req, res, 'dashboard/demo-service/index', context)
+}
+
+export {
+  get
+}
