de7ff5cdeb376f704ff55bd2220cc9acac570e56
Nathaniel Steers <nathaniel.steers@digital.cabinet-office.gov.uk>
2025-08-12 17:27:00 +0100

    [squash] delete session data when returning to payment links index

--- src/controllers/simplified-account/services/payment-links/payment-links.controller.ts
+++ src/controllers/simplified-account/services/payment-links/payment-links.controller.ts
@@ -1,5 +1,4 @@
-import * as create from './create/payment-link-information.controller'
-import * as reference from './create/payment-link-reference.controller'
+import * as create from './create'
 import * as edit from './edit/edit-payment-link.controller'
 import * as remove from './delete/delete-payment-link.controller'
 import { ServiceRequest, ServiceResponse } from '@utils/types/express'
@@ -9,14 +8,11 @@ import ProductType from '@models/products/product-type'
 import { penceToPoundsWithCurrency } from '@utils/currency-formatter'
 import formatServiceAndAccountPathsFor from '@utils/simplified-account/format/format-service-and-account-paths-for'
 import paths from '@root/paths'
-import { PaymentLinkCreationSession } from './constants'
+import lodash from 'lodash'
+import { CREATE_SESSION_KEY } from '@controllers/simplified-account/services/payment-links/create/constants'
 
 async function get(req: ServiceRequest, res: ServiceResponse) {
-  const session = req.session as unknown as PaymentLinkCreationSession
-  if (session.pageData?.createPaymentLink) {
-    delete session.pageData.createPaymentLink
-  }
-
+  lodash.unset(req, CREATE_SESSION_KEY)
   const products = await getProducts(req.account.id, ProductType.ADHOC)
   return response(req, res, 'simplified-account/services/payment-links/index', {
     isAdmin: req.user.isAdminUserForService(req.service.externalId),
@@ -40,7 +36,6 @@ async function get(req: ServiceRequest, res: ServiceResponse) {
 export {
   get,
   create,
-  reference,
   edit,
   remove
 }
