19123ed15546a7a3d262f32d88a4298292dce371
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2025-08-14 15:26:49 +0100

    PP-14351 add amount to paths

--- src/paths.js
+++ src/paths.js
@@ -159,6 +159,7 @@ module.exports = {
       index: '/payment-links',
       create: '/payment-links/create',
       reference: '/payment-links/reference',
+      amount: '/payment-links/amount',
       edit: '/payment-links/:productExternalId/edit',
       delete: '/payment-links/:productExternalId/delete',
     },
