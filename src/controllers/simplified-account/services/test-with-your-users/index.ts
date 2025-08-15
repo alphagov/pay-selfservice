3b165311a8bf4f7a9acb4f5bfc35cd42c69c5035
Dom Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-08-15 09:25:03 +0100

    PP-14193 Clean up old test with your users code (#4696)

---
+++ src/controllers/simplified-account/services/test-with-your-users/index.ts
@@ -0,0 +1,13 @@
+import * as index from './index.controller'
+import * as links from './links.controller'
+import * as disable from './disable.controller'
+import * as create from './create.controller'
+import * as confirm from './confirm.controller'
+
+export {
+  index,
+  links,
+  disable,
+  create,
+  confirm
+}
