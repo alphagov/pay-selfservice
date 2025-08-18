e18c0690c1c77e177dda217a2cff68de0383b4a9
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2025-08-18 16:56:37 +0100

    PP-14351 WIP amount controller

---
+++ src/controllers/simplified-account/services/payment-links/create/payment-link-amount.controller.ts
@@ -0,0 +1,101 @@
+import paths from '@root/paths'
+import formatServiceAndAccountPathsFor from '@utils/simplified-account/format/format-service-and-account-paths-for'
+import { ServiceRequest, ServiceResponse } from '@utils/types/express'
+import { response } from '@utils/response'
+import { CREATE_SESSION_KEY, PaymentLinkCreationSession } from './constants'
+import lodash from 'lodash'
+import { paymentLinkSchema } from '@utils/simplified-account/validation/payment-link.schema'
+import { validationResult } from 'express-validator'
+import formatValidationErrors from '@utils/simplified-account/format/format-validation-errors'
+import formatAccountPathsFor from '@utils/format-account-paths-for'
+import { poundsToPence } from '@utils/currency-formatter'
+
+
+function get(req: ServiceRequest, res: ServiceResponse) {
+  const { service, account } = req
+  const currentSession = lodash.get(req, CREATE_SESSION_KEY, {} as PaymentLinkCreationSession)
+  if (lodash.isEmpty(currentSession)) {
+    return res.redirect(
+      formatServiceAndAccountPathsFor(paths.simplifiedAccount.paymentLinks.index, service.externalId, account.type)
+    )
+  }
+  const isWelsh = currentSession.language === 'cy'
+
+  const formValues = {
+    amountTypeGroup: 'fixed',
+    paymentAmount: currentSession.paymentLinkAmount ? poundsToPence(currentSession.paymentLinkAmount) : undefined,
+  }
+
+  return response(req, res, 'simplified-account/services/payment-links/create/amount', {
+    service,
+    account,
+    backLink: formatServiceAndAccountPathsFor(
+      paths.simplifiedAccount.paymentLinks.reference,
+      service.externalId,
+      account.type
+    ),
+    formValues,
+    isWelsh,
+    serviceMode: account.type,
+  })
+}
+
+interface CreateLinkAmountBody {
+  amountTypeGroup: 'fixed' | 'variable'
+  paymentAmount: string
+  amountHint: string
+}
+
+async function post(req: ServiceRequest<CreateLinkAmountBody>, res: ServiceResponse) {
+  const { service, account } = req
+  const currentSession = lodash.get(req, CREATE_SESSION_KEY, {} as PaymentLinkCreationSession)
+  if (lodash.isEmpty(currentSession)) {
+    return res.redirect(
+      formatServiceAndAccountPathsFor(paths.simplifiedAccount.paymentLinks.index, service.externalId, account.type)
+    )
+  }
+  const isWelsh = currentSession.language === 'cy'
+
+  const validations = [paymentLinkSchema.amount.type.validate]
+  validations.push(paymentLinkSchema.amount.price.validate)
+
+  // if (req.body.amountTypeGroup === 'fixed') {
+  //   validations.push(paymentLinkSchema.amount.price.validate)
+  // } else {
+  //   validations.push(paymentLinkSchema.amount.hint.validate)
+  // }
+
+  await Promise.all(validations.map((validation) => validation.run(req)))
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    const formattedErrors = formatValidationErrors(errors)
+    const backLinkUrl = formatServiceAndAccountPathsFor(
+      paths.simplifiedAccount.paymentLinks.reference,
+      service.externalId,
+      account.type
+    )
+
+    return response(req, res, 'simplified-account/services/payment-links/create/amount', {
+      service,
+      account,
+      errors: {
+        summary: formattedErrors.errorSummary,
+        formErrors: formattedErrors.formErrors,
+      },
+      backLink: backLinkUrl,
+      formValues: req.body,
+      isWelsh,
+      serviceMode: account.type,
+      createJourney: true
+    })
+  }
+
+  lodash.set(req, CREATE_SESSION_KEY, {
+      ...lodash.get(req, CREATE_SESSION_KEY, {}),
+      paymentLinkAmount: poundsToPence(parseFloat(req.body.paymentAmount)),
+    } as PaymentLinkCreationSession)
+    return res.redirect(formatAccountPathsFor(paths.account.paymentLinks.review, account.externalId) as string)
+}
+
+export { get, post }
