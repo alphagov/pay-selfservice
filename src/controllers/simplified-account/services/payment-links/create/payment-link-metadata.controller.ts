29393ebc06024e55f74495b04a2143abd28b4c0c
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2025-08-27 13:53:49 +0100

    PP-14386 add validations

--- src/controllers/simplified-account/services/payment-links/create/payment-link-metadata.controller.ts
+++ src/controllers/simplified-account/services/payment-links/create/payment-link-metadata.controller.ts
@@ -4,6 +4,9 @@ import { ServiceRequest, ServiceResponse } from '@utils/types/express'
 import formatServiceAndAccountPathsFor from '@utils/simplified-account/format/format-service-and-account-paths-for'
 import lodash from 'lodash'
 import { CREATE_SESSION_KEY, PaymentLinkCreationSession } from './constants'
+import { paymentLinkSchema } from '@utils/simplified-account/validation/payment-link.schema'
+import { validationResult } from 'express-validator'
+import formatValidationErrors from '@utils/simplified-account/format/format-validation-errors'
 
 function getSession<T>(req: ServiceRequest<T>) {
   return lodash.get(req, CREATE_SESSION_KEY, {} as PaymentLinkCreationSession)
@@ -35,63 +38,52 @@ interface CreateLinkMetadataBody {
   cellContent: string
 }
 
-function post(req: ServiceRequest<CreateLinkMetadataBody>, res: ServiceResponse) {
+async function post(req: ServiceRequest<CreateLinkMetadataBody>, res: ServiceResponse) {
   const { service, account } = req
   const currentSession = getSession(req)
 
-  // const validations = [
-  //   paymentLinkSchema.metadata.columnHeader.add.validate(product.metadata ?? {}),
-  //   paymentLinkSchema.metadata.cellContent.validate,
-  // ]
+  const validations = [
+    paymentLinkSchema.metadata.columnHeader.add.validate(currentSession.metadata ?? {}),
+    paymentLinkSchema.metadata.cellContent.validate,
+  ]
 
-  // for (const validation of validations) {
-  //   await validation.run(req)
-  // }
+  for (const validation of validations) {
+    await validation.run(req)
+  }
 
-  // const errors = validationResult(req)
+  const errors = validationResult(req)
 
-  // if (!errors.isEmpty()) {
-  //   const formattedErrors = formatValidationErrors(errors)
-  //   return response(req, res, 'simplified-account/services/payment-links/edit/metadata', {
-  //     errors: {
-  //       summary: formattedErrors.errorSummary,
-  //       formErrors: formattedErrors.formErrors,
-  //     },
-  //     backLink: formatServiceAndAccountPathsFor(
-  //       paths.simplifiedAccount.paymentLinks.edit.index,
-  //       req.service.externalId,
-  //       req.account.type,
-  //       product.externalId
-  //     ),
-  //     serviceMode: req.account.type,
-  //     isWelsh: product.language === 'cy',
-  //     createJourney: true,
-  //     formValues: req.body,
-  //   })
-  // }
+  if (!errors.isEmpty()) {
+    const formattedErrors = formatValidationErrors(errors)
+    const backLinkUrl = formatServiceAndAccountPathsFor(
+      paths.simplifiedAccount.paymentLinks.reference,
+      service.externalId,
+      account.type
+    )
+    return response(req, res, 'simplified-account/services/payment-links/create/metadata', {
+      service,
+      account,
+      errors: {
+        summary: formattedErrors.errorSummary,
+        formErrors: formattedErrors.formErrors,
+      },
+      backLink: backLinkUrl,
+      formValues: req.body,
+       
+      serviceMode: account.type,
+      // isWelsh: product.language === 'cy',
+      createJourney: true,
+      
+    })
+  }
 
-  
-
-  // return response(req, res, 'simplified-account/services/payment-links/create/amount', {
-  //   service,
-  //   account,
-  //   // errors: {
-  //   //   summary: formattedErrors.errorSummary,
-  //   //   formErrors: formattedErrors.formErrors,
-  //   // },
-  //   backLink: backLinkUrl,
-  //   formValues: req.body,
-  //   isWelsh,
-  //   serviceMode: account.type,
-  //   createJourney: true
-  // })
-lodash.set(req, CREATE_SESSION_KEY, {
-  ...lodash.get(req, CREATE_SESSION_KEY, {}),
-  metadata: {
-    metadataColumnHeader: req.body.reportingColumn,
-    metadataCellContent: req.body.cellContent,
-  },
-} as PaymentLinkCreationSession);
+  lodash.set(req, CREATE_SESSION_KEY, {
+    ...lodash.get(req, CREATE_SESSION_KEY, {}),
+    metadata: {
+      ...currentSession.metadata,
+      [req.body.reportingColumn]: req.body.cellContent,
+    }
+  } as PaymentLinkCreationSession)
   return res.redirect(formatServiceAndAccountPathsFor(paths.simplifiedAccount.paymentLinks.review, service.externalId, account.type))
 }
 
