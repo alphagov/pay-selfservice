b55af7e3536780c1aa0ada2eecf032f607846d8b
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2025-09-02 16:53:57 +0100

    PP-14386 add controller test

---
+++ src/controllers/simplified-account/services/payment-links/create/metadata/payment-link-metadata.controller.test.ts
@@ -0,0 +1,106 @@
+import ControllerTestBuilder from '@test/test-helpers/simplified-account/controllers/ControllerTestBuilder.class'
+import sinon from 'sinon'
+import GatewayAccountType from '@models/gateway-account/gateway-account-type'
+import { PaymentLinkCreationSession } from '.././constants'
+
+const SERVICE_EXTERNAL_ID = 'service123abc'
+const GATEWAY_ACCOUNT_ID = 117
+const GATEWAY_ACCOUNT_EXTERNAL_ID = 'account123abc'
+
+const mockResponse = sinon.spy()
+
+const { nextRequest, call, res } = new ControllerTestBuilder(
+  '@controllers/simplified-account/services/payment-links/create/metadata/payment-link-metadata.controller'
+)
+  .withStubs({
+    '@utils/response': { response: mockResponse },
+  })
+  .withAccount({
+    id: GATEWAY_ACCOUNT_ID,
+    externalId: GATEWAY_ACCOUNT_EXTERNAL_ID,
+    type: GatewayAccountType.TEST,
+  })
+  .withService({
+    name: 'McDuck Enterprises',
+    serviceName: { en: 'McDuck Enterprises', cy: 'Mentrau McDuck' },
+    externalId: SERVICE_EXTERNAL_ID
+  })
+  .build()
+
+describe('controller: services/payment-links/create/metadata/payment-link-metadata', () => {
+  describe('get', () => {
+    describe('with existing session data', () => {
+          beforeEach(async () => {
+            mockResponse.resetHistory()
+            const sessionData: Partial<PaymentLinkCreationSession> = {
+              paymentLinkTitle: 'Test Payment Link',
+              paymentLinkDescription: 'Test Description',
+              language: 'en',
+              serviceNamePath: 'mcduck-enterprises',
+              productNamePath: 'test-payment-link',
+              paymentAmountType: 'variable',
+              metadata: {
+                existing_column: 'existing_value',
+              },
+            }
+    
+            nextRequest({
+              session: {
+                pageData: {
+                  createPaymentLink: sessionData,
+                },
+              },
+            })
+    
+            await call('get')
+          })
+    
+          it('should call the response method', () => {
+            sinon.assert.calledOnce(mockResponse)
+          })
+    
+          it('should pass correct template path to the response method', () => {
+            sinon.assert.calledWith(
+              mockResponse,
+              sinon.match.any,
+              sinon.match.any,
+              'simplified-account/services/payment-links/create/metadata'
+            )
+          })
+    
+          it('should set backLink in context', () => {
+            const context = mockResponse.args[0][3] as Record<string, unknown>
+            sinon.assert.match(context.backLink, sinon.match.string)
+            sinon.assert.match(context.backLink, sinon.match(/payment-links.*review/))
+          })
+    
+          it('should set createJourney in context', () => {
+            const context = mockResponse.args[0][3] as Record<string, unknown>
+            sinon.assert.match(context.createJourney, true)
+          })
+    
+          it('should set isWelsh to false for English language session', () => {
+            const context = mockResponse.args[0][3] as Record<string, unknown>
+            sinon.assert.match(context.isWelsh, false)
+          })
+        })
+
+    describe('with empty session data', () => {
+      beforeEach(async () => {
+        res.redirect.resetHistory()
+
+        nextRequest({
+          session: {},
+          body: {},
+        })
+
+        await call('get')
+      })
+
+      it('should redirect to payment links index', () => {
+        sinon.assert.calledOnce(res.redirect)
+        sinon.assert.calledWith(res.redirect, sinon.match(/payment-links/))
+      })
+    })
+  })
+})
