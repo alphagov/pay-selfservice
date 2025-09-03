f0d09e9e44a3d7a97948e7daa462228f777558a2
Marco Tranchino <marco.tranchino@digital.cabinet-office.gov.uk>
2025-09-03 15:20:44 +0100

    PP-14438 Improve payment link creation success banner
    With this change, we are improving the success banner at payment link
    creation, so that it displays an additional message with a link to test the
    newly created payment link if the payment link has been created in a test
    service or for a live service in sandbox mode.
    
    No message aside from the banner title will be displayed if the service is
    live and in normal mode.
    
    Further information in Jira.
    
    https://payments-platform.atlassian.net/browse/PP-14438

--- src/controllers/simplified-account/services/payment-links/create/payment-link-review.controller.test.ts
+++ src/controllers/simplified-account/services/payment-links/create/payment-link-review.controller.test.ts
@@ -1,6 +1,8 @@
 import ControllerTestBuilder from '@test/test-helpers/simplified-account/controllers/ControllerTestBuilder.class'
 import sinon from 'sinon'
 import GatewayAccountType from '@models/gateway-account/gateway-account-type'
+import GatewayAccount from '@models/gateway-account/GatewayAccount.class'
+import { validGatewayAccount } from '@test/fixtures/gateway-account.fixtures'
 import { FROM_REVIEW_QUERY_PARAM, PaymentLinkCreationSession } from './constants'
 import { CreateProductRequest } from '@models/products/CreateProductRequest.class'
 import productTypes from '@utils/product-types'
@@ -242,7 +244,19 @@ describe('controller: services/payment-links/create/payment-link-review', () => 
           flash: req.flash,
           user: {
             email: 'test@example.com'
-          }
+          },
+          service: {
+            name: 'McDuck Enterprises',
+            serviceName: { en: 'McDuck Enterprises', cy: 'Mentrau McDuck' },
+            externalId: SERVICE_EXTERNAL_ID
+          },
+          account: new GatewayAccount(
+            validGatewayAccount({
+              gateway_account_id: GATEWAY_ACCOUNT_ID,
+              external_id: SERVICE_EXTERNAL_ID,
+              type: GatewayAccountType.TEST
+              })
+            )
         })
 
         await call('post')
@@ -281,21 +295,93 @@ describe('controller: services/payment-links/create/payment-link-review', () => 
         sinon.assert.calledWith(mockCreateProduct, expectedCreateProductRequest)
       })
 
-      it('should set success flash message', () => {
+      it('should redirect to payment links index after successful creation', () => {
+        sinon.assert.calledOnce(res.redirect)
+        sinon.assert.calledWith(res.redirect, sinon.match(/payment-links/))
+      })
+
+      it('should set success message informing the user about testing the payment link (test service or live service in Sandbox mode)', () => {
         sinon.assert.calledWith(
           req.flash,
           'messages',
           sinon.match({
             state: 'success',
             icon: '&check;',
-            heading: 'Test Payment Link has been created'
+            heading: 'Test Payment Link has been created',
+            body: sinon.match("test your payment link")
           })
         )
       })
+    })
 
-      it('should redirect to payment links index after successful creation', () => {
-        sinon.assert.calledOnce(res.redirect)
-        sinon.assert.calledWith(res.redirect, sinon.match(/payment-links/))
+    describe('when the user confirms the creation of the payment link for a live service in normal mode', () => {
+      const mockPaymentLink = {
+        name: 'Test Payment Link',
+        links: {
+          pay: {
+            href: 'https://pay-test-link.gov.uk'
+          }
+        }
+      }
+
+      beforeEach(async () => {
+        req.flash.resetHistory()
+        res.redirect.resetHistory()
+        mockCreateProduct.resetHistory()
+        mockCreateProduct.resolves(mockPaymentLink)
+        mockCreatePaymentLinkToken.resetHistory()
+        mockCreatePaymentLinkToken.resolves('big-beautiful-token')
+
+        const sessionData: Partial<PaymentLinkCreationSession> = {
+          paymentLinkTitle: 'Test Payment Link',
+          paymentLinkDescription: 'Test Description',
+          language: 'en',
+          serviceNamePath: 'test-service',
+          productNamePath: 'test-payment-link',
+          paymentReferenceType: 'custom',
+          paymentReferenceLabel: 'Order Number',
+          paymentReferenceHint: 'Enter your order number',
+          paymentLinkAmount: 1500
+        }
+
+        nextRequest({
+          session: {
+            pageData: {
+              createPaymentLink: sessionData,
+            },
+          },
+          flash: req.flash,
+          user: {
+            email: 'test@example.com'
+          },
+          service: {
+            name: 'McDuck Enterprises',
+            serviceName: { en: 'McDuck Enterprises', cy: 'Mentrau McDuck' },
+            externalId: SERVICE_EXTERNAL_ID
+          },
+          account: new GatewayAccount(
+            validGatewayAccount({
+              gateway_account_id: GATEWAY_ACCOUNT_ID,
+              external_id: SERVICE_EXTERNAL_ID,
+              type: GatewayAccountType.LIVE
+              })
+            )
+        })
+
+        await call('post')
+      })
+
+      it('should set success flash message with empty body (live service in normal mode)', () => {
+        sinon.assert.calledWith(
+          req.flash,
+          'messages',
+          sinon.match({
+            state: 'success',
+            icon: '&check;',
+            heading: 'Test Payment Link has been created',
+            body: ''
+          })
+        )
       })
     })
   })
