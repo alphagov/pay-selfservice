3957fbcf7a6e7b101ebf957f7b625a148ef7732f
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2025-09-10 15:15:16 +0100

    PP-14411 extract support commands

--- test/cypress/support/index.js
+++ test/cypress/support/index.js
@@ -9,6 +9,7 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 import 'wick-a11y'
+import './payment-links'
 
 Cypress.Commands.add('setEncryptedCookies', (userId, pageData = {}) => {
   cy.task('getCookies', {
@@ -29,34 +30,6 @@ Cypress.Commands.add('a11yCheck', (excludeSelectors = { exclude: ['.govuk-skip-l
     }
   )
 })
-
-Cypress.Commands.add('createPaymentLinkWithTitle', (title, createUrl) => {
-  cy.visit(createUrl);
-  cy.get('#service-content').find('form').find('#name')
-    .click().focused()
-    .type(title)
-  cy.get('#service-content').find('form').find('button').click()
-});
-
-Cypress.Commands.add('createPaymentLinkWithReference', (title, createUrl) => {
-  cy.createPaymentLinkWithTitle(title, createUrl);
-  cy.get('#reference-type-standard').click()
-  cy.get('#service-content').find('form').find('button').click()
-});
-
-Cypress.Commands.add('createPaymentLinkWithAmount', (title, createUrl) => {
-  cy.createPaymentLinkWithReference(title, createUrl);
-  cy.get('#amount-type-variable').click()
-  cy.get('#service-content').find('form').find('button').click()
-});
-
-Cypress.Commands.add('createPaymentLinkWithMetadata', (title, createUrl, columnHeader, cellConetent) => {
-  cy.createPaymentLinkWithAmount(title, createUrl);
-  cy.get('.govuk-button--secondary').click()
-  cy.get('#service-content').find('form').find('#reporting-column').click().focused().clear().type(columnHeader)
-  cy.get('#service-content').find('form').find('#cell-content').click().focused().type(cellConetent)
-  cy.get('#service-content').find('form').find('button').contains('Add reporting column').click()
-});
 
 beforeEach(() => {
   cy.task('clearStubs')
