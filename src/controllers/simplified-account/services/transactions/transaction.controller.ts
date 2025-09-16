114a9bfda037aaa7d11b79d802cefff26fbff9eb
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2025-09-16 17:04:04 +0100

    PP-14283 WIP transaction controller

---
+++ src/controllers/simplified-account/services/transactions/transaction.controller.ts
@@ -0,0 +1,23 @@
+import { ServiceRequest, ServiceResponse } from '@utils/types/express'
+import { searchTransactions } from '@services/transactions.service'
+import { response } from '@utils/response'
+
+// async function get(req: ServiceRequest, res: ServiceResponse) {
+//   const { service, account } = req
+//   const gatewayAccountId = account.id
+//   const results = await searchTransactions(gatewayAccountId)
+
+//   return res.status(200).json(results)
+// }
+
+async function get(req: ServiceRequest, res: ServiceResponse) {
+  const { service, account } = req
+  const gatewayAccountId = account.id
+  const results = await searchTransactions(gatewayAccountId)
+
+  return response(req, res, 'simplified-account/transactions/index', {
+    results: results.transactions
+  })
+}
+
+export { get }
