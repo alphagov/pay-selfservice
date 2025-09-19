9aa95faa054eeed88b479058b7aafe22eedfe909
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2025-09-19 12:01:59 +0100

    PP-14283 add pagination WIP

--- src/controllers/simplified-account/services/transactions/transaction.controller.ts
+++ src/controllers/simplified-account/services/transactions/transaction.controller.ts
@@ -2,6 +2,9 @@ import { ServiceRequest, ServiceResponse } from '@utils/types/express'
 import { searchTransactions } from '@services/transactions.service'
 import { response } from '@utils/response'
 import { isBritishSummerTime } from '@utils/dates'
+import getPagination from '@utils/simplified-account/pagination'
+import formatServiceAndAccountPathsFor from '@utils/simplified-account/format/format-service-and-account-paths-for'
+import paths from '@root/paths'
 
 // async function get(req: ServiceRequest, res: ServiceResponse) {
 //   const { service, account } = req
@@ -15,10 +18,35 @@ async function get(req: ServiceRequest, res: ServiceResponse) {
   const { service, account } = req
   const gatewayAccountId = account.id
   const results = await searchTransactions(gatewayAccountId)
+  const PAGE_SIZE = 10
+
+  let currentPage = 1
+  const filters = {}
+
+  const transactionsUrl = formatServiceAndAccountPathsFor(
+    paths.simplifiedAccount.transactions.index,
+    service.externalId,
+    account.type
+  )
+
+  const totalPages = Math.ceil(results.total / PAGE_SIZE)
+  if (totalPages > 0 && currentPage > totalPages) {
+    currentPage = totalPages
+  }
+
+  const pagination = getPagination(currentPage, PAGE_SIZE, results.total, (pageNumber) => {
+    let path = `${transactionsUrl}?page=${pageNumber}`
+    if (filters && Object.keys(filters).length !== 0) {
+      const filterParams = new URLSearchParams(filters).toString()
+      path = `${path}&${filterParams}`
+    }
+    return path
+  })
 
   return response(req, res, 'simplified-account/transactions/index', {
     results: results.transactions,
-    isBST: isBritishSummerTime() as boolean
+    isBST: isBritishSummerTime() as boolean,
+    pagination: pagination
   })
 }
 
