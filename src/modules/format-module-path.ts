0413fa5efcea86d0406459acf784a263733b19c8
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-10-08 17:57:47 +0100

    [squash] cleanup

--- src/modules/format-module-path.ts
+++
@@ -1,7 +0,0 @@
-import { BaseModuleConstructor } from "@root/modules/app-module";
-import { formattedPathFor } from "@utils/simplified-account/format/format-path";
-
-export function formatModulePath (module: BaseModuleConstructor, ...params: string[]): string {
-  return formattedPathFor(module.path!, ...params)
-}
-
