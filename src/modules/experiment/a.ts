6a32e9b3c6769712a97aec9a4cc4f78644a5418b
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-10-09 15:22:59 +0100

    [squash] cleanup

--- src/modules/experiment/a.ts
+++
@@ -1,7 +0,0 @@
-import { b } from './b'
-
-export const a: { a: number; b: unknown } = {
-  a: 1,
-  b: () => b.b,
-}
-// export const aVal = 1
--- src/modules/experiment/b.ts
+++
@@ -1,8 +0,0 @@
-import { a } from './a'
-
-export const b: { a: unknown; b: number } = {
-  a: () => a.a,
-  b: 2,
-}
-
-// export const bVal = 2
