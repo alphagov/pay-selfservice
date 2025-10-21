8ebdb8dc402dea7332060d33169440032a052b9e
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-10-21 15:57:01 +0100

    [squash] convert rest of pact tests to new client

--- test/pact/adminusers-client/forgotten-password/create-forgotten-password.pact.test.js
+++ test/pact/adminusers-client/forgotten-password/create-forgotten-password.pact.test.js
@@ -2,10 +2,10 @@ const { Pact } = require('@pact-foundation/pact')
 const path = require('path')
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
-const getAdminUsersClient = require('../../../../src/services/clients/adminusers.client')
 const userFixtures = require('../../../fixtures/user.fixtures')
 const PactInteractionBuilder = require('../../../test-helpers/pact/pact-interaction-builder').PactInteractionBuilder
 const { pactify } = require('../../../test-helpers/pact/pactifier').defaultPactifier
+const AdminUsersClient = require('@services/clients/pay/AdminUsersClient.class')
 
 let adminUsersClient
 chai.use(chaiAsPromised)
@@ -19,12 +19,12 @@ describe('adminusers client - create forgotten password', function () {
     log: path.resolve(process.cwd(), 'logs', 'mockserver-integration.log'),
     dir: path.resolve(process.cwd(), 'pacts'),
     spec: 2,
-    pactfileWriteMode: 'merge'
+    pactfileWriteMode: 'merge',
   })
 
   before(async () => {
     const opts = await provider.setup()
-    adminUsersClient = getAdminUsersClient({ baseUrl: `http://127.0.0.1:${opts.port}` })
+    adminUsersClient = new AdminUsersClient(`http://127.0.0.1:${opts.port}`)
   })
   after(() => provider.finalize())
 
@@ -33,22 +33,24 @@ describe('adminusers client - create forgotten password', function () {
     const request = userFixtures.validForgottenPasswordCreateRequest(existingUserEmail)
 
     before((done) => {
-      provider.addInteraction(
-        new PactInteractionBuilder(FORGOTTEN_PASSWORD_PATH)
-          .withState(`a user exists with email ${existingUserEmail}`)
-          .withUponReceiving('a valid forgotten password request')
-          .withMethod('POST')
-          .withRequestBody(request)
-          .withStatusCode(200)
-          .withResponseHeaders({})
-          .build()
-      ).then(() => done())
+      provider
+        .addInteraction(
+          new PactInteractionBuilder(FORGOTTEN_PASSWORD_PATH)
+            .withState(`a user exists with email ${existingUserEmail}`)
+            .withUponReceiving('a valid forgotten password request')
+            .withMethod('POST')
+            .withRequestBody(request)
+            .withStatusCode(200)
+            .withResponseHeaders({})
+            .build()
+        )
+        .then(() => done())
     })
 
     afterEach(() => provider.verify())
 
     it('should create a forgotten password entry successfully', function (done) {
-      adminUsersClient.createForgottenPassword(request.username).should.notify(done)
+      adminUsersClient.users.createForgottenPassword(request.username).should.notify(done)
     })
   })
 
@@ -58,24 +60,29 @@ describe('adminusers client - create forgotten password', function () {
     const badForgottenPasswordResponse = userFixtures.badForgottenPasswordResponse()
 
     before((done) => {
-      provider.addInteraction(
-        new PactInteractionBuilder(FORGOTTEN_PASSWORD_PATH)
-          .withUponReceiving('an invalid forgotten password request')
-          .withMethod('POST')
-          .withRequestBody(request)
-          .withStatusCode(400)
-          .withResponseBody(pactify(badForgottenPasswordResponse))
-          .build()
-      ).then(() => done())
+      provider
+        .addInteraction(
+          new PactInteractionBuilder(FORGOTTEN_PASSWORD_PATH)
+            .withUponReceiving('an invalid forgotten password request')
+            .withMethod('POST')
+            .withRequestBody(request)
+            .withStatusCode(400)
+            .withResponseBody(pactify(badForgottenPasswordResponse))
+            .build()
+        )
+        .then(() => done())
     })
 
     afterEach(() => provider.verify())
 
     it('should error when forgotten password creation if mandatory fields are missing', function (done) {
-      adminUsersClient.createForgottenPassword(request.username).should.be.rejected.then(function (err) {
-        expect(err.errorCode).to.equal(400)
-        expect(err.message).to.equal(badForgottenPasswordResponse.errors[0])
-      }).should.notify(done)
+      adminUsersClient.users
+        .createForgottenPassword(request.username)
+        .should.be.rejected.then(function (err) {
+          expect(err.errorCode).to.equal(400)
+          expect(err.message).to.equal(badForgottenPasswordResponse.errors[0])
+        })
+        .should.notify(done)
     })
   })
 
@@ -83,24 +90,29 @@ describe('adminusers client - create forgotten password', function () {
     const request = userFixtures.validForgottenPasswordCreateRequest('nonexisting')
 
     before((done) => {
-      provider.addInteraction(
-        new PactInteractionBuilder(FORGOTTEN_PASSWORD_PATH)
-          .withState('a user does not exist')
-          .withUponReceiving('a forgotten password request for non existent user')
-          .withMethod('POST')
-          .withRequestBody(request)
-          .withStatusCode(404)
-          .withResponseHeaders({})
-          .build()
-      ).then(() => done())
+      provider
+        .addInteraction(
+          new PactInteractionBuilder(FORGOTTEN_PASSWORD_PATH)
+            .withState('a user does not exist')
+            .withUponReceiving('a forgotten password request for non existent user')
+            .withMethod('POST')
+            .withRequestBody(request)
+            .withStatusCode(404)
+            .withResponseHeaders({})
+            .build()
+        )
+        .then(() => done())
     })
 
     afterEach(() => provider.verify())
 
     it('should error when forgotten password creation if no user found', function (done) {
-      adminUsersClient.createForgottenPassword(request.username).should.be.rejected.then(function (response) {
-        expect(response.errorCode).to.equal(404)
-      }).should.notify(done)
+      adminUsersClient.users
+        .createForgottenPassword(request.username)
+        .should.be.rejected.then(function (response) {
+          expect(response.errorCode).to.equal(404)
+        })
+        .should.notify(done)
     })
   })
 })
