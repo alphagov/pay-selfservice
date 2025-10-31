bd5d7b0f0c6499650cf20a8bb6f2c6ea1202a919
Rafiq Gasmi <rafiq.gasmi@digital.cabinet-office.gov.uk>
2025-10-31 11:49:45 +0000

    PP-14612 - revert refactoring

--- src/utils/simplified-account/format/format-service-and-account-paths-for.ts
+++ src/utils/simplified-account/format/format-service-and-account-paths-for.ts
@@ -1,38 +1,24 @@
 import paths from '@root/paths'
 import urlJoin from '@utils/simplified-account/format/url'
 
-const escapeRegExp = (s: string): string => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
-
-export const formattedPathFor = (route: string, ...pathParams: (string | undefined)[]): string => {
-  const paramNames = route.split('/').filter((segment) => segment.startsWith(':'))
-  const encodedValues = pathParams.map((v) => (v === undefined ? undefined : encodeURIComponent(String(v))))
-
-  return paramNames.reduce((currentRoute, paramName, index) => {
-    const value = encodedValues[index]
-    if (value === undefined) return currentRoute
-
-    const nameWithoutColon = paramName.slice(1)
-    const pattern = new RegExp(`:${escapeRegExp(nameWithoutColon)}(?=/|$)`, 'g')
-    return currentRoute.replace(pattern, value)
-  }, route)
-}
-
 function formatServiceAndAccountPathsFor(
-  route: string,
+  path: string,
   serviceExternalId: string,
   accountType: string,
   ...params: string[]
-): string {
-  const completePath = urlJoin(paths.simplifiedAccount.root, route)
+) {
+  const completePath = urlJoin(paths.simplifiedAccount.root, path)
   return formattedPathFor(completePath, serviceExternalId, accountType, ...params)
 }
 
-export default formatServiceAndAccountPathsFor
-
-const maybeModule = typeof module !== 'undefined' ? (module as unknown as { exports?: unknown }) : undefined
-if (maybeModule) {
-  maybeModule.exports = Object.assign(formatServiceAndAccountPathsFor, {
-    formattedPathFor,
-    default: formatServiceAndAccountPathsFor,
+const formattedPathFor = (path: string, ...pathParams: string[]) => {
+  const paramNames = path.split('/').filter((segment) => segment.startsWith(':'))
+  paramNames.forEach((paramName, index) => {
+    if (pathParams[index]) {
+      path = path.replace(paramName, encodeURIComponent(pathParams[index]))
+    }
   })
+  return path
 }
+
+export = formatServiceAndAccountPathsFor
