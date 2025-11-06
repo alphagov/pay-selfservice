676b7487a57b355bc0cace49fe929330f7461a1c
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-11-06 16:12:44 +0000

    [squash] formatting

--- src/controllers/user/phone-number/get.controller.js
+++ src/controllers/user/phone-number/get.controller.js
@@ -1,9 +1,7 @@
-'use strict'
-
 const { response } = require('@utils/response')
 const userService = require('@services/user.service')
 
-module.exports = async function showUpdatePhoneNumber (req, res, next) {
+module.exports = async function showUpdatePhoneNumber(req, res, next) {
   try {
     const { telephoneNumber } = await userService.findByExternalId(req.user.externalId)
     return response(req, res, 'user/edit-phone-number', { telephoneNumber })
--- src/controllers/user/phone-number/post.controller.js
+++ src/controllers/user/phone-number/post.controller.js
@@ -1,19 +1,17 @@
-'use strict'
-
 const { response } = require('@utils/response')
 const userService = require('@services/user.service')
 const paths = require('@root/paths')
 const { invalidTelephoneNumber } = require('@utils/telephone-number-utils')
 const { validationErrors } = require('@utils/validation/field-validation-checks')
 
-module.exports = async function updatePhoneNumber (req, res, next) {
+module.exports = async function updatePhoneNumber(req, res, next) {
   const telephoneNumber = req.body.phone
   if (invalidTelephoneNumber(telephoneNumber)) {
     const pageData = {
       telephoneNumber,
       errors: {
-        phone: validationErrors.invalidTelephoneNumber
-      }
+        phone: validationErrors.invalidTelephoneNumber,
+      },
     }
     return response(req, res, 'user/edit-phone-number', pageData)
   }
