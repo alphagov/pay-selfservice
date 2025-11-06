58010dddccb16f9b4f84af8971238ea82006aa94
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-11-06 15:16:49 +0000

    [squash] cleanup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-const CORRELATION_HEADER = process.env.CORRELATION_HEADER_NAME || 'x-request-id'
+const CORRELATION_HEADER = process.env.CORRELATION_HEADER_NAME ?? 'x-request-id'
 const PRODUCTS_URL = process.env.PRODUCTS_URL
 const PUBLIC_AUTH_URL = process.env.PUBLIC_AUTH_URL
 
