208c4633c70b3e75016bfc1e0d6319d06396aa40
Dom Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-11-07 09:27:58 +0000

    PP-13526 Cleanup old team members code (#4613)
    * PP-13526 Cleanup old team members code

--- src/controllers/user/phone-number/get.controller.js
+++ src/controllers/user/phone-number/get.controller.js
@@ -1,12 +1,10 @@
-'use strict'
+const { response } = require('@utils/response')
+const userService = require('@services/user.service')
 
-const { response } = require('../../../utils/response')
-const userService = require('../../../services/user.service')
-
-module.exports = async function showUpdatePhoneNumber (req, res, next) {
+module.exports = async function showUpdatePhoneNumber(req, res, next) {
   try {
     const { telephoneNumber } = await userService.findByExternalId(req.user.externalId)
-    return response(req, res, 'team-members/edit-phone-number', { telephoneNumber })
+    return response(req, res, 'user/edit-phone-number', { telephoneNumber })
   } catch (err) {
     next(err)
   }
--- src/controllers/user/phone-number/post.controller.js
+++ src/controllers/user/phone-number/post.controller.js
@@ -1,21 +1,19 @@
-'use strict'
+const { response } = require('@utils/response')
+const userService = require('@services/user.service')
+const paths = require('@root/paths')
+const { invalidTelephoneNumber } = require('@utils/telephone-number-utils')
+const { validationErrors } = require('@utils/validation/field-validation-checks')
 
-const { response } = require('../../../utils/response')
-const userService = require('../../../services/user.service')
-const paths = require('../../../paths')
-const { invalidTelephoneNumber } = require('../../../utils/telephone-number-utils')
-const { validationErrors } = require('../../../utils/validation/field-validation-checks')
-
-module.exports = async function updatePhoneNumber (req, res, next) {
+module.exports = async function updatePhoneNumber(req, res, next) {
   const telephoneNumber = req.body.phone
   if (invalidTelephoneNumber(telephoneNumber)) {
     const pageData = {
       telephoneNumber,
       errors: {
-        phone: validationErrors.invalidTelephoneNumber
-      }
+        phone: validationErrors.invalidTelephoneNumber,
+      },
     }
-    return response(req, res, 'team-members/edit-phone-number', pageData)
+    return response(req, res, 'user/edit-phone-number', pageData)
   }
 
   try {
