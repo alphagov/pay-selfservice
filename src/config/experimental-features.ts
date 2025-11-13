74e48a17245f297357400ff957953be423d57c82
Dom Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2025-11-13 13:30:38 +0000

    PP-14680 Use combination of features flag & list (#4806)
    * PP-14680 Use combination of features flag & list to control experimental features

--- src/config/experimental-features.ts
+++ src/config/experimental-features.ts
@@ -1,13 +1,13 @@
-const EXPERIMENTAL_FEATURES_FLAG = process.env.EXPERIMENTAL_FEATURES_FLAG ?? ''
+const EXPERIMENTAL_FEATURES_FLAG = process.env.EXPERIMENTAL_FEATURES_FLAG === 'true'
+const EXPERIMENTAL_FEATURES_LIST = process.env.EXPERIMENTAL_FEATURES_LIST ?? ''
 
-const ENABLED_FEATURES =
-  EXPERIMENTAL_FEATURES_FLAG === 'true' ? ['all'] : EXPERIMENTAL_FEATURES_FLAG.toLowerCase().split(',')
+const ENABLED_FEATURES = EXPERIMENTAL_FEATURES_LIST.toLowerCase().split(',')
 const FEATURE_SET = new Set(ENABLED_FEATURES)
 
 const Features = {
   enabled: ENABLED_FEATURES,
   isEnabled: (featureName: string) => {
-    return FEATURE_SET.has('all') || FEATURE_SET.has(featureName)
+    return EXPERIMENTAL_FEATURES_FLAG && (FEATURE_SET.has('all') || FEATURE_SET.has(featureName))
   },
   PAYMENT_LINKS: 'payment_links',
   MY_SERVICES: 'my_services',
