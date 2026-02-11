030cbdc3a37a3d9b5d207d9621278dd2c66beaa5
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2026-02-11 12:54:39 +0000

    [squash] fix tests

--- test/test-helpers/suppress-logs.js
+++ test/test-helpers/suppress-logs.js
@@ -1,4 +1,4 @@
 'use strict'
 
-const logger = require('../../src/utils/logger')(__filename)
-logger.transports.forEach(t => (t.silent = true))
+const logger = require('@utils/logger/logger')(__filename)
+logger.transports.forEach((t) => (t.silent = true))
