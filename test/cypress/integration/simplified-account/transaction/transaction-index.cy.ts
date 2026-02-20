67868917cdd91cc7156f3b0ec6acc28292225d19
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2026-02-20 14:26:54 +0000

    PP-14616 page content tests

--- test/cypress/integration/simplified-account/transaction/transaction-index.cy.ts
+++ test/cypress/integration/simplified-account/transaction/transaction-index.cy.ts
@@ -1,12 +1,10 @@
 import userStubs from '@test/cypress/stubs/user-stubs'
 import { WORLDPAY } from '@models/constants/payment-providers'
 import gatewayAccountStubs from '@test/cypress/stubs/gateway-account-stubs'
-import { getTransactionsForGatewayAccount } from '@test/cypress/stubs/simplified-account/transaction-stubs'
 import { TransactionFixture } from '@test/fixtures/transaction/transaction.fixture'
 import { checkServiceNavigation } from '../common/assertions'
 import { TEST } from '@models/gateway-account/gateway-account-type'
 import transactionStubs from '@test/cypress/stubs/transaction-stubs'
-import { Transaction } from '@models/transaction/Transaction.class'
 import { last12MonthsStartDate } from '@utils/simplified-account/services/dashboard/datetime-utils'
 
 const TRANSACTION = new TransactionFixture()
@@ -21,7 +19,7 @@ const SERVICE_NAME = {
 
 const TRANSACTIONS_LIST_URL = `/service/${SERVICE_EXTERNAL_ID}/account/${TEST}/transactions`
 
-const setupStubs = (gatewayAccountType = 'test', transactions: TransactionFixture[]) => {
+const sharedStubs = (gatewayAccountType = 'test') => {
   cy.task('setupStubs', [
     userStubs.getUserSuccess({
       userExternalId: USER_EXTERNAL_ID,
@@ -35,14 +33,6 @@ const setupStubs = (gatewayAccountType = 'test', transactions: TransactionFixtur
       payment_provider: WORLDPAY,
     }),
     gatewayAccountStubs.getCardTypesSuccess(),
-
-    transactionStubs.getLedgerTransactionsSuccess({
-      gatewayAccountId: GATEWAY_ACCOUNT_ID,
-      transactions,
-      filters: { from_date: last12MonthsStartDate },
-      displaySize: 20,
-      transactionLength: 1
-    })
   ])
 }
 
@@ -52,15 +42,23 @@ describe('Transactions index', () => {
     cy.setEncryptedCookies(USER_EXTERNAL_ID)
   })
 
-
   describe('Common page content', () => {
     beforeEach(() => {
-      setupStubs('test', [TRANSACTION])
+      sharedStubs('test'),
+        cy.task('setupStubs', [
+          transactionStubs.getLedgerTransactionsSuccess({
+            gatewayAccountId: GATEWAY_ACCOUNT_ID,
+            transactions: [TRANSACTION],
+            filters: { from_date: last12MonthsStartDate },
+            displaySize: 20,
+            transactionLength: 1
+          })
+        ])
       cy.visit(TRANSACTIONS_LIST_URL, { failOnStatusCode: false })
     })
 
-    it('should show the payment links navigation item in the side bar in an active state', () => {
-      checkServiceNavigation('Payment links', TRANSACTIONS_LIST_URL)
+    it('should show the transactions item in the side bar in an active state', () => {
+      checkServiceNavigation('Transactions', TRANSACTIONS_LIST_URL)
     })
 
     it('accessibility check', () => {
