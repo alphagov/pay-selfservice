22ed1e4009563978ccda181ec1ab5505efea0364
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2026-03-27 15:12:04 +0000

    PP-14622 add Cypress test

---
+++ test/cypress/integration/simplified-account/all-service-transactions/all-service-transactions-index.cy.ts
@@ -0,0 +1,127 @@
+import userStubs from '@test/cypress/stubs/user-stubs'
+import gatewayAccountStubs, { getCardTypesSuccess } from '@test/cypress/stubs/gateway-account-stubs'
+import { TransactionFixture } from '@test/fixtures/transaction/transaction.fixture'
+import { LIVE, TEST } from '@models/gateway-account/gateway-account-type'
+import { last12MonthsStartDate } from '@utils/simplified-account/services/dashboard/datetime-utils'
+import { getTransactionEvents, getTransactionForGatewayAccount } from '@test/cypress/stubs/simplified-account/transaction-stubs'
+import ROLES from '@test/fixtures/roles.fixtures'
+import { TransactionEventFixture } from '@test/fixtures/transaction/transaction-event.fixture'
+import transactionStubs from '@test/cypress/stubs/transaction-stubs'
+
+const TRANSACTION = new TransactionFixture()
+const TRANSACTION_CREATED_TIMESTAMP = TRANSACTION.createdDate
+
+const TRANSACTION_EVENTS = [
+  new TransactionEventFixture({
+    amount: 1250,
+    state: {
+      finished: false,
+      status: 'CREATED',
+    },
+    resourceType: 'PAYMENT',
+    eventType: 'PAYMENT_CREATED',
+    timestamp: TRANSACTION_CREATED_TIMESTAMP,
+  }),
+]
+
+const USER_EXTERNAL_ID = 'user123abc'
+const SERVICE_EXTERNAL_ID = 'service456def'
+const GATEWAY_ACCOUNT_ID = '1'
+const SERVICE_NAME = {
+  en: 'McDuck Enterprises',
+  cy: 'Mentrau McDuck',
+}
+const USER_EMAIL = 's.mcduck@example.com'
+
+const TEST_TRANSACTIONS_LIST_URL = `/transactions/${TEST}`
+const LIVE_TRANSACTIONS_LIST_URL = `/transactions/${LIVE}`
+
+const HEADING_SUFFIX = 'transactions: all services'
+
+const userAndGatewayAccountStubs = [
+  userStubs.getUserSuccess({
+    userExternalId: USER_EXTERNAL_ID,
+    email: USER_EMAIL,
+    serviceExternalId: SERVICE_EXTERNAL_ID,
+    gatewayAccountId: GATEWAY_ACCOUNT_ID,
+    serviceName: SERVICE_NAME,
+    role: ROLES['view-and-refund'],
+  }),
+]
+
+describe('All service transactions index', () => {
+  beforeEach(() => {
+    cy.setEncryptedCookies(USER_EXTERNAL_ID)
+    cy.task('setupStubs', [
+      ...userAndGatewayAccountStubs,
+      getTransactionForGatewayAccount(GATEWAY_ACCOUNT_ID, TRANSACTION.externalId).success(TRANSACTION),
+      getTransactionEvents(GATEWAY_ACCOUNT_ID, TRANSACTION.externalId).success(TRANSACTION_EVENTS),
+      getCardTypesSuccess(),
+      transactionStubs.getLedgerTransactionsSuccess({
+        gatewayAccountId: GATEWAY_ACCOUNT_ID,
+        transactions: [TRANSACTION],
+        filters: { from_date: last12MonthsStartDate },
+        displaySize: 20,
+        transactionLength: 1,
+      }),
+
+      gatewayAccountStubs.getGatewayAccountByServiceIdsSuccess({
+        serviceExternalId: SERVICE_EXTERNAL_ID,
+        type: 'test',
+        gatewayAccountId: GATEWAY_ACCOUNT_ID,
+      }),
+      gatewayAccountStubs.getGatewayAccountByServiceIdsSuccess({
+        serviceExternalId: SERVICE_EXTERNAL_ID,
+        type: 'live',
+        gatewayAccountId: GATEWAY_ACCOUNT_ID,
+      }),
+
+    ])
+  })
+
+  describe('Live page content', () => {
+    beforeEach(() => {
+      cy.visit(LIVE_TRANSACTIONS_LIST_URL)
+    })
+
+    it('accessibility check', () => {
+      cy.a11yCheck()
+    })
+
+    it('should display correct heading', () => {
+      const heading = `Live ${HEADING_SUFFIX}`
+
+      cy.title().should('eq', `${heading} - GOV.UK Pay`)
+      cy.get('h1').should('contain.text', heading)
+    })
+
+    it('should navigate to test transactions', () => {
+      cy.contains('a.govuk-link', 'View test transactions').should('be.visible').click()
+      cy.get('h1').should('contain.text', 'Test transactions: all services')
+      cy.url().should('include', TEST_TRANSACTIONS_LIST_URL)
+    })
+  })
+
+  describe('Test page content', () => {
+    beforeEach(() => {
+      cy.visit(TEST_TRANSACTIONS_LIST_URL)
+    })
+
+    it('accessibility check', () => {
+      cy.a11yCheck()
+    })
+
+    it('should display correct heading', () => {
+      const heading = `Test ${HEADING_SUFFIX}`
+
+      cy.title().should('eq', `${heading} - GOV.UK Pay`)
+      cy.get('h1').should('contain.text', heading)
+    })
+
+    it('should navigate to live transactions', () => {
+      cy.contains('a.govuk-link', 'View live transactions').should('be.visible').click()
+      cy.get('h1').should('contain.text', 'Live transactions: all services')
+      cy.url().should('include', LIVE_TRANSACTIONS_LIST_URL)
+    })
+  })
+})
