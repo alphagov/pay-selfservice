e32383f9892e76cb4126fcdb39027e0f2a3c6277
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2026-04-14 13:58:14 +0100

    PP-15159 prettier and secrets baseline

--- test/cypress/integration/all-service-transactions/all-service-transactions.cy.js
+++ test/cypress/integration/all-service-transactions/all-service-transactions.cy.js
@@ -12,29 +12,29 @@ const gatewayAccountStripe = {
   gatewayAccountExternalId: 'a-valid-external-id-1',
   type: 'live',
   paymentProvider: 'stripe',
-  recurringEnabled: true
+  recurringEnabled: true,
 }
 const gatewayAccount2 = {
   gatewayAccountId: 43,
   gatewayAccountExternalId: 'a-valid-external-id-2',
   type: 'live',
-  recurringEnabled: true
+  recurringEnabled: true,
 }
 const gatewayAccount3 = {
   gatewayAccountId: 44,
   gatewayAccountExternalId: 'a-valid-external-id-3',
   type: 'test',
-  recurringEnabled: true
+  recurringEnabled: true,
 }
 const userStub = userStubs.getUserSuccessWithMultipleServices(userExternalId, [
   {
     gatewayAccountId: gatewayAccountStripe.gatewayAccountId,
-    serviceName: 'Service 1'
+    serviceName: 'Service 1',
   },
   {
     gatewayAccountIds: [gatewayAccount2.gatewayAccountId, gatewayAccount3.gatewayAccountId],
-    serviceName: 'Service 2'
-  }
+    serviceName: 'Service 2',
+  },
 ])
 
 const liveTransactions = [
@@ -42,14 +42,14 @@ const liveTransactions = [
     gateway_account_id: String(gatewayAccountStripe.gatewayAccountId),
     reference: 'ref1',
     transaction_id: 'transaction-id-1',
-    live: true
+    live: true,
   },
   {
     gateway_account_id: String(gatewayAccount2.gatewayAccountId),
     reference: 'ref2',
     transaction_id: 'transaction-id-2',
-    live: true
-  }
+    live: true,
+  },
 ]
 
 const testTransactions = [
@@ -62,14 +62,14 @@ const testTransactions = [
     state: { finished: true, status: 'success' },
     refund_summary_status: 'available',
     refund_summary_available: 5000,
-    refund_summary_submitted: 0
+    refund_summary_submitted: 0,
   },
   {
     gateway_account_id: String(gatewayAccount3.gatewayAccountId),
     reference: 'ref4',
     transaction_id: 'transaction-id-4',
-    live: false
-  }
+    live: false,
+  },
 ]
 
 describe('All service transactions', () => {
@@ -80,16 +80,20 @@ describe('All service transactions', () => {
   it('should display All Service Transactions list page with live transactions and ability to view test transactions', () => {
     cy.task('setupStubs', [
       userStub,
-      gatewayAccountStubs.getGatewayAccountsSuccessForMultipleAccounts([gatewayAccountStripe, gatewayAccount2, gatewayAccount3]),
+      gatewayAccountStubs.getGatewayAccountsSuccessForMultipleAccounts([
+        gatewayAccountStripe,
+        gatewayAccount2,
+        gatewayAccount3,
+      ]),
       transactionStubs.getLedgerTransactionsSuccess({
         gatewayAccountIds: [gatewayAccountStripe.gatewayAccountId, gatewayAccount2.gatewayAccountId],
-        transactions: liveTransactions
+        transactions: liveTransactions,
       }),
       transactionStubs.getLedgerTransactionsSuccess({
         gatewayAccountIds: [gatewayAccount3.gatewayAccountId],
-        transactions: testTransactions
+        transactions: testTransactions,
       }),
-      gatewayAccountStubs.getCardTypesSuccess()
+      gatewayAccountStubs.getCardTypesSuccess(),
     ])
 
     cy.visit(transactionsUrl)
@@ -102,8 +106,12 @@ describe('All service transactions', () => {
     })
 
     cy.get('.transactions-list--row').should('have.length', 2)
-    cy.get('#charge-id-transaction-id-1').should('exist').should('have.attr', 'href', '/redirect/transactions/transaction-id-1')
-    cy.get('#charge-id-transaction-id-2').should('exist').should('have.attr', 'href', '/redirect/transactions/transaction-id-2')
+    cy.get('#charge-id-transaction-id-1')
+      .should('exist')
+      .should('have.attr', 'href', '/redirect/transactions/transaction-id-1')
+    cy.get('#charge-id-transaction-id-2')
+      .should('exist')
+      .should('have.attr', 'href', '/redirect/transactions/transaction-id-2')
 
     cy.visit(transactionsUrl)
     cy.log('Switch to view test transactions')
@@ -123,24 +131,34 @@ describe('All service transactions', () => {
   it('should have links back to all service transactions when navigating to transaction detail page', () => {
     cy.task('setupStubs', [
       userStub,
-      gatewayAccountStubs.getGatewayAccountsSuccessForMultipleAccounts([gatewayAccountStripe, gatewayAccount2, gatewayAccount3]),
+      gatewayAccountStubs.getGatewayAccountsSuccessForMultipleAccounts([
+        gatewayAccountStripe,
+        gatewayAccount2,
+        gatewayAccount3,
+      ]),
       transactionStubs.getLedgerTransactionsSuccess({
         gatewayAccountIds: [gatewayAccount3.gatewayAccountId],
-        transactions: testTransactions
+        transactions: testTransactions,
       }),
       transactionStubs.getLedgerTransactionsSuccess({
         gatewayAccountIds: [gatewayAccount3.gatewayAccountId],
         transactions: [testTransactions[0]],
         filters: {
-          reference: 'ref3'
-        }
+          reference: 'ref3',
+        },
       }),
       gatewayAccountStubs.getCardTypesSuccess(),
-      transactionStubs.getLedgerTransactionSuccess({ transactionDetails: testTransactions[0], gatewayAccountId: gatewayAccount3.gatewayAccountId }),
+      transactionStubs.getLedgerTransactionSuccess({
+        transactionDetails: testTransactions[0],
+        gatewayAccountId: gatewayAccount3.gatewayAccountId,
+      }),
       transactionStubs.getLedgerTransactionSuccess({ transactionDetails: testTransactions[0] }),
       gatewayAccountStubs.getGatewayAccountSuccess(gatewayAccount3),
       gatewayAccountStubs.getGatewayAccountByExternalIdSuccess(gatewayAccount3),
-      transactionStubs.getLedgerEventsSuccess({ transactionId: 'transaction-id-3', gatewayAccountId: gatewayAccount3.gatewayAccountId })
+      transactionStubs.getLedgerEventsSuccess({
+        transactionId: 'transaction-id-3',
+        gatewayAccountId: gatewayAccount3.gatewayAccountId,
+      }),
     ])
 
     cy.visit('/all-service-transactions/test')
@@ -152,10 +170,8 @@ describe('All service transactions', () => {
 
     cy.get('#charge-id-transaction-id-3').click()
 
-    cy.get('.transaction-details tbody').find('tr').first().find('td').first().should('contain',
-      'Service 2')
-    cy.get('.transaction-details tbody').find('tr').eq(1).find('td').first().should('contain',
-      'ref3')
+    cy.get('.transaction-details tbody').find('tr').first().find('td').first().should('contain', 'Service 2')
+    cy.get('.transaction-details tbody').find('tr').eq(1).find('td').first().should('contain', 'ref3')
 
     cy.get('.govuk-breadcrumbs').within(() => {
       cy.get('.govuk-breadcrumbs__list-item').should('have.length', 2)
@@ -165,7 +181,11 @@ describe('All service transactions', () => {
 
     cy.get('.govuk-back-link')
       .should('have.text', 'Back to transactions for all services')
-      .should('have.attr', 'href', '/all-service-transactions/test?reference=ref3&email=&cardholderName=&lastDigitsCardNumber=&fromDate=&fromTime=&toDate=&toTime=&metadataValue=&agreementId=')
+      .should(
+        'have.attr',
+        'href',
+        '/all-service-transactions/test?reference=ref3&email=&cardholderName=&lastDigitsCardNumber=&fromDate=&fromTime=&toDate=&toTime=&metadataValue=&agreementId='
+      )
 
     cy.get('a.refund__toggle').click()
     cy.get('button').contains('Confirm refund').click()
@@ -178,28 +198,42 @@ describe('All service transactions', () => {
 
     cy.get('.govuk-back-link')
       .should('have.text', 'Back to transactions for all services')
-      .should('have.attr', 'href', '/all-service-transactions/test?reference=ref3&email=&cardholderName=&lastDigitsCardNumber=&fromDate=&fromTime=&toDate=&toTime=&metadataValue=&agreementId=')
+      .should(
+        'have.attr',
+        'href',
+        '/all-service-transactions/test?reference=ref3&email=&cardholderName=&lastDigitsCardNumber=&fromDate=&fromTime=&toDate=&toTime=&metadataValue=&agreementId='
+      )
   })
 
   it('should redirect from /all-services-transactions/live to failure page when ledger returns 504 and allow user to navigate to no autosearch transactions page', () => {
     cy.task('setupStubs', [
       userStub,
-      gatewayAccountStubs.getGatewayAccountsSuccessForMultipleAccounts([gatewayAccountStripe, gatewayAccount2, gatewayAccount3]),
+      gatewayAccountStubs.getGatewayAccountsSuccessForMultipleAccounts([
+        gatewayAccountStripe,
+        gatewayAccount2,
+        gatewayAccount3,
+      ]),
 
-      transactionStubs.getLedgerTransactionsFailure({
-        account_id: `${gatewayAccountStripe.gatewayAccountId},${gatewayAccount2.gatewayAccountId}`,
-        page: 1,
-        display_size: 100,
-        limit_total: true,
-        limit_total_size: 5001
-      }, 504),
-      gatewayAccountStubs.getCardTypesSuccess()
+      transactionStubs.getLedgerTransactionsFailure(
+        {
+          account_id: `${gatewayAccountStripe.gatewayAccountId},${gatewayAccount2.gatewayAccountId}`,
+          page: 1,
+          display_size: 100,
+          limit_total: true,
+          limit_total_size: 5001,
+        },
+        504
+      ),
+      gatewayAccountStubs.getCardTypesSuccess(),
     ])
 
     cy.visit('/all-service-transactions/live', { failOnStatusCode: false })
 
     cy.get('.govuk-heading-l').should('have.text', 'An error occurred')
-    cy.get('#errorMsg').should('have.text', 'The search has timed out. Try searching for a specific date range or applying other filters.')
+    cy.get('#errorMsg').should(
+      'have.text',
+      'The search has timed out. Try searching for a specific date range or applying other filters.'
+    )
 
     cy.get('.govuk-body').get('a').contains('Back to transactions search').click()
 
@@ -209,22 +243,32 @@ describe('All service transactions', () => {
   it('should redirect from /all-services-transactions/test to failure page when ledger returns 504 and allow user to navigate to no autosearch transactions page', () => {
     cy.task('setupStubs', [
       userStub,
-      gatewayAccountStubs.getGatewayAccountsSuccessForMultipleAccounts([gatewayAccountStripe, gatewayAccount2, gatewayAccount3]),
+      gatewayAccountStubs.getGatewayAccountsSuccessForMultipleAccounts([
+        gatewayAccountStripe,
+        gatewayAccount2,
+        gatewayAccount3,
+      ]),
 
-      transactionStubs.getLedgerTransactionsFailure({
-        account_id: `${gatewayAccount3.gatewayAccountId}`,
-        page: 1,
-        display_size: 100,
-        limit_total: true,
-        limit_total_size: 5001
-      }, 504),
-      gatewayAccountStubs.getCardTypesSuccess()
+      transactionStubs.getLedgerTransactionsFailure(
+        {
+          account_id: `${gatewayAccount3.gatewayAccountId}`,
+          page: 1,
+          display_size: 100,
+          limit_total: true,
+          limit_total_size: 5001,
+        },
+        504
+      ),
+      gatewayAccountStubs.getCardTypesSuccess(),
     ])
 
     cy.visit('/all-service-transactions/test', { failOnStatusCode: false })
 
     cy.get('.govuk-heading-l').should('have.text', 'An error occurred')
-    cy.get('#errorMsg').should('have.text', 'The search has timed out. Try searching for a specific date range or applying other filters.')
+    cy.get('#errorMsg').should(
+      'have.text',
+      'The search has timed out. Try searching for a specific date range or applying other filters.'
+    )
 
     cy.get('.govuk-body').get('a').contains('Back to transactions search').click()
 
@@ -234,22 +278,32 @@ describe('All service transactions', () => {
   it('should redirect from /all-services-transactions to failure page when ledger returns 504 and allow user to navigate to no autosearch transactions page', () => {
     cy.task('setupStubs', [
       userStub,
-      gatewayAccountStubs.getGatewayAccountsSuccessForMultipleAccounts([gatewayAccountStripe, gatewayAccount2, gatewayAccount3]),
+      gatewayAccountStubs.getGatewayAccountsSuccessForMultipleAccounts([
+        gatewayAccountStripe,
+        gatewayAccount2,
+        gatewayAccount3,
+      ]),
 
-      transactionStubs.getLedgerTransactionsFailure({
-        account_id: `${gatewayAccountStripe.gatewayAccountId},${gatewayAccount2.gatewayAccountId}`,
-        page: 1,
-        display_size: 100,
-        limit_total: true,
-        limit_total_size: 5001
-      }, 504),
-      gatewayAccountStubs.getCardTypesSuccess()
+      transactionStubs.getLedgerTransactionsFailure(
+        {
+          account_id: `${gatewayAccountStripe.gatewayAccountId},${gatewayAccount2.gatewayAccountId}`,
+          page: 1,
+          display_size: 100,
+          limit_total: true,
+          limit_total_size: 5001,
+        },
+        504
+      ),
+      gatewayAccountStubs.getCardTypesSuccess(),
     ])
 
     cy.visit('/all-service-transactions', { failOnStatusCode: false })
 
     cy.get('.govuk-heading-l').should('have.text', 'An error occurred')
-    cy.get('#errorMsg').should('have.text', 'The search has timed out. Try searching for a specific date range or applying other filters.')
+    cy.get('#errorMsg').should(
+      'have.text',
+      'The search has timed out. Try searching for a specific date range or applying other filters.'
+    )
 
     cy.get('.govuk-body').get('a').contains('Back to transactions search').click()
 
