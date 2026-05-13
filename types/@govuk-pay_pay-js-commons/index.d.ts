9f0d519ccc45150f593a0c6ea536a7a731e735ec
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2026-05-13 12:58:30 +0100

    [squash] add custom types to typeRoots

--- types/@govuk-pay_pay-js-commons/index.d.ts
+++ types/@govuk-pay_pay-js-commons/index.d.ts
@@ -1,3 +1,5 @@
+// typeRoots option in tsconfig assumes all directories are packages
+// can't have types in nested directory @govuk-pay/pay-js-commons
 declare module '@govuk-pay/pay-js-commons/lib/utils/axios-base-client/errors' {
   export class RESTClientError extends Error {
     name: string
