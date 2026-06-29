8066099cf672f84731ca963325cf5d62639e61b0
Pippa101 <94748151+Pippa101@users.noreply.github.com>
2026-06-29 11:00:08 +0100

    PP-14569: Add ENABLE_GO_LIVE_REQUESTS_FOR_ADYEN to the features list (#4961)

--- test/cypress/integration/request-to-go-live/agreement.cy.js
+++ test/cypress/integration/request-to-go-live/agreement.cy.js
@@ -122,6 +122,65 @@ describe('Request to go live: agreement', () => {
     })
   })
 
+  describe('Adyen has been chosen as the PSP', () => {
+    it('should display "Read and accept our legal terms" page when in CHOSEN_PSP_ADYEN', () => {
+      utils.setupGetUserAndGatewayAccountStubs(utils.buildServiceRoleForGoLiveStage('CHOSEN_PSP_ADYEN'))
+
+      cy.visit(requestToGoLiveAgreementUrl)
+
+      cy.get('h1').should('contain', 'Read and accept our legal terms')
+
+      cy.get('fieldset').should(
+        'contain',
+        'These include the legal terms of Adyen, GOV.UK Pay’s payment service provider.'
+      )
+      cy.get('fieldset').should(
+        'contain',
+        'You must also accept Adyen’s legal terms: Adyen Connected Account Agreement.'
+      )
+      cy.get('ul.govuk-list>li').eq(0).should('contain', 'Crown body memorandum of understanding')
+      cy.get('ul.govuk-list>li').eq(1).should('contain', 'Non-Crown body contract')
+
+      cy.get('#request-to-go-live-current-step').should('exist')
+      cy.get('#request-to-go-live-agreement-form').should('exist')
+
+      cy.get('#agreement').should('exist')
+      cy.get('#agreement').should('not.be.checked')
+
+      cy.get('#request-to-go-live-agreement-form > button').should('exist')
+      cy.get('#request-to-go-live-agreement-form > button').should('contain', 'Continue')
+
+      cy.log('Check that an error is displayed when checkbox is not checked')
+      cy.get('#request-to-go-live-agreement-form > button').click()
+
+      cy.get('h2').should('contain', 'There is a problem')
+      cy.get('ul.govuk-error-summary__list > li:nth-child(1) > a').should(
+        'contain',
+        'You need to accept our legal terms to continue'
+      )
+      cy.get('ul.govuk-error-summary__list > li:nth-child(1) > a').should('have.attr', 'href', '#agreement')
+
+      cy.get('.govuk-form-group--error')
+        .should('exist')
+        .within(() => {
+          cy.get('.govuk-error-message').should('contain', 'You need to accept our legal terms to continue')
+        })
+
+      cy.task('clearStubs')
+      cy.task('setupStubs', [
+        ...getStubsForPageSubmission('CHOSEN_PSP_ADYEN', 'TERMS_AGREED_ADYEN'),
+        goLiveRequestStubs.postStripeAgreementIpAddress(serviceExternalId, '127.0.0.1'), //We don't currently have the equivalent for Adyen
+      ])
+
+      cy.get('#agreement').check()
+      cy.get('#request-to-go-live-agreement-form > button').click()
+
+      cy.location().should((location) => {
+        expect(location.pathname).to.eq(`/service/${serviceExternalId}/request-to-go-live`)
+      })
+    })
+  })
+
   describe('Government banking PSP - Takes payments over the phone stage has been completed', () => {
     it('should display "Read and accept our legal terms" page when service stage is GOV_BANKING_MOTO_OPTION_COMPLETED', () => {
       utils.setupGetUserAndGatewayAccountStubs(
