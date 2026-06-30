9ea6d805bcc21c6cd4c50a536bc6eb82e2d8e15c
Dominic Belcher <dominic.belcher@digital.cabinet-office.gov.uk>
2026-06-30 15:53:24 +0100

    [squash] cleanup

--- src/models/view-mode/ViewMode.class.ts
+++ src/models/view-mode/ViewMode.class.ts
@@ -72,16 +72,9 @@ export class ViewMode {
     const hasServicesInMode = gatewayAccountIdsForMode.length !== 0
     const hasServicesInOppositeMode = allGatewayAccounts.length > gatewayAccountsForMode.length
 
-    console.log(allGatewayAccounts)
-
-    console.log(gatewayAccountsForMode.map((ga) => ga.paymentProvider))
-    console.log(Object.values(PaymentProviders))
-
     const paymentProviders = Object.values(PaymentProviders).filter((pspName) => {
       return gatewayAccountsForMode.some((gatewayAccount) => gatewayAccount.paymentProvider === pspName)
     })
-
-    console.log(paymentProviders)
 
     return new ViewMode(
       modeFilter,
